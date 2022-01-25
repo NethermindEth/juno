135c5b6790c8c6d1851230254f25e2c463b0d11f
Tomasz K. Stanczak <tkstanczak_github@demerzel.co>
2022-01-25 04:24:09 +0000

    feeder works locally

--- src/gateway.go
+++ src/gateway.go
@@ -1,4 +1,4 @@
 package main
 
-const goerliGateway string = "https://alpha4.starknet.io/feeder_gateway"
-const mainnetGateway string = "https://alpha-mainnet.starknet.io/feeder_gateway"
+const goerliGateway string = "https://alpha4.starknet.io"
+const mainnetGateway string = "https://alpha-mainnet.starknet.io"
