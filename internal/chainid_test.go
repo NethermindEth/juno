fc09aff9b82763077e36c9b97c178f2fc1d6c3d5
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-07 15:58:37 -0500

    Split test for encode chain and basic benchmarking

--- internal/chainid_test.go
+++ internal/chainid_test.go
@@ -2,16 +2,34 @@ package chainid
 
 import "testing"
 
-func TestEncodeChainId(t *testing.T) {
+func TestEncodeMainnetChainId(t *testing.T) {
 	mainnetChainId := EncodeChainId("SN_MAIN")
 	expectedMainnetChainId := "0x534e5f4d41494e"
 	if mainnetChainId != expectedMainnetChainId {
-			t.Errorf("Got %q, expected %q", mainnetChainId, expectedMainnetChainId)
+		t.Errorf("Got %q, expected %q", mainnetChainId, expectedMainnetChainId)
 	}
+}
 
+func TestEncodeGoerliId(t *testing.T) {
 	goerliChainId := EncodeChainId("SN_GOERLI")
 	expectedGoerliChainId := "0x534e5f474f45524c49"
 	if goerliChainId != expectedGoerliChainId {
-			t.Errorf("Got %q, expected %q", goerliChainId, expectedGoerliChainId)
+		t.Errorf("Got %q, expected %q", goerliChainId, expectedGoerliChainId)
+	}
+}
+
+func BenchmarkEncodeMainnetChainId(t *testing.B) {
+	mainnetChainId := EncodeChainId("SN_MAIN")
+	expectedMainnetChainId := "0x534e5f4d41494e"
+	if mainnetChainId != expectedMainnetChainId {
+		t.Errorf("Got %q, expected %q", mainnetChainId, expectedMainnetChainId)
+	}
+}
+
+func BenchmarkEncodeGoerliChainId(t *testing.B) {
+	goerliChainId := EncodeChainId("SN_GOERLI")
+	expectedGoerliChainId := "0x534e5f474f45524c49"
+	if goerliChainId != expectedGoerliChainId {
+		t.Errorf("Got %q, expected %q", goerliChainId, expectedGoerliChainId)
 	}
 }
