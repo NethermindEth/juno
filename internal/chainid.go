280bf80453623ef69b74a44cba2992a9b1c527e1
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-09 13:44:57 -0500

    Add comment to chain_id

--- internal/chainid.go
+++ internal/chainid.go
@@ -2,7 +2,7 @@ package chainid
 
 import "github.com/ethereum/go-ethereum/common/hexutil"
 
-// Takes in a Starknet chain ID constant as a string (e.g. "SN_MAIN")
+// EncodeChainId Takes in a Starknet chain ID constant as a string (e.g. "SN_MAIN")
 // and returns the Starknet chain ID as a hex string
 func EncodeChainId(chain string) string {
 	b := []byte(chain)
