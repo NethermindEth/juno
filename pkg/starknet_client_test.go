e24d1969b2b19ef566f1762fbd539c0bc53e4735
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-09 00:05:58 -0500

    Add basic test for StarkNet CRPC endpoint

--- pkg/starknet_client_test.go
+++ pkg/starknet_client_test.go
@@ -1,1 +1,27 @@
 package starknet_client
+
+import (
+	"context"
+	"github.com/NethermindEth/juno/configs"
+	"testing"
+)
+
+func newClient() (*Client, error) {
+	return Dial(configs.TestRPCEndpoint)
+}
+
+func TestClient_GetBlockByNumber(t *testing.T) {
+	c, err := newClient()
+	if err != nil {
+		t.Errorf("Failed Client Creation")
+	}
+	blockHash := BlockHash{
+		Hash: "latest",
+	}
+	requestedScope := TxnHash
+	response, err := c.GetBlockByNumber(context.Background(), blockHash, requestedScope)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(response)
+}
