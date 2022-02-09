cea8d259eca83232ff62b7bbbe8a3aa7eb4501a6
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-09 00:04:57 -0500

    Add TestRPCEndpoint to configs

--- configs/gateway.go
+++ configs/gateway.go
@@ -1,6 +1,7 @@
 package configs
 
 const (
-	GoerliGateway  = "https://alpha4.starknet.io"
-	MainnetGateway = "https://alpha-mainnet.starknet.io"
+	GoerliGateway   = "https://alpha4.starknet.io"
+	MainnetGateway  = "https://alpha-mainnet.starknet.io"
+	TestRPCEndpoint = "https://alpha4.starknet.io"
 )
