ad34330e793636b4b9d59fc3c21d8b9b78d892d4
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-09 13:43:49 -0500

    Update with gofmt chain_id files

--- internal/chainid.go
+++ internal/chainid.go
@@ -5,6 +5,6 @@ import "github.com/ethereum/go-ethereum/common/hexutil"
 // Takes in a Starknet chain ID constant as a string (e.g. "SN_MAIN")
 // and returns the Starknet chain ID as a hex string
 func EncodeChainId(chain string) string {
-		b := []byte(chain)
-		return hexutil.Encode(b)
+	b := []byte(chain)
+	return hexutil.Encode(b)
 }
