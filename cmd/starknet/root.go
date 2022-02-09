6feeefff0ebed51f85ec2cd5718952fe7f37f981
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-09 13:49:32 -0500

    Added cli instructions for starknet call

---
+++ cmd/starknet/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+func Execute() {
+	var echoTimes int
+
+	var (
+		contractAddress string
+		contractAbi     string
+		functionName    string
+		arguments       []string
+		blockHash       string
+		blockNumber     string
+	)
+
+	var cmdCall = &cobra.Command{
+		Use:   "call \n  --address <contract_address>\n  --abi <contract_abi>\n  --function <function_name>\n  --inputs <arguments>\n  --block_hash <block_hash>\n  --block_number <block_number>",
+		Short: "Calls a StarkNet contract without affecting the state",
+		Long:  "Calls a StarkNet contract without affecting the state",
+		Args:  cobra.RangeArgs(4, 6),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i := 0; i < echoTimes; i++ {
+				fmt.Println("Echo: " + strings.Join(args, " "))
+			}
+		},
+	}
+
+	cmdCall.PersistentFlags().StringVar(&contractAddress, "address", "", "address of the called contract")
+	cmdCall.PersistentFlags().StringVar(&contractAbi, "abi", "", "path to a JSON file containing the called contract’s abi")
+	cmdCall.PersistentFlags().StringVar(&functionName, "function", "", "name of the function which is called")
+	cmdCall.PersistentFlags().StringArrayVar(&arguments, "inputs", []string{}, "inputs to the called function, represented by a list of space-delimited values")
+	cmdCall.Flags().StringVar(&blockHash, "block_hash", "", "the hash of the block used as the context for the call operation. If this argument is omitted, the latest block is used")
+	cmdCall.Flags().StringVar(&blockNumber, "block_number", "", "same as block_hash, but specifies the context block by number")
+
+	var rootCmd = &cobra.Command{
+		Use:   "starknet",
+		Short: "Starknet Go Client",
+		Long:  "StarkNet Go Client Long",
+	}
+	rootCmd.AddCommand(cmdCall)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
