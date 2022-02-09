0c2f61ebe051c19097042b1bc2b38066e239db14
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-08 22:39:51 -0500

    Add basic rpc calls for StarkNet client

---
+++ pkg/starknet_client.go
@@ -0,0 +1,45 @@
+package starknet_client
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+// Client defines typed wrappers for the StarkNet RPC API.
+type Client struct {
+	c *rpc.Client
+}
+
+// Dial connects a client to the given URL.
+func Dial(rawUrl string) (*Client, error) {
+	return DialContext(context.Background(), rawUrl)
+}
+
+func DialContext(ctx context.Context, rawUrl string) (*Client, error) {
+	c, err := rpc.DialContext(ctx, rawUrl)
+	if err != nil {
+		return nil, err
+	}
+	return NewClient(c), nil
+}
+
+// NewClient creates a client that uses the given RPC client.
+func NewClient(c *rpc.Client) *Client {
+	return &Client{c}
+}
+
+func (ec *Client) Close() {
+	ec.c.Close()
+}
+
+// StarkNet Access
+
+// GetBlockByNumber Get block information given the block id
+func (ec *Client) GetBlockByNumber(ctx context.Context, blockHash BlockHash, requestedScope RequestedScope) (*BlockResponse, error) {
+	var result BlockResponse
+	err := ec.c.CallContext(ctx, &result, "starknet_getBlockByHash", blockHash, requestedScope)
+	if err != nil {
+		return nil, err
+	}
+	return &result, err
+}
