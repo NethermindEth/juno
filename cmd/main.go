c46cb80de4a4d72b2990abc81c6c28990fa8c0e0
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-10 19:39:13 -0500

    Add cmd processing to main

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	cmd "github.com/NethermindEth/juno/cmd/starknet"
 	"github.com/NethermindEth/juno/pkg/rpc"
 )
 
 func main() {
 	fmt.Println("Juno, Starknet Client in Go")
+	cmd.Execute()
 	end := make(chan error)
 	go rpc.Handlers(end)
 	err := <-end
@@ -14,4 +16,14 @@ func main() {
 		fmt.Printf("Error in the RPC Server: %s\n", err.Error())
 		return
 	}
+	//baseURL := configs.MainnetGateway
+	//prv := provider.NewProvider(baseURL)
+	//// opt := provider.BlockOptions{}
+	//ctx := context.Background()
+	//block, err := prv.Block(ctx, nil)
+	//if err != nil {
+	//	fmt.Printf("Failed to retrieve block: %s", err)
+	//}
+	//
+	//fmt.Println(block.BlockHash)
 }
