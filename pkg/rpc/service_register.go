5ab7c4a1233ed86e70dd5a7b707ce70d89f832f4
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-11 11:50:44 -0500

    Add printer for structs

--- pkg/rpc/service_register.go
+++ pkg/rpc/service_register.go
@@ -1,5 +1,10 @@
 package rpc
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Dispatcher struct {
 	handlers []HandleParamsResulter
 }
@@ -14,4 +19,13 @@ func (us *Dispatcher) Handlers() []HandleParamsResulter {
 
 func NewRPCDispatcher(handlers []HandleParamsResulter) *Dispatcher {
 	return &Dispatcher{handlers: handlers}
+}
+
+func StructPrinter(i interface{}) {
+	b, err := json.Marshal(i)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }
