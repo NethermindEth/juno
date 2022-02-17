02755b1563622fd35906f46feffc48a0358e3cda
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-16 19:52:13 -0500

    Move base types for the handle of StarknetGetBlockByHash rpc call

--- pkg/rpc/type.go
+++ pkg/rpc/type.go
@@ -1,1 +1,11 @@
 package rpc
+
+import (
+	cmd "github.com/NethermindEth/juno/cmd/starknet"
+	pkg "github.com/NethermindEth/juno/pkg"
+)
+
+type BlockHashParams struct {
+	RequestedScope pkg.RequestedScope `json:"requestedScope"`
+	BlockHash      cmd.BlockHash      `json:"block_hash"`
+}
