0995b0321a0e3f51b5b55be9892ecd66ceef86a1
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-17 08:15:01 -0500

    Remove unused rpc handler

--- pkg/rpc/utils.go
+++ pkg/rpc/utils.go
@@ -5,22 +5,6 @@ import (
 	"fmt"
 )
 
-type Dispatcher struct {
-	handlers []HandleParamsResulter
-}
-
-func (us *Dispatcher) MethodName(h HandleParamsResulter) string {
-	return h.Name()
-}
-
-func (us *Dispatcher) Handlers() []HandleParamsResulter {
-	return us.handlers
-}
-
-func NewRPCDispatcher(handlers []HandleParamsResulter) *Dispatcher {
-	return &Dispatcher{handlers: handlers}
-}
-
 func StructPrinter(i interface{}) {
 	b, err := json.Marshal(i)
 	if err != nil {
