5731832bf17c2e2b3010e580d2d1fd4a58a440ce
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-22 19:03:34 -0500

    Use logrus as logging in main

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"context"
-	"fmt"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/NethermindEth/juno/configs"
 	"github.com/tarrencev/go-starknet/provider"
 )
 
 func main() {
+	log.Info("Starting Juno, StarkNet Go Client")
 	baseURL := configs.MainnetGateway
 	prv := provider.NewProvider(baseURL)
 	// opt := provider.BlockOptions{}
 	ctx := context.Background()
 	block, err := prv.Block(ctx, nil)
 	if err != nil {
-		fmt.Printf("Failed to retrieve block: %s", err)
+		log.WithFields(log.Fields{
+			"With Error": err,
+		}).Error("Failed to retrieve block")
 	}
-
-	fmt.Println(block.BlockHash)
+	log.WithFields(log.Fields{
+		"blockHash": block.BlockHash,
+	}).Debug("Block Hash retrieved from provider")
 }
