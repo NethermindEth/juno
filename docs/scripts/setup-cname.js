4bb62e69de4511109a13b7d52215bc65a64d787f
Miguel Tenorio <46824157+AntiD2ta@users.noreply.github.com>
2022-02-24 20:41:41 -0500

    Delete setup-cname.js

--- docs/scripts/setup-cname.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-var fs = require('fs');
-
-fs.writeFile('./static/CNAME', process.env.TARGET_URL, function (err) {
-  if (err) throw err;
-  console.log('CNAME created ');
-}); 
