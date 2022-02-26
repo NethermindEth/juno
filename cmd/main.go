d5dfbf322c1ab6b5d088ba71dc2e7526ebcdc4db
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-02-26 12:22:43 -0500

    Add example about how to store in database

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"github.com/NethermindEth/juno/internal/log"
-
 	"github.com/NethermindEth/juno/configs"
+	"github.com/NethermindEth/juno/internal/log"
+	"github.com/NethermindEth/juno/pkg/db"
 	"github.com/tarrencev/go-starknet/provider"
 )
 
@@ -21,4 +21,31 @@ func main() {
 		logger.With("With Error", err).Error("Failed to retrieve block")
 	}
 	logger.With("blockHash", block.BlockHash).Debug("Block Hash retrieved from provider, ")
+
+	database := db.NewKeyValueDatabase("example", 0)
+
+	val, err := database.Get([]byte("blockHash"))
+	if err != nil {
+		logger.With("Error", err).Error("Error getting values from")
+		return
+	}
+	logger.With("blockHash", string(val)).Info("Got latest BlockHash Value from DB")
+
+	if block.BlockHash == string(val) {
+		logger.Info("Still the same blockHash")
+		return
+	}
+	logger.Info("Storing the new blockHash")
+
+	err = database.Put([]byte("blockHash"), []byte(block.BlockHash))
+	if err != nil {
+		logger.With("Error", err).Error("Error putting in database")
+		return
+	}
+	n, err := database.NumberOfItems()
+	if err != nil {
+		logger.With("Error", err).Error("Error getting the number of items in database")
+		return
+	}
+	logger.With("Number of Items on DB", n).Info("Got the number of items on DB")
 }
