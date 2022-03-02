74a10cbe8fd166859c0bdc35dfeac249c17d65d1
Marcos Antonio Maceo <35319980+stdevMac@users.noreply.github.com>
2022-03-01 22:46:27 -0500

    lmbdx integration as key-value pair database (#80)
    * Add interfaces for Database
    
    * Add mdbx to go.{mod.sum}
    
    * Modify interface
    
    * Implementation of database interface
    
    * Add tests files for database
    
    * Add test for add key and solve small errors
    
    * Solve small errors in database tests
    
    * Add sirupsen/log for logging to app
    
    * Add instructions for log in README
    
    * Use logrus as logging in main
    
    * Add debugging for encoding of chain ID
    
    * Add logs for sn_keccak
    
    * Add method for get the number of records inside DB interface
    
    * [log] Add comments and logs and restructuration for implementation of new interface in DB
    
    * Change name for kv-database initializer
    
    * Add more logs to database and solve small typos
    
    * Add first set of tests for database
    
    * Remove unused lines
    
    * Update tests for database to receive db path as params
    
    * Add benchmarking for databases
    
    * Set log level to error and debug in tests
    
    * Add new functionalities to init
    
    * Add comments for database_test
    
    * Improve code-coverage for database tests
    
    * Add zap to go modules as new logging library
    
    * Create using Singleton pattern logger for app using zap
    
    * Change in main new logging with zap for logrus
    
    * Remove logrus from go mod
    
    * Change zap for logrus in internal code
    
    * Add docs for logging in readme using zap
    
    * Update implementations to use With
    
    * change logs from logrus to zap
    
    * Modify comment in database log
    
    * Add example about how to store in database
    
    * Update image of the logs
    
    * Add image to README
    
    * Add image to README
    
    * Update image of the logs
    
    * Remove unused mutex in database
    
    * Add courtney for code-coverage
    
    * Solve code coverage issues
    
    * Fix workflow to use courtney for code-coverage
    
    * Change tests layout
    
    * Update makefile courtney installation
    
    * Add go get in makefile for project
    
    * Add gopath to command
    
    * Add gopath initialization in makefile
    
    * Add initialization for gopath in makefile
    
    * Change to install latest
    
    * Add DemerzelSolutions fork as base for courtney coverage
    
    * Install courtney using cli instead package
    
    * Add go get to deps install
    
    * Set .out instead .txt as coverage output
    
    * Add workflow_dispatch trigger to juno-test workflow
    
    Co-authored-by: AntiD2ta <mtpotro41@gmail.com>

---
+++ pkg/db/database_test.go
@@ -0,0 +1,270 @@
+package db
+
+import (
+	log "github.com/sirupsen/logrus"
+	"strconv"
+	"testing"
+)
+
+var (
+	KeyValueTest = map[string]string{}
+)
+
+func init() {
+	log.SetLevel(log.ErrorLevel)
+	for i := 0; i < 350; i++ {
+		val := strconv.Itoa(i)
+		KeyValueTest["key"+val] = "value" + val
+	}
+}
+
+// setupDatabaseForTest creates a new Database for Tests
+func setupDatabaseForTest(path string) *KeyValueDatabase {
+	return NewKeyValueDatabase(path, 0)
+}
+
+// TestAddKey Check that a single value is inserted without error
+func TestAddKey(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	err := db.Put([]byte("key"), []byte("value"))
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+}
+
+// TestNumberOfItems Checks that in every moment the collection contains the right amount of items
+func TestNumberOfItems(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	n, err := db.NumberOfItems()
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if n != 0 {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	for k, v := range KeyValueTest {
+		err := db.Put([]byte(k), []byte(v))
+		if err != nil {
+			t.Log(err)
+			t.Fail()
+			return
+		}
+	}
+	n, err = db.NumberOfItems()
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if int(n) != len(KeyValueTest) {
+		t.Log(err)
+		t.Fail()
+	}
+}
+
+// TestAddMultipleKeys Checks that after insert some keys the collection contains the right amount of items
+func TestAddMultipleKeys(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	for k, v := range KeyValueTest {
+		err := db.Put([]byte(k), []byte(v))
+		if err != nil {
+			t.Log(err)
+			t.Fail()
+			return
+		}
+	}
+	n, err := db.NumberOfItems()
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if int(n) != len(KeyValueTest) {
+		t.Log(err)
+		t.Fail()
+	}
+}
+
+// TestHasKey Check that one key exist after insertion
+func TestHasKey(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	goodKey := []byte("good_key")
+	err := db.Put(goodKey, []byte("value"))
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	has, err := db.Has(goodKey)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if !has {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+}
+
+// TestHasNotKey Check that a key don't exist
+func TestHasNotKey(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	goodKey := []byte("good_key")
+	badKey := []byte("bad_key")
+	err := db.Put(goodKey, []byte("value"))
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	has2, err := db.Has(badKey)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if has2 {
+		t.Log(err)
+		t.Fail()
+	}
+}
+
+// TestGetKey Check that a key is property retrieved
+func TestGetKey(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	goodKey := []byte("good_key")
+	goodValue := []byte("value")
+	err := db.Put(goodKey, goodValue)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	val, err := db.Get(goodKey)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if string(val) != string(goodValue) {
+		t.Log(err)
+		t.Fail()
+	}
+}
+
+// TestGetNotKey Check that a key don't exist and what happen if it doesn't exist
+func TestGetNotKey(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	goodKey := []byte("good_key")
+	goodValue := []byte("value")
+	badKey := []byte("bad_key")
+	err := db.Put(goodKey, goodValue)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	key, err := db.Get(badKey)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	if key != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+}
+
+func TestDelete(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	goodKey := []byte("good_key")
+	goodValue := []byte("value")
+	err := db.Put(goodKey, goodValue)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	err = db.Delete(goodKey)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+	key, err := db.Has(goodKey)
+	if key {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+}
+
+func TestBegin(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	db.Begin()
+}
+func TestRollBack(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	db.Rollback()
+}
+func TestClose(t *testing.T) {
+	db := setupDatabaseForTest(t.TempDir())
+	db.Close()
+}
+
+// BenchmarkEntriesInDatabase Benchmark the entry of key-value pairs to the db
+func BenchmarkEntriesInDatabase(b *testing.B) {
+	log.SetLevel(log.ErrorLevel)
+	db := setupDatabaseForTest(b.TempDir())
+	for i := 0; i < b.N; i++ {
+		val := []byte(strconv.Itoa(i))
+		err := db.Put(val, val)
+		if err != nil {
+			b.Error("Benchmarking fails, error storing values")
+			return
+		}
+	}
+	n, err := db.NumberOfItems()
+	if err != nil {
+		b.Errorf("Benchmarking fails, error getting the number of items: %s\n", err)
+		b.Fail()
+		return
+	}
+	if int(n) != b.N {
+		b.Error("Benchmarking fails, mismatch between number of items to insert and the number inside db")
+		b.Fail()
+		return
+	}
+
+}
+
+// BenchmarkConsultsToDatabase Benchmark the consult to a db
+func BenchmarkConsultsToDatabase(b *testing.B) {
+	db := setupDatabaseForTest(b.TempDir())
+	for i := 0; i < b.N; i++ {
+		val := []byte(strconv.Itoa(i))
+		err := db.Put(val, val)
+		if err != nil {
+			b.Error("Benchmarking fails, error storing values")
+			b.Fail()
+			return
+		}
+		get, err := db.Get(val)
+		if err != nil {
+			b.Errorf("Benchmarking fails, error getting values: %s\n", err)
+			b.Fail()
+			return
+		}
+		if string(get) != string(val) {
+			b.Error("Benchmarking fails, mismatch between expected return value and returned value")
+			b.Fail()
+		}
+	}
+}
