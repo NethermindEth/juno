7161108845f6ce9d48503415c0e9c15d18d2739b
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-03-02 22:20:56 -0500

    Add first test for server

--- pkg/rpc/server_test.go
+++ pkg/rpc/server_test.go
@@ -1,15 +1,63 @@
 package rpc
 
 import (
+	"bytes"
 	"context"
 	cmd "github.com/NethermindEth/juno/cmd/starknet"
+	"github.com/NethermindEth/juno/internal/log"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
+
+func getServerHandler() *MethodRepository {
+	server := NewMethodRepositoryWithMethods(HandlerRPC{})
+	return server
+}
+
+type rpcTest struct {
+	request  string
+	response string
+}
+
+func testServer(t *testing.T, tests []rpcTest) {
+	server := getServerHandler()
+
+	for _, v := range tests {
+		req := httptest.NewRequest(http.MethodPost, "/rpc", bytes.NewBuffer([]byte(v.request)))
+		w := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/json")
+		server.ServeHTTP(w, req)
+		res := w.Result()
+		data, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Errorf("expected error to be nil got %v", err)
+			_ = res.Body.Close()
+		}
+		s := string(data)
+		if s != v.response {
+			t.Errorf("expected %v, got %v", v.response, string(data))
+			_ = res.Body.Close()
+		}
+
+	}
+}
+
+func TestHandlerRPC_StarknetCall(t *testing.T) {
+	testServer(t, []rpcTest{
+		{
+			request:  "{\n  \"jsonrpc\": \"2.0\",\n  \"method\": \"starknet_call\",\"params\": [{\"request\":{\"contract_address\":\"address\",\"entry_point_selector\":\"selector\",\"CallData\":[\"string1\",\"string2\"]}},{\"block_hash\":\"latest\"}],  \"id\": \"243a718a-2ebb-4e32-8cc8-210c39e8a14b\"}",
+			response: "{\"jsonrpc\":\"2.0\",\"result\":[\"Response\",\"of\",\"starknet_call\"],\"id\":\"243a718a-2ebb-4e32-8cc8-210c39e8a14b\"}\n",
+		},
+	})
+}
 
 var handlerRpc *HandlerRPC
 
 func init() {
 	handlerRpc = getHandler()
+	logger = log.GetLogger()
 }
 
 func getHandler() *HandlerRPC {
