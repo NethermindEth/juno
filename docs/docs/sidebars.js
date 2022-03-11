4e6334ba0a45f4c8cbf06dbdd4a05da95818a488
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-03-11 09:31:15 -0500

    Add initial documentation in docosaurus

--- docs/docs/sidebars.js
+++ docs/docs/sidebars.js
@@ -1,30 +1,38 @@
 module.exports = {
-  docs: [
-    {
-      "Welcome": [
-        'welcome',
-      ],
-    },
-    {
-      "Contribute": [
-        'getting-started-juno',
-        'getting-started-cli',
-        'getting-started-go',
+    docs: [
         {
-          "Juno Docs": [
-          'contribution-guide', 'cheatsheet']
+            "Welcome": [
+                'welcome',
+            ],
         },
-      ],
-    },
-    {
-      type: 'link',
-      label: 'Juno Discord',
-      href: 'https://discord.gg/sqFY2D48GS',
-    },
-    {
-      type: 'link',
-      href: 'https://github.com/NethermindEth/juno',
-      label: 'Juno GitHub',
-    },
-  ],
+        {
+            "Contribute": [
+                'getting-started-juno',
+                'getting-started-cli',
+                'getting-started-go',
+                {
+                    "Juno Docs": [
+                        'contribution-guide', 'cheatsheet']
+                },
+            ],
+        },
+        {
+            "Integrations": [
+                'cairo-integration',
+                'starknet-cryptography',
+                'starknet-state',
+                'l1-integration'
+            ]
+        },
+        {
+            type: 'link',
+            label: 'Juno Discord',
+            href: 'https://discord.gg/sqFY2D48GS',
+        },
+        {
+            type: 'link',
+            href: 'https://github.com/NethermindEth/juno',
+            label: 'Juno GitHub',
+        },
+    ],
 };
