549a26978528c5485a4979f40a73d6175de3bb2a
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-03-17 16:09:56 -0400

    [docosaurus] Fix sidebar.js fileD

--- docs/docs/sidebars.js
+++ docs/docs/sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             "Contribute": [
                 'getting-started-juno',
-                'getting-started-cli',
+                'getting-started-starknet',
                 'getting-started-go',
                 {
                     "Juno Docs": [
