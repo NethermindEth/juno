32bde049e3a63ac1a5cd85fb32e3edf1dc2fa9d5
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-04-06 13:48:08 -0400

    Add feeder to state retrieval

--- pkg/ethereum/state.go
+++ pkg/ethereum/state.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"github.com/NethermindEth/juno/internal/log"
 	"github.com/NethermindEth/juno/pkg/db"
+	"github.com/NethermindEth/juno/pkg/feeder_gateway"
 	"github.com/ethereum/go-ethereum/ethclient"
 
 	"github.com/ethereum/go-ethereum/core/types"
@@ -13,26 +14,29 @@ import (
 
 // Synchronizer represents the base struct for Ethereum Synchronization
 type Synchronizer struct {
-	client *ethclient.Client
-	db     *db.Databaser
+	ethereumClient      *ethclient.Client
+	feederGatewayClient *feeder_gateway.Client
+	db                  *db.Databaser
 }
 
 // NewSynchronizer creates a new Synchronizer
-func NewSynchronizer(ethereumNode string, db *db.Databaser) *Synchronizer {
+func NewSynchronizer(ethereumNode, feederGateway string, db *db.Databaser) *Synchronizer {
 	client, err := ethclient.Dial(ethereumNode)
 	if err != nil {
 		log.Default.With("Error", err).Fatal("Unable to connect to Ethereum Client")
 	}
+	feeder := feeder_gateway.NewClient(feederGateway, "/feeder_gateway", nil)
 	return &Synchronizer{
-		client: client,
-		db:     db,
+		ethereumClient:      client,
+		feederGatewayClient: feeder,
+		db:                  db,
 	}
 }
 
 // UpdateStateRoot keeps updating the Ethereum State Root as a process
 func (s Synchronizer) UpdateStateRoot() error {
 	headers := make(chan *types.Header)
-	sub, err := s.client.SubscribeNewHead(context.Background(), headers)
+	sub, err := s.ethereumClient.SubscribeNewHead(context.Background(), headers)
 	if err != nil {
 		log.Default.With("Error", err).Fatalf("Unable to subscribe to block headers")
 		return err
@@ -56,7 +60,7 @@ func (s Synchronizer) Close(ctx context.Context) {
 	log.Default.Info("Closing Layer 1 Synchronizer")
 	select {
 	case <-ctx.Done():
-		s.client.Close()
+		s.ethereumClient.Close()
 	default:
 	}
 }
