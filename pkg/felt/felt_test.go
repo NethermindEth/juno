a7d1de5cf2b94e598b5c64ad0033af51cb63841c
Tshaka Eric Lekholoane <_@tshaka.co>
2022-04-20 15:51:27 +0200

    refactor: propose changes
    - Move the Felt logic to its own package.
    - Remove redundant allocations.
    - Use the new gofmt specifiers to indicate links in the documentation.
    - Generalise the fmt.Stringer conformance.

--- pkg/felt/felt_test.go
+++ pkg/felt/felt_test.go
@@ -1,4 +1,4 @@
-package types
+package felt 
 
 import (
 	"math"
@@ -7,29 +7,24 @@ import (
 	"testing"
 )
 
-func copyP() *big.Int {
-	cp := new(big.Int).Set(p)
-	return cp
+// P returns a copy of the prime number 2²⁵¹ + 17·2¹⁹² + 1.
+func P() *big.Int {
+	copy := new(big.Int).Set(p)
+	return copy
 }
 
-func TestAsInt(t *testing.T) {
+func TestInt(t *testing.T) {
 	var tests = [...]struct {
 		input *Felt
 		want  *big.Int
 	}{
-		{
-			input: (*Felt)(big.NewInt(0)),
-			want:  big.NewInt(0),
-		},
-		{
-			input: (*Felt)(big.NewInt(23)),
-			want:  big.NewInt(23),
-		},
+		{(*Felt)(big.NewInt(0)), big.NewInt(0)},
+		{(*Felt)(big.NewInt(23)), big.NewInt(23)},
 	}
 	for _, test := range tests {
-		result := test.input.asInt()
+		result := test.input.int()
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("asInt(%s) = %s, want %s", test.input, result.Text(16), test.want.Text(16))
+			t.Errorf("Felt.int() = %x, want %x", result, test.want)
 		}
 	}
 }
@@ -39,18 +34,9 @@ func TestReduce(t *testing.T) {
 		input *Felt
 		want  *Felt
 	}{
-		{
-			input: (*Felt)(big.NewInt(0)),
-			want:  (*Felt)(big.NewInt(0)),
-		},
-		{
-			input: (*Felt)(copyP()),
-			want:  (*Felt)(big.NewInt(0)),
-		},
-		{
-			input: (*Felt)(new(big.Int).Add(copyP(), big.NewInt(1))),
-			want:  (*Felt)(big.NewInt(1)),
-		},
+		{(*Felt)(big.NewInt(0)), (*Felt)(big.NewInt(0))},
+		{(*Felt)(P()), (*Felt)(big.NewInt(0))},
+		{(*Felt)(new(big.Int).Add(P(), big.NewInt(1))), (*Felt)(big.NewInt(1))},
 	}
 	for _, test := range tests {
 		input := *test.input
@@ -61,28 +47,19 @@ func TestReduce(t *testing.T) {
 	}
 }
 
-func TestNewIn(t *testing.T) {
+func TestNew(t *testing.T) {
 	var tests = [...]struct {
 		input int64
 		want  *Felt
 	}{
-		{
-			input: 1,
-			want:  (*Felt)(big.NewInt(1)),
-		},
-		{
-			input: math.MaxInt64,
-			want:  (*Felt)(big.NewInt(math.MaxInt64)),
-		},
-		{
-			input: 10,
-			want:  (*Felt)(big.NewInt(10)),
-		},
+		{1, (*Felt)(big.NewInt(1))},
+		{math.MaxInt64, (*Felt)(big.NewInt(math.MaxInt64))},
+		{10, (*Felt)(big.NewInt(10))},
 	}
 	for _, test := range tests {
-		result := NewInt(test.input)
+		result := New(test.input)
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("NewInt(%d)=%s, want %s", test.input, result, test.want)
+			t.Errorf("New(%d) = %s, want %s", test.input, result, test.want)
 		}
 	}
 }
@@ -91,19 +68,13 @@ func TestSet(t *testing.T) {
 	var tests = [...]struct {
 		z, x *Felt
 	}{
-		{
-			z: NewInt(0),
-			x: NewInt(1),
-		},
-		{
-			z: NewInt(2),
-			x: NewInt(3),
-		},
+		{New(0), New(1)},
+		{New(2), New(3)},
 	}
 	for _, test := range tests {
 		test.z.Set(test.x)
 		if test.z.Cmp(test.x) != 0 {
-			t.Errorf("z.Set(%s)=%s, want %s", test.x, test.z, test.x)
+			t.Errorf("z.Set(%s) = %s, want %s", test.x, test.z, test.x)
 		}
 		if test.z == test.x {
 			t.Error("z and x points to the same address after Set operation")
@@ -143,10 +114,10 @@ func TestSetString(t *testing.T) {
 	for _, test := range tests {
 		result, ok := new(Felt).SetString(test.s, test.base)
 		if ok != test.ok {
-			t.Errorf("ok=%t, want %t", ok, test.ok)
+			t.Errorf("ok = %t, want %t", ok, test.ok)
 		}
 		if ok && result.Cmp(test.want) != 0 {
-			t.Errorf("SetString(%s)=%s, want %s", test.s, result, test.want)
+			t.Errorf("SetString(%s, %d) = %s, want %s", test.s, test.base, result, test.want)
 		}
 	}
 }
@@ -155,29 +126,14 @@ func TestAdd(t *testing.T) {
 	var tests = [...]struct {
 		a, b, want *Felt
 	}{
-		{
-			// 0 + 10 = 10
-			a:    NewInt(0),
-			b:    NewInt(10),
-			want: NewInt(10),
-		},
-		{
-			// (P - 1) + 10 = 9
-			a:    (*Felt)(new(big.Int).Sub(copyP(), big.NewInt(1))),
-			b:    NewInt(10),
-			want: NewInt(9),
-		},
-		{
-			// 3 + 3 = 6
-			a:    NewInt(3),
-			b:    NewInt(3),
-			want: NewInt(6),
-		},
+		{New(0), New(10), New(10)},
+		{(*Felt)(new(big.Int).Sub(P(), big.NewInt(1))), New(10), New(9)},
+		{New(3), New(3), New(6)},
 	}
 	for _, test := range tests {
 		result := new(Felt).Add(test.a, test.b)
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("Add(%s, %s)=%s, want %s", test.a, test.b, result, test.want)
+			t.Errorf("Add(%s, %s) = %s, want %s", test.a, test.b, result, test.want)
 		}
 	}
 }
@@ -186,36 +142,16 @@ func TestSub(t *testing.T) {
 	var tests = [...]struct {
 		a, b, want *Felt
 	}{
-		{
-			// 0 - 1 = P - 1
-			a:    NewInt(0),
-			b:    NewInt(1),
-			want: (*Felt)(new(big.Int).Sub(copyP(), big.NewInt(1))),
-		},
-		{
-			// 3 - 3 = 0
-			a:    NewInt(3),
-			b:    NewInt(3),
-			want: NewInt(0),
-		},
-		{
-			// 10 - 20 = P - 10
-			a:    NewInt(10),
-			b:    NewInt(20),
-			want: (*Felt)(new(big.Int).Sub(copyP(), big.NewInt(10))),
-		},
-		{
-			// 10 - (P - 1) = 11
-			a:    NewInt(10),
-			b:    (*Felt)(new(big.Int).Sub(copyP(), big.NewInt(1))),
-			want: NewInt(11),
-		},
+		{New(0), New(1), (*Felt)(new(big.Int).Sub(P(), big.NewInt(1)))},
+		{New(3), New(3), New(0)},
+		{New(10), New(20), (*Felt)(new(big.Int).Sub(P(), big.NewInt(10)))},
+		{New(10), (*Felt)(new(big.Int).Sub(P(), big.NewInt(1))), New(11)},
 	}
 	for _, test := range tests {
 		result := new(Felt).Sub(test.a, test.b)
-		t.Logf("Sub(%s, %s)=%s, want %s", test.a, test.b, result, test.want)
+		t.Logf("Sub(%s, %s) = %s, want %s", test.a, test.b, result, test.want)
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("Sub(%s, %s)=%s, want %s", test.a, test.b, result, test.want)
+			t.Errorf("Sub(%s, %s) = %s, want %s", test.a, test.b, result, test.want)
 		}
 	}
 }
@@ -224,60 +160,36 @@ func TestMul(t *testing.T) {
 	var tests = [...]struct {
 		a, b, want *Felt
 	}{
+		{New(0), New(10), New(0)},
+		{New(2), New(2), New(4)},
 		{
-			// 0 * 10 = 0
-			a:    NewInt(0),
-			b:    NewInt(10),
-			want: NewInt(0),
-		},
-		{
-			// 2 * 2 = 4
-			a:    NewInt(2),
-			b:    NewInt(2),
-			want: NewInt(4),
-		},
-		{
-			// 2 * ⎣P//2⎦ = P - 1
-			a:    NewInt(2),
-			b:    (*Felt)(new(big.Int).Div(copyP(), big.NewInt(2))),
-			want: (*Felt)(new(big.Int).Sub(copyP(), big.NewInt(1))),
+			// 2 * ⎣P / 2⎦ = P - 1.
+			New(2),
+			(*Felt)(new(big.Int).Div(P(), big.NewInt(2))),
+			(*Felt)(new(big.Int).Sub(P(), big.NewInt(1))),
 		},
 	}
 	for _, test := range tests {
 		result := new(Felt).Mul(test.a, test.b)
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("Mul(%s, %s)=%s, want %s", test.a, test.b, result, test.want)
+			t.Errorf("Mul(%s, %s) = %s, want %s", test.a, test.b, result, test.want)
 		}
 	}
 }
 
 func TestExp(t *testing.T) {
+	// a ** b.
 	var tests = [...]struct {
 		a, b, want *Felt
 	}{
-		{
-			// 2 ** 2 = 4
-			a:    NewInt(2),
-			b:    NewInt(2),
-			want: NewInt(4),
-		},
-		{
-			// 51 ** 0 = 1
-			a:    NewInt(51),
-			b:    NewInt(0),
-			want: NewInt(1),
-		},
-		{
-			// 0 ** 10000 = 0
-			a:    NewInt(0),
-			b:    NewInt(10000),
-			want: NewInt(0),
-		},
+		{New(2), New(2), New(4)},
+		{New(51), New(0), New(1)},
+		{New(0), New(10_000), New(0)},
 	}
 	for _, test := range tests {
 		result := new(Felt).Exp(test.a, test.b)
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("Exp(%s, %s)=%s, want %s", test.a, test.b, result, test.want)
+			t.Errorf("Exp(%s, %s) = %s, want %s", test.a, test.b, result, test.want)
 		}
 	}
 }
@@ -286,16 +198,10 @@ func TestDiv(t *testing.T) {
 	var tests = [...]struct {
 		a, b, want *Felt
 	}{
+		{New(6), New(3), New(2)},
 		{
-			// 6 / 3 = 2
-			a:    NewInt(6),
-			b:    NewInt(3),
-			want: NewInt(2),
-		},
-		{
-			// 7 / 3 = 2aaaaaaaaaaaab0555555555555555555555555555555555555555555555558
-			a: NewInt(7),
-			b: NewInt(3),
+			a: New(7),
+			b: New(3),
 			want: func() *Felt {
 				x, _ := new(big.Int).SetString("2aaaaaaaaaaaab0555555555555555555555555555555555555555555555558", 16)
 				return (*Felt)(x)
@@ -305,7 +211,7 @@ func TestDiv(t *testing.T) {
 	for _, test := range tests {
 		result := new(Felt).Div(test.a, test.b)
 		if result.Cmp(test.want) != 0 {
-			t.Errorf("Div(%s, %s)=%s, want %s", test.a, test.b, result, test.want)
+			t.Errorf("Div(%s, %s) = %s, want %s", test.a, test.b, result, test.want)
 		}
 	}
 }
@@ -315,29 +221,14 @@ func TestCmp(t *testing.T) {
 		a, b *Felt
 		want int
 	}{
-		{
-			// 2 cmp 2 = 0
-			a:    NewInt(2),
-			b:    NewInt(2),
-			want: 0,
-		},
-		{
-			// 51 cmp 0 = 1
-			a:    NewInt(51),
-			b:    NewInt(0),
-			want: 1,
-		},
-		{
-			// 10 cmp 10000 = -1
-			a:    NewInt(10),
-			b:    NewInt(10000),
-			want: -1,
-		},
+		{New(2), New(2), 0},
+		{New(51), New(0), 1},
+		{New(10), New(10_000), -1},
 	}
 	for _, test := range tests {
 		result := test.a.Cmp(test.b)
 		if result != test.want {
-			t.Errorf("%s.Cmp(%s)=%d, want %d", test.a, test.b, result, test.want)
+			t.Errorf("%s.Cmp(%s) = %d, want %d", test.a, test.b, result, test.want)
 		}
 	}
 }
@@ -368,7 +259,7 @@ func TestText(t *testing.T) {
 	for _, test := range tests {
 		result := test.a.Text(test.base)
 		if strings.Compare(result, test.want) != 0 {
-			t.Errorf("%s.Text(%d)=%s, want %s", test.a.asInt().Text(16), test.base, result, test.want)
+			t.Errorf("%s.Text(%d) = %s, want %s", test.a.int().Text(16), test.base, result, test.want)
 		}
 	}
 }
@@ -379,55 +270,23 @@ func TestFeltUnmarshalJSON(t *testing.T) {
 		felt *Felt
 		err  bool
 	}{
-		{
-			data: []byte("\"0xa\""),
-			felt: NewInt(10),
-			err:  false,
-		},
-		{
-			data: []byte("\"10\""),
-			felt: NewInt(10),
-			err:  false,
-		},
-		{
-			data: []byte("23"),
-			felt: NewInt(23),
-			err:  false,
-		},
-		{
-			data: []byte("\"0xa"),
-			felt: new(Felt),
-			err:  true,
-		},
-		{
-			data: []byte("\"0x2z4a\""),
-			felt: new(Felt),
-			err:  true,
-		},
-		{
-			data: []byte("\"234a\""),
-			felt: new(Felt),
-			err:  true,
-		},
-		{
-			data: []byte("234a\""),
-			felt: new(Felt),
-			err:  true,
-		},
-		{
-			data: []byte("234a"),
-			felt: new(Felt),
-			err:  true,
-		},
+		{[]byte("\"0xa\""), New(10), false},
+		{[]byte("\"10\""), New(10), false},
+		{[]byte("23"), New(23), false},
+		{[]byte("\"0xa"), new(Felt), true},
+		{[]byte("\"0x2z4a\""), new(Felt), true},
+		{[]byte("\"234a\""), new(Felt), true},
+		{[]byte("234a\""), new(Felt), true},
+		{[]byte("234a"), new(Felt), true},
 	}
 	for _, test := range tests {
 		f := new(Felt)
 		err := f.UnmarshalJSON(test.data)
 		if err != nil && !test.err {
-			t.Errorf("unexpected error: %s", err.Error())
+			t.Errorf("unexpected error: %v", err)
 		}
 		if err == nil && test.err {
-			t.Errorf("expected error")
+			t.Errorf("expected error: %v", err)
 		}
 		if f.Cmp(test.felt) != 0 {
 			t.Errorf("unexpected felt %s unmarshaling data %v", f, test.data)
@@ -435,28 +294,32 @@ func TestFeltUnmarshalJSON(t *testing.T) {
 	}
 }
 
+// XXX: I don't think we should confine ourselves to the hexadecimal
+// representation so this test should reflect should reflect the ability
+// to format against different printf specifiers instead.
+
 func TestString(t *testing.T) {
 	var tests = [...]struct {
 		felt *Felt
 		want string
 	}{
 		{
-			felt: NewInt(10),
+			felt: New(10),
 			want: "0xa",
 		},
 		{
-			felt: NewInt(0),
+			felt: New(0),
 			want: "0x0",
 		},
 		{
-			felt: NewInt(1000),
+			felt: New(1000),
 			want: "0x3e8",
 		},
 	}
 	for _, test := range tests {
 		result := test.felt.String()
 		if strings.Compare(result, test.want) != 0 {
-			t.Errorf("%s.String()=%s, want %s", test.felt.asInt().Text(16), result, test.want)
+			t.Errorf("%s.String()=%s, want %s", test.felt.int().Text(16), result, test.want)
 		}
 	}
 }
