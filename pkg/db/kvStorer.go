6dfcb1beb38b084841f3c85a4de43ff1e9ec17f4
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-05-10 08:41:45 -0400

    Add Storer interface as KvStorer

--- pkg/db/kvStorer.go
+++ pkg/db/kvStorer.go
@@ -19,25 +19,16 @@ func (k KeyValueStore) Delete(key []byte) {
 }
 
 func (k KeyValueStore) Get(key []byte) ([]byte, bool) {
-	//log.Default.With("Key", string(append(k.prefix, key...))).Info("Getting keys from db")
-	//val, ok := k.ephemeral.Get(key)
 	get, err := (*k.db).Get(append(k.prefix, key...))
 	if err != nil {
 		return nil, false
 	}
-	//if string(val) != string(get) || (ok != (get != nil)) {
-	//	log.Default.With("Ephemeral", val, "Lmdbx", get, "Key", string(key)).Panic("values aren't equals")
-	//}
 	return get, get != nil
 }
 
 func (k KeyValueStore) Put(key, val []byte) {
-	//log.Default.With("Value", string(val), "Key", string(append(k.prefix, key...))).
-	//	Info("Putting values on db")
-	//k.ephemeral.Put(key, val)
 	err := (*k.db).Put(append(k.prefix, key...), val)
 	if err != nil {
-		panic(any("Error"))
 		return
 	}
 }
