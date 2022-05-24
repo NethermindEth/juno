aa51ff0e5ba19991c14c5c3a7b74a9de8509bffe
devenmatthews <70613391+devenmatthews@users.noreply.github.com>
2022-05-24 12:09:36 -0700

    syntax fixes

--- pkg/rest/rest.go
+++ pkg/rest/rest.go
@@ -11,27 +11,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//writes all http.Responses to output in JSON format
-// func responseToJSON(w http.ResponseWriter, resp *http.Response) {
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	//Convert the body to type string
-// 	sb := string(body)
-// 	json.NewEncoder(w).Encode(sb)
-// }
+var feederClient *feeder.Client
 
-//cannot unmarshal array into Go struct field StarknetBlock.transactions of type feeder.TxnSpecificInfo
+//Returns Starknet Block
 func getBlock(w http.ResponseWriter, r *http.Request) {
-
 	var (
 		res feeder.StarknetBlock
 		err error
 	)
-
-	feederClient := feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
-
 	blockNumber, ok_blockNumber := r.URL.Query()["blockNumber"]
 	_blockNumber := strings.Join(blockNumber, "")
 
@@ -49,36 +36,8 @@ func getBlock(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Transaction Status failed: invalid input")
 }
 
-//return block with optional arguments for blockhash or blocknumber. Returns latest block if not specified.
-// func getBlock(w http.ResponseWriter, r *http.Request) {
-// 	var get_block_url string
-// 	blockNumber, ok_blockNumber := r.URL.Query()["blockNumber"]
-// 	blockHash, ok_blockHash := r.URL.Query()["blockHash"]
-// 	//return latest block if block_hash and block_number not specified
-// 	if !ok_blockNumber && !ok_blockHash {
-// 		fmt.Println(`latest block`)
-// 		get_block_url = "https://alpha-mainnet.starknet.io/feeder_gateway/get_block"
-// 		//return block by block_hash if block_hash is specified
-// 	} else if ok_blockHash {
-// 		blockHash := strings.Join(blockHash, "")
-// 		get_block_url = fmt.Sprintf("https://alpha-mainnet.starknet.io/feeder_gateway/get_block?blockHash=%s", blockHash)
-// 		//return block by block_number if only block_number is specified
-// 	} else if ok_blockNumber {
-// 		blockNumber := strings.Join(blockNumber, "")
-// 		get_block_url = fmt.Sprintf("https://alpha-mainnet.starknet.io/feeder_gateway/get_block?blockNumber=%s", blockNumber)
-// 	}
-// 	resp, err := http.Get(get_block_url)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	responseToJSON(w, resp)
-// }
-
-//(feederClient *feeder.Client)
+//returns CodeInfo
 func getCode(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Code request handled")
-	//globally defined feed_gateway_client
-	feederClient := feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
 
 	blockNumber, ok_blockNumber := r.URL.Query()["blockNumber"]
 	var _blockNumber string
@@ -100,7 +59,6 @@ func getCode(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		//sb := string(strings.Join(res, ""))
 		json.NewEncoder(w).Encode(res)
 		return
 	}
@@ -108,22 +66,20 @@ func getCode(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "GetCode Request Failed: invalid inputs")
 }
 
+//returns StorageInfo
 func getStorageAt(w http.ResponseWriter, r *http.Request) {
 	query_args := r.URL.Query()
 	fmt.Fprintf(w, "before")
-	feederClient := feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
 	fmt.Fprintf(w, "start")
 	res, err := feederClient.GetStorageAt(strings.Join(query_args["key"], ""), strings.Join(query_args["contractAddress"], ""), strings.Join(query_args["blockNumber"], ""), strings.Join(query_args["blockHash"], ""))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	//fmt.Fprintf(w, res)
 	json.NewEncoder(w).Encode(res)
 }
 
-//works
+//Returns Transaction Status
 func getTransactionStatus(w http.ResponseWriter, r *http.Request) {
-	feederClient := feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
 
 	txHash, ok_txHash := r.URL.Query()["transactionHash"]
 	_txHash := strings.Join(txHash, "")
@@ -142,18 +98,13 @@ func getTransactionStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Transaction Status failed: invalid input")
 }
 
-// func requestNotFound(w http.ResponseWriter, r *http.Request) {
-// 	w.WriteHeader(http.StatusNotFound)
-// 	w.Write([]byte("request not found"))
-// }
-
 func main() {
 
 	router := mux.NewRouter()
 	router.UseEncodedPath()
 
 	//globally defined feed_gateway_client
-	//feederClient := feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
+	feederClient = feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
 
 	//get_block endpoint
 	router.HandleFunc("/get_block", getBlock).Methods("GET")
