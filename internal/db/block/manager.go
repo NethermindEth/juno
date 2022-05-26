569f5ddf60cb81dad9a4a0ebdadd27597b0516cc
stdevAdrianPaez <adriantpaez@gmail.com>
2022-05-26 09:57:31 -0400

    Update manager and create the service

--- internal/db/block/manager.go
+++ internal/db/block/manager.go
@@ -1,8 +1,8 @@
 package block
 
 import (
-	"github.com/NethermindEth/juno/pkg/db"
-	"math/big"
+	"github.com/NethermindEth/juno/internal/db"
+	"google.golang.org/protobuf/proto"
 )
 
 // Manager is a Block database manager to save and search the blocks.
@@ -11,36 +11,38 @@ type Manager struct {
 }
 
 // NewManager returns a new Block manager using the given database.
-func NewManager(databaser db.Databaser) *Manager {
-	return &Manager{database: databaser}
+func NewManager(database db.Databaser) *Manager {
+	return &Manager{database: database}
 }
 
 // GetBlock search the block with the given block hash. If the block does not
 // exist then returns nil. If any error happens, then panic.
-func (manager *Manager) GetBlock(blockHash big.Int) *BlockData {
-	rawKeys := blockHash.Bytes()
-	rawResult, err := manager.database.Get(rawKeys)
+func (manager *Manager) GetBlock(blockHash []byte) *Block {
+	rawResult, err := manager.database.Get(blockHash)
 	if err != nil {
 		panic(any(err))
 	}
-	var block BlockData
-	err = block.Unmarshal(rawResult)
+	block := &Block{}
+	err = proto.Unmarshal(rawResult, block)
 	if err != nil {
 		panic(any(err))
 	}
-	return &block
+	return block
 }
 
 // PutBlock saves the given block with the given hash as key. If any error happens
 // then panic.
-func (manager *Manager) PutBlock(blockHash big.Int, block BlockData) {
-	rawKey := blockHash.Bytes()
-	rawValue, err := block.Marshal()
+func (manager *Manager) PutBlock(blockHash []byte, block *Block) {
+	rawValue, err := proto.Marshal(block)
 	if err != nil {
 		panic(any(err))
 	}
-	err = manager.database.Put(rawKey, rawValue)
+	err = manager.database.Put(blockHash, rawValue)
 	if err != nil {
 		panic(any(err))
 	}
+}
+
+func (manager *Manager) Close() {
+	manager.database.Close()
 }
