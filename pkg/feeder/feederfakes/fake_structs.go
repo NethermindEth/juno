9035909a18824e2894dad24d2b81b9e8cfcbbfe7
devenmatthews <70613391+devenmatthews@users.noreply.github.com>
2022-05-26 17:13:38 -0700

    fix tests

--- pkg/feeder/feederfakes/fake_structs.go
+++ pkg/feeder/feederfakes/fake_structs.go
@@ -22,10 +22,11 @@ func ReturnFakeCodeInfo() *feeder.CodeInfo {
 
 func ReturnAbiInfo(*abi.Abi) *abi.Abi {
 	//var json []byte
-	json := "[{\"inputs\": [{\"name\": \"implementation\", \"type\": \"felt\"}], \"name\": \"constructor\", \"outputs\": [], \"type\": \"constructor\"}, {\"inputs\": [{\"name\": \"selector\", \"type\": \"felt\"}, {\"name\": \"calldata_size\", \"type\": \"felt\"}, {\"name\": \"calldata\", \"type\": \"felt*\"}], \"name\": \"__default__\", \"outputs\": [{\"name\": \"retdata_size\", \"type\": \"felt\"}, {\"name\": \"retdata\", \"type\": \"felt*\"}], \"type\": \"function\"}, {\"inputs\": [{\"name\": \"selector\", \"type\": \"felt\"}, {\"name\": \"calldata_size\", \"type\": \"felt\"}, {\"name\": \"calldata\", \"type\": \"felt*\"}], \"name\": \"__l1_default__\", \"outputs\": [], \"type\": \"l1_handler\"}, {\"inputs\": [], \"name\": \"get_implementation\", \"outputs\": [{\"name\": \"implementation\", \"type\": \"felt\"}], \"stateMutability\": \"view\", \"type\": \"function\"}]"
-	bjson := []byte(json)
+	_json := "[{\"inputs\": [{\"name\": \"implementation\", \"type\": \"felt\"}], \"name\": \"constructor\", \"outputs\": [], \"type\": \"constructor\"}, {\"inputs\": [{\"name\": \"selector\", \"type\": \"felt\"}, {\"name\": \"calldata_size\", \"type\": \"felt\"}, {\"name\": \"calldata\", \"type\": \"felt*\"}], \"name\": \"__default__\", \"outputs\": [{\"name\": \"retdata_size\", \"type\": \"felt\"}, {\"name\": \"retdata\", \"type\": \"felt*\"}], \"type\": \"function\"}, {\"inputs\": [{\"name\": \"selector\", \"type\": \"felt\"}, {\"name\": \"calldata_size\", \"type\": \"felt\"}, {\"name\": \"calldata\", \"type\": \"felt*\"}], \"name\": \"__l1_default__\", \"outputs\": [], \"type\": \"l1_handler\"}, {\"inputs\": [], \"name\": \"get_implementation\", \"outputs\": [{\"name\": \"implementation\", \"type\": \"felt\"}], \"stateMutability\": \"view\", \"type\": \"function\"}]"
+	bjson := []byte(_json)
 	var a abi.Abi
-	a.UnmarshalJSON(bjson)
+	json.Unmarshal(bjson, &a)
+	//abi.Abi.UnmarshalJSON(bjson)
 
 	return &a
 }
