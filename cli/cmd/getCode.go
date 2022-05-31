fd6777ca1cfb3384e62bf33ba38180bd877b6e21
D-DePablos <d.depablosaguero@gmail.com>
2022-05-31 13:20:03 +0100

    create get_code command template

--- cli/cmd/getCode.go
+++ cli/cmd/getCode.go
@@ -8,14 +8,10 @@ import (
 
 // getCodeCmd represents the getCode command
 var getCodeCmd = &cobra.Command{
-	Use:   "getCode",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "get_code CONTRACT_ADDRESS",
+	Short: `List of bytecodes of deployed transaction.`,
+	Long:  `https://www.cairo-lang.org/docs/hello_starknet/cli.html#get-code`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("getCode called")
 	},
