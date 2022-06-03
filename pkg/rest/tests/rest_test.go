5f19812daa4987f0db3ef4927dca9c618e7ccf7f
devenmatthews <70613391+devenmatthews@users.noreply.github.com>
2022-06-02 17:14:09 -0700

    get rid of feeder_gateway tests

--- pkg/rest/tests/rest_test.go
+++ pkg/rest/tests/rest_test.go
@@ -1,118 +1,331 @@
-package rest
+// package rest_test
+package tests
 
-//uses feederfakes package to calls with known answers
+// NOTE: feederfakes creates an import cycle so testing has to be in a
+// different package.
+
 import (
-	"testing"
-
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"testing"
 
 	"github.com/NethermindEth/juno/pkg/feeder"
 	"github.com/NethermindEth/juno/pkg/feeder/feederfakes"
+	"github.com/NethermindEth/juno/pkg/rest"
+	"github.com/bxcodec/faker"
 	"github.com/stretchr/testify/assert"
 )
 
-var realClient *feeder.Client
+var (
+	httpClient = &feederfakes.FakeHttpClient{}
+	client     *feeder.Client
+)
 
 func init() {
-	realClient = feeder.NewClient("https://alpha-mainnet.starknet.io", "/feeder_gateway", nil)
+	var r rest.Server
+	r.ListenAndServe()
+	var p feeder.HttpClient
+	p = httpClient
+	client = feeder.NewClient("https:/localhost:8100", "/feeder_gateway/", &p)
 }
 
-func TestRealGetFullContract(t *testing.T) {
-	//a := feederfakes.ReturnFakeFullContract()
-	getBlock, err := realClient.GetFullContract("0x03a0ae1aaefeed60bafd6990f06d0b68fb593b5d9395ff726868ee61a6e1beb3", "", "3")
+func generateResponse(body string) *http.Response {
+	return &http.Response{
+		Status:        "200 OK",
+		StatusCode:    200,
+		Proto:         "HTTP/1.1",
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		Body:          ioutil.NopCloser(bytes.NewBufferString(body)),
+		ContentLength: int64(len(body)),
+		Header:        make(http.Header, 0),
+	}
+}
+
+func StructFaker(a interface{}) (string, error) {
+	s := reflect.ValueOf(a)
+	err := faker.FakeData(&s)
+	if err != nil {
+		return "", err
+	}
+	body, err := json.Marshal(s)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func TestClient(t *testing.T) {
+	_ = feeder.NewClient("https:/local", "/feeder_gateway/", nil)
+}
+
+func TestGetContractAddress(t *testing.T) {
+	// XXX: Use raw string literal to formal JSON.
+	body := "{\"GpsStatementVerifier\":\"0x47312450B3Ac8b5b8e247a6bB6d523e7605bDb60\",\"Starknet\":\"0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4\"}\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig feeder.ContractAddresses
+	err := json.Unmarshal([]byte(body), &cOrig)
 	if err != nil {
 		t.Fatal()
 	}
-	//k := (getBlock
-	m := getBlock["abi"]
-	if m != nil {
-		assert.True(t, true, "Full Contract response don't match")
-	} else {
-		assert.True(t, false, "Ful Contract does not contain abi - failed")
+	contractAddresses, err := client.GetContractAddresses()
+	if err != nil {
+		t.Fatal()
+		return
 	}
+	assert.Equal(t, &cOrig, contractAddresses, "Contract Address don't match")
 }
 
-func TestRealGetBlock(t *testing.T) {
-	a := feederfakes.ReturnFakeBlockInfo()
-	getBlock, err := realClient.GetBlock("", "0")
+func TestCallContract(t *testing.T) {
+	a := make(map[string][]string)
+	body, err := json.Marshal(a)
+
+	httpClient.DoReturns(generateResponse(string(body)), nil)
+	contractResponse, err := client.CallContract(feeder.InvokeFunction{}, "", "latest")
 	if err != nil {
 		t.Fatal()
 	}
-	assert.Equal(t, a, getBlock, "GetBlock response don't match")
+	assert.Equal(t, &a, contractResponse, "CallContract response don't match")
 }
 
-func TestRealGetCode(t *testing.T) {
-	a := feederfakes.ReturnFakeCodeInfo()
-	getCode, err := realClient.GetCode("0x0090bff87efa37c2a8d0fd8b903ca1220dbb375ad18346e0da59af7f7e6c4285", "", "latest")
+func TestGetBlock(t *testing.T) {
+	a := feeder.StarknetBlock{}
+	body, err := StructFaker(a)
 	if err != nil {
 		t.Fatal()
 	}
-	assert.Equal(t, a, getCode, "GetCode response don't match")
+	httpClient.DoReturns(generateResponse(body), nil)
+	starknetBlock, err := client.GetBlock("", "latest")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &a, starknetBlock, "StarknetBlock don't match")
 }
 
-func TestRealGetCode_FullCoverage(t *testing.T) {
+func TestGetStateUpdate(t *testing.T) {
+	// XXX: Use raw string literal to formal JSON.
+	body := "{\"block_hash\": \"0x6c9a1403d0d573ff7ce46b5ac0fba02b289bed60e26cacbc08f335e0a75fbbe\", \"new_root\": \"070d8a4d9843d6fb1a2b0564334133fc00a70b0ec9b9ccd791489c1c17a4a963\", \"old_root\": \"0025b10263a0ce4f984d313e8df3fae4dd473e6164b0c5e261a9af35892eafed\", \"state_diff\": {\"storage_diffs\": {\"0x5e4cac746d8709c776e901bcd5d23b2c2f696a0c850d47738bcbbddb1845ce5\": [], \"0x7091a8bbcb0f313b54af52c20f9b159258dca539d3caf0f7f007aae9b2252a9\": [{\"key\": \"0xf920571b9f85bdd92a867cfdc73319d0f8836f0e69e06e4c5566b6203f75cc\", \"value\": \"0x90aa7a9203bff78bfb24f0753c180a33d4bad95b1f4f510b36b00993815704\"}, {\"key\": \"0x1ccc09c8a19948e048de7add6929589945e25f22059c7345aaf7837188d8d05\", \"value\": \"0x1275efe83de1d12dce4bf30c44833afc7922b98f5cf9bcccc7cf941f4a3d814\"}], \"0x7d75ba4ac1ba55e1592375486d7937c1c55aab8fc3215de1429824d1c77da7a\": [{\"key\": \"0xf920571b9f85bdd92a867cfdc73319d0f8836f0e69e06e4c5566b6203f75cc\", \"value\": \"0x90aa7a9203bff78bfb24f0753c180a33d4bad95b1f4f510b36b00993815704\"}, {\"key\": \"0x1ccc09c8a19948e048de7add6929589945e25f22059c7345aaf7837188d8d05\", \"value\": \"0x5fcde101109bfe594ea7d47ec511213bf1a74d68840a1b36db7b649821a0e69\"}], \"0x4cf0f19cf486a762ea864077d368d8215f7d220b784a3360da52312e759ca66\": [{\"key\": \"0x37501df619c4fc4e96f6c0243f55e3abe7d1aca7db9af8f3740ba3696b3fdac\", \"value\": \"0x9\"}], \"0xec9b5d89811008b9a87145fd95f88827fbf0b8c273a7a7a51ad3523cdf9f55\": [{\"key\": \"0x5\", \"value\": \"0x0\"}, {\"key\": \"0x31d93a9cf0c6f9756c8323917128dd1fdf100f5fc3148f652204056ba88e26e\", \"value\": \"0x7e5\"}], \"0x7394cbe418daa16e42b87ba67372d4ab4a5df0b05c6e554d158458ce245bc10\": [{\"key\": \"0x7c68b6f31d543ce2476fc12f2411efd82f69d7d591505b7837ecff577125d87\", \"value\": \"0xa2a15d09519be00000\"}, {\"key\": \"0x1557182e4359a1f0c6301278e8f5b35a776ab58d39892581e357578fb287836\", \"value\": \"0x78118d2063b6ace88899ed8e4cfb5f3f\"}], \"0x741a8ac043f744786b0a61a7cb29238b5fba637484348a2ff30e9b1276ba41f\": [{\"key\": \"0x31d93a9cf0c6f9756c8323917128dd1fdf100f5fc3148f652204056ba88e26e\", \"value\": \"0x7c7\"}], \"0x11d0bd2a59aec732a27c3532decaa00db297c9d832f32eeefd35d27285302f2\": [{\"key\": \"0x5\", \"value\": \"0x64\"}]}, \"deployed_contracts\": [{\"address\": \"0x5e4cac746d8709c776e901bcd5d23b2c2f696a0c850d47738bcbbddb1845ce5\", \"contract_hash\": \"02864c45bd4ba3e66d8f7855adcadf07205c88f43806ffca664f1f624765207e\"}, {\"address\": \"0x7091a8bbcb0f313b54af52c20f9b159258dca539d3caf0f7f007aae9b2252a9\", \"contract_hash\": \"071c3c99f5cf76fc19945d4b8b7d34c7c5528f22730d56192b50c6bbfd338a64\"}, {\"address\": \"0x7d75ba4ac1ba55e1592375486d7937c1c55aab8fc3215de1429824d1c77da7a\", \"contract_hash\": \"071c3c99f5cf76fc19945d4b8b7d34c7c5528f22730d56192b50c6bbfd338a64\"}]}}\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig feeder.StateUpdateResponse
+	err := json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	getStateUpdate, err := client.GetStateUpdate("hash", "")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, getStateUpdate, "State Update response don't match")
+}
+
+func TestGetFullContract(t *testing.T) {
+	body := "{\"block_hash\": \"0x03a0ae1aaefeed60bafd6990f06d0b68fb593b5d9395ff726868ee61a6e1beb3\", \"block_number\": \"3\"}\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig map[string]interface{}
+	err := json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	getStateUpdate, err := client.GetFullContract("address", "hash", "number")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, cOrig, getStateUpdate, "State Update response don't match")
+}
+
+func TestGetCode(t *testing.T) {
+	a := feeder.CodeInfo{}
+	body, err := StructFaker(a)
+	if err != nil {
+		t.Fatal()
+	}
+	httpClient.DoReturns(generateResponse(body), nil)
+	getCode, err := client.GetCode("hash", "", "latest")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &a, getCode, "GetCode response don't match")
+}
+
+func TestGetCode_FullCoverage(t *testing.T) {
 	a := feederfakes.ReturnAbiInfo_Full()
 	assert.Equal(t, "Struct-custom", a.Structs[0].Name)
 }
 
-func TestRealGetCode_FailType(t *testing.T) {
+func TestGetCode_FailType(t *testing.T) {
 	a := feederfakes.ReturnAbiInfo_Fail()
 	err := fmt.Errorf("unexpected type %s", "unknown")
 	assert.Equal(t, err, a)
 }
 
-func TestRealGetContractAddress(t *testing.T) {
-	a := feederfakes.ReturnFakeContractAddressInfo()
-	getContractAdresses, err := realClient.GetContractAddresses()
+func TestGetTransaction(t *testing.T) {
+	a := feeder.TransactionInfo{}
+	err := faker.FakeData(&a)
 	if err != nil {
 		t.Fatal()
 	}
-	assert.Equal(t, a, getContractAdresses, "GetContractAddress response don't match")
+	body, err := json.Marshal(a)
+	if err != nil {
+		t.Fatal()
+	}
+	httpClient.DoReturns(generateResponse(string(body)), nil)
+	var cOrig feeder.TransactionInfo
+	err = json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionInfo, err := client.GetTransaction("", "id")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionInfo, "GetTransaction response don't match")
 }
 
-func TestRealGetStateUpdate(t *testing.T) {
-	a := feederfakes.ReturnFakeStateUpdateInfo()
-	getStateUpdate, err := realClient.GetStateUpdate("", "2")
+func TestGetTransactionbyHash(t *testing.T) {
+	a := feeder.TransactionInfo{}
+	err := faker.FakeData(&a)
 	if err != nil {
 		t.Fatal()
 	}
-	assert.Equal(t, a, getStateUpdate, "GetStateUpdate response don't match")
+	body, err := json.Marshal(a)
+	if err != nil {
+		t.Fatal()
+	}
+	httpClient.DoReturns(generateResponse(string(body)), nil)
+	var cOrig feeder.TransactionInfo
+	err = json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionInfo, err := client.GetTransaction("hash", "id")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionInfo, "GetTransaction response don't match")
 }
 
-func TestRealGetTransactionReceipt(t *testing.T) {
-	a := feederfakes.ReturnFakeTransactionReceiptInfo()
-	getTransactionReceipt, err := realClient.GetTransactionReceipt("0xe0a2e45a80bb827967e096bcf58874f6c01c191e0a0530624cba66a508ae75", "")
+func TestGetTransactionReceipt(t *testing.T) {
+	a := feeder.TransactionReceipt{}
+	err := faker.FakeData(&a)
 	if err != nil {
 		t.Fatal()
 	}
-	assert.Equal(t, a, getTransactionReceipt, "GetTransactionReceipt response don't match")
+	body, err := json.Marshal(a)
+	if err != nil {
+		t.Fatal()
+	}
+	httpClient.DoReturns(generateResponse(string(body)), nil)
+	var cOrig feeder.TransactionReceipt
+	err = json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionReceipt, err := client.GetTransactionReceipt("", "id")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionReceipt, "GetTransactionReceipt response don't match")
 }
 
-func TestRealGetTransaction(t *testing.T) {
-	a := feederfakes.ReturnFakeTransactionInfo()
-	getTransaction, err := realClient.GetTransaction("0xe0a2e45a80bb827967e096bcf58874f6c01c191e0a0530624cba66a508ae75", "")
+func TestGetTransactionStatus(t *testing.T) {
+	a := feeder.TransactionStatus{}
+	err := faker.FakeData(&a)
 	if err != nil {
 		t.Fatal()
 	}
-	println(a.TransactionInBlockInformation.BlockHash)
-	assert.Equal(t, a, getTransaction, "GetTransaction response don't match")
+	body, err := json.Marshal(a)
+	if err != nil {
+		t.Fatal()
+	}
+	httpClient.DoReturns(generateResponse(string(body)), nil)
+	var cOrig feeder.TransactionStatus
+	err = json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionStatus, err := client.GetTransactionStatus("", "id")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionStatus, "GetTransactionStatus response don't match")
 }
 
-func TestRealGetTransactionStatus(t *testing.T) {
-	a := feederfakes.ReturnFakeTransactionStatusInfo()
-	getTransactionStatus, err := realClient.GetTransactionStatus("0xe0a2e45a80bb827967e096bcf58874f6c01c191e0a0530624cba66a508ae75", "")
+func TestGetBlockHashById(t *testing.T) {
+	body := "\"hash\"\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig string
+	err := json.Unmarshal([]byte(body), &cOrig)
 	if err != nil {
 		t.Fatal()
 	}
-	assert.Equal(t, a, getTransactionStatus, "GetTransactionStatus response don't match")
+	blockHash, err := client.GetBlockHashById("id")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, blockHash, "GetBlockHashById response don't match")
 }
 
-func TestRealGetStorageAt(t *testing.T) {
-	a := feederfakes.ReturnFakeStorageAt()
-	getStorageAt, err := realClient.GetStorageAt("0x2f64e2c2650a3663169758c92d58acae85177fe218469e7e07a358f3ea1654d", "5", "0x4223f3e4f2d1e6c9753b04974acdf045e602ccfe784ea6d3722697bda0fc4d2", "100")
+func TestGetBlockIdByHash(t *testing.T) {
+	body := "\"id\"\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig string
+	err := json.Unmarshal([]byte(body), &cOrig)
 	if err != nil {
-		t.Error(err)
 		t.Fatal()
 	}
-	assert.Equal(t, a, getStorageAt, "GetStorageAt responses didn't match")
+	blockId, err := client.GetBlockIDByHash("hash")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, blockId, "GetBlockIdByHash response don't match")
+}
+
+func TestGetTransactionHashById(t *testing.T) {
+	body := "\"hash\"\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig string
+	err := json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionHash, err := client.GetTransactionHashByID("hash")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionHash, "GetTransactionHashById response don't match")
+}
+
+func TestGetTransactionIdByHash(t *testing.T) {
+	body := "\"hash\"\n"
+	httpClient.DoReturns(generateResponse(body), nil)
+	var cOrig string
+	err := json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionId, err := client.GetTransactionIDByHash("hash")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionId, "GetTransactionIdByHash response don't match")
+}
+
+func TestGetStorageAt(t *testing.T) {
+	var body feeder.StorageInfo
+	body = "\"storage\"\n"
+	httpClient.DoReturns(generateResponse(string(body)), nil)
+	var cOrig feeder.StorageInfo
+	err := json.Unmarshal([]byte(body), &cOrig)
+	if err != nil {
+		t.Fatal()
+	}
+	transactionId, err := client.GetStorageAt("address", "key", "hash", "")
+	if err != nil {
+		t.Fatal()
+	}
+	assert.Equal(t, &cOrig, transactionId, "GetTransactionIdByHash response don't match")
 }
