b010461526002f63140c77260f7f2f816ccad955
BitBaseBit <vardygreg23@gmail.com>
2022-06-07 16:30:26 +0100

    fix typo's

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Juno Documentation 🌠',
-    tagline: 'Starknet is cool',
+    tagline: 'StarkNet is cool',
     url: 'https://gojuno.xyz',
     baseUrl: '/',
     onBrokenLinks: 'throw',
