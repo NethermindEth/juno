bc0fa546a86debd3fc345c6164bf9466e87fd9b9
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-06-09 13:59:03 -0400

    Format code

--- pkg/feeder/feeder_test.go
+++ pkg/feeder/feeder_test.go
@@ -8,11 +8,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/NethermindEth/juno/pkg/feeder"
 	"io/ioutil"
 	"net/http"
 	"reflect"
 	"testing"
+
+	"github.com/NethermindEth/juno/pkg/feeder"
 
 	"github.com/NethermindEth/juno/pkg/feeder/feederfakes"
 	"github.com/bxcodec/faker"
@@ -154,6 +155,7 @@ func stateUpdateResponseToGoerli(res feeder.StateUpdateResponseGoerli) *feeder.S
 		},
 	}
 }
+
 func TestGetFullContract(t *testing.T) {
 	body := "{\"block_hash\": \"0x03a0ae1aaefeed60bafd6990f06d0b68fb593b5d9395ff726868ee61a6e1beb3\", \"block_number\": \"3\"}\n"
 	httpClient.DoReturns(generateResponse(body), nil)
