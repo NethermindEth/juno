d0bec77ea3ef0fe088d4132b49937ca245abab22
devenmatthews <70613391+devenmatthews@users.noreply.github.com>
2022-06-09 00:03:12 -0700

    http mux calls handler methods

--- pkg/rest/handler.go
+++ pkg/rest/handler.go
@@ -8,35 +8,35 @@ import (
 
 var handler RestHandler
 
-// handlerGetBlock returns Starknet Block using handler.feederGateway
-func handlerGetBlock(w http.ResponseWriter, r *http.Request) {
-	handler.GetBlock(w, r)
-}
+// // handlerGetBlock returns Starknet Block using handler.feederGateway
+// func handlerGetBlock(w http.ResponseWriter, r *http.Request) {
+// 	handler.GetBlock(w, r)
+// }
 
-// handlerGetCode returns CodeInfo using Block Identifier & Contract Address
-func handlerGetCode(w http.ResponseWriter, r *http.Request) {
-	handler.GetCode(w, r)
-}
+// // handlerGetCode returns CodeInfo using Block Identifier & Contract Address
+// func handlerGetCode(w http.ResponseWriter, r *http.Request) {
+// 	handler.GetCode(w, r)
+// }
 
-// handlerGetStorageAt returns StorageInfo
-func handlerGetStorageAt(w http.ResponseWriter, r *http.Request) {
-	handler.GetStorageAt(w, r)
-}
+// // handlerGetStorageAt returns StorageInfo
+// func handlerGetStorageAt(w http.ResponseWriter, r *http.Request) {
+// 	handler.GetStorageAt(w, r)
+// }
 
-// handlerGetTransactionStatus returns Transaction Status
-func handlerGetTransactionStatus(w http.ResponseWriter, r *http.Request) {
-	handler.GetTransactionStatus(w, r)
-}
+// // handlerGetTransactionStatus returns Transaction Status
+// func handlerGetTransactionStatus(w http.ResponseWriter, r *http.Request) {
+// 	handler.GetTransactionStatus(w, r)
+// }
 
 func NewServer(rest_port string, feeder_gateway string) *Server {
-	m := http.NewServeMux()
-
-	m.HandleFunc("/get_block", handlerGetBlock)
-	m.HandleFunc("/get_code", handlerGetCode)
-	m.HandleFunc("/get_storage_at", handlerGetStorageAt)
-	m.HandleFunc("/get_transaction_status", handlerGetTransactionStatus)
 
 	handler.RestFeeder = feeder.NewClient(feeder_gateway, "/feeder_gateway", nil)
+	m := http.NewServeMux()
+
+	m.HandleFunc("/get_block", handler.GetBlock)
+	m.HandleFunc("/get_code", handler.GetCode)
+	m.HandleFunc("/get_storage_at", handler.GetStorageAt)
+	m.HandleFunc("/get_transaction_status", handler.GetTransactionStatus)
 
 	return &Server{server: http.Server{Addr: rest_port, Handler: m}}
 }
