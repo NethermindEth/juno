487a9b8c5846c1cd997b9e5a3ba4645c5ac286ee
devenmatthews <70613391+devenmatthews@users.noreply.github.com>
2022-06-11 16:28:32 -0700

    fix handler

--- pkg/rest/handler.go
+++ pkg/rest/handler.go
@@ -21,10 +21,10 @@ func NewServer(rest_port string, feeder_gateway string) *Server {
 	m.HandleFunc("/get_full_contract", rest_handler.GetFullContract)
 	m.HandleFunc("/get_state_update", rest_handler.GetStateUpdate)
 	m.HandleFunc("/get_contract_addresses", rest_handler.GetContractAddresses)
-	m.HandleFunc("/get_block_hash_by_ID", rest_handler.GetBlockHashById)
-	m.HandleFunc("/get_contract_addresses", rest_handler.GetBlockIDByHash)
-	m.HandleFunc("/get_contract_addresses", rest_handler.GetTransactionHashByID)
-	m.HandleFunc("/get_contract_addresses", rest_handler.GetTransactionIDByHash)
+	m.HandleFunc("/get_block_hash_by_id", rest_handler.GetBlockHashById)
+	m.HandleFunc("/get_block_id_by_hash", rest_handler.GetBlockIDByHash)
+	m.HandleFunc("/get_transaction_hash_by_id", rest_handler.GetTransactionHashByID)
+	m.HandleFunc("/get_transaction_id_by_hash", rest_handler.GetTransactionIDByHash)
 
 	return &Server{server: http.Server{Addr: rest_port, Handler: m}}
 }
