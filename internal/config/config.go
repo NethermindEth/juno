646d33a35df68fba5c862f657201b79593727104
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-06-13 16:30:34 -0400

    Remove unused params in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -43,7 +43,6 @@ type Config struct {
 	RPC      rpcConfig      `yaml:"rpc" mapstructure:"rpc"`
 	REST     restConfig     `yaml:"rest" mapstructure:"rest"`
 	DbPath   string         `yaml:"db_path" mapstructure:"db_path"`
-	Network  string         `yaml:"starknet_network" mapstructure:"starknet_network"`
 	Starknet starknetConfig `yaml:"starknet" mapstructure:"starknet"`
 }
 
@@ -65,10 +64,6 @@ var (
 
 // Runtime is the runtime configuration of the application.
 var Runtime *Config
-
-const goerliStarknetGateway = "http://alpha4.starknet.io"
-
-const goerli = "http://alpha4.starknet.io"
 
 func init() {
 	// Set user config directory.
@@ -124,7 +119,6 @@ func New() {
 		RPC:      rpcConfig{Enabled: false, Port: 8080},
 		REST:     restConfig{Enabled: false, Port: 8100},
 		DbPath:   DataDir,
-		Network:  goerli,
 		Starknet: starknetConfig{Enabled: true, ApiSync: true, FeederGateway: "https://alpha-mainnet.starknet.io"},
 	})
 	errpkg.CheckFatal(err, "Failed to marshal Config instance to byte data.")
