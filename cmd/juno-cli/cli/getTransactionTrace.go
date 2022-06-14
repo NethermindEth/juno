e117a256336a499aeba170858c02d107c9639151
Diego de Pablos <d.depablosaguero@gmail.com>
2022-06-14 10:31:16 +0100

    add getBlock and getTransactionTrace

---
+++ cmd/juno-cli/cli/getTransactionTrace.go
@@ -0,0 +1,32 @@
+package cli
+
+import (
+	"github.com/NethermindEth/juno/pkg/feeder"
+	"github.com/spf13/cobra"
+)
+
+// getTransactionTraceCmd represents the getTransactionTrace command
+var getTransactionTraceCmd = &cobra.Command{
+	Use:   "get_transaction_trace [TRANSACTION_HASH] [flags]",
+	Short: "Information containing inner calls for an external transaction, in chronological order.",
+	Long:  `See https://www.cairo-lang.org/docs/hello_starknet/cli.html#get-transaction-trace`,
+	Run: func(cmd *cobra.Command, args []string) {
+		res, _ := getTxTrace(args[0])
+		if pretty, _ := cmd.Flags().GetBool("pretty"); pretty {
+			prettyPrint(res)
+		} else {
+			normalReturn(res)
+		}
+	},
+}
+
+func getTxTrace(txHash string) (*feeder.TransactionTrace, error) {
+	client := initClient()
+	// Call to get tx trace - txID not used.
+	res, _ := client.GetTransactionTrace(txHash, "")
+	return res, nil
+}
+
+func init() {
+	rootCmd.AddCommand(getTransactionTraceCmd)
+}
