7f6efb1b92c9214da6ad2b5af150598b37ce5075
Jorge Mederos <jmederosalvarado@gmail.com>
2022-06-16 22:05:59 -0400

    key type polishing

--- pkg/trie/key.go
+++ pkg/trie/key.go
@@ -1,81 +1,55 @@
 package trie
 
 import (
+	"bytes"
 	"fmt"
 	"math/big"
 )
 
-type Key interface {
-	Len() int
-	Get(i int) bool
-	Set(i int)
-	Clear(i int)
-}
-
-func BigToKey(length int, b *big.Int) (Key, error) {
-	if b.BitLen() > length {
-		return nil, fmt.Errorf("key too large: %d > %d", b.BitLen(), length)
-	}
-
-	// implementation using bitSetKey
-	bs := &bitSetKey{}
-	bs.length = length
-	bs.bytes = make([]byte, (length+7)/8)
-	b.FillBytes(bs.bytes)
-	return bs, nil
-
-	// implementation using boolSliceKey
-	// bs := &boolSliceKey{bits: make([]bool, length)}
-	// for i := 0; i < b.BitLen(); i++ {
-	// 	bs.bits[(length-b.BitLen())+i] = b.Bit(i) == 1
-	// }
-	// return bs, nil
-}
-
-// using a bool array underneath
-// NOTE: please don't use this for anything other than testing
-type boolSliceKey struct {
-	bits []bool
-}
-
-func (k *boolSliceKey) Len() int {
-	return len(k.bits)
-}
-
-func (k *boolSliceKey) Get(i int) bool {
-	return k.bits[i]
-}
-
-func (k *boolSliceKey) Set(i int) {
-	k.bits[i] = true
-}
-
-func (k *boolSliceKey) Clear(i int) {
-	k.bits[i] = false
-}
-
-// using a byte array underneath
-// TODO: probably doesn't work
-type bitSetKey struct {
+type Key struct {
 	length int
 	bytes  []byte
 }
 
-func (bs *bitSetKey) Set(i int) {
+func BigToKey(length int, b *big.Int) (*Key, error) {
+	// if b.BitLen() > length {
+	// 	return nil, fmt.Errorf("key too large: %d > %d", b.BitLen(), length)
+	// }
+	// bs := &Key{length, make([]byte, (length+7)/8)}
+	// b.FillBytes(bs.bytes)
+	// return bs, nil
+	return BytesToKey(length, b.Bytes())
+}
+
+func BytesToKey(length int, b []byte) (*Key, error) {
+	b = bytes.TrimLeft(b, "\x00")
+	if len(b)*8 > length {
+		return nil, fmt.Errorf("key too large: %d > %d", len(b)*8, length)
+	}
+	bs := &Key{length, make([]byte, (length+7)/8)}
+	copy(bs.bytes[len(bs.bytes)-len(b):], b)
+	return bs, nil
+}
+
+func (bs *Key) Set(i int) {
 	i += len(bs.bytes)*8 - bs.length
 	bs.bytes[i/8] |= 1 << (7 - i%8)
 }
 
-func (bs *bitSetKey) Clear(i int) {
+func (bs *Key) Clear(i int) {
 	i += len(bs.bytes)*8 - bs.length
 	bs.bytes[i/8] &= ^(1 << (7 - i%8))
 }
 
-func (bs *bitSetKey) Get(i int) bool {
+func (bs *Key) Get(i int) bool {
 	i += len(bs.bytes)*8 - bs.length
 	return bs.bytes[i/8]&(1<<(7-i%8)) != 0
 }
 
-func (bs *bitSetKey) Len() int {
+func (bs *Key) Len() int {
 	return bs.length
+}
+
+func (bs *Key) Bytes() []byte {
+	return bs.bytes
 }
