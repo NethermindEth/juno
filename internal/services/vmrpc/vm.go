680ebdfca78a98d834d3440f4119e63a317f604d
Tshaka Eric Lekholoane <_@tshaka.co>
2022-06-23 15:01:09 +0200

    refactor: simulate db lookups post trie insertion

--- internal/services/vmrpc/vm.go
+++ internal/services/vmrpc/vm.go
@@ -3,7 +3,9 @@ package vmrpc
 import (
 	"context"
 	_ "embed"
+	"fmt"
 	"math/big"
+	"strings"
 
 	"github.com/NethermindEth/juno/pkg/store"
 	"github.com/NethermindEth/juno/pkg/trie"
@@ -20,7 +22,7 @@ var def string
 
 // DEBUG.
 var db = map[string]string{
-	"patricia_node:0704dfcbc470377c68e6f5ffb83970ebd0d7c48d5b8d2f4ed61a24e795e034bd": "002e9723e54711aec56e3fb6ad1bb8272f64ec92e0a43a20feed943b1d4f73c5057dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374fb",
+	// "patricia_node:0704dfcbc470377c68e6f5ffb83970ebd0d7c48d5b8d2f4ed61a24e795e034bd": "002e9723e54711aec56e3fb6ad1bb8272f64ec92e0a43a20feed943b1d4f73c5057dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374fb",
 	"contract_state:002e9723e54711aec56e3fb6ad1bb8272f64ec92e0a43a20feed943b1d4f73c5": `{
 			"storage_commitment_tree": {
 				"root": "04fb440e8ca9b74fc12a22ebffe0bc0658206337897226117b985434c239c028", 
@@ -28,7 +30,7 @@ var db = map[string]string{
 			}, 
 			"contract_hash": "050b2148c0d782914e0b12a1a32abe5e398930b7e914f82c65cb7afce0a0ab9b"
 		}`,
-	"patricia_node:04fb440e8ca9b74fc12a22ebffe0bc0658206337897226117b985434c239c028":            "00000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000084fb",
+	// "patricia_node:04fb440e8ca9b74fc12a22ebffe0bc0658206337897226117b985434c239c028":            "00000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000084fb",
 	"starknet_storage_leaf:0000000000000000000000000000000000000000000000000000000000000003":    "0000000000000000000000000000000000000000000000000000000000000003",
 	"contract_definition_fact:050b2148c0d782914e0b12a1a32abe5e398930b7e914f82c65cb7afce0a0ab9b": def,
 }
@@ -36,13 +38,13 @@ var db = map[string]string{
 // DEBUG.
 // Databases that provide the back end to the contract storage and
 // global state tries.
-var storage, state store.KVStorer
+var storage, state store.Ephemeral
 
 // DEBUG.
 func init() {
 	// STORAGE.
-	storage = store.New()
-	contract, _ := trie.New(storage, trie.EmptyNode.Hash(), 251)
+	old_storage := store.New()
+	contract, _ := trie.New(old_storage, trie.EmptyNode.Hash(), 251)
 
 	// Storage modifications.
 	mods := []struct {
@@ -60,8 +62,8 @@ func init() {
 	}
 
 	// STATE.
-	state = store.New()
-	global, _ := trie.New(state, trie.EmptyNode.Hash(), 251)
+	old_state := store.New()
+	global, _ := trie.New(old_state, trie.EmptyNode.Hash(), 251)
 
 	// Contract address.
 	key, _ := new(big.Int).SetString("57dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374", 16)
@@ -74,6 +76,25 @@ func init() {
 	// global.Put(types.BigToFelt(key), types.BigToFelt(val))
 	feltKey, feltVal := types.BigToFelt(key), types.BigToFelt(val)
 	global.Put(&feltKey, &feltVal)
+
+	// Simulate new node serialisation.
+	state = store.New()
+	nodes := old_state.List()
+	for _, pair := range nodes {
+		key := "patricia_node:" + fmt.Sprintf("%.64x", types.BytesToFelt(pair[0]).Big())
+		val := new(trie.Node)
+		val.UnmarshalJSON(pair[1])
+		state.Put([]byte(key), []byte(val.CairoRepr()))
+	}
+
+	storage = store.New()
+	nodes = old_storage.List()
+	for _, pair := range nodes {
+		key := "patricia_node:" + fmt.Sprintf("%.64x", types.BytesToFelt(pair[0]).Big())
+		val := new(trie.Node)
+		val.UnmarshalJSON(pair[1])
+		storage.Put([]byte(key), []byte(val.CairoRepr()))
+	}
 }
 
 func NewStorageRPCServer() *storageRPCServer {
@@ -110,5 +131,17 @@ func (s *storageRPCServer) GetValue(ctx context.Context, request *GetValueReques
 	//
 	//	4.	starknet_storage_leaf. Here the key suffix *is* the value so
 	//			that could be returned without any lookup on the Python side.
-	return &GetValueResponse{Value: []byte(db[string(request.GetKey())])}, nil
+	parts := strings.Split(string(request.GetKey()), ":")
+	switch prefix := parts[0]; prefix {
+	case "patricia_node":
+		// Check for node in state trie first.
+		val, ok := state.Get(request.GetKey())
+		if !ok {
+			// If the key does not exist, check the storage trie.
+			val, _ = storage.Get(request.GetKey())
+		}
+		return &GetValueResponse{Value: val}, nil
+	default:
+		return &GetValueResponse{Value: []byte(db[string(request.GetKey())])}, nil
+	}
 }
