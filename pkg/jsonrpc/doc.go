7b82aea02a206be0cc38ee0e574d2a826ad25a4e
stdevAdrianPaez <adriantpaez@gmail.com>
2022-06-27 20:15:18 -0400

    Update pkg/jsonrpc doc

--- pkg/jsonrpc/doc.go
+++ pkg/jsonrpc/doc.go
@@ -1,2 +1,5 @@
-// Package jsonrpc djflksjdfkljdk sldfjlksd
+// Package jsonrpc is a JSON-RPC 2.0 library for Go. This implementation abstracts the low-level 
+// details of JSON-RPC 2.0 and provides a high-level programming interface. The idea is that this
+// implementation is not oriented to the specific connection protocol (TCP, HTTP, etc.). It is 
+// intended to be used by other libraries that manage the connections.
 package jsonrpc
