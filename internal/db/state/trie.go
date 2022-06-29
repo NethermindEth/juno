678329b5f7d6f71318155741adb50d7a96e467c5
stdevAdrianPaez <adriantpaez@gmail.com>
2022-06-28 21:32:23 -0400

    Fix bug on StoreTrieNode

--- internal/db/state/trie.go
+++ internal/db/state/trie.go
@@ -47,27 +47,31 @@ func (m *Manager) GetTrieNode(hash *types.Felt) (trie.TrieNode, error) {
 
 func (m *Manager) StoreTrieNode(node trie.TrieNode) error {
 	key := node.Hash().Bytes()
+	var pbTrieNode isTrieNode_Node
 	switch node := node.(type) {
 	case *trie.BinaryNode:
-		rawNode, err := proto.Marshal(&BinaryNode{
-			LeftH:  node.LeftH.Bytes(),
-			RightH: node.RightH.Bytes(),
-		})
-		if err != nil {
-			panic(err)
+		pbTrieNode = &TrieNode_BinaryNode{
+			BinaryNode: &BinaryNode{
+				LeftH:  node.LeftH.Bytes(),
+				RightH: node.RightH.Bytes(),
+			},
 		}
-		return m.trieDatabase.Put(key, rawNode)
 	case *trie.EdgeNode:
-		rawNode, err := proto.Marshal(&EdgeNode{
-			Length: uint32(node.Path().Len()),
-			Path:   node.Path().Bytes(),
-			Bottom: node.Bottom().Bytes(),
-		})
-		if err != nil {
-			panic(err)
+		pbTrieNode = &TrieNode_EdgeNode{
+			&EdgeNode{
+				Length: uint32(node.Path().Len()),
+				Path:   node.Path().Bytes(),
+				Bottom: node.Bottom().Bytes(),
+			},
 		}
-		return m.trieDatabase.Put(key, rawNode)
 	default:
 		panic(fmt.Sprintf("unknown node type: %T", node))
 	}
+	rawNode, err := proto.Marshal(&TrieNode{
+		Node: pbTrieNode,
+	})
+	if err != nil {
+		return err
+	}
+	return m.trieDatabase.Put(key, rawNode)
 }
