9852b7d4793008ecd0a7c8cadd1e15cf76ec8021
Diego de Pablos <d.depablosaguero@gmail.com>
2022-07-01 11:11:49 +0100

    add unimportant flag to health service

--- cmd/juno/cli/root.go
+++ cmd/juno/cli/root.go
@@ -91,7 +91,7 @@ var (
 			processHandler.Add("Contract Hash Storage Service", false, services.ContractHashService.Run, services.ContractHashService.Close)
 
 			// Initialise Health Check
-			processHandler.Add("Health Check", services.HealthService.Run, services.HealthService.Close)
+			processHandler.Add("Health Check", true, services.HealthService.Run, services.HealthService.Close)
 
 			// Subscribe the Starknet Synchronizer to the main loop if it is enabled in
 			// the config.
