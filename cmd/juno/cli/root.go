5ebe37cf6036e6f9d02fe64cb4a81d5793304ffe
IronGauntlets <aneequesafdar@gmail.com>
2022-07-04 13:09:27 +0100

    Move signal interrupt into to separate function

--- cmd/juno/cli/root.go
+++ cmd/juno/cli/root.go
@@ -4,143 +4,145 @@ package cli
 import (
 	_ "embed"
 	"fmt"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"strconv"
-	"syscall"
-
-	"github.com/ethereum/go-ethereum/ethclient"
-
 	"github.com/NethermindEth/juno/internal/config"
 	"github.com/NethermindEth/juno/internal/db"
 	"github.com/NethermindEth/juno/internal/errpkg"
 	"github.com/NethermindEth/juno/internal/log"
 	metric "github.com/NethermindEth/juno/internal/metrics/prometheus"
-	"github.com/NethermindEth/juno/internal/process"
 	"github.com/NethermindEth/juno/internal/services"
 	"github.com/NethermindEth/juno/pkg/feeder"
 	"github.com/NethermindEth/juno/pkg/rest"
-	"github.com/NethermindEth/juno/pkg/rpc"
 	"github.com/NethermindEth/juno/pkg/starknet"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"syscall"
 )
 
 // Cobra configuration.
 var (
 	// cfgFile is the path of the juno configuration file.
 	cfgFile string
-	// dataDir is the path of the directory to read and save user-specific
-	// application data
+	// dataDir is the path of the directory to read and save user-specific application data
 	dataDir string
 	// longMsg is the long message shown in the "juno --help" output.
 	//go:embed long.txt
 	longMsg string
-
-	processHandler *process.Handler
-	// rootCmd is the root command of the application.
-	rootCmd = &cobra.Command{
-		Use:   "juno [options]",
-		Short: "Starknet client implementation in Go.",
-		Run: func(_ *cobra.Command, _ []string) {
-			processHandler = process.NewHandler()
-
-			// Handle signal interrupts and exits.
-			sig := make(chan os.Signal, 1)
-			signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-			go func(sig chan os.Signal) {
-				<-sig
-				log.Default.Info("Trying to close...")
-				cleanup()
-				os.Exit(0)
-			}(sig)
-
-			feederGatewayClient := feeder.NewClient(config.Runtime.Starknet.FeederGateway, "/feeder_gateway", nil)
-			// Subscribe the RPC client to the main loop if it is enabled in
-			// the config.
-			if config.Runtime.RPC.Enabled {
-				s := rpc.NewServer(":"+strconv.Itoa(config.Runtime.RPC.Port), feederGatewayClient)
-				// Initialize the RPC Service.
-				processHandler.Add("RPC", true, s.ListenAndServe, s.Close)
-			}
-
-			if config.Runtime.Metrics.Enabled {
-				s := metric.SetupMetric(":" + strconv.Itoa(config.Runtime.Metrics.Port))
-				// Initialize the Metrics Service.
-				processHandler.Add("Metrics", false, s.ListenAndServe, s.Close)
-			}
-
-			if err := db.InitializeMDBXEnv(config.Runtime.DbPath, 100, 0); err != nil {
-				log.Default.With("Error", err).Fatal("Error starting the database environment")
-			}
-
-			// Initialize ABI Service
-			processHandler.Add("ABI Service", false, services.AbiService.Run, services.AbiService.Close)
-
-			// Initialize State storage service
-			processHandler.Add("State Storage Service", false, services.StateService.Run, services.StateService.Close)
-
-			// Initialize Transactions Storage Service
-			processHandler.Add("Transactions Storage Service", false, services.TransactionService.Run, services.TransactionService.Close)
-
-			// Initialize Block Storage Service
-			processHandler.Add("Block Storage Service", false, services.BlockService.Run, services.BlockService.Close)
-
-			// Initialize Contract Hash storage service
-			processHandler.Add("Contract Hash Storage Service", false, services.ContractHashService.Run, services.ContractHashService.Close)
-
-			// Subscribe the Starknet Synchronizer to the main loop if it is enabled in
-			// the config.
-			if config.Runtime.Starknet.Enabled {
-				var ethereumClient *ethclient.Client
-				if !config.Runtime.Starknet.ApiSync {
-					var err error
-					ethereumClient, err = ethclient.Dial(config.Runtime.Ethereum.Node)
-					if err != nil {
-						log.Default.With("Error", err).Fatal("Unable to connect to Ethereum Client")
-					}
-				}
-				// Synchronizer for Starknet State
-				env, err := db.GetMDBXEnv()
-				if err != nil {
-					log.Default.Fatal(err)
-				}
-				synchronizerDb, err := db.NewMDBXDatabase(env, "SYNCHRONIZER")
-				if err != nil {
-					log.Default.With("Error", err).Fatal("Error starting the SYNCHRONIZER database")
-				}
-				stateSynchronizer := starknet.NewSynchronizer(synchronizerDb, ethereumClient, feederGatewayClient)
-				// Initialize the Starknet Synchronizer Service.
-				processHandler.Add("Starknet Synchronizer", true, stateSynchronizer.UpdateState,
-					stateSynchronizer.Close)
-			}
-
-			// Subscribe the REST API client to the main loop if it is enabled in
-			// the config.
-			if config.Runtime.REST.Enabled {
-				s := rest.NewServer(":"+strconv.Itoa(config.Runtime.REST.Port), config.Runtime.Starknet.FeederGateway, config.Runtime.REST.Prefix)
-				// Initialize the REST Service.
-				processHandler.Add("REST", true, s.ListenAndServe, s.Close)
-			}
-
-			primaryServiceCheck := processHandler.PrimaryServiceChecker()
-
-			if primaryServiceCheck > 0 {
-				// endless running process
-				log.Default.Info("Starting all processes...")
-				processHandler.Run()
-				cleanup()
-			} else {
-				cleanup()
-			}
-		},
-	}
 )
 
-func cleanup() {
-	processHandler.Close()
-	log.Default.Info("App closing...Bye!!!")
+// rootCmd is the root command of the application.
+var rootCmd = &cobra.Command{
+	Use:   "juno [options]",
+	Short: "Starknet client implementation in Go.",
+	Run: func(_ *cobra.Command, _ []string) {
+		setupSignalInterruptHandler()
+
+		feederGatewayClient := feeder.NewClient(config.Runtime.Starknet.FeederGateway, "/feeder_gateway", nil)
+		// Subscribe the RPC client to the main loop if it is enabled in
+		// the config.
+		if config.Runtime.RPC.Enabled {
+			s := rpc.NewServer(":"+strconv.Itoa(config.Runtime.RPC.Port), feederGatewayClient)
+			// Initialize the RPC Service.
+			processHandler.Add("RPC", true, s.ListenAndServe, s.Close)
+		}
+
+		if config.Runtime.Metrics.Enabled {
+			s := metric.SetupMetric(":" + strconv.Itoa(config.Runtime.Metrics.Port))
+			// Initialize the Metrics Service.
+			processHandler.Add("Metrics", false, s.ListenAndServe, s.Close)
+		}
+
+		if err := db.InitializeMDBXEnv(config.Runtime.DbPath, 100, 0); err != nil {
+			log.Default.With("Error", err).Fatal("Error starting the database environment")
+		}
+
+		// Initialize ABI Service
+		processHandler.Add("ABI Service", false, services.AbiService.Run, services.AbiService.Close)
+
+		// Initialize State storage service
+		processHandler.Add("State Storage Service", false, services.StateService.Run, services.StateService.Close)
+
+		// Initialize Transactions Storage Service
+		processHandler.Add("Transactions Storage Service", false, services.TransactionService.Run, services.TransactionService.Close)
+
+		// Initialize Block Storage Service
+		processHandler.Add("Block Storage Service", false, services.BlockService.Run, services.BlockService.Close)
+
+		// Initialize Contract Hash storage service
+		processHandler.Add("Contract Hash Storage Service", false, services.ContractHashService.Run, services.ContractHashService.Close)
+
+		// Subscribe the Starknet Synchronizer to the main loop if it is enabled in
+		// the config.
+		if config.Runtime.Starknet.Enabled {
+			var ethereumClient *ethclient.Client
+			if !config.Runtime.Starknet.ApiSync {
+				var err error
+				ethereumClient, err = ethclient.Dial(config.Runtime.Ethereum.Node)
+				if err != nil {
+					log.Default.With("Error", err).Fatal("Unable to connect to Ethereum Client")
+				}
+			}
+			// Synchronizer for Starknet State
+			env, err := db.GetMDBXEnv()
+			if err != nil {
+				log.Default.Fatal(err)
+			}
+			synchronizerDb, err := db.NewMDBXDatabase(env, "SYNCHRONIZER")
+			if err != nil {
+				log.Default.With("Error", err).Fatal("Error starting the SYNCHRONIZER database")
+			}
+			stateSynchronizer := starknet.NewSynchronizer(synchronizerDb, ethereumClient, feederGatewayClient)
+			// Initialize the Starknet Synchronizer Service.
+			processHandler.Add("Starknet Synchronizer", true, stateSynchronizer.UpdateState,
+				stateSynchronizer.Close)
+		}
+
+		// Subscribe the REST API client to the main loop if it is enabled in
+		// the config.
+		if config.Runtime.REST.Enabled {
+			s := rest.NewServer(":"+strconv.Itoa(config.Runtime.REST.Port), config.Runtime.Starknet.FeederGateway, config.Runtime.REST.Prefix)
+			// Initialize the REST Service.
+			processHandler.Add("REST", true, s.ListenAndServe, s.Close)
+		}
+
+		primaryServiceCheck := processHandler.PrimaryServiceChecker()
+
+		if primaryServiceCheck > 0 {
+			// endless running process
+			log.Default.Info("Starting all processes...")
+			processHandler.Run()
+			cleanup()
+		} else {
+			cleanup()
+		}
+	},
+}
+
+// Execute handle flags for Cobra execution.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Default.With("Error", err).Error("Failed to execute CLI.")
+	}
+}
+
+func setupSignalInterruptHandler() {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	go func(sig chan os.Signal) {
+		<-sig
+		log.Default.Info("Trying to close...")
+		close()
+		os.Exit(0)
+	}(sig)
+}
+
+func close() {
+
 }
 
 // init defines flags and handles configuration.
@@ -202,11 +204,4 @@ func initConfig() {
 		"Rest Enabled", config.Runtime.REST.Enabled,
 		"Rest Prefix", config.Runtime.REST.Prefix,
 	).Info("Config values.")
-}
-
-// Execute handle flags for Cobra execution.
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		log.Default.With("Error", err).Error("Failed to execute CLI.")
-	}
 }
