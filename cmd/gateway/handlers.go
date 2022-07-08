456bec7703092f34c2adbcd5b2d8f5a0f72645b6
Tshaka Eric Lekholoane <_@tshaka.co>
2022-07-08 15:36:25 +0200

    feat: add dummy handlers with preliminary checks
    Added the following routes along with some basic validation checks for
    user input:
    
      - get_block_hash_by_id
      - get_block_id_hash
      - get_code
      - get_contract_addresses
      - get_storage_at
      - get_transaction
      - get_transaction_hash_by_id
      - get_transaction_id_by_hash

--- cmd/gateway/handlers.go
+++ cmd/gateway/handlers.go
@@ -13,17 +13,20 @@ func handlerGetBlock(w http.ResponseWriter, r *http.Request) {
 
 	// If not parameters are given, default to the latest block.
 	if len(r.URL.Query()) == 0 {
+		// TODO: Return the latest block.
 		w.Write([]byte("Default to latest block."))
 		return
 	}
 
-	// Exception list of expected parameters. A map is used to overcome
-	// the lack of a set data structure in the standard library.
+	// Slight difference here is that the feeder will collect all
+	// parameters and specify which are incorrect in the error message
+	// whereas this implementation will send an error at the first
+	// occurrence of an invalid parameter without checking the rest.
 	allowed := map[string]bool{"blockHash": true, "blockNumber": true}
 	for param := range r.URL.Query() {
 		if !allowed[param] {
 			message := fmt.Sprintf(
-				`Query fields should be a subset of {'blockHash', 'blockNumber}; `+
+				`Query fields should be a subset of {'blockHash', 'blockNumber'}; `+
 					`got: {%q}.`,
 				param)
 			clientErr(w, http.StatusBadRequest, message)
@@ -31,8 +34,8 @@ func handlerGetBlock(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// Requires one of.
 	if r.URL.Query().Has("blockHash") && r.URL.Query().Has("blockNumber") {
-		// Requires one of.
 		message := fmt.Sprintf(
 			`Exactly one identifier - blockNumber or blockHash, have to be `+
 				`specified; got: blockNumber=%s, blockHash=%s.`,
@@ -44,12 +47,276 @@ func handlerGetBlock(w http.ResponseWriter, r *http.Request) {
 
 	switch {
 	case r.URL.Query().Has("blockHash"):
-		// TODO: Get block by hash.
-		fmt.Fprintf(w, "Get block by hash, %s.", r.URL.Query().Get("blockHash"))
+		// TODO: Fetch block from database using its hash.
+		fmt.Fprintf(w, "Get block by hash: %s.", r.URL.Query().Get("blockHash"))
 	case r.URL.Query().Has("blockNumber"):
-		// TODO: Get block by number.
-		fmt.Fprintf(w, "Get block by number, %s.", r.URL.Query().Get("blockNumber"))
+		// TODO: Fetch block from database using its number.
+		fmt.Fprintf(w, "Get block by number: %s.", r.URL.Query().Get("blockNumber"))
 	}
+}
+
+func handlerGetBlockHashByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	if len(r.URL.Query()) == 0 {
+		clientErr(w, http.StatusBadRequest, "Key not found: 'blockId'")
+		return
+	}
+
+	if r.URL.Query().Has("blockId") {
+		// TODO: Fetch block hash from database using its ID.
+		fmt.Fprintf(w, "Get block hash by id: %s.", r.URL.Query().Get("blockId"))
+		return
+	}
+	clientErr(w, http.StatusBadRequest, "Key not found: 'blockId'")
+}
+
+func handlerGetBlockIDByHash(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	if len(r.URL.Query()) == 0 {
+		clientErr(w, http.StatusBadRequest, "Block hash must be given.")
+		return
+	}
+
+	if r.URL.Query().Has("blockHash") {
+		// TODO: Fetch block id from database using its hash.
+		fmt.Fprintf(w, "Get block id by hash: %s.", r.URL.Query().Get("blockHash"))
+		return
+	}
+	clientErr(w, http.StatusBadRequest, "Block hash must be given.")
+}
+
+func handlerGetCode(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	// Slight difference here is that the feeder will collect all
+	// parameters and specify which are incorrect in the error message
+	// whereas this implementation will send an error at the first
+	// occurrence of an invalid parameter without checking the rest.
+	allowed := map[string]bool{
+		"blockHash": true, "blockNumber": true, "contractAddress": true,
+	}
+	for param := range r.URL.Query() {
+		if !allowed[param] {
+			message := fmt.Sprintf(
+				`Query fields should be a subset of {'blockHash', 'blockNumber', `+
+					`'contractAddress'}; got: {%q}.`,
+				param)
+			clientErr(w, http.StatusBadRequest, message)
+			return
+		}
+	}
+
+	// Requires one of.
+	if r.URL.Query().Has("blockHash") && r.URL.Query().Has("blockNumber") {
+		message := fmt.Sprintf(
+			`Exactly one identifier - blockNumber or blockHash, have to be `+
+				`specified; got: blockNumber=%s, blockHash=%s.`,
+			r.URL.Query().Get("blockNumber"),
+			r.URL.Query().Get("blockHash"))
+		clientErr(w, http.StatusBadRequest, message)
+		return
+	}
+
+	switch {
+	case r.URL.Query().Has("blockHash"):
+		// TODO: Fetch code from database relative to the given block hash.
+		fmt.Fprintf(
+			w, "Get code relative to block hash: %s.", r.URL.Query().Get("blockHash"))
+	case r.URL.Query().Has("blockNumber"):
+		// TODO: Fetch code from database relative to the given block
+		// number.
+		fmt.Fprintf(
+			w, "Get code relative to block number: %s.", r.URL.Query().Get("blockHash"))
+	}
+}
+
+func handlerGetContractAddresses(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	// TODO: Return the address of the StarkNet and GPS Statement Verifier
+	// contracts on Ethereum.
+	w.Write([]byte("Get the contract address of StarkNet and the GpsStatementVerifier on Ethereum."))
+}
+
+func handlerGetStorageAt(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	switch query := r.URL.Query(); {
+	case len(query) == 0, !query.Has("key"):
+		clientErr(w, http.StatusBadRequest, "Key not found: 'key'")
+		return
+	case !query.Has("contractAddress"):
+		clientErr(w, http.StatusBadRequest, "Key not found: 'contractAddress'")
+		return
+	}
+
+	// Slight difference here is that the feeder will collect all
+	// parameters and specify which are incorrect in the error message
+	// whereas this implementation will send an error at the first
+	// occurrence of an invalid parameter without checking the rest.
+	allowed := map[string]bool{
+		"blockHash":       true,
+		"blockNumber":     true,
+		"contractAddress": true,
+		"key":             true,
+	}
+	for param := range r.URL.Query() {
+		if !allowed[param] {
+			message := fmt.Sprintf(
+				`Query fields should be a subset of {'blockHash', 'blockNumber', `+
+					`'contractAddress', 'key'}; got: {%q}.`,
+				param)
+			clientErr(w, http.StatusBadRequest, message)
+			return
+		}
+	}
+
+	// Requires one of.
+	if r.URL.Query().Has("blockHash") && r.URL.Query().Has("blockNumber") {
+		message := fmt.Sprintf(
+			`Exactly one identifier - blockNumber or blockHash, have to be `+
+				`specified; got: blockNumber=%s, blockHash=%s.`,
+			r.URL.Query().Get("blockNumber"),
+			r.URL.Query().Get("blockHash"))
+		clientErr(w, http.StatusBadRequest, message)
+		return
+	}
+
+	switch {
+	case r.URL.Query().Has("blockHash"):
+		// TODO: Fetch the value of the storage slot at the given key
+		// relative to the given block specified by block hash.
+		fmt.Fprintf(
+			w,
+			"Get value of storage slot %s at contract %s relative to block (hash) %s.",
+			r.URL.Query().Get("key"),
+			r.URL.Query().Get("contractAddress"),
+			r.URL.Query().Get("blockHash"),
+		)
+	case r.URL.Query().Has("blockNumber"):
+		// TODO: Fetch the value of the storage slot at the given key
+		// relative to the given block specified by block number.
+		fmt.Fprintf(
+			w,
+			"Get value of storage slot %s at contract %s relative to block (number) %s.",
+			r.URL.Query().Get("key"),
+			r.URL.Query().Get("contractAddress"),
+			r.URL.Query().Get("blockNumber"),
+		)
+	}
+}
+
+func handlerGetTransaction(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	if len(r.URL.Query()) == 0 {
+		message := `Transaction hash should be a hexadecimal string starting ` +
+			`with 0x; got None.`
+		clientErr(w, http.StatusBadRequest, message)
+		return
+	}
+
+	// Slight difference here is that the feeder will collect all
+	// parameters and specify which are incorrect in the error message
+	// whereas this implementation will send an error at the first
+	// occurrence of an invalid parameter without checking the rest.
+	for param := range r.URL.Query() {
+		if param != "transactionHash" {
+			message := fmt.Sprintf(
+				`Query fields should be a subset of {'transactionHash'}; got: {%q}.`,
+				param)
+			clientErr(w, http.StatusBadRequest, message)
+			return
+		}
+	}
+
+	if r.URL.Query().Has("transactionHash") {
+		// TODO: Fetch transaction from database using its hash.
+		fmt.Fprintf(
+			w, "Get transaction by hash : %s.", r.URL.Query().Get("transactionHash"))
+		return
+	}
+
+	// XXX: Never reached?
+	panic("Reached code marked as unreachable.")
+}
+
+func handlerGetTransactionHashByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	if len(r.URL.Query()) == 0 {
+		clientErr(w, http.StatusBadRequest, "Key not found: 'transactionId'")
+		return
+	}
+
+	if r.URL.Query().Has("transactionId") {
+		// TODO: Fetch transaction from database using its ID.
+		fmt.Fprintf(
+			w, "Get transaction by id: %s.", r.URL.Query().Get("transactionId"))
+		return
+	}
+	clientErr(w, http.StatusBadRequest, "Key not found: 'transactionId'")
+}
+
+func handlerGetTransactionIDByHash(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		clientErr(w, http.StatusMethodNotAllowed, "")
+		return
+	}
+
+	if len(r.URL.Query()) == 0 {
+		message := `Transaction hash should be a hexadecimal string starting ` +
+			`with 0x; got None.`
+		clientErr(w, http.StatusBadRequest, message)
+		return
+	}
+
+	// Slight difference here is that the feeder will collect all
+	// parameters and specify which are incorrect in the error message
+	// whereas this implementation will send an error at the first
+	// occurrence of an invalid parameter without checking the rest.
+	for param := range r.URL.Query() {
+		if param != "transactionHash" {
+			message := fmt.Sprintf(
+				`Query fields should be a subset of {'transactionHash'}; got: {%q}.`,
+				param)
+			clientErr(w, http.StatusBadRequest, message)
+			return
+		}
+	}
+
+	if r.URL.Query().Has("transactionHash") {
+		// TODO: Fetch transaction id from database using its hash.
+		fmt.Fprintf(
+			w, "Get transaction id by hash: %s.", r.URL.Query().Get("transactionHash"))
+		return
+	}
+
+	// XXX: Never reached?
+	panic("Reached code marked as unreachable.")
 }
 
 func handlerNotFound(w http.ResponseWriter, r *http.Request) {
--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -10,6 +10,14 @@ func main() {
 
 	mux.HandleFunc("/", handlerNotFound)
 	mux.HandleFunc("/v1/get_block", handlerGetBlock)
+	mux.HandleFunc("/v1/get_block_hash_by_id", handlerGetBlockHashByID)
+	mux.HandleFunc("/v1/get_block_id_by_hash", handlerGetBlockIDByHash)
+	mux.HandleFunc("/v1/get_code", handlerGetCode)
+	mux.HandleFunc("/v1/get_contract_addresses", handlerGetContractAddresses)
+	mux.HandleFunc("/v1/get_storage_at", handlerGetStorageAt)
+	mux.HandleFunc("/v1/get_transaction", handlerGetTransaction)
+	mux.HandleFunc("/v1/get_transaction_hash_by_id", handlerGetTransactionHashByID)
+	mux.HandleFunc("/v1/get_transaction_id_by_hash", handlerGetTransactionIDByHash)
 
 	log.Println("serving on http://localhost:4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
