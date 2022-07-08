912ba99908db146839d15bd100092fe376ceeade
Tshaka Eric Lekholoane <_@tshaka.co>
2022-07-08 16:20:56 +0200

    refactor: factor out routes to their own file
    Move the http.ServeMux creation and route registration to a separate
    file to avoid further crowding out of the main function.

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -16,22 +16,10 @@ func main() {
 	addr := flag.String("addr", ":4000", "local network address")
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlerNotFound)
-	mux.HandleFunc("/v1/get_block", handlerGetBlock)
-	mux.HandleFunc("/v1/get_block_hash_by_id", handlerGetBlockHashByID)
-	mux.HandleFunc("/v1/get_block_id_by_hash", handlerGetBlockIDByHash)
-	mux.HandleFunc("/v1/get_code", handlerGetCode)
-	mux.HandleFunc("/v1/get_contract_addresses", handlerGetContractAddresses)
-	mux.HandleFunc("/v1/get_storage_at", handlerGetStorageAt)
-	mux.HandleFunc("/v1/get_transaction", handlerGetTransaction)
-	mux.HandleFunc("/v1/get_transaction_hash_by_id", handlerGetTransactionHashByID)
-	mux.HandleFunc("/v1/get_transaction_id_by_hash", handlerGetTransactionIDByHash)
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: logErr,
-		Handler:  mux,
+		Handler:  routes(),
 	}
 
 	logInfo.Printf("serving on http://localhost%s", *addr)
---
+++ cmd/gateway/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlerNotFound)
+	mux.HandleFunc("/v1/get_block", handlerGetBlock)
+	mux.HandleFunc("/v1/get_block_hash_by_id", handlerGetBlockHashByID)
+	mux.HandleFunc("/v1/get_block_id_by_hash", handlerGetBlockIDByHash)
+	mux.HandleFunc("/v1/get_code", handlerGetCode)
+	mux.HandleFunc("/v1/get_contract_addresses", handlerGetContractAddresses)
+	mux.HandleFunc("/v1/get_storage_at", handlerGetStorageAt)
+	mux.HandleFunc("/v1/get_transaction", handlerGetTransaction)
+	mux.HandleFunc("/v1/get_transaction_hash_by_id", handlerGetTransactionHashByID)
+	mux.HandleFunc("/v1/get_transaction_id_by_hash", handlerGetTransactionIDByHash)
+
+	return mux
+}
