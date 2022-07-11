ea78386280d4e80ee2936c7d2b66630038157a4f
stdevAdrianPaez <adriantpaez@gmail.com>
2022-07-11 15:10:52 -0400

    Fix typo

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,6 @@
 package cache
 
-// Cache represents an on-memory cache, regardless of cache policy.
+// Cache represents an in-memory cache, regardless of cache policy.
 type Cache interface {
 	Put(key []byte, value []byte)
 	Get(key []byte) []byte
