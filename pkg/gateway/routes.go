3a6ab542298760cc814f653447c940436589d2e6
Tshaka Eric Lekholoane <_@tshaka.co>
2022-07-15 11:25:19 +0200

    refactor: rename handlers
    The handler prefix does not seem necessary if dependency injection is
    not going to be used and thus the distinction required.

--- pkg/gateway/routes.go
+++ pkg/gateway/routes.go
@@ -5,16 +5,16 @@ import "net/http"
 func routes() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handlerNotFound)
-	mux.HandleFunc("/v1/get_block", handlerGetBlock)
-	mux.HandleFunc("/v1/get_block_hash_by_id", handlerGetBlockHashByID)
-	mux.HandleFunc("/v1/get_block_id_by_hash", handlerGetBlockIDByHash)
-	mux.HandleFunc("/v1/get_code", handlerGetCode)
-	mux.HandleFunc("/v1/get_contract_addresses", handlerGetContractAddresses)
-	mux.HandleFunc("/v1/get_storage_at", handlerGetStorageAt)
-	mux.HandleFunc("/v1/get_transaction", handlerGetTransaction)
-	mux.HandleFunc("/v1/get_transaction_hash_by_id", handlerGetTransactionHashByID)
-	mux.HandleFunc("/v1/get_transaction_id_by_hash", handlerGetTransactionIDByHash)
+	mux.HandleFunc("/", notFound)
+	mux.HandleFunc("/v1/get_block", getBlock)
+	mux.HandleFunc("/v1/get_block_hash_by_id", getBlockHashByID)
+	mux.HandleFunc("/v1/get_block_id_by_hash", getBlockIDByHash)
+	mux.HandleFunc("/v1/get_code", getCode)
+	mux.HandleFunc("/v1/get_contract_addresses", getContractAddresses)
+	mux.HandleFunc("/v1/get_storage_at", getStorageAt)
+	mux.HandleFunc("/v1/get_transaction", getTransaction)
+	mux.HandleFunc("/v1/get_transaction_hash_by_id", getTransactionHashByID)
+	mux.HandleFunc("/v1/get_transaction_id_by_hash", getTransactionIDByHash)
 
 	// Wrap the ServeMux in middleware.
 	return recoverPanic(logRequest(mux))
