bcfb850a7ca419589b68708b21b166656a88926b
Josh Klopfenstein <joshklop10@gmail.com>
2022-07-15 19:48:29 -0500

    Configure trailing slash
    See https://docusaurus.io/docs/deployment#docusaurusconfigjs-settings

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
     organizationName: 'NethermindEth', // Usually your GitHub org/user name.
     projectName: 'juno', // Usually your repo name.
     deploymentBranch: 'gh-pages',
+    trailingSlash: false,
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
