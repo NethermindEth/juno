1c1eb503203f125a7086a3faf29ffc800397232e
adriantpaez <adriantpaez@gmail.com>
2022-07-29 13:41:50 -0400

    Update RPC response struct
    Add omitempty tag to the Id field

--- pkg/jsonrpc/response.go
+++ pkg/jsonrpc/response.go
@@ -4,5 +4,5 @@ type rpcResponse struct {
 	Jsonrpc string `json:"jsonrpc"`
 	Result  any    `json:"result,omitempty"`
 	Error   any    `json:"error,omitempty"`
-	Id      any    `json:"id"`
+	Id      any    `json:"id,omitempty"`
 }
