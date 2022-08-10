69ed8ac4ad76ac5beb7dec9cadaa7ad8df28ccc2
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-08-10 10:54:02 +0100

    Add colour coded log levels (#335)

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -4,8 +4,9 @@ import "testing"
 
 func TestReplaceGlobalLogger(t *testing.T) {
 	type args struct {
-		enableJsonOutput bool
-		verbosityLevel   string
+		enableJsonOutput   bool
+		verbosityLevel     string
+		enableColorEncoder bool
 	}
 	tests := []struct {
 		name string
@@ -15,23 +16,25 @@ func TestReplaceGlobalLogger(t *testing.T) {
 		{
 			name: "replace logger with good configuration (console encoding) should not return error",
 			args: args{
-				enableJsonOutput: false,
-				verbosityLevel:   "debug",
+				enableJsonOutput:   false,
+				verbosityLevel:     "debug",
+				enableColorEncoder: false,
 			},
 			err: nil,
 		},
 		{
 			name: "replace logger with good configuration (json encoding) should not return error",
 			args: args{
-				enableJsonOutput: true,
-				verbosityLevel:   "debug",
+				enableJsonOutput:   true,
+				verbosityLevel:     "debug",
+				enableColorEncoder: true,
 			},
 			err: nil,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			if err := ReplaceGlobalLogger(test.args.enableJsonOutput, test.args.verbosityLevel); err != nil {
+			if err := ReplaceGlobalLogger(test.args.enableJsonOutput, test.args.verbosityLevel, test.args.enableColorEncoder); err != nil {
 				t.Errorf("ReplaceGlobalLogger() error = %v", err)
 			}
 		})
