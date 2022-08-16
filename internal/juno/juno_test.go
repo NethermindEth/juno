0bd8a09ef721bd5dfae3b85201ebbdc682318569
IronGauntlets <aneeque@nethermind.io>
2022-08-16 18:09:42 +0100

    Simplifies config, cmd package and introduces Node struct
    Simplifies config and cmd package and introduces Node struct in preparation for
    p2p implementation and e2e tests.
    
    Update the configuration file and relevant flags.
    
    To run Juno with the config file run the following command:
    
    $ ./build/juno --config "path/to/config/file"
    
    The new configuration file looks like the following:
    
    verbosity: "debug"
    rpc-port: 4576
    metrics: true
    db-path: "/home/.juno"
    network: 1
    eth-node: "https://some-ethnode:5673"
    
    Each option has relevant flags such that `rpc-port` can be defined via the
    command line through `--rpc-port 5353`. The precedence of options is as follows:
     1. Flags
     2. Config file
     3. Flag defaults
    
    - `verbosity` sets the global logger's level.
    - `rpc-port` sets the rpc-port for the rpc server (default `6060`). It is always
       started.
    - `metrics` if sets start the metrics server on port `9090`.
    - `db-path` sets the path to the database. If it is unset then the default is
      `$HOME/.juno/$NETWORK` (same as geth).
    - `network` defines which network to synchronise with. `0 = goerli` and `1 =
       mainnet`. Synchronisation is always enabled.
    - `eth-node` sets the ethereum node to synchronise with. If unset then feeder
       gateway is used.
    
    Node struct
    
    type Node struct {
    	cfg *Config
    
    	feederClient   *feeder.Client
    	virtualMachine *cairovm.VirtualMachine
    	synchronizer   *syncer.Synchronizer
    
    	syncManager        *sync.Manager
    	stateManager       *state.Manager
    	transactionManager *transaction.Manager
    	blockManager       *block.Manager
    
    	rpcServer     *rpc.HttpRpc
    	metricsServer *prometheus.Server
    }
    
    This Node struct will allow us to programmatically start and stop Juno nodes and
    represent a p2p peer on the network.
    
    Co-authored-by: Josh Klopfenstein <joshklop10@gmail.com>
    Co-authored-by: Tshaka Eric Lekholoane <_@tshaka.co>

---
+++ internal/juno/juno_test.go
@@ -0,0 +1,56 @@
+package juno
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/NethermindEth/juno/internal/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("network", func(t *testing.T) {
+		networks := []utils.Network{
+			utils.GOERLI, utils.MAINNET, utils.Network(2),
+			utils.Network(20), utils.Network(100),
+		}
+		for _, n := range networks {
+			t.Run(fmt.Sprintf("%d", n), func(t *testing.T) {
+				cfg := &Config{Network: n}
+
+				_, err := New(cfg)
+
+				if cfg.Network == utils.GOERLI || cfg.Network == utils.MAINNET {
+					assert.NoError(t, err)
+				} else {
+					assert.Error(t, err, ErrUnknownNetwork)
+				}
+			})
+		}
+	})
+
+	t.Run("default db-path", func(t *testing.T) {
+		defaultDataDir, err := utils.DefaultDataDir()
+		require.NoError(t, err)
+
+		networks := []utils.Network{utils.GOERLI, utils.MAINNET}
+
+		for _, n := range networks {
+			t.Run(n.String(), func(t *testing.T) {
+				cfg := &Config{Network: n, DatabasePath: ""}
+				expectedCfg := &Config{
+					Network:      n,
+					DatabasePath: filepath.Join(defaultDataDir, n.String()),
+				}
+				node, err := New(cfg)
+				require.NoError(t, err)
+
+				junoN, ok := node.(*Node)
+				require.Equal(t, true, ok)
+				assert.Equal(t, expectedCfg, junoN.cfg)
+			})
+		}
+	})
+}
