e531a0fb8a6128edf7fcc0389e79782bff6f8182
adriantpaez <adriantpaez@gmail.com>
2022-08-25 10:47:50 -0400

    Update vm.go and vm.py to encode BinaryNode in PY

--- internal/cairovm/vmrpc/vm.go
+++ internal/cairovm/vmrpc/vm.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/NethermindEth/juno/pkg/felt"
 	"github.com/NethermindEth/juno/pkg/state"
+	"github.com/NethermindEth/juno/pkg/trie"
 
 	statedb "github.com/NethermindEth/juno/internal/db/state"
 )
@@ -25,11 +26,22 @@ func (s *storageRPCServer) GetPatriciaNode(ctx context.Context, request *GetValu
 	if err != nil {
 		return nil, err
 	}
-	return &VMTrieNode{
+	nodeP := &VMTrieNode{
 		Len:    uint32(node.Path().Len()),
 		Path:   node.Path().Bytes(),
 		Bottom: node.Bottom().ByteSlice(),
-	}, nil
+	}
+	switch n := node.(type) {
+	case *trie.EdgeNode:
+		nodeP.Type = NodeType_EDGE_NODE
+	case *trie.BinaryNode:
+		nodeP.Type = NodeType_BINARY_NODE
+		nodeP.Left = n.LeftH.ByteSlice()
+		nodeP.Right = n.RightH.ByteSlice()
+	default:
+		return nil, fmt.Errorf("unknown node type")
+	}
+	return nodeP, nil
 }
 
 func (s *storageRPCServer) GetContractState(ctx context.Context, request *GetValueRequest) (*VMContractState, error) {
--- internal/cairovm/vmrpc/vm.py
+++ internal/cairovm/vmrpc/vm.py
@@ -80,11 +80,14 @@ class StorageRPCClient(Storage):
             request = vm_pb2.GetValueRequest(key=suffix)
             if prefix == b'patricia_node':
                 response = await stub.GetPatriciaNode(request)
-                return (
-                        response.bottom
-                        + response.path
-                        + response.len.to_bytes(1, "big").strip(b"\x00")
-                )
+                if response.type == vm_pb2.NodeType.EDGE_NODE:
+                    return (
+                            response.bottom
+                            + response.path
+                            + response.len.to_bytes(1, "big").strip(b"\x00")
+                    )
+                elif response.type == vm_pb2.NodeType.BINARY_NODE:
+                    return response.left + response.right
             elif prefix == b"contract_state":
                 response = await stub.GetContractState(request)
                 return (
