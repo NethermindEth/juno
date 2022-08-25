16bab7060fa37dd456cd57f0010e684cbd04a512
adriantpaez <adriantpaez@gmail.com>
2022-08-25 14:20:37 -0400

    Fix CairoVM serialization

--- internal/cairovm/vmrpc/vm.py
+++ internal/cairovm/vmrpc/vm.py
@@ -82,12 +82,12 @@ class StorageRPCClient(Storage):
                 response = await stub.GetPatriciaNode(request)
                 if response.type == vm_pb2.NodeType.EDGE_NODE:
                     return (
-                            response.bottom
-                            + response.path
-                            + response.len.to_bytes(1, "big").strip(b"\x00")
+                            response.bottom.rjust(32, b'\00')
+                            + response.path.rjust(32, b'\00')
+                            + response.len.to_bytes(1, "big")
                     )
                 elif response.type == vm_pb2.NodeType.BINARY_NODE:
-                    return response.left + response.right
+                    return response.left.rjust(32, b'\00') + response.right.rjust(32, b'\00')
             elif prefix == b"contract_state":
                 response = await stub.GetContractState(request)
                 return (
