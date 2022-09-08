fd56397cf2cd6026d832c0097a2a01b841c1fbde
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2022-09-08 16:57:53 -0400

    [sync][test] add testing for conversion from txn comming from the feeder to the database

--- internal/sync/sync_test.go
+++ internal/sync/sync_test.go
@@ -81,3 +81,90 @@ func TestStatus(t *testing.T) {
 
 	assert.DeepEqual(t, got, want, gocmp.Comparer(func(x *felt.Felt, y *felt.Felt) bool { return x.CmpCompat(y) == 0 }))
 }
+
+func TestTransactionConverter(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		input *feeder.TransactionInfo
+		want  types.IsTransaction
+	}{
+		{
+			name: "transaction invoke V0",
+			input: &feeder.TransactionInfo{
+				Transaction: feeder.TxnSpecificInfo{
+					TransactionHash:    "0x6525d9aa309e5c80abbdafcc434d53202e06866597cd6dbbc91e5894fad7155",
+					ContractAddress:    "0x2fb7ff5b1b474e8e691f5bebad9aa7aa3009f6ef22ccc2816f96cdfe217604d",
+					Version:            "0x0",
+					EntryPointSelector: "0x12ead94ae9d3f9d2bdb6b847cf255f1f398193a1f88884a0ae8e18f24a037b6",
+					EntryPointType:     "EXTERNAL",
+					Calldata:           []string{"0xe3402af6cc1bca3f22d738ab935a5dd8ad1fb230"},
+					Signature:          []string{},
+					MaxFee:             "0x0",
+					Type:               "INVOKE_FUNCTION",
+				},
+			},
+			want: &types.TransactionInvokeV0{
+				Hash:               new(felt.Felt).SetHex("0x6525d9aa309e5c80abbdafcc434d53202e06866597cd6dbbc91e5894fad7155"),
+				ContractAddress:    new(felt.Felt).SetHex("0x2fb7ff5b1b474e8e691f5bebad9aa7aa3009f6ef22ccc2816f96cdfe217604d"),
+				EntryPointSelector: new(felt.Felt).SetHex("0x12ead94ae9d3f9d2bdb6b847cf255f1f398193a1f88884a0ae8e18f24a037b6"),
+				CallData:           []*felt.Felt{new(felt.Felt).SetHex("0xe3402af6cc1bca3f22d738ab935a5dd8ad1fb230")},
+				Signature:          []*felt.Felt{},
+				MaxFee:             new(felt.Felt).SetHex("0x0"),
+			},
+		},
+		{
+			name: "transaction invoke V1",
+			input: &feeder.TransactionInfo{
+				Transaction: feeder.TxnSpecificInfo{
+					TransactionHash:    "0x44dc8c3e13c29fb71cf5d7356ede2d175360ec17d282bb7d12959cf034f7fb9",
+					SenderAddress:      "0x5fb7f82414f88e8418bb5f973bbc8fcb660a91913da262f47ecf8e898b83b09",
+					Version:            "0x1",
+					EntryPointSelector: "0x12ead94ae9d3f9d2bdb6b847cf255f1f398193a1f88884a0ae8e18f24a037b6",
+					EntryPointType:     "EXTERNAL",
+					Calldata: []string{
+						"0x1",
+						"0x79bfdfb3397f4b369e98c3114d0ce71f7fc2283205f408c578fcdf8e827d458",
+						"0x12ead94ae9d3f9d2bdb6b847cf255f1f398193a1f88884a0ae8e18f24a037b6",
+						"0x0",
+						"0x1",
+						"0x1",
+						"0xa3bc7d5b2afe2999c817dfbcab22bad20ed7c895",
+					},
+					Signature: []string{
+						"0x72be86958215ae0976704139f29fba3bc450c71883d2d6d93c9256c9160302c",
+						"0x741560dbb0527c2c3ac5bd0cff1142d07b55fa2096d2aa158f9ba3ffd06ef85",
+					},
+					MaxFee: "0x2386f26fc10000",
+					Type:   "INVOKE_FUNCTION",
+					Nonce:  "0x1",
+				},
+			},
+			want: &types.TransactionInvokeV1{
+				Hash:          new(felt.Felt).SetHex("0x44dc8c3e13c29fb71cf5d7356ede2d175360ec17d282bb7d12959cf034f7fb9"),
+				SenderAddress: new(felt.Felt).SetHex("0x5fb7f82414f88e8418bb5f973bbc8fcb660a91913da262f47ecf8e898b83b09"),
+				CallData: []*felt.Felt{
+					new(felt.Felt).SetHex("0x1"),
+					new(felt.Felt).SetHex("0x79bfdfb3397f4b369e98c3114d0ce71f7fc2283205f408c578fcdf8e827d458"),
+					new(felt.Felt).SetHex("0x12ead94ae9d3f9d2bdb6b847cf255f1f398193a1f88884a0ae8e18f24a037b6"),
+					new(felt.Felt).SetHex("0x0"),
+					new(felt.Felt).SetHex("0x1"),
+					new(felt.Felt).SetHex("0x1"),
+					new(felt.Felt).SetHex("0xa3bc7d5b2afe2999c817dfbcab22bad20ed7c895"),
+				},
+				Signature: []*felt.Felt{
+					new(felt.Felt).SetHex("0x72be86958215ae0976704139f29fba3bc450c71883d2d6d93c9256c9160302c"),
+					new(felt.Felt).SetHex("0x741560dbb0527c2c3ac5bd0cff1142d07b55fa2096d2aa158f9ba3ffd06ef85"),
+				},
+				MaxFee: new(felt.Felt).SetHex("0x2386f26fc10000"),
+				Nonce:  new(felt.Felt).SetHex("0x1"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := feederTransactionToDBTransaction(tt.input)
+			assert.DeepEqual(t, got, tt.want)
+		})
+	}
+}
