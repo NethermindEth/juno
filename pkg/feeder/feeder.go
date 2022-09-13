8d79e3b11e997c4943624cd4f3185621c230bed1
Josh Klopfenstein <joshklop10@gmail.com>
2022-09-13 12:39:38 -0500

    Update pkg/feeder/feeder.go
    Co-authored-by: Tshaka Eric Lekholoane <_@tshaka.co>

--- pkg/feeder/feeder.go
+++ pkg/feeder/feeder.go
@@ -490,7 +490,7 @@ func (c Client) GetTransactionTrace(txHash, txID string) (*TransactionTrace, err
 	_, err = c.do(req, &res)
 	if err != nil {
 		metr.IncreaseTxTraceFailed()
-		c.logger.Errorw("Error connecting to the gateway.", "error", err, "url", c.BaseURL)
+		c.logger.Errorw("Error connecting to the gateway", "error", err, "url", c.BaseURL)
 		return nil, err
 	}
 	metr.IncreaseTxTraceReceived()
