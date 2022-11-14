0d869ce28abf4f1e362a5ff98214fd6631c3fc18
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2022-11-14 10:38:00 +0300

    Add TrieNode structure

--- core/trie_test.go
+++ core/trie_test.go
@@ -5,7 +5,63 @@ import (
 	"testing"
 
 	"github.com/NethermindEth/juno/core/felt"
+	"github.com/bits-and-blooms/bitset"
 )
+
+func TestNode_Marshall(t *testing.T) {
+	value, _ := new(felt.Felt).SetRandom()
+	path1 := bitset.FromWithLength(44, []uint64{44})
+	path2 := bitset.FromWithLength(22, []uint64{22})
+
+	tests := [...]TrieNode{
+		{
+			value: value,
+			left:  nil,
+			right: nil,
+		},
+		{
+			value: value,
+			left:  path1,
+			right: nil,
+		},
+		{
+			value: value,
+			left:  nil,
+			right: path2,
+		},
+		{
+			value: value,
+			left:  path1,
+			right: path2,
+		},
+		{
+			value: value,
+			left:  path2,
+			right: path1,
+		},
+	}
+	for _, test := range tests {
+		data, _ := test.MarshalBinary()
+		unmarshaled := new(TrieNode)
+		_ = unmarshaled.UnmarshalBinary(data)
+		if !test.Equal(unmarshaled) {
+			t.Error("TestNode_Marshall failed")
+		}
+	}
+
+	malformed := new([felt.Bytes + 1]byte)
+	malformed[felt.Bytes] = 'l'
+	if err := new(TrieNode).UnmarshalBinary(malformed[2:]); err == nil {
+		t.Error("TestNode_Marshall failed")
+	}
+	if err := new(TrieNode).UnmarshalBinary(malformed[:]); err == nil {
+		t.Error("TestNode_Marshall failed")
+	}
+	malformed[felt.Bytes] = 'z'
+	if err := new(TrieNode).UnmarshalBinary(malformed[:]); err == nil {
+		t.Error("TestNode_Marshall failed")
+	}
+}
 
 func TestPathFromKey(t *testing.T) {
 	key, _ := new(felt.Felt).SetRandom()
@@ -36,17 +92,28 @@ type (
 	}
 )
 
-func (s *testTrieStorage) Put(key *StoragePath, value StorageValue) error {
-	keyEnc, _ := key.MarshalBinary()
-	s.storage[hex.EncodeToString(keyEnc)] = hex.EncodeToString(value)
+func (s *testTrieStorage) Put(key *StoragePath, value *StorageValue) error {
+	keyEnc, err := key.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	vEnc, err := value.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	s.storage[hex.EncodeToString(keyEnc)] = hex.EncodeToString(vEnc)
 	return nil
 }
 
-func (s *testTrieStorage) Get(key *StoragePath) (StorageValue, error) {
+func (s *testTrieStorage) Get(key *StoragePath) (*StorageValue, error) {
 	keyEnc, _ := key.MarshalBinary()
 	value, found := s.storage[hex.EncodeToString(keyEnc)]
 	if !found {
 		panic("not found")
 	}
-	return hex.DecodeString(value)
+
+	v := new(StorageValue)
+	decoded, _ := hex.DecodeString(value)
+	err := v.UnmarshalBinary(decoded)
+	return v, err
 }
