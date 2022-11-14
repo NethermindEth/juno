6d7a01f272078c57b1d53fda0388e1035aa3279d
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2022-11-14 10:38:00 +0300

    Add TrieNode structure

--- core/trie.go
+++ core/trie.go
@@ -1,13 +1,16 @@
 package core
 
 import (
+	"bytes"
+	"errors"
+
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/bits-and-blooms/bitset"
 )
 
 type (
 	StoragePath  = bitset.BitSet
-	StorageValue = []byte
+	StorageValue = TrieNode
 )
 
 type (
@@ -16,8 +19,77 @@ type (
 )
 
 type TrieStorage interface {
-	Put(key *StoragePath, value StorageValue) error
-	Get(key *StoragePath) (StorageValue, error)
+	Put(key *StoragePath, value *StorageValue) error
+	Get(key *StoragePath) (*StorageValue, error)
+}
+
+type TrieNode struct {
+	value *TrieValue
+	left  *StoragePath
+	right *StoragePath
+}
+
+func (n *TrieNode) Equal(other *TrieNode) bool {
+	return n.value.Equal(other.value) && n.left.Equal(other.left) && n.right.Equal(n.right)
+}
+
+func (n *TrieNode) MarshalBinary() ([]byte, error) {
+	var ret []byte
+	valueB := n.value.Bytes()
+	ret = append(ret, valueB[:]...)
+
+	if n.left != nil {
+		ret = append(ret, 'l')
+		leftB, err := n.left.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, leftB...)
+	}
+
+	if n.right != nil {
+		ret = append(ret, 'r')
+		rightB, err := n.right.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, rightB...)
+	}
+	return ret, nil
+}
+
+func (n *TrieNode) UnmarshalBinary(data []byte) error {
+	if len(data) < felt.Bytes {
+		return errors.New("Malformed TrieNode bytedata")
+	}
+	n.value = new(TrieValue).SetBytes(data[:felt.Bytes])
+	data = data[felt.Bytes:]
+
+	stream := bytes.NewReader(data)
+	for stream.Len() > 0 {
+		head, err := stream.ReadByte()
+		if err != nil {
+			return err
+		}
+
+		var pathP **StoragePath
+		switch head {
+		case 'l':
+			pathP = &(n.left)
+		case 'r':
+			pathP = &(n.right)
+		default:
+			return errors.New("Malformed TrieNode bytedata")
+		}
+
+		*pathP = new(StoragePath)
+		_, err = (*pathP).ReadFrom(stream)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 type Trie struct {
