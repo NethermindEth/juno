2a9797c2eb48f5eccede72e1615e83501190d329
IronGauntlets <aneeque@nethermind.io>
2022-11-22 20:36:10 +0000

    Add StarkNet Keccak

---
+++ core/keccak.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"github.com/NethermindEth/juno/core/felt"
+	"golang.org/x/crypto/sha3"
+)
+
+var h = sha3.NewLegacyKeccak256()
+
+// StarkNetKeccak implements [StarkNet keccak]
+//
+// [StarkNet keccak]: https://docs.starknet.io/documentation/develop/Hashing/hash-functions/#starknet_keccak
+func StarkNetKeccak(b []byte) (*felt.Felt, error) {
+	h.Reset()
+	_, err := h.Write(b)
+	if err != nil {
+		return nil, err
+	}
+	d := h.Sum(nil)
+	// Remove the first 6 bits from the first byte
+	d[0] &= 3
+	return new(felt.Felt).SetBytes(d), nil
+}
---
+++ core/keccak_test.go
@@ -0,0 +1,28 @@
+package core
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStarkNetKeccak(t *testing.T) {
+	tests := [...]struct {
+		input, want string
+	}{
+		{"", "01d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"},
+		{"abc", "0203657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45"},
+		{"test", "0022ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658"},
+		{"starknet", "014909ac0d4a034239ea4f7265fac97d189ff7430fec65bce3879ab4b5a8d058"},
+		{"keccak", "0335a135a69c769066bbb4d17b2fa3ec922c028d4e4bf9d0402e6f7c12b31813"},
+	}
+	for _, test := range tests {
+		d, err := StarkNetKeccak([]byte(test.input))
+		if err != nil {
+			t.Fatalf("expected no error but got %s", err)
+		}
+		got := fmt.Sprintf("%x", d.Bytes())
+		if test.want != got {
+			t.Errorf("expected hash for \"%s\" = %q but got %q", test.input, test.want, got)
+		}
+	}
+}
