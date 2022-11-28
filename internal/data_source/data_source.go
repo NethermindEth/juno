5a65a557cd556528af30e34a8f7d1ee11faa3f92
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2022-11-28 21:25:05 +0000

    Add StateUpdate
    as a core struct that can be queried from a DataSource

--- internal/data_source/data_source.go
+++ internal/data_source/data_source.go
@@ -9,4 +9,5 @@ type DataSource interface {
 	GetBlockByNumber(blockNumber uint64) (*core.Block, error)
 	GetTransaction(transactionHash *felt.Felt) (*core.Transaction, error)
 	GetClass(classHash *felt.Felt) (*core.Class, error)
+	GetStateUpdate(blockNumber uint64) (*core.StateUpdate, error)
 }
