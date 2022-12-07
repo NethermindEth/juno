8703411be53b0da46ce65aa06247955fc9516161
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2022-12-08 03:03:04 +0530

    Added pedersen benchmark test (#454)
    * Added pedersen benchmark test
    
    Pedersen hash benchmark test using fixed inputs. Helps to compare Juno's pedersen hash implementation with Pathfinder's implementation
    
    * Update core/crypto/pedersen_hash_test.go
    
    Co-authored-by: IronGauntlets <aneeque@nethermind.io>
    
    * Updated the benchmark test
    
    Avoided running benchmark as sub benchmark
    
    Co-authored-by: IronGauntlets <aneeque@nethermind.io>

--- core/crypto/pedersen_hash_test.go
+++ core/crypto/pedersen_hash_test.go
@@ -163,3 +163,24 @@ func BenchmarkPedersenArray(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkPedersen(b *testing.B) {
+	e0, err := new(felt.Felt).SetString("0x3d937c035c878245caf64531a5756109c53068da139362728feb561405371cb")
+	if err != nil {
+		b.Errorf("Error occured %s", err)
+	}
+
+	e1, err := new(felt.Felt).SetString("0x208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a")
+	if err != nil {
+		b.Errorf("Error occured %s", err)
+	}
+
+	var f *felt.Felt
+	for n := 0; n < b.N; n++ {
+		f, err = Pedersen(e0, e1)
+		if err != nil {
+			b.Errorf("expected no error but got %s", err)
+		}
+	}
+	feltBench = f
+}
