faf0959599478071e9b96496dff66c561ba526f7
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-12-09 13:56:17 +0100

    update class datasource

--- internal/data_source/data_source.go
+++ internal/data_source/data_source.go
@@ -2,12 +2,13 @@ package dataSource
 
 import (
 	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/contract"
 	"github.com/NethermindEth/juno/core/felt"
 )
 
 type DataSource interface {
 	GetBlockByNumber(blockNumber uint64) (*core.Block, error)
 	GetTransaction(transactionHash *felt.Felt) (*core.Transaction, error)
-	GetClass(classHash *felt.Felt) (*core.Class, error)
+	GetClass(classHash *felt.Felt) (*contract.Class, error)
 	GetStateUpdate(blockNumber uint64) (*core.StateUpdate, error)
 }
