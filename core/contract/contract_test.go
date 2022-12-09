3f5ad0c51ecd3bb125137437f7f52fea2f1698d9
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-12-09 13:49:12 +0100

    add contract links

--- core/contract/contract_test.go
+++ core/contract/contract_test.go
@@ -29,14 +29,17 @@ func TestClassHash(t *testing.T) {
 		file string
 	}{
 		{
+			// https://alpha4.starknet.io/feeder_gateway/get_class_by_hash?classHash=0x0079e2d211e70594e687f9f788f71302e6eecb61d98efce48fbe8514948c8118
 			want: "0x0079e2d211e70594e687f9f788f71302e6eecb61d98efce48fbe8514948c8118",
 			file: "examples/contract_definition.json", // Cairo 0.10
 		},
 		{
+			// https://alpha4.starknet.io/feeder_gateway/get_class_by_hash?classHash=0x010455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8
 			want: "0x010455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8",
 			file: "examples/genesis_contract.json", // Genesis contract
 		},
 		{
+			// https://alpha4.starknet.io/feeder_gateway/get_class_by_hash?classHash=0x056b96c1d1bbfa01af44b465763d1b71150fa00c6c9d54c3947f57e979ff68c3
 			want: "0x056b96c1d1bbfa01af44b465763d1b71150fa00c6c9d54c3947f57e979ff68c3",
 			file: "examples/contract_0_8.json", // Cairo 0.8
 		},
