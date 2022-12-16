1d7cdbd6fa0bceafdb1e34addd4e8450aef65f77
Josh Klopfenstein <joshklop10@gmail.com>
2022-12-16 14:35:33 -0600

    Implement class hash

--- core/contract.go
+++ core/contract.go
@@ -1,13 +1,14 @@
 package core
 
 import (
+	"github.com/NethermindEth/juno/core/crypto"
 	"github.com/NethermindEth/juno/core/felt"
 )
 
 // Class unambiguously defines a [Contract]'s semantics.
 type Class struct {
 	// The version of the class, currently always 0.
-	APIVersion uint
+	APIVersion *felt.Felt
 	// External functions defined in the class.
 	Externals []EntryPoint
 	// Functions that receive L1 messages. See
@@ -16,17 +17,61 @@ type Class struct {
 	// Constructors for the class. Currently, only one is allowed.
 	Constructors []EntryPoint
 	// An ascii-encoded array of builtin names imported by the class.
-	Builtins []string
+	Builtins []*felt.Felt
 	// The starknet_keccak hash of the ".json" file compiler output.
 	ProgramHash *felt.Felt
 	Bytecode    []*felt.Felt
 }
 
-// Hash computes the [Pedersen Hash] of the class.
-//
-// [Pedersen Hash]: https://docs.starknet.io/documentation/develop/Contracts/contract-hash/#how_the_class_hash_is_computed
-func (c *Class) Hash() {
-	// TODO
+func (c *Class) Hash() (*felt.Felt, error) {
+	externalsHash, err := crypto.PedersenArray(flatten(c.Externals)...)
+	if err != nil {
+		return nil, err
+	}
+	l1HandlersHash, err := crypto.PedersenArray(flatten(c.L1Handlers)...)
+	if err != nil {
+		return nil, err
+	}
+	constructorsHash, err := crypto.PedersenArray(flatten(c.Constructors)...)
+	if err != nil {
+		return nil, err
+	}
+	builtinsHash, err := crypto.PedersenArray(c.Builtins...)
+	if err != nil {
+		return nil, err
+	}
+	bytecodeHash, err := crypto.PedersenArray(c.Bytecode...)
+	if err != nil {
+		return nil, err
+	}
+	return crypto.PedersenArray(
+		c.APIVersion,
+		externalsHash,
+		l1HandlersHash,
+		constructorsHash,
+		builtinsHash,
+		c.ProgramHash,
+		bytecodeHash,
+	)
+}
+
+func flatten(entryPoints []EntryPoint) []*felt.Felt {
+	result := make([]*felt.Felt, len(entryPoints)*2)
+	for i, entryPoint := range entryPoints {
+		// It is important that Selector is first because it
+		// influences the class hash.
+		result[2*i] = entryPoint.Selector
+		result[2*i+1] = entryPoint.Offset
+	}
+	return result
+}
+
+// EntryPoint uniquely identifies a Cairo function to execute.
+type EntryPoint struct {
+	// starknet_keccak hash of the function signature.
+	Selector *felt.Felt
+	// The offset of the instruction in the class's bytecode.
+	Offset *felt.Felt
 }
 
 // Contract is an instance of a [Class].
@@ -38,12 +83,4 @@ type Contract struct {
 	ClassHash *felt.Felt
 	// Root of the contract's storage trie.
 	StorageRoot *felt.Felt // TODO: is this field necessary?
-}
-
-// EntryPoint uniquely identifies a Cairo function to execute.
-type EntryPoint struct {
-	// starknet_keccak hash of the function signature.
-	Selector *felt.Felt
-	// The offset of the instruction in the class's bytecode.
-	Offset uint
 }
