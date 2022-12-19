84b2befb15e150fe7d20e21d3062f27c59233341
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-12-19 16:13:57 +0100

    update networks

--- utils/network.go
+++ utils/network.go
@@ -5,6 +5,8 @@ type Network uint8
 const (
 	GOERLI Network = iota
 	MAINNET
+	GOERLI2
+	INTEGRATION
 )
 
 func (n Network) String() string {
@@ -13,6 +15,10 @@ func (n Network) String() string {
 		return "goerli"
 	case MAINNET:
 		return "mainnet"
+	case GOERLI2:
+		return "goerli2"
+	case INTEGRATION:
+		return "integration"
 	default:
 		return ""
 	}
@@ -24,6 +30,34 @@ func (n Network) URL() string {
 		return "https://alpha4.starknet.io"
 	case MAINNET:
 		return "https://alpha-mainnet.starknet.io"
+	case GOERLI2:
+		return "https://alpha4.starknet.io"
+	case INTEGRATION:
+		return "https://external.integration.starknet.io"
+	default:
+		return ""
+	}
+}
+
+type Chain string
+
+const (
+	Goerli      Chain = "SN_GOERLI"
+	Mainnet     Chain = "SN_MAINNET"
+	Goerli2     Chain = "SN_GOERLI2"
+	Integration Chain = "SN_INTEGRATION"
+)
+
+func (n Network) ChainId() Chain {
+	switch n {
+	case GOERLI:
+		return Goerli
+	case MAINNET:
+		return Mainnet
+	case GOERLI2:
+		return Goerli2
+	case INTEGRATION:
+		return Integration
 	default:
 		return ""
 	}
