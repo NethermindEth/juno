e4e3ad85a16ad9536318f43073f1bc1979d9cde1
Josh Klopfenstein <joshklop10@gmail.com>
2022-12-22 05:26:14 -0600

    Unmarshal `fp.Element`s properly

--- clients/gateway.go
+++ clients/gateway.go
@@ -2,10 +2,13 @@ package clients
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"math/big"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strconv"
 
 	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
@@ -71,6 +74,10 @@ type StateUpdate struct {
 	} `json:"state_diff"`
 }
 
+func (su *StateUpdate) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, su)
+}
+
 func (c *GatewayClient) GetStateUpdate(blockNumber uint64) (*StateUpdate, error) {
 	queryUrl := c.buildQueryString("get_state_update", map[string]string{
 		"blockNumber": strconv.FormatUint(blockNumber, 10),
@@ -105,12 +112,20 @@ type Transaction struct {
 	SenderAddress *fp.Element `json:"sender_address"`
 }
 
+func (t *Transaction) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, t)
+}
+
 type TransactionStatus struct {
 	Status           string       `json:"status"`
 	BlockHash        *fp.Element  `json:"block_hash"`
 	BlockNumber      *big.Int     `json:"block_number"`
 	TransactionIndex *big.Int     `json:"transaction_index"`
 	Transaction      *Transaction `json:"transaction"`
+}
+
+func (ts *TransactionStatus) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, ts)
 }
 
 func (c *GatewayClient) GetTransaction(transactionHash *fp.Element) (*TransactionStatus, error) {
@@ -133,6 +148,10 @@ type Event struct {
 	Keys []*fp.Element `json:"keys"`
 }
 
+func (e *Event) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, e)
+}
+
 type L1ToL2Message struct {
 	From     string        `json:"from_address"`
 	Payload  []*fp.Element `json:"payload"`
@@ -141,10 +160,18 @@ type L1ToL2Message struct {
 	Nonce    *fp.Element   `json:"nonce"`
 }
 
+func (m *L1ToL2Message) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, m)
+}
+
 type L2ToL1Message struct {
 	From    *fp.Element   `json:"from_address"`
 	Payload []*fp.Element `json:"payload"`
 	To      string        `json:"to_address"`
+}
+
+func (m *L2ToL1Message) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, m)
 }
 
 type ExecutionResources struct {
@@ -160,6 +187,10 @@ type ExecutionResources struct {
 	MemoryHoles uint64 `json:"n_memory_holes"`
 }
 
+func (er *ExecutionResources) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, er)
+}
+
 type TransactionReceipt struct {
 	ActualFee          *fp.Element         `json:"actual_fee"`
 	Events             []*Event            `json:"events"`
@@ -168,6 +199,10 @@ type TransactionReceipt struct {
 	L2ToL1Message      *[]L2ToL1Message    `json:"l2_to_l1_messages"`
 	TransactionHash    *fp.Element         `json:"transaction_hash"`
 	TransactionIndex   *big.Int            `json:"transaction_index"`
+}
+
+func (r *TransactionReceipt) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, r)
 }
 
 // Block object returned by the gateway in JSON format for "get_block" endpoint
@@ -182,6 +217,10 @@ type Block struct {
 	Timestamp    uint64                `json:"timestamp"`
 	Version      string                `json:"starknet_version"`
 	Receipts     []*TransactionReceipt `json:"transaction_receipts"`
+}
+
+func (b *Block) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, b)
 }
 
 func (c *GatewayClient) GetBlock(blockNumber uint64) (*Block, error) {
@@ -201,6 +240,10 @@ func (c *GatewayClient) GetBlock(blockNumber uint64) (*Block, error) {
 type EntryPoint struct {
 	Selector *fp.Element `json:"selector"`
 	Offset   *fp.Element `json:"offset"`
+}
+
+func (ep *EntryPoint) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, ep)
 }
 
 type Abi []struct {
@@ -234,6 +277,10 @@ type ClassDefinition struct {
 	} `json:"program"`
 }
 
+func (c *ClassDefinition) UnmarshalJSON(data []byte) error {
+	return customUnmarshalJSON(data, c)
+}
+
 func (c *GatewayClient) GetClassDefinition(classHash *fp.Element) (*ClassDefinition, error) {
 	queryUrl := c.buildQueryString("get_class_by_hash", map[string]string{
 		"classHash": "0x" + classHash.Text(16),
@@ -246,4 +293,113 @@ func (c *GatewayClient) GetClassDefinition(classHash *fp.Element) (*ClassDefinit
 	}
 
 	return class, nil
+}
+
+// customUnmarshalJSON unmarshals out exactly like json.Unmarshal,
+// except that [fp.Element]s are assumed to be hex strings with or
+// without the 0x prefix. It also assumes maps have string keys.
+func customUnmarshalJSON(data []byte, out any) error {
+	v := reflect.ValueOf(out)
+	if v.Kind() != reflect.Pointer {
+		return errors.New("out must be pointer")
+	}
+	for v.Kind() == reflect.Pointer {
+		v = v.Elem()
+	}
+
+	switch v.Kind() {
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			f := v.Type().Field(i)
+			if tag, ok := f.Tag.Lookup("json"); ok {
+				var objmap map[string]*json.RawMessage
+				if err := json.Unmarshal(data, &objmap); err != nil {
+					return err
+				}
+				fmt.Println(tag)
+				if x, ok := objmap[tag]; ok && x != nil {
+					y, err := x.MarshalJSON()
+					if err != nil {
+						return err
+					}
+					if err := customUnmarshalJSON(y, v.Field(i).Addr().Interface()); err != nil {
+						return err
+					}
+				}
+			}
+		}
+	case reflect.Map:
+		var objmap map[string]*json.RawMessage
+		if err := json.Unmarshal(data, &objmap); err != nil {
+			return err
+		}
+
+		v.Set(reflect.MakeMapWithSize(v.Type(), len(objmap)))
+		for key, value := range objmap {
+			x, err := value.MarshalJSON()
+			if err != nil {
+				return err
+			}
+			y := reflect.New(v.Type().Elem())
+			if err := customUnmarshalJSON(x, y.Interface()); err != nil {
+				return err
+			}
+			v.SetMapIndex(reflect.ValueOf(key), y.Elem())
+		}
+	case reflect.Slice:
+		var objslice []*json.RawMessage
+		if err := json.Unmarshal(data, &objslice); err != nil {
+			return err
+		}
+		v.Set(reflect.MakeSlice(v.Type(), len(objslice), len(objslice)))
+		for i, obj := range objslice {
+			x, err := obj.MarshalJSON()
+			if err != nil {
+				return err
+			}
+			if err := customUnmarshalJSON(x, v.Index(i).Addr().Interface()); err != nil {
+				return err
+			}
+		}
+	default:
+		outV := reflect.ValueOf(out)
+		if outV.Type().String() == "**fp.Element" {
+			return unmarshalElement(data, outV.Elem())
+		} else {
+			if err := json.Unmarshal(data, out); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func unmarshalElement(data []byte, v reflect.Value) error {
+	s := string(data)
+	if len(s) > fp.Bits*3 {
+		return errors.New("value too large (max = Element.Bits * 3)")
+	}
+
+	// Remove leading and trailing quotes if any
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
+		// Remove 0x prefix
+		if s[:2] == "0x" {
+			s = s[2:]
+		}
+	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+
+	vv := new(big.Int)
+
+	if _, ok := vv.SetString(s, 16); !ok {
+		return errors.New("can't parse into a big.Int: " + s)
+	}
+
+	z := new(fp.Element).SetBigInt(vv)
+	v.Set(reflect.ValueOf(z))
+	return nil
 }
