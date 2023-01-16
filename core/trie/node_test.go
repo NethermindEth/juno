361383611d9e18bbd7ec1622d5b010c46c29cde3
sc <2194167956@qq.com>
2023-01-16 22:16:46 +0800

    Add test for unmarshal node

--- core/trie/node_test.go
+++ core/trie/node_test.go
@@ -2,6 +2,7 @@ package trie
 
 import (
 	"encoding/hex"
+	"errors"
 	"testing"
 
 	"github.com/NethermindEth/juno/core/felt"
@@ -84,6 +85,61 @@ func TestNodeMarshalAndUnmarshalBinary(t *testing.T) {
 				}
 				if !test.node.Equal(unmarshalled) {
 					t.Errorf("expected node: %v but got %v", test.node, unmarshalled)
+				}
+			})
+		}
+	})
+	t.Run("Test edge case of malformed node", func(t *testing.T) {
+		value, err := new(felt.Felt).SetRandom()
+		if err != nil {
+			t.Fatalf("expected no error but got %s", err)
+		}
+		path1 := bitset.FromWithLength(44, []uint64{11})
+		path2 := bitset.FromWithLength(22, []uint64{22})
+		path3 := bitset.FromWithLength(22, []uint64{33})
+		path4 := bitset.FromWithLength(22, []uint64{44})
+		flexibleMarshal := func(val *felt.Felt, left []bitset.BitSet, right []bitset.BitSet) []byte {
+			var ret []byte
+			valueB := val.Bytes()
+			ret = append(ret, valueB[:]...)
+			for _, ele := range left {
+				ret = append(ret, 'l')
+				leftB, _ := ele.MarshalBinary()
+				ret = append(ret, leftB...)
+			}
+
+			for _, ele := range right {
+				ret = append(ret, 'r')
+				rightB, _ := ele.MarshalBinary()
+				ret = append(ret, rightB...)
+			}
+			return ret
+		}
+
+		tests := [...]struct {
+			name       string
+			marshalBin []byte
+			errStr     string
+		}{
+			{
+				name:       "node with only 2 left children",
+				marshalBin: flexibleMarshal(value, []bitset.BitSet{*path1, *path2}, []bitset.BitSet{}),
+			},
+			{
+				name:       "node with only 2 right children",
+				marshalBin: flexibleMarshal(value, []bitset.BitSet{}, []bitset.BitSet{*path3, *path4}),
+			},
+			{
+				name:       "node with 2 left and 2 right children",
+				marshalBin: flexibleMarshal(value, []bitset.BitSet{*path1, *path2}, []bitset.BitSet{*path3, *path4}),
+			},
+		}
+		unmarshalled := new(Node)
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				err = unmarshalled.UnmarshalBinary(test.marshalBin)
+				if errors.Is(err, ErrMalformedNode{}) {
+					t.Errorf("expected error not right: got %s, wanted ErrMalformedNode", err)
 				}
 			})
 		}
