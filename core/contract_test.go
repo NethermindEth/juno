090a93a195b53ad65b4593ea8c74b2edbeb8d6ce
Jelilat Anofiu <anofiujelailah@gmail.com>
2023-01-19 15:06:50 +0100

    Implement contract address (#480)
    Co-authored-by: Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
    Co-authored-by: IronGauntlets <aneeque@nethermind.io>

--- core/contract_test.go
+++ core/contract_test.go
@@ -17,6 +17,12 @@ var (
 	bytecodeCairo10Bytes []byte
 )
 
+func hexToFelt(hex string) *felt.Felt {
+	// We know our test hex values are valid, so we'll ignore the potential error
+	f, _ := new(felt.Felt).SetString(hex)
+	return f
+}
+
 func TestClassHash(t *testing.T) {
 	var bytecodeGenesis []*felt.Felt
 	if err := json.Unmarshal(bytecodeGenesisBytes, &bytecodeGenesis); err != nil {
@@ -29,12 +35,6 @@ func TestClassHash(t *testing.T) {
 	var bytecodeCairo10 []*felt.Felt
 	if err := json.Unmarshal(bytecodeCairo10Bytes, &bytecodeCairo10); err != nil {
 		t.Fatalf("unexpected error while unmarshalling bytecodeBytes: %s", err)
-	}
-
-	// We know our test hex values are valid, so we'll ignore the potential error
-	hexToFelt := func(hex string) *felt.Felt {
-		f, _ := new(felt.Felt).SetString(hex)
-		return f
 	}
 
 	tests := []struct {
@@ -233,6 +233,40 @@ func TestClassHash(t *testing.T) {
 			classHash := tt.class.Hash()
 			if !classHash.Equal(tt.want) {
 				t.Errorf("wrong hash: got %s, want %s", classHash.Text(16), tt.want.Text(16))
+			}
+		})
+	}
+}
+
+func TestContractAddress(t *testing.T) {
+	tests := []struct {
+		callerAddress       *felt.Felt
+		classHash           *felt.Felt
+		salt                *felt.Felt
+		constructorCalldata []*felt.Felt
+		want                *felt.Felt
+	}{
+		{
+			// https://alpha-mainnet.starknet.io/feeder_gateway/get_transaction?transactionHash=0x6486c6303dba2f364c684a2e9609211c5b8e417e767f37b527cda51e776e6f0
+			callerAddress: hexToFelt("0x0000000000000000000000000000000000000000"),
+			classHash:     hexToFelt("0x46f844ea1a3b3668f81d38b5c1bd55e816e0373802aefe732138628f0133486"),
+			salt:          hexToFelt("0x74dc2fe193daf1abd8241b63329c1123214842b96ad7fd003d25512598a956b"),
+			constructorCalldata: []*felt.Felt{
+				hexToFelt("0x6d706cfbac9b8262d601c38251c5fbe0497c3a96cc91a92b08d91b61d9e70c4"),
+				hexToFelt("0x79dc0da7c54b95f10aa182ad0a46400db63156920adb65eca2654c0945a463"),
+				hexToFelt("0x2"),
+				hexToFelt("0x6658165b4984816ab189568637bedec5aa0a18305909c7f5726e4a16e3afef6"),
+				hexToFelt("0x6b648b36b074a91eee55730f5f5e075ec19c0a8f9ffb0903cefeee93b6ff328"),
+			},
+			want: hexToFelt("0x3ec215c6c9028ff671b46a2a9814970ea23ed3c4bcc3838c6d1dcbf395263c3"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("Address", func(t *testing.T) {
+			address := ContractAddress(tt.callerAddress, tt.classHash, tt.salt, tt.constructorCalldata)
+			if !address.Equal(tt.want) {
+				t.Errorf("wrong address: got %s, want %s", address.Text(16), tt.want.Text(16))
 			}
 		})
 	}
