8562fc4aad6726e7447a931dbad9d232d3c4e02d
IronGauntlets <aneeque@nethermind.io>
2023-01-20 16:04:44 +0000

    Refactor Blockchain and add NextHeight()
    Refactor:
     - Embed RWMutex in Blockchain Struct.
     - Rename Blockchain receiver from 's' to 'b'.

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -6,20 +6,30 @@ import "sync"
 type Blockchain struct {
 	sync.RWMutex
 
-	height uint64
+	height *uint64
 	// todo: much more
 }
 
 func NewBlockchain() *Blockchain {
 	return &Blockchain{
 		RWMutex: sync.RWMutex{},
-		height:  0,
 	}
 }
 
 // Height returns the latest block height
-func (s *Blockchain) Height() uint64 {
-	s.RLock()
-	defer s.RUnlock()
-	return s.height
+func (b *Blockchain) Height() uint64 {
+	b.RLock()
+	defer b.RUnlock()
+	return *b.height
+}
+
+// NextHeight returns the current height plus 1
+func (b *Blockchain) NextHeight() uint64 {
+	b.RLock()
+	defer b.RUnlock()
+
+	if b.height == nil {
+		return 0
+	}
+	return *b.height + 1
 }
