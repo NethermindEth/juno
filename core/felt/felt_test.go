c3ac8153e6d0a24025f18233e030c790bff0b8fd
0xkeivin <keivincatwork@gmail.com>
2023-01-26 18:50:54 +0800

    added 2 test cases

--- core/felt/felt_test.go
+++ core/felt/felt_test.go
@@ -1,8 +1,10 @@
 package felt
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,4 +15,57 @@ func TestUnmarshalJson(t *testing.T) {
 	var without Felt
 	assert.NoError(t, without.UnmarshalJSON([]byte("4437ab")))
 	assert.Equal(t, true, without.Equal(&with))
+}
+
+func TestIsZero(t *testing.T) {
+	tests := []struct {
+		name string
+		f    Felt
+		want bool
+	}{
+		{
+			name: "zero",
+			f:    Felt{},
+			want: true,
+		},
+		{
+			name: "non zero",
+			f:    Felt{val: fp.Element{1, 2, 3, 4}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.f.IsZero(); got != tt.want {
+				t.Errorf("Felt.IsZero() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsOne(t *testing.T) {
+	tests := []struct {
+		name string
+		f    Felt
+		want bool
+	}{
+		{
+			name: "one",
+			f:    Felt{val: fp.Element{18446744073709551585, 18446744073709551615, 18446744073709551615, 576460752303422960}},
+			want: true,
+		},
+		{
+			name: "non one",
+			f:    Felt{val: fp.Element{1, 2, 3, 4}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.f.IsOne(); got != tt.want {
+				fmt.Printf("got: %v\n", got)
+				t.Errorf("Felt.IsOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
