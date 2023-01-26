3cfd9ad119f6075f08ac824c8d5a0f79eef874f1
IronGauntlets <aneeque@nethermind.io>
2023-01-26 22:42:31 +0000

    Add basic block sync loop

--- sync/sync.go
+++ sync/sync.go
@@ -20,8 +20,7 @@ type Synchronizer struct {
 
 func NewSynchronizer(bc *blockchain.Blockchain, starkNetData starknetdata.StarkNetData) *Synchronizer {
 	return &Synchronizer{
-		running: 0,
-
+		running:      0,
 		Blockchain:   bc,
 		StarkNetData: starkNetData,
 		quit:         make(chan struct{}),
@@ -30,18 +29,44 @@ func NewSynchronizer(bc *blockchain.Blockchain, starkNetData starknetdata.StarkN
 
 // Run starts the Synchronizer, returns an error if the loop is already running
 func (s *Synchronizer) Run() error {
-	running := atomic.CompareAndSwapUint64(&s.running, 0, 1)
-	if !running {
+	if running := atomic.CompareAndSwapUint64(&s.running, 0, 1); !running {
 		return errors.New("synchronizer is already running")
 	}
-	defer atomic.CompareAndSwapUint64(&s.running, 1, 0)
-
-	<-s.quit
-	return nil
+	return s.SyncBlocks()
 }
 
 // Shutdown attempts to stop the Synchronizer, should block until loop acknowledges the request
 func (s *Synchronizer) Shutdown() error {
+	if stopped := atomic.CompareAndSwapUint64(&s.running, 1, 0); !stopped {
+		return errors.New("synchronizer is stopped")
+	}
 	close(s.quit)
 	return nil
+}
+
+func (s *Synchronizer) SyncBlocks() error {
+	for {
+		select {
+		case <-s.quit:
+			// Clean up and return
+			return nil
+		default:
+			// Call the gateway to get blocks and state update
+			nextHeight := s.Blockchain.NextHeight()
+			block, err := s.StarkNetData.BlockByNumber(nextHeight)
+			if err != nil {
+				return err
+			}
+			if err = s.Blockchain.Verify(block); err != nil {
+				return err
+			}
+			stateUpdate, err := s.StarkNetData.StateUpdate(nextHeight)
+			if err != nil {
+				return err
+			}
+			if err = s.Blockchain.Store(block, stateUpdate); err != nil {
+				return err
+			}
+		}
+	}
 }
