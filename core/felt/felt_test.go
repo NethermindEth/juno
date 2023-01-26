78fce55d0656d1875fd1db5e04b388589a2e95e8
0xkeivin <keivincatwork@gmail.com>
2023-01-27 02:19:34 +0800

    added TestHalve

--- core/felt/felt_test.go
+++ core/felt/felt_test.go
@@ -69,3 +69,31 @@ func TestIsOne(t *testing.T) {
 		})
 	}
 }
+
+func TestHalve(t *testing.T) {
+	tests := []struct {
+		name string
+		f    Felt
+		want Felt
+	}{
+		{
+			name: "zero halved",
+			f:    Felt{val: fp.Element{0, 0, 0, 0}},
+			want: Felt{val: fp.Element{0, 0, 0, 0}},
+		},
+		{
+			name: "one halved",
+			f:    Felt{val: fp.Element{18446744073709551585, 18446744073709551615, 18446744073709551615, 576460752303422960}},
+			want: Felt{val: fp.Element{18446744073709551601, 18446744073709551615, 18446744073709551615, 576460752303423232}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.f.Halve()
+			fmt.Printf("\ntt.f: %v\n\n", tt.want.val.String())
+			if !tt.f.Equal(&tt.want) {
+				t.Errorf("Felt.Halve() = %v, want %v", tt.f, tt.want)
+			}
+		})
+	}
+}
