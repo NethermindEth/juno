5793498156aadb675d011e008a252e2bfff0a8a7
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-02-02 13:20:24 +0300

    Use custom encoding mode to support demarshaling Transaction
    Without registering the specific transaction types to CBOR, it is
    not possible to demarshal them properly in to a Transaction interface.
    
    Registering types allows CBOR to create a tagged entry in the byte
    stream, so the tag can be used to extract type information while
    demarshaling that value.

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -8,14 +8,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	ts = cbor.NewTagSet()
+	// https://www.iana.org/assignments/cbor-tags/cbor-tags.xhtml
+	// 65536-15309735 	Unassigned
+	tagNum  = uint64(65536)
+	encMode cbor.EncMode
+	decMode cbor.DecMode
+)
+
+func initEncModes() {
+	var err error
+	encMode, err = cbor.CanonicalEncOptions().EncModeWithTags(ts)
+	if err != nil {
+		panic(err)
+	}
+
+	decMode, err = cbor.DecOptions{}.DecModeWithTags(ts)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func RegisterType(rType reflect.Type) error {
+	if err := ts.Add(
+		cbor.TagOptions{EncTag: cbor.EncTagRequired, DecTag: cbor.DecTagRequired},
+		rType,
+		tagNum,
+	); err != nil {
+		return err
+	}
+	initEncModes()
+	tagNum++
+	return nil
+}
+
+func init() {
+	initEncModes()
+}
+
 // Marshal returns encoding of param v
 func Marshal(v any) ([]byte, error) {
-	return cbor.Marshal(v)
+	return encMode.Marshal(v)
 }
 
 // Unmarshal decodes param v from []byte b
 func Unmarshal(b []byte, v any) error {
-	return cbor.Unmarshal(b, v)
+	return decMode.Unmarshal(b, v)
 }
 
 // TestSymmetry checks if a type can be marshaled and unmarshalled with no issues
