4f7d02fd1ec4c4cd8d22ab00a9e59972d0fcfeeb
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-02-09 15:29:24 +0300

    Parallelize sync process
    fetching and sanity checking of blocks and updates are happening on
    concurrent routines and applied to blockchain in order.

--- go.mod
+++ go.mod
@@ -8,9 +8,10 @@ require (
 	github.com/dgraph-io/badger/v3 v3.2103.4
 	github.com/ethereum/go-ethereum v1.10.26
 	github.com/fxamacker/cbor/v2 v2.4.0
+	github.com/sourcegraph/conc v0.2.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
-	github.com/stretchr/testify v1.8.0
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.24.0
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 )
@@ -19,12 +20,16 @@ require gopkg.in/yaml.v2 v2.4.0 // indirect
 
 require (
 	github.com/cespare/xxhash v1.1.0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cockroachdb/errors v1.9.0 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20211118104740-dabe8e521a4f // indirect
+	github.com/cockroachdb/redact v1.1.3 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/getsentry/sentry-go v0.13.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
@@ -33,7 +38,9 @@ require (
 	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/klauspost/compress v1.12.3 // indirect
+	github.com/klauspost/compress v1.15.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
@@ -41,6 +48,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/sourcegraph/sourcegraph/lib v0.0.0-20221216004406-749998a2ac74 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
--- sync/sync.go
+++ sync/sync.go
@@ -3,17 +3,27 @@ package sync
 import (
 	"context"
 	"errors"
-	"sync/atomic"
+	"fmt"
+	"runtime"
 
 	"github.com/NethermindEth/juno/blockchain"
+	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/starknetdata"
 	"github.com/NethermindEth/juno/utils"
+	"github.com/sourcegraph/conc/stream"
 )
+
+type ErrSyncFailed struct {
+	Height uint64
+	Err    error
+}
+
+func (e ErrSyncFailed) Error() string {
+	return fmt.Sprintf("Sync failed on block #%d with %s", e.Height, e.Err.Error())
+}
 
 // Synchronizer manages a list of StarknetData to fetch the latest blockchain updates
 type Synchronizer struct {
-	running uint64
-
 	Blockchain   *blockchain.Blockchain
 	StarknetData starknetdata.StarknetData
 
@@ -22,7 +32,6 @@ type Synchronizer struct {
 
 func NewSynchronizer(bc *blockchain.Blockchain, starkNetData starknetdata.StarknetData, log utils.SimpleLogger) *Synchronizer {
 	return &Synchronizer{
-		running:      0,
 		Blockchain:   bc,
 		StarknetData: starkNetData,
 		log:          log,
@@ -31,38 +40,90 @@ func NewSynchronizer(bc *blockchain.Blockchain, starkNetData starknetdata.Starkn
 
 // Run starts the Synchronizer, returns an error if the loop is already running
 func (s *Synchronizer) Run(ctx context.Context) error {
-	if running := atomic.CompareAndSwapUint64(&s.running, 0, 1); !running {
-		return errors.New("synchronizer is already running")
-	}
 	return s.SyncBlocks(ctx)
 }
 
-func (s *Synchronizer) SyncBlocks(ctx context.Context) error {
+func (s *Synchronizer) fetcherTask(ctx context.Context, height uint64, verifiers *stream.Stream, errChan chan ErrSyncFailed) stream.Callback {
 	for {
 		select {
 		case <-ctx.Done():
-			return ctx.Err()
+			return func() {}
 		default:
-			// Call the gateway to get blocks and state update
-			nextHeight := uint64(0)
-			if h, err := s.Blockchain.Height(); err == nil {
-				nextHeight = h + 1
-			}
-			block, err := s.StarknetData.BlockByNumber(ctx, nextHeight)
+			block, err := s.StarknetData.BlockByNumber(ctx, height)
 			if err != nil {
-				return err
+				continue
 			}
-			s.log.Infow("Fetched block", "number", block.Number, "hash", block.Hash.Text(16))
-			stateUpdate, err := s.StarknetData.StateUpdate(ctx, nextHeight)
+			stateUpdate, err := s.StarknetData.StateUpdate(ctx, height)
 			if err != nil {
-				return err
+				continue
 			}
-			s.log.Infow("Fetched state update", "newRoot", stateUpdate.NewRoot.Text(16), "hash", stateUpdate.BlockHash.Text(16))
-			if err = s.Blockchain.Store(block, stateUpdate); err != nil {
-				return err
+
+			return func() {
+				verifiers.Go(func() stream.Callback { return s.verifierTask(ctx, block, stateUpdate, errChan) })
 			}
-			s.log.Infow("Stored block", "number", block.Number, "hash", block.Hash.Text(16))
-			s.log.Infow("Applied state update", "newRoot", stateUpdate.NewRoot.Text(16), "hash", stateUpdate.BlockHash.Text(16))
+		}
+	}
+}
+
+func (s *Synchronizer) verifierTask(ctx context.Context, block *core.Block, stateUpdate *core.StateUpdate, errChan chan ErrSyncFailed) stream.Callback {
+	err := s.Blockchain.SanityCheckNewHeight(block, stateUpdate)
+	return func() {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err != nil {
+				s.log.Warnw("Sanity checks failed", "number", block.Number, "hash", block.Hash.Text(16))
+				errChan <- ErrSyncFailed{block.Number, err}
+				return
+			}
+			err = s.Blockchain.Store(block, stateUpdate)
+			if err != nil {
+				s.log.Warnw("Failed storing Block", "number", block.Number, "hash", block.Hash.Text(16),
+					"err", err.Error())
+				errChan <- ErrSyncFailed{block.Number, err}
+				return
+			}
+
+			s.log.Infow("Stored Block", "number", block.Number, "hash", block.Hash.Text(16),
+				"root", block.GlobalStateRoot.Text(16))
+		}
+	}
+}
+
+func (s *Synchronizer) SyncBlocks(syncCtx context.Context) error {
+	errChan := make(chan ErrSyncFailed)
+	fetchers := stream.New().WithMaxGoroutines(runtime.NumCPU())
+	verifiers := stream.New().WithMaxGoroutines(runtime.NumCPU())
+
+	streamCtx, streamCancel := context.WithCancel(syncCtx)
+	nextHeight := uint64(0)
+	if h, err := s.Blockchain.Height(); err == nil {
+		nextHeight = h + 1
+	}
+
+	for {
+		select {
+		case err := <-errChan:
+			streamCancel() // cancel all running tasks
+			streamCtx, streamCancel = context.WithCancel(syncCtx)
+			nextHeight = err.Height // keep syncing from failed height
+			s.log.Warnw("Rolling back sync process to failed height", "height", err.Height)
+		case <-syncCtx.Done():
+			fetchers.Wait()
+			verifiers.Wait()
+			if errors.Is(syncCtx.Err(), context.Canceled) {
+				return nil
+			} else {
+				return syncCtx.Err()
+			}
+		default:
+			curHeight := nextHeight
+			curStreamCtx := streamCtx
+			fetchers.Go(func() stream.Callback {
+				return s.fetcherTask(curStreamCtx, curHeight, verifiers, errChan)
+			})
+			nextHeight++
 		}
 	}
 }
