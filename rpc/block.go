c9836aa03a224d9926707106024ce04141f0c1b0
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-02-22 14:31:32 +0300

    Implement starknet_getBlockWithTxs

--- rpc/block.go
+++ rpc/block.go
@@ -7,11 +7,38 @@ import (
 	"github.com/NethermindEth/juno/core/felt"
 )
 
+// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L1999-L2008
+type BlockStatus uint8
+
+const (
+	BlockStatusPending BlockStatus = iota
+	BlockStatusAcceptedL2
+	BlockStatusAcceptedL1
+	BlockStatusRejected
+)
+
+func (s BlockStatus) MarshalJSON() ([]byte, error) {
+	switch s {
+	case BlockStatusPending:
+		return []byte("\"PENDING\""), nil
+	case BlockStatusAcceptedL2:
+		return []byte("\"ACCEPTED_ON_L2\""), nil
+	case BlockStatusAcceptedL1:
+		return []byte("\"ACCEPTED_ON_L1\""), nil
+	case BlockStatusRejected:
+		return []byte("\"REJECTED\""), nil
+	default:
+		return nil, errors.New("unknown block status")
+	}
+}
+
+// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L520-L534
 type BlockNumberAndHash struct {
 	Number uint64     `json:"block_number"`
 	Hash   *felt.Felt `json:"block_hash"`
 }
 
+// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L814
 type BlockId struct {
 	Pending bool
 	Latest  bool
@@ -46,30 +73,7 @@ func (b *BlockId) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-type BlockStatus uint8
-
-const (
-	BlockStatusPending BlockStatus = iota
-	BlockStatusAcceptedL2
-	BlockStatusAcceptedL1
-	BlockStatusRejected
-)
-
-func (s BlockStatus) MarshalJSON() ([]byte, error) {
-	switch s {
-	case BlockStatusPending:
-		return []byte("\"PENDING\""), nil
-	case BlockStatusAcceptedL2:
-		return []byte("\"ACCEPTED_ON_L2\""), nil
-	case BlockStatusAcceptedL1:
-		return []byte("\"ACCEPTED_ON_L1\""), nil
-	case BlockStatusRejected:
-		return []byte("\"REJECTED\""), nil
-	default:
-		return nil, errors.New("unknown block status")
-	}
-}
-
+// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L1072
 type BlockHeader struct {
 	Hash             *felt.Felt `json:"block_hash"`
 	ParentHash       *felt.Felt `json:"parent_hash"`
@@ -79,6 +83,14 @@ type BlockHeader struct {
 	SequencerAddress *felt.Felt `json:"sequencer_address,omitempty"`
 }
 
+// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L1131
+type BlockWithTxs struct {
+	Status BlockStatus `json:"status"`
+	BlockHeader
+	Transactions []*Transaction `json:"transactions"`
+}
+
+// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L1109
 type BlockWithTxHashes struct {
 	Status BlockStatus `json:"status"`
 	BlockHeader
