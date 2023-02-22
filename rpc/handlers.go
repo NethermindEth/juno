a10e814293f5dc8a8f07d4d59174a9d99fe8ffa5
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-02-22 11:00:55 +0300

    Implement starknet_getBlockWithTxHashes

--- rpc/handlers.go
+++ rpc/handlers.go
@@ -2,11 +2,15 @@ package rpc
 
 import (
 	"github.com/NethermindEth/juno/blockchain"
+	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/jsonrpc"
 )
 
-var ErrNoBlock = &jsonrpc.Error{Code: 32, Message: "There are no blocks"}
+var (
+	ErrBlockNotFound = &jsonrpc.Error{Code: 24, Message: "Block not found"}
+	ErrNoBlock       = &jsonrpc.Error{Code: 32, Message: "There are no blocks"}
+)
 
 type Handler struct {
 	bcReader blockchain.Reader
@@ -40,4 +44,40 @@ func (h *Handler) BlockNumberAndHash() (*BlockNumberAndHash, *jsonrpc.Error) {
 	} else {
 		return &BlockNumberAndHash{Number: block.Number, Hash: block.Hash}, nil
 	}
+}
+
+func (h *Handler) GetBlockWithTxHashes(id *BlockId) (*BlockWithTxHashes, *jsonrpc.Error) {
+	var block *core.Block
+	var err error
+	if id.Latest {
+		block, err = h.bcReader.Head()
+	} else if id.Pending {
+		// todo
+	} else if id.Hash != nil {
+		block, err = h.bcReader.GetBlockByHash(id.Hash)
+	} else {
+		block, err = h.bcReader.GetBlockByNumber(id.Number)
+	}
+
+	if err != nil || block == nil {
+		return nil, ErrBlockNotFound
+	}
+
+	txnHashes := make([]*felt.Felt, len(block.Transactions))
+	for index, txn := range block.Transactions {
+		txnHashes[index] = txn.Hash()
+	}
+
+	return &BlockWithTxHashes{
+		Status: BlockStatusAcceptedL2, // todo
+		BlockHeader: BlockHeader{
+			Hash:             block.Hash,
+			ParentHash:       block.ParentHash,
+			Number:           block.Number,
+			NewRoot:          block.GlobalStateRoot,
+			Timestamp:        block.Timestamp,
+			SequencerAddress: block.SequencerAddress,
+		},
+		TxnHashes: txnHashes,
+	}, nil
 }
