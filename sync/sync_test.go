a7ba821174bba61502c6a86bb047afdcedd6f08e
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-03-15 14:49:54 +0300

    Remove errChan in Synchronizer
    It lead to numerous deadlocks so far, passing a cancel function to
    workers is much simpler and robust
    
    also adds tests for synchronizer retry mechanisms

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -2,19 +2,27 @@ package sync
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/NethermindEth/juno/blockchain"
 	"github.com/NethermindEth/juno/clients/feeder"
+	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/db/pebble"
+	"github.com/NethermindEth/juno/mocks"
 	adaptfeeder "github.com/NethermindEth/juno/starknetdata/feeder"
 	"github.com/NethermindEth/juno/utils"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSyncBlocks(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
 	client, closeFn := feeder.NewTestClient(utils.MAINNET)
 	defer closeFn()
 	gw := adaptfeeder.New(client)
@@ -45,12 +53,14 @@ func TestSyncBlocks(t *testing.T) {
 		testDB := pebble.NewMemTest()
 		bc := blockchain.New(testDB, utils.MAINNET)
 		synchronizer := New(bc, gw, log)
-		ctx, _ := context.WithTimeout(context.Background(), time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 
 		require.NoError(t, synchronizer.Run(ctx))
+		cancel()
 
 		testBlockchain(t, bc)
 	})
+
 	t.Run("sync multiple blocks in a non-empty db", func(t *testing.T) {
 		testDB := pebble.NewMemTest()
 		bc := blockchain.New(testDB, utils.MAINNET)
@@ -61,9 +71,63 @@ func TestSyncBlocks(t *testing.T) {
 		require.NoError(t, bc.Store(b0, s0, nil))
 
 		synchronizer := New(bc, gw, log)
-		ctx, _ := context.WithTimeout(context.Background(), time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 
 		require.NoError(t, synchronizer.Run(ctx))
+		cancel()
+
+		testBlockchain(t, bc)
+	})
+
+	t.Run("sync multiple blocks, with an unreliable gw", func(t *testing.T) {
+		testDB := pebble.NewMemTest()
+		bc := blockchain.New(testDB, utils.MAINNET)
+
+		mockSNData := mocks.NewMockStarknetData(mockCtrl)
+
+		syncingHeight := uint64(0)
+
+		mockSNData.EXPECT().BlockByNumber(gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, height uint64) (*core.Block, error) {
+			// reject any other requests
+			if height != syncingHeight {
+				return nil, errors.New("try again")
+			}
+			return gw.BlockByNumber(context.Background(), syncingHeight)
+		}).AnyTimes()
+
+		reqCount := 0
+		mockSNData.EXPECT().StateUpdate(gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, height uint64) (*core.StateUpdate, error) {
+			// reject any other requests
+			if height != syncingHeight {
+				return nil, errors.New("try again")
+			}
+
+			reqCount++
+			ret, err := gw.StateUpdate(context.Background(), syncingHeight)
+			require.NoError(t, err)
+
+			if reqCount == 1 {
+				return nil, errors.New("try again")
+			} else if reqCount == 2 {
+				ret.BlockHash = new(felt.Felt) // fail sanity checks
+			} else if reqCount == 3 {
+				ret.OldRoot = new(felt.Felt).SetUint64(1) // fail store
+			} else {
+				reqCount = 0
+				syncingHeight++
+			}
+
+			return ret, nil
+		}).AnyTimes()
+		mockSNData.EXPECT().Class(gomock.Any(), gomock.Any()).DoAndReturn(func(ctx context.Context, hash *felt.Felt) (*core.Class, error) {
+			return gw.Class(ctx, hash)
+		}).AnyTimes()
+
+		synchronizer := New(bc, mockSNData, log)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		require.NoError(t, synchronizer.Run(ctx))
+		cancel()
 
 		testBlockchain(t, bc)
 	})
