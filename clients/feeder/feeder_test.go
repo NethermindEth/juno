76abae1ad7e3691838bfdf2261a285900261e03c
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-03-24 15:23:04 +0300

    Refactor feeder_test.go

--- clients/feeder/feeder_test.go
+++ clients/feeder/feeder_test.go
@@ -2,10 +2,8 @@ package feeder_test
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"testing"
 
 	"github.com/NethermindEth/juno/clients/feeder"
@@ -15,95 +13,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func testClient(clientURL string) *feeder.Client {
-	return feeder.NewClient(clientURL).WithBackoff(feeder.NopBackoff).WithMaxRetries(0)
-}
-
-func TestStateUpdateUnmarshal(t *testing.T) {
-	jsonData := []byte(`{
-  "block_hash": "0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943",
-  "new_root": "021870ba80540e7831fb21c591ee93481f5ae1bb71ff85a86ddd465be4eddee6",
-  "old_root": "0000000000000000000000000000000000000000000000000000000000000000",
-  "state_diff": {
-    "storage_diffs": {
-      "0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6": [
-        {
-          "key": "0x5",
-          "value": "0x22b"
-        }
-      ]
-    },
-    "nonces": { 
-		"0x37" : "0x44"
-	},
-    "deployed_contracts": [
-      {
-        "address": "0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6",
-        "class_hash": "0x10455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8"
-      }
-	],
-    "declared_contracts": [
-		"0x3744"
-	]
-  }
-}`)
-
-	var update feeder.StateUpdate
-	err := json.Unmarshal(jsonData, &update)
-	assert.Equal(t, nil, err, "Unexpected error")
-	expected, _ := new(felt.Felt).SetString("0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943")
-	assert.Equal(t, true, update.BlockHash.Equal(expected))
-	expected, _ = new(felt.Felt).SetString("0x021870ba80540e7831fb21c591ee93481f5ae1bb71ff85a86ddd465be4eddee6")
-	assert.Equal(t, true, update.NewRoot.Equal(expected))
-	expected, _ = new(felt.Felt).SetString("0x0000000000000000000000000000000000000000000000000000000000000000")
-	assert.Equal(t, true, update.OldRoot.Equal(expected))
-	assert.Equal(t, 1, len(update.StateDiff.StorageDiffs))
-
-	diffs, found := update.StateDiff.StorageDiffs["0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6"]
-	assert.Equal(t, true, found)
-	assert.Equal(t, 1, len(diffs))
-
-	expected, _ = new(felt.Felt).SetString("0x5")
-	assert.Equal(t, true, diffs[0].Key.Equal(expected))
-	expected, _ = new(felt.Felt).SetString("0x22b")
-	assert.Equal(t, true, diffs[0].Value.Equal(expected))
-
-	assert.Equal(t, 1, len(update.StateDiff.DeployedContracts))
-	expected, _ = new(felt.Felt).SetString("0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6")
-	assert.Equal(t, true, update.StateDiff.DeployedContracts[0].Address.Equal(expected))
-	expected, _ = new(felt.Felt).SetString("0x10455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8")
-	assert.Equal(t, true, update.StateDiff.DeployedContracts[0].ClassHash.Equal(expected))
-
-	assert.Equal(t, 1, len(update.StateDiff.DeclaredContracts))
-	expected, _ = new(felt.Felt).SetString("0x3744")
-	assert.Equal(t, true, update.StateDiff.DeclaredContracts[0].Equal(expected))
-
-	assert.Equal(t, 1, len(update.StateDiff.Nonces))
-	expected, _ = new(felt.Felt).SetString("0x44")
-	value, ok := update.StateDiff.Nonces["0x37"]
-	assert.Equal(t, true, ok)
-	assert.Equal(t, true, value.Equal(expected))
-}
-
 func TestDeclareTransactionUnmarshal(t *testing.T) {
-	declareJSON := []byte(`{
-      "transaction_hash":"0x93f542728e403f1edcea4a41f1509a39be35ebcad7d4b5aa77623e5e6480d",
-      "version":"0x1",
-      "max_fee":"0x5af3107a4000",
-      "signature":[
-         "0x516b5999b47509105675dd4c6ed9c373448038cfd00549fe868695916eee0ff",
-         "0x6c0189aaa56bfcb2a3e97198d04bd7a9750a4354b88f4e5edf57cf4d966ddda"
-      ],
-      "nonce":"0x1d",
-      "class_hash":"0x2ed6bb4d57ad27a22972b81feb9d09798ff8c273684376ec72c154d90343453",
-      "sender_address":"0xb8a60857ed233885155f1d839086ca7ad03e6d4237cc10b085a4652a61a23",
-      "type":"DECLARE"
-   }`)
-	var declareTx feeder.Transaction
-	err := json.Unmarshal(declareJSON, &declareTx)
-	if err != nil {
-		t.Error(err)
-	}
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
+
+	txnHash, _ := new(felt.Felt).SetString("0x93f542728e403f1edcea4a41f1509a39be35ebcad7d4b5aa77623e5e6480d")
+	status, err := client.Transaction(context.Background(), txnHash)
+	require.NoError(t, err)
+
+	declareTx := status.Transaction
 	assert.Equal(t, "0x93f542728e403f1edcea4a41f1509a39be35ebcad7d4b5aa77623e5e6480d", declareTx.Hash.String())
 	assert.Equal(t, "0x1", declareTx.Version.String())
 	assert.Equal(t, "0x5af3107a4000", declareTx.MaxFee.String())
@@ -117,30 +35,17 @@ func TestDeclareTransactionUnmarshal(t *testing.T) {
 }
 
 func TestInvokeTransactionUnmarshal(t *testing.T) {
-	invokeJSON := []byte(`{
-      "transaction_hash":"0x631333277e88053336d8c302630b4420dc3ff24018a1c464da37d5e36ea19df",
-      "version":"0x44",
-      "max_fee":"0x37",
-      "signature":[
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
 
-      ],
-      "contract_address":"0x17daeb497b6fe0f7adaa32b44677c3a9712b6856b792ad993fcef20aed21ac8",
-      "entry_point_selector":"0x218f305395474a84a39307fa5297be118fe17bf65e27ac5e2de6617baa44c64",
-      "calldata":[
-         "0x346f2b6376b4b57f714ba187716fce9edff1361628cc54783ed0351538faa5e",
-         "0x2"
-      ],
-      "type":"INVOKE_FUNCTION"
-   }`)
-	var invokeTx feeder.Transaction
-	err := json.Unmarshal(invokeJSON, &invokeTx)
-	if err != nil {
-		t.Error(err)
-	}
+	txnHash, _ := new(felt.Felt).SetString("0x631333277e88053336d8c302630b4420dc3ff24018a1c464da37d5e36ea19df")
+	status, err := client.Transaction(context.Background(), txnHash)
+	require.NoError(t, err)
 
+	invokeTx := status.Transaction
 	assert.Equal(t, "0x631333277e88053336d8c302630b4420dc3ff24018a1c464da37d5e36ea19df", invokeTx.Hash.String())
-	assert.Equal(t, "0x44", invokeTx.Version.String())
-	assert.Equal(t, "0x37", invokeTx.MaxFee.String())
+	assert.Equal(t, "0x0", invokeTx.Version.String())
+	assert.Equal(t, "0x0", invokeTx.MaxFee.String())
 	assert.Equal(t, 0, len(invokeTx.Signature))
 	assert.Equal(t, "0x17daeb497b6fe0f7adaa32b44677c3a9712b6856b792ad993fcef20aed21ac8", invokeTx.ContractAddress.String())
 	assert.Equal(t, "0x218f305395474a84a39307fa5297be118fe17bf65e27ac5e2de6617baa44c64", invokeTx.EntryPointSelector.String())
@@ -152,26 +57,14 @@ func TestInvokeTransactionUnmarshal(t *testing.T) {
 
 //nolint:dupl
 func TestDeployTransactionUnmarshal(t *testing.T) {
-	deployJSON := []byte(`{
-      "transaction_hash":"0x6d3e06989ee2245139cd677f59b4da7f360a27b2b614a4eb088fdf5862d23ee",
-      "version":"0x0",
-      "contract_address":"0x7cc55b21de4b7d6d7389df3b27de950924ac976d263ac8d71022d0b18155fc",
-      "contract_address_salt":"0x614b9e0c3cb7a8f4ed73b673eba239c41a172859bf129c4b269c4b8057e21d8",
-      "class_hash":"0x3131fa018d520a037686ce3efddeab8f28895662f019ca3ca18a626650f7d1e",
-      "constructor_calldata":[
-         "0x69577e6756a99b584b5d1ce8e60650ae33b6e2b13541783458268f07da6b38a",
-         "0x2dd76e7ad84dbed81c314ffe5e7a7cacfb8f4836f01af4e913f275f89a3de1a",
-         "0x1",
-         "0x614b9e0c3cb7a8f4ed73b673eba239c41a172859bf129c4b269c4b8057e21d8"
-      ],
-      "type":"DEPLOY"
-   }`)
-	var deployTx feeder.Transaction
-	err := json.Unmarshal(deployJSON, &deployTx)
-	if err != nil {
-		t.Error(err)
-	}
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
 
+	txnHash, _ := new(felt.Felt).SetString("0x6d3e06989ee2245139cd677f59b4da7f360a27b2b614a4eb088fdf5862d23ee")
+	status, err := client.Transaction(context.Background(), txnHash)
+	require.NoError(t, err)
+
+	deployTx := status.Transaction
 	assert.Equal(t, "0x6d3e06989ee2245139cd677f59b4da7f360a27b2b614a4eb088fdf5862d23ee", deployTx.Hash.String())
 	assert.Equal(t, "0x0", deployTx.Version.String())
 	assert.Equal(t, "0x7cc55b21de4b7d6d7389df3b27de950924ac976d263ac8d71022d0b18155fc", deployTx.ContractAddress.String())
@@ -186,33 +79,14 @@ func TestDeployTransactionUnmarshal(t *testing.T) {
 }
 
 func TestDeployAccountTransactionUnmarshal(t *testing.T) {
-	deployJSON := []byte(`{
-      "transaction_hash":"0x32b272b6d0d584305a460197aa849b5c7a9a85903b66e9d3e1afa2427ef093e",
-      "version":"0x1",
-      "max_fee":"0x59f5f9f474b0",
-      "signature":[
-         "0x467ae89bbbbaa0139e8f8a02ddc614bd80252998f3c033239f59f9f2ab973c5",
-         "0x92938929b5afcd596d651a6d28ed38baf90b000192897617d98de19d475331"
-      ],
-      "nonce":"0x0",
-      "contract_address":"0x104714313388bd0ab569ac247fed6cf0b7a2c737105c00d64c23e24bd8dea40",
-      "contract_address_salt":"0x25b9dbdab19b190a556aa42cdfbc07ad6ffe415031e42a8caffd4a2438d5cc3",
-      "class_hash":"0x25ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918",
-      "constructor_calldata":[
-         "0x33434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2",
-         "0x79dc0da7c54b95f10aa182ad0a46400db63156920adb65eca2654c0945a463",
-         "0x2",
-         "0x25b9dbdab19b190a556aa42cdfbc07ad6ffe415031e42a8caffd4a2438d5cc3",
-         "0x0"
-      ],
-      "type":"DEPLOY_ACCOUNT"
-   }`)
-	var deployTx feeder.Transaction
-	err := json.Unmarshal(deployJSON, &deployTx)
-	if err != nil {
-		t.Error(err)
-	}
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
 
+	txnHash, _ := new(felt.Felt).SetString("0x32b272b6d0d584305a460197aa849b5c7a9a85903b66e9d3e1afa2427ef093e")
+	status, err := client.Transaction(context.Background(), txnHash)
+	require.NoError(t, err)
+
+	deployTx := status.Transaction
 	assert.Equal(t, "0x32b272b6d0d584305a460197aa849b5c7a9a85903b66e9d3e1afa2427ef093e", deployTx.Hash.String())
 	assert.Equal(t, "0x1", deployTx.Version.String())
 	assert.Equal(t, "0x59f5f9f474b0", deployTx.MaxFee.String())
@@ -235,26 +109,14 @@ func TestDeployAccountTransactionUnmarshal(t *testing.T) {
 
 //nolint:dupl
 func TestL1HandlerTransactionUnmarshal(t *testing.T) {
-	handlerJSON := []byte(`{
-      "transaction_hash":"0x218adbb5aea7985d67fe49b45d44a991380b63db41622f9f4adc36274d02190",
-      "version":"0x0",
-      "contract_address":"0x73314940630fd6dcda0d772d4c972c4e0a9946bef9dabf4ef84eda8ef542b82",
-      "entry_point_selector":"0x2d757788a8d8d6f21d1cd40bce38a8222d70654214e96ff95d8086e684fbee5",
-      "nonce":"0x1654d",
-      "calldata":[
-         "0xae0ee0a63a2ce6baeeffe56e7714fb4efe48d419",
-         "0x218559e75713ca564d6eaf043b73388e9ac7c2f459ef8905988052051d3ef5e",
-         "0x2386f26fc10000",
-         "0x0"
-      ],
-      "type":"L1_HANDLER"
-   }`)
-	var handlerTx feeder.Transaction
-	err := json.Unmarshal(handlerJSON, &handlerTx)
-	if err != nil {
-		t.Error(err)
-	}
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
 
+	txnHash, _ := new(felt.Felt).SetString("0x218adbb5aea7985d67fe49b45d44a991380b63db41622f9f4adc36274d02190")
+	status, err := client.Transaction(context.Background(), txnHash)
+	require.NoError(t, err)
+
+	handlerTx := status.Transaction
 	assert.Equal(t, "0x218adbb5aea7985d67fe49b45d44a991380b63db41622f9f4adc36274d02190", handlerTx.Hash.String())
 	assert.Equal(t, "0x0", handlerTx.Version.String())
 	assert.Equal(t, "0x73314940630fd6dcda0d772d4c972c4e0a9946bef9dabf4ef84eda8ef542b82", handlerTx.ContractAddress.String())
@@ -273,9 +135,7 @@ func TestBlockWithoutSequencerAddressUnmarshal(t *testing.T) {
 	defer closeFn()
 
 	block, err := client.Block(context.Background(), 11817)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	assert.Equal(t, "0x24c692acaed3b486990bd9d2b2fbbee802b37b3bd79c59f295bad3277200a83", block.Hash.String())
 	assert.Equal(t, "0x3873ccb937f14429b169c654dda28886d2cc2d6ea17b3cff9748fe5cfdb67e0", block.ParentHash.String())
@@ -378,67 +238,28 @@ func TestBuildQueryString_WithErrorUrl(t *testing.T) {
 }
 
 func TestStateUpdate(t *testing.T) {
-	jsonData := []byte(`{
-		"block_hash": "0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943",
-		"new_root": "021870ba80540e7831fb21c591ee93481f5ae1bb71ff85a86ddd465be4eddee6",
-		"old_root": "0000000000000000000000000000000000000000000000000000000000000000",
-		"state_diff": {
-		  "storage_diffs": {
-			"0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6": [
-			  {
-				"key": "0x5",
-				"value": "0x22b"
-			  }
-			]
-		  },
-		  "nonces": {
-			"0x37" : "0x44"
-		  },
-		  "deployed_contracts": [
-			{
-			  "address": "0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6",
-			  "class_hash": "0x10455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8"
-			}
-		  ],
-		  "declared_contracts": [
-				"0x3744"
-		  ]
-		}
-	  }`)
-
-	var update feeder.StateUpdate
-	err := json.Unmarshal(jsonData, &update)
-	assert.Equal(t, nil, err, "Unexpected error")
-
-	//nolint:dupl
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/get_state_update" {
-			queryMap, err := url.ParseQuery(r.URL.RawQuery)
-			require.NoError(t, err, "No Query value")
-
-			queryBlockNumebr := queryMap["blockNumber"]
-
-			t.Log(queryBlockNumebr[0])
-
-			if queryBlockNumebr[0] == "10" {
-				w.WriteHeader(200)
-				marshedUpdate, err := json.Marshal(update)
-				require.NoError(t, err)
-
-				_, err = w.Write(marshedUpdate)
-				require.NoError(t, err)
-			} else {
-				w.WriteHeader(404)
-			}
-		}
-	}))
-	defer srv.Close()
-	client := testClient(srv.URL)
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
 
 	t.Run("Test normal case", func(t *testing.T) {
-		stateUpdate, err := client.StateUpdate(context.Background(), 10)
-		assert.Equal(t, nil, err, "Unexpected error")
-		assert.Equal(t, update, *stateUpdate)
+		stateUpdate, err := client.StateUpdate(context.Background(), 0)
+		require.NoError(t, err)
+
+		assert.Equal(t, "0x47c3637b57c2b079b93c61539950c17e868a28f46cdef28f88521067f21e943", stateUpdate.BlockHash.String())
+		assert.Equal(t, "0x21870ba80540e7831fb21c591ee93481f5ae1bb71ff85a86ddd465be4eddee6", stateUpdate.NewRoot.String())
+		assert.Equal(t, "0x0", stateUpdate.OldRoot.String())
+		assert.Equal(t, 0, len(stateUpdate.StateDiff.Nonces))
+		assert.Equal(t, 0, len(stateUpdate.StateDiff.DeclaredContracts))
+		assert.Equal(t, 5, len(stateUpdate.StateDiff.DeployedContracts))
+		assert.Equal(t, "0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6", stateUpdate.StateDiff.DeployedContracts[0].Address.String())
+		assert.Equal(t, "0x10455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8", stateUpdate.StateDiff.DeployedContracts[0].ClassHash.String())
+		assert.Equal(t, 5, len(stateUpdate.StateDiff.StorageDiffs))
+
+		diff, ok := stateUpdate.StateDiff.StorageDiffs["0x735596016a37ee972c42adef6a3cf628c19bb3794369c65d2c82ba034aecf2c"]
+		require.True(t, ok)
+		assert.Equal(t, 2, len(diff))
+		assert.Equal(t, "0x5", diff[0].Key.String())
+		assert.Equal(t, "0x64", diff[0].Value.String())
 	})
 	t.Run("Test block number out of boundary", func(t *testing.T) {
 		stateUpdate, err := client.StateUpdate(context.Background(), 1000000)
@@ -448,72 +269,20 @@ func TestStateUpdate(t *testing.T) {
 }
 
 func TestTransaction(t *testing.T) {
-	jsonTransactionStatus := []byte(`{
-		"block_hash": "0x0",
-		"block_number": 0,
-		"status": "ACCEPTED_ON_L2",
-		"transaction": {
-			"calldata": [
-				"0x1",
-				"0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6",
-				"0x362398bec32bc0ebb411203221a35a0301193a96f317ebe5e40be9f60d15320",
-				"0x0",
-				"0x1",
-				"0x1",
-				"0x4d2"
-			],
-			"contract_address": "0x20cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6",
-			"max_fee": "0x0",
-			"nonce": "0x3",
-			"signature": [
-				"0x26ee1f973def2e6d5c7f32aaad96c84dab32df6a62ee0e8b530a72bc5478fe6",
-				"0x502b15e440cc2a8a1966f0c973015096715e366fde36a4659c56f84249688e"
-			],
-			"transaction_hash": "0x69d743891f69d758928e163eff1e3d7256752f549f134974d4aa8d26d5d7da8",
-			"type": "INVOKE_FUNCTION",
-			"version": "0x1"
-		},
-		"transaction_index": 1
-	}
-	`)
-	var transactionStatus feeder.TransactionStatus
-	err := json.Unmarshal(jsonTransactionStatus, &transactionStatus)
-	assert.NoError(t, err)
+	client, closeFn := feeder.NewTestClient(utils.MAINNET)
+	defer closeFn()
 
-	//nolint:dupl
-	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/get_transaction" {
-			queryMap, err := url.ParseQuery(r.URL.RawQuery)
-			require.NoError(t, err, "No Query value")
-
-			transactionHash := queryMap["transactionHash"]
-			t.Log(transactionHash[0])
-			if transactionHash[0] == "0x0" {
-				w.WriteHeader(200)
-				marshaledStr, err := json.Marshal(transactionStatus)
-				require.NoError(t, err)
-
-				_, err = w.Write(marshaledStr)
-				require.NoError(t, err)
-			} else {
-				w.WriteHeader(404)
-			}
-		}
-	}))
-	defer srv.Close()
 	t.Run("Test normal case", func(t *testing.T) {
-		transactionHash, _ := new(felt.Felt).SetString("0x00")
-		client := testClient(srv.URL)
+		transactionHash, _ := new(felt.Felt).SetString("0x631333277e88053336d8c302630b4420dc3ff24018a1c464da37d5e36ea19df")
 		actualStatus, err := client.Transaction(context.Background(), transactionHash)
-		assert.Equal(t, nil, err, "Unexpected error")
-		assert.Equal(t, transactionStatus, *actualStatus)
+		assert.NoError(t, err)
+		assert.NotNil(t, actualStatus)
 	})
 	t.Run("Test case when transaction_hash does not exist", func(t *testing.T) {
 		transactionHash, _ := new(felt.Felt).SetString("0xffff")
-		client := testClient(srv.URL)
 		actualStatus, err := client.Transaction(context.Background(), transactionHash)
 		assert.Nil(t, actualStatus, "Unexpected error")
-		assert.NotNil(t, err)
+		assert.Error(t, err)
 	})
 }
 
