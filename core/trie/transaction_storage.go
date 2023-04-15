c5b614e821738d686278353a808a62b323a6b857
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-04-15 13:33:00 +0300

    Reuse buffers from a pool for encoding in TransactionStorage

--- core/trie/transaction_storage.go
+++ core/trie/transaction_storage.go
@@ -1,12 +1,28 @@
 package trie
 
 import (
+	"bytes"
+	"sync"
+
 	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/encoder"
 	"github.com/bits-and-blooms/bitset"
 )
 
 var _ Storage = (*TransactionStorage)(nil)
+
+// bufferPool caches unused buffer objects for later reuse.
+var bufferPool = sync.Pool{
+	New: func() any {
+		return new(bytes.Buffer)
+	},
+}
+
+func getBuffer() *bytes.Buffer {
+	buffer := bufferPool.Get().(*bytes.Buffer)
+	buffer.Reset()
+	return buffer
+}
 
 // TransactionStorage is a database transaction on a trie.
 type TransactionStorage struct {
@@ -23,37 +39,44 @@ func NewTransactionStorage(txn db.Transaction, prefix []byte) *TransactionStorag
 
 // dbKey creates a byte array to be used as a key to our KV store
 // it simply appends the given key to the configured prefix
-func (t *TransactionStorage) dbKey(key *bitset.BitSet) ([]byte, error) {
-	keyBytes, err := key.MarshalBinary()
+func (t *TransactionStorage) dbKey(key *bitset.BitSet, buffer *bytes.Buffer) (int64, error) {
+	_, err := buffer.Write(t.prefix)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
-	return append(t.prefix, keyBytes...), nil
+	keyLen, err := key.WriteTo(buffer)
+	return int64(len(t.prefix)) + keyLen, err
 }
 
 func (t *TransactionStorage) Put(key *bitset.BitSet, value *Node) error {
-	dbKey, err := t.dbKey(key)
+	buffer := getBuffer()
+	defer bufferPool.Put(buffer)
+	keyLen, err := t.dbKey(key, buffer)
 	if err != nil {
 		return err
 	}
 
-	valueBytes, err := encoder.Marshal(value)
+	enc := encoder.NewEncoder(buffer)
+	err = enc.Encode(value)
 	if err != nil {
 		return err
 	}
 
-	return t.txn.Set(dbKey, valueBytes)
+	encodedBytes := buffer.Bytes()
+	return t.txn.Set(encodedBytes[:keyLen], encodedBytes[keyLen:])
 }
 
 func (t *TransactionStorage) Get(key *bitset.BitSet) (*Node, error) {
-	dbKey, err := t.dbKey(key)
+	buffer := getBuffer()
+	defer bufferPool.Put(buffer)
+	_, err := t.dbKey(key, buffer)
 	if err != nil {
 		return nil, err
 	}
 
 	var node *Node
-	if err = t.txn.Get(dbKey, func(val []byte) error {
+	if err = t.txn.Get(buffer.Bytes(), func(val []byte) error {
 		node = new(Node)
 		return encoder.Unmarshal(val, node)
 	}); err != nil {
@@ -63,9 +86,11 @@ func (t *TransactionStorage) Get(key *bitset.BitSet) (*Node, error) {
 }
 
 func (t *TransactionStorage) Delete(key *bitset.BitSet) error {
-	dbKey, err := t.dbKey(key)
+	buffer := getBuffer()
+	defer bufferPool.Put(buffer)
+	_, err := t.dbKey(key, buffer)
 	if err != nil {
 		return err
 	}
-	return t.txn.Delete(dbKey)
+	return t.txn.Delete(buffer.Bytes())
 }
