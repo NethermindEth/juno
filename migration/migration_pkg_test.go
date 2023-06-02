f5b8630ede869f6d2c7c6fcebdf01172fc46ee2f
Josh Klopfenstein <git@joshklop.com>
2023-06-02 13:15:19 -0500

    Remove unnecessary db.ContractRootKey bucket
    We can use db.ContractStorage+<address> as the root key.
    
    With this change, we manage root keys the same way for both the state trie
    and the contract storage tries, enabling us to remove duplicated code
    in future commits.
    
    This is a breaking db layout change. An associated migration is provided.

--- migration/migration_pkg_test.go
+++ migration/migration_pkg_test.go
@@ -3,6 +3,7 @@ package migration
 import (
 	"testing"
 
+	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/db/pebble"
 	"github.com/stretchr/testify/require"
@@ -24,4 +25,42 @@ func TestRevision0000(t *testing.T) {
 		}))
 		require.EqualError(t, testDB.View(revision0000), "initial DB should be empty")
 	})
+}
+
+func TestRelocateContractStorageRootKeys(t *testing.T) {
+	testDB := pebble.NewMemTest()
+	t.Cleanup(func() {
+		require.NoError(t, testDB.Close())
+	})
+
+	txn := testDB.NewTransaction(true)
+
+	numberOfContracts := 5
+
+	// Populate the database with entries in the old location.
+	for i := 0; i < numberOfContracts; i++ {
+		exampleBytes := new(felt.Felt).SetUint64(uint64(i)).Bytes()
+		// Use exampleBytes for the key suffix (the contract address) and the value.
+		err := txn.Set(db.Unused.Key(exampleBytes[:]), exampleBytes[:])
+		require.NoError(t, err)
+	}
+
+	require.NoError(t, relocateContractStorageRootKeys(txn))
+
+	// Each root-key entry should have been moved to its new location
+	// and the old entry should not exist.
+	for i := 0; i < numberOfContracts; i++ {
+		exampleBytes := new(felt.Felt).SetUint64(uint64(i)).Bytes()
+
+		// New entry exists.
+		require.NoError(t, txn.Get(db.ContractStorage.Key(exampleBytes[:]), func(val []byte) error {
+			require.Equal(t, exampleBytes[:], val, "the correct value was not transferred to the new location")
+			return nil
+		}))
+
+		// Old entry does not exist.
+		oldKey := db.Unused.Key(exampleBytes[:])
+		err := txn.Get(oldKey, func(val []byte) error { return nil })
+		require.ErrorIs(t, db.ErrKeyNotFound, err)
+	}
 }
