NethermindEth/juno-1

Add l1 verifier
Key additions and modifications:

- Add the L1HeadKey key to the database. It holds the latest core.L1Head
- Add blockchain.SetL1Head
- The l1 package contains most of the new logic.
- We mock the l1.Subscriber interface in tests.
  In practice, the interface is implemented by the l1.EthSubscriber, which
  is uses go-ethereum's ethclient and the generated
  internal/contract/starknet.go.
- codecov should ignore the generated l1/contract/starknet.go file.

Workflow:

node.Run -> l1.Client.Run -> (update is received from subscription) -> blockchain.SetL1Head

Co-authored-by: Wojciech Zieba <wojciech.zieba@nethermind.io>

blockchain/blockchain.go
@@ -229,6 +229,28 @@ func (b *Blockchain) Receipt(hash *felt.Felt) (*core.TransactionReceipt, *felt.F
 	})
 }
 
+func (b *Blockchain) L1Head() (*core.L1Head, error) {
+	var update *core.L1Head
+	if err := b.database.View(func(txn db.Transaction) error {
+		return txn.Get(db.L1UpdateKey.Key(), func(updateBytes []byte) error {
+			return encoder.Unmarshal(updateBytes, &update)
+		})
+	}); err != nil {
+		return nil, err
+	}
+	return update, nil
+}
+
+func (b *Blockchain) SetL1Head(update *core.L1Head) error {
+	updateBytes, err := encoder.Marshal(update)
+	if err != nil {
+		return err
+	}
+	return b.database.Update(func(txn db.Transaction) error {
+		return txn.Set(db.L1UpdateKey.Key(), updateBytes)
+	})
+}
+
 // Store takes a block and state update and performs sanity checks before putting in the database.
 func (b *Blockchain) Store(block *core.Block, stateUpdate *core.StateUpdate, newClasses map[felt.Felt]core.Class) error {
 	return b.database.Update(func(txn db.Transaction) error {

node/node.go
@@ -12,17 +12,20 @@ import (
 	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/db/pebble"
 	"github.com/NethermindEth/juno/jsonrpc"
+	"github.com/NethermindEth/juno/l1"
 	"github.com/NethermindEth/juno/pprof"
 	"github.com/NethermindEth/juno/rpc"
 	"github.com/NethermindEth/juno/service"
 	adaptfeeder "github.com/NethermindEth/juno/starknetdata/feeder"
 	"github.com/NethermindEth/juno/sync"
 	"github.com/NethermindEth/juno/utils"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/sourcegraph/conc"
 )
 
 const (
-	defaultPprofPort = uint16(9080)
+	defaultPprofPort   = uint16(9080)
+	confirmationPeriod = uint64(16)
 )
 
 // Config is the top-level juno configuration.
@@ -31,6 +34,7 @@ type Config struct {
 	RPCPort      uint16         `mapstructure:"rpc-port"`
 	DatabasePath string         `mapstructure:"db-path"`
 	Network      utils.Network  `mapstructure:"network"`
+	EthNode      string         `mapstructure:"eth-node"`
 	Pprof        bool           `mapstructure:"pprof"`
 	Colour       bool           `mapstructure:"colour"`
 }
@@ -188,6 +192,26 @@ func (n *Node) Run(ctx context.Context) {
 	http := makeHTTP(n.cfg.RPCPort, rpc.New(n.blockchain, synchronizer, n.cfg.Network, gatewayClient, n.log), n.log)
 
 	n.services = []service.Service{synchronizer, http}
+
+	if n.cfg.EthNode == "" {
+		n.log.Warnw("Ethereum node address not found; will not verify against L1")
+	} else {
+		var coreContractAddress common.Address
+		coreContractAddress, err = n.cfg.Network.CoreContractAddress()
+		if err != nil {
+			n.log.Errorw("Error finding core contract address for network", "err", err, "network", n.cfg.Network)
+			return
+		}
+		var ethSubscriber *l1.EthSubscriber
+		ethSubscriber, err = l1.NewEthSubscriber(n.cfg.EthNode, coreContractAddress)
+		if err != nil {
+			n.log.Errorw("Error creating ethWatcher", "err", err)
+			return
+		}
+		defer ethSubscriber.Close()
+		l1Client := l1.NewClient(ethSubscriber, n.blockchain, confirmationPeriod, n.log)
+		n.services = append(n.services, l1Client)
+	}
 
 	if n.cfg.Pprof {
 		n.services = append(n.services, pprof.New(defaultPprofPort, n.log))

NethermindEth/juno

blockchain/blockchain.go
@@ -229,6 +229,28 @@ func (b *Blockchain) Receipt(hash *felt.Felt) (*core.TransactionReceipt, *felt.F
 	})
 }
 
+func (b *Blockchain) L1Head() (*core.L1Head, error) {
+	var update *core.L1Head
+	if err := b.database.View(func(txn db.Transaction) error {
+		return txn.Get(db.L1Height.Key(), func(updateBytes []byte) error {
+			return encoder.Unmarshal(updateBytes, &update)
+		})
+	}); err != nil {
+		return nil, err
+	}
+	return update, nil
+}
+
+func (b *Blockchain) SetL1Head(update *core.L1Head) error {
+	updateBytes, err := encoder.Marshal(update)
+	if err != nil {
+		return err
+	}
+	return b.database.Update(func(txn db.Transaction) error {
+		return txn.Set(db.L1Height.Key(), updateBytes)
+	})
+}
+
 // Store takes a block and state update and performs sanity checks before putting in the database.
 func (b *Blockchain) Store(block *core.Block, stateUpdate *core.StateUpdate, newClasses map[felt.Felt]core.Class) error {
 	return b.database.Update(func(txn db.Transaction) error {

node/node.go
@@ -12,17 +12,20 @@ import (
 	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/db/pebble"
 	"github.com/NethermindEth/juno/jsonrpc"
+	"github.com/NethermindEth/juno/l1"
 	"github.com/NethermindEth/juno/pprof"
 	"github.com/NethermindEth/juno/rpc"
 	"github.com/NethermindEth/juno/service"
 	adaptfeeder "github.com/NethermindEth/juno/starknetdata/feeder"
 	"github.com/NethermindEth/juno/sync"
 	"github.com/NethermindEth/juno/utils"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/sourcegraph/conc"
 )
 
 const (
-	defaultPprofPort = uint16(9080)
+	defaultPprofPort   = uint16(9080)
+	l1BlockConfirmationPeriod = uint64(16)
 )
 
 // Config is the top-level juno configuration.
@@ -31,6 +34,7 @@ type Config struct {
 	RPCPort      uint16         `mapstructure:"rpc-port"`
 	DatabasePath string         `mapstructure:"db-path"`
 	Network      utils.Network  `mapstructure:"network"`
+	EthNode      string         `mapstructure:"eth-node"`
 	Pprof        bool           `mapstructure:"pprof"`
 	Colour       bool           `mapstructure:"colour"`
 }
@@ -188,6 +192,26 @@ func (n *Node) Run(ctx context.Context) {
 	http := makeHTTP(n.cfg.RPCPort, rpc.New(n.blockchain, synchronizer, n.cfg.Network, gatewayClient, n.log), n.log)
 
 	n.services = []service.Service{synchronizer, http}
+
+	if n.cfg.EthNode == "" {
+		n.log.Warnw("Ethereum node address not found; will not verify against L1")
+	} else {
+		var coreContractAddress common.Address
+		coreContractAddress, err = n.cfg.Network.CoreContractAddress()
+		if err != nil {
+			n.log.Errorw("Error finding core contract address for network", "err", err, "network", n.cfg.Network)
+			return
+		}
+		var ethSubscriber *l1.EthSubscriber
+		ethSubscriber, err = l1.NewEthSubscriber(n.cfg.EthNode, coreContractAddress)
+		if err != nil {
+			n.log.Errorw("Error creating ethSubscriber", "err", err)
+			return
+		}
+		defer ethSubscriber.Close()
+		l1Client := l1.NewClient(ethSubscriber, n.blockchain, l1BlockConfirmationPeriod, n.log)
+		n.services = append(n.services, l1Client)
+	}
 
 	if n.cfg.Pprof {
 		n.services = append(n.services, pprof.New(defaultPprofPort, n.log))