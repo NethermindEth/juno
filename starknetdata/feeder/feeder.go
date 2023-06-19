636ad6c12e70eba0d57d2418f5d05cc7d997084a
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-06-19 18:38:09 +0300

    Move adapter functions to a separate package

--- starknetdata/feeder/feeder.go
+++ starknetdata/feeder/feeder.go
@@ -2,18 +2,14 @@ package feeder
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
 	"strconv"
 
+	"github.com/NethermindEth/juno/adapters/feeder2core"
 	"github.com/NethermindEth/juno/clients/feeder"
 	"github.com/NethermindEth/juno/core"
-	"github.com/NethermindEth/juno/core/crypto"
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/starknetdata"
-	"github.com/NethermindEth/juno/utils"
-	"github.com/ethereum/go-ethereum/common"
 )
 
 var _ starknetdata.StarknetData = (*Feeder)(nil)
@@ -55,130 +51,7 @@ func (f *Feeder) block(ctx context.Context, blockID string) (*core.Block, error)
 	if blockID == "pending" && response.Status != "PENDING" {
 		return nil, errors.New("no pending block")
 	}
-	return adaptBlock(response)
-}
-
-func adaptBlock(response *feeder.Block) (*core.Block, error) {
-	if response == nil {
-		return nil, errors.New("nil client block")
-	}
-
-	txns := make([]core.Transaction, len(response.Transactions))
-	receipts := make([]*core.TransactionReceipt, len(response.Receipts))
-	eventCount := uint64(0)
-	for i, txn := range response.Transactions {
-		var err error
-		txns[i], err = adaptTransaction(txn)
-		if err != nil {
-			return nil, err
-		}
-		receipts[i] = adaptTransactionReceipt(response.Receipts[i])
-		eventCount += uint64(len(response.Receipts[i].Events))
-	}
-
-	return &core.Block{
-		Header: &core.Header{
-			Hash:             response.Hash,
-			ParentHash:       response.ParentHash,
-			Number:           response.Number,
-			GlobalStateRoot:  response.StateRoot,
-			Timestamp:        response.Timestamp,
-			ProtocolVersion:  response.Version,
-			ExtraData:        nil,
-			SequencerAddress: response.SequencerAddress,
-			TransactionCount: uint64(len(response.Transactions)),
-			EventCount:       eventCount,
-			EventsBloom:      core.EventsBloom(receipts),
-		},
-		Transactions: txns,
-		Receipts:     receipts,
-	}, nil
-}
-
-func adaptTransactionReceipt(response *feeder.TransactionReceipt) *core.TransactionReceipt {
-	if response == nil {
-		return nil
-	}
-
-	events := make([]*core.Event, len(response.Events))
-	for i, event := range response.Events {
-		events[i] = adaptEvent(event)
-	}
-
-	l2ToL1Messages := make([]*core.L2ToL1Message, len(response.L2ToL1Message))
-	for i, msg := range response.L2ToL1Message {
-		l2ToL1Messages[i] = adaptL2ToL1Message(msg)
-	}
-
-	return &core.TransactionReceipt{
-		Fee:                response.ActualFee,
-		TransactionHash:    response.TransactionHash,
-		Events:             events,
-		ExecutionResources: adaptExecutionResources(response.ExecutionResources),
-		L1ToL2Message:      adaptL1ToL2Message(response.L1ToL2Message),
-		L2ToL1Message:      l2ToL1Messages,
-	}
-}
-
-func adaptEvent(response *feeder.Event) *core.Event {
-	if response == nil {
-		return nil
-	}
-
-	return &core.Event{
-		Data: response.Data,
-		From: response.From,
-		Keys: response.Keys,
-	}
-}
-
-func adaptExecutionResources(response *feeder.ExecutionResources) *core.ExecutionResources {
-	if response == nil {
-		return nil
-	}
-
-	return &core.ExecutionResources{
-		BuiltinInstanceCounter: adaptBuiltinInstanceCounter(response.BuiltinInstanceCounter),
-		MemoryHoles:            response.MemoryHoles,
-		Steps:                  response.Steps,
-	}
-}
-
-func adaptBuiltinInstanceCounter(response feeder.BuiltinInstanceCounter) core.BuiltinInstanceCounter {
-	return core.BuiltinInstanceCounter{
-		Bitwise:    response.Bitwise,
-		EcOp:       response.EcOp,
-		Ecsda:      response.Ecsda,
-		Output:     response.Output,
-		Pedersen:   response.Pedersen,
-		RangeCheck: response.RangeCheck,
-	}
-}
-
-func adaptL1ToL2Message(response *feeder.L1ToL2Message) *core.L1ToL2Message {
-	if response == nil {
-		return nil
-	}
-
-	return &core.L1ToL2Message{
-		From:     common.HexToAddress(response.From),
-		Nonce:    response.Nonce,
-		Payload:  response.Payload,
-		Selector: response.Selector,
-		To:       response.To,
-	}
-}
-
-func adaptL2ToL1Message(response *feeder.L2ToL1Message) *core.L2ToL1Message {
-	if response == nil {
-		return nil
-	}
-
-	return &core.L2ToL1Message{
-		From:    response.From,
-		Payload: response.Payload,
-		To:      common.HexToAddress(response.To),
-	}
+	return feeder2core.AdaptBlock(response)
 }
 
 // Transaction gets the transaction for a given transaction hash from the feeder,
@@ -189,88 +62,12 @@ func (f *Feeder) Transaction(ctx context.Context, transactionHash *felt.Felt) (c
 		return nil, err
 	}
 
-	tx, err := adaptTransaction(response.Transaction)
+	tx, err := feeder2core.AdaptTransaction(response.Transaction)
 	if err != nil {
 		return nil, err
 	}
 
 	return tx, nil
-}
-
-func adaptTransaction(transaction *feeder.Transaction) (core.Transaction, error) {
-	txType := transaction.Type
-	switch txType {
-	case "DECLARE":
-		return adaptDeclareTransaction(transaction), nil
-	case "DEPLOY":
-		return adaptDeployTransaction(transaction), nil
-	case "INVOKE_FUNCTION":
-		return adaptInvokeTransaction(transaction), nil
-	case "DEPLOY_ACCOUNT":
-		return adaptDeployAccountTransaction(transaction), nil
-	case "L1_HANDLER":
-		return adaptL1HandlerTransaction(transaction), nil
-	default:
-		return nil, fmt.Errorf("unknown transaction type %q", txType)
-	}
-}
-
-func adaptDeclareTransaction(t *feeder.Transaction) *core.DeclareTransaction {
-	return &core.DeclareTransaction{
-		TransactionHash:      t.Hash,
-		SenderAddress:        t.SenderAddress,
-		MaxFee:               t.MaxFee,
-		TransactionSignature: t.Signature,
-		Nonce:                t.Nonce,
-		Version:              t.Version,
-		ClassHash:            t.ClassHash,
-		CompiledClassHash:    t.CompiledClassHash,
-	}
-}
-
-func adaptDeployTransaction(t *feeder.Transaction) *core.DeployTransaction {
-	return &core.DeployTransaction{
-		TransactionHash:     t.Hash,
-		ContractAddressSalt: t.ContractAddressSalt,
-		ContractAddress:     t.ContractAddress,
-		ClassHash:           t.ClassHash,
-		ConstructorCallData: t.ConstructorCallData,
-		Version:             t.Version,
-	}
-}
-
-func adaptInvokeTransaction(t *feeder.Transaction) *core.InvokeTransaction {
-	return &core.InvokeTransaction{
-		TransactionHash:      t.Hash,
-		ContractAddress:      t.ContractAddress,
-		EntryPointSelector:   t.EntryPointSelector,
-		Nonce:                t.Nonce,
-		CallData:             t.CallData,
-		TransactionSignature: t.Signature,
-		MaxFee:               t.MaxFee,
-		Version:              t.Version,
-		SenderAddress:        t.SenderAddress,
-	}
-}
-
-func adaptL1HandlerTransaction(t *feeder.Transaction) *core.L1HandlerTransaction {
-	return &core.L1HandlerTransaction{
-		TransactionHash:    t.Hash,
-		ContractAddress:    t.ContractAddress,
-		EntryPointSelector: t.EntryPointSelector,
-		Nonce:              t.Nonce,
-		CallData:           t.CallData,
-		Version:            t.Version,
-	}
-}
-
-func adaptDeployAccountTransaction(t *feeder.Transaction) *core.DeployAccountTransaction {
-	return &core.DeployAccountTransaction{
-		DeployTransaction:    *adaptDeployTransaction(t),
-		MaxFee:               t.MaxFee,
-		TransactionSignature: t.Signature,
-		Nonce:                t.Nonce,
-	}
 }
 
 // Class gets the class for a given class hash from the feeder,
@@ -288,74 +85,12 @@ func (f *Feeder) Class(ctx context.Context, classHash *felt.Felt) (core.Class, e
 			return nil, cErr
 		}
 
-		return adaptCairo1Class(response.V1, compiledClass)
+		return feeder2core.AdaptCairo1Class(response.V1, compiledClass)
 	case response.V0 != nil:
-		return adaptCairo0Class(response.V0)
+		return feeder2core.AdaptCairo0Class(response.V0)
 	default:
 		return nil, errors.New("empty class")
 	}
-}
-
-func adaptCairo1Class(response *feeder.SierraDefinition, compiledClass json.RawMessage) (core.Class, error) {
-	var err error
-
-	class := new(core.Cairo1Class)
-	class.SemanticVersion = response.Version
-	class.Program = response.Program
-	class.ProgramHash = crypto.PoseidonArray(class.Program...)
-
-	class.Abi = response.Abi
-	class.AbiHash, err = crypto.StarknetKeccak([]byte(class.Abi))
-	if err != nil {
-		return nil, err
-	}
-
-	class.EntryPoints.External = make([]core.SierraEntryPoint, len(response.EntryPoints.External))
-	for index, v := range response.EntryPoints.External {
-		class.EntryPoints.External[index] = core.SierraEntryPoint{Index: v.Index, Selector: v.Selector}
-	}
-	class.EntryPoints.L1Handler = make([]core.SierraEntryPoint, len(response.EntryPoints.L1Handler))
-	for index, v := range response.EntryPoints.L1Handler {
-		class.EntryPoints.L1Handler[index] = core.SierraEntryPoint{Index: v.Index, Selector: v.Selector}
-	}
-	class.EntryPoints.Constructor = make([]core.SierraEntryPoint, len(response.EntryPoints.Constructor))
-	for index, v := range response.EntryPoints.Constructor {
-		class.EntryPoints.Constructor[index] = core.SierraEntryPoint{Index: v.Index, Selector: v.Selector}
-	}
-
-	if err = json.Unmarshal(compiledClass, &class.Compiled); err != nil {
-		return nil, err
-	}
-	return class, nil
-}
-
-func adaptCairo0Class(response *feeder.Cairo0Definition) (core.Class, error) {
-	class := new(core.Cairo0Class)
-	class.Abi = response.Abi
-
-	class.Externals = []core.EntryPoint{}
-	for _, v := range response.EntryPoints.External {
-		class.Externals = append(class.Externals, core.EntryPoint{Selector: v.Selector, Offset: v.Offset})
-	}
-
-	class.L1Handlers = []core.EntryPoint{}
-	for _, v := range response.EntryPoints.L1Handler {
-		class.L1Handlers = append(class.L1Handlers, core.EntryPoint{Selector: v.Selector, Offset: v.Offset})
-	}
-
-	class.Constructors = []core.EntryPoint{}
-	for _, v := range response.EntryPoints.Constructor {
-		class.Constructors = append(class.Constructors, core.EntryPoint{Selector: v.Selector, Offset: v.Offset})
-	}
-
-	base64Program, err := utils.Gzip64Encode(response.Program)
-	if err != nil {
-		return nil, err
-	}
-
-	class.Program = base64Program
-
-	return class, nil
 }
 
 func (f *Feeder) stateUpdate(ctx context.Context, blockID string) (*core.StateUpdate, error) {
@@ -364,7 +99,7 @@ func (f *Feeder) stateUpdate(ctx context.Context, blockID string) (*core.StateUp
 		return nil, err
 	}
 
-	return adaptStateUpdate(response)
+	return feeder2core.AdaptStateUpdate(response)
 }
 
 // StateUpdate gets the state update for a given block number from the feeder,
@@ -377,65 +112,4 @@ func (f *Feeder) StateUpdate(ctx context.Context, blockNumber uint64) (*core.Sta
 // then adapts it to the core.StateUpdate type.
 func (f *Feeder) StateUpdatePending(ctx context.Context) (*core.StateUpdate, error) {
 	return f.stateUpdate(ctx, "pending")
-}
-
-func adaptStateUpdate(response *feeder.StateUpdate) (*core.StateUpdate, error) {
-	stateDiff := new(core.StateDiff)
-	stateDiff.DeclaredV0Classes = response.StateDiff.OldDeclaredContracts
-
-	stateDiff.DeclaredV1Classes = make([]core.DeclaredV1Class, len(response.StateDiff.DeclaredClasses))
-	for index, declaredV1Class := range response.StateDiff.DeclaredClasses {
-		stateDiff.DeclaredV1Classes[index] = core.DeclaredV1Class{
-			ClassHash:         declaredV1Class.ClassHash,
-			CompiledClassHash: declaredV1Class.CompiledClassHash,
-		}
-	}
-
-	stateDiff.ReplacedClasses = make([]core.ReplacedClass, len(response.StateDiff.ReplacedClasses))
-	for index, replacedClass := range response.StateDiff.ReplacedClasses {
-		stateDiff.ReplacedClasses[index] = core.ReplacedClass{
-			Address:   replacedClass.Address,
-			ClassHash: replacedClass.ClassHash,
-		}
-	}
-
-	stateDiff.DeployedContracts = make([]core.DeployedContract, len(response.StateDiff.DeployedContracts))
-	for index, deployedContract := range response.StateDiff.DeployedContracts {
-		stateDiff.DeployedContracts[index] = core.DeployedContract{
-			Address:   deployedContract.Address,
-			ClassHash: deployedContract.ClassHash,
-		}
-	}
-
-	stateDiff.Nonces = make(map[felt.Felt]*felt.Felt)
-	for addrStr, nonce := range response.StateDiff.Nonces {
-		addr, err := new(felt.Felt).SetString(addrStr)
-		if err != nil {
-			return nil, err
-		}
-		stateDiff.Nonces[*addr] = nonce
-	}
-
-	stateDiff.StorageDiffs = make(map[felt.Felt][]core.StorageDiff)
-	for addrStr, diffs := range response.StateDiff.StorageDiffs {
-		addr, err := new(felt.Felt).SetString(addrStr)
-		if err != nil {
-			return nil, err
-		}
-
-		stateDiff.StorageDiffs[*addr] = []core.StorageDiff{}
-		for _, diff := range diffs {
-			stateDiff.StorageDiffs[*addr] = append(stateDiff.StorageDiffs[*addr], core.StorageDiff{
-				Key:   diff.Key,
-				Value: diff.Value,
-			})
-		}
-	}
-
-	return &core.StateUpdate{
-		BlockHash: response.BlockHash,
-		NewRoot:   response.NewRoot,
-		OldRoot:   response.OldRoot,
-		StateDiff: stateDiff,
-	}, nil
 }
