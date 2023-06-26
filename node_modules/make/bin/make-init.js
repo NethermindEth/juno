0936db09b7cbf453aec8cae3a084bb3d43c24b88
Rhythm Garg <rhythmgarg05@gmail.com>
2023-06-26 18:55:50 +0530

    trying to remove the unnecessary files.

--- node_modules/make/bin/make-init.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { Template } = require('..');
-
-var run = new Template({
-  namespace: 'make:init'
-});
-
-run.init();
--- node_modules/make/index.js
+++
@@ -1,8 +0,0 @@
-var make = module.exports;
-
-make.log = require('./src/log');
-make.parser = require('./src/parser');
-
-make.CLI = require('./src/cli');
-make.Make = require('./src/make');
-make.Template = require('./src/template');
