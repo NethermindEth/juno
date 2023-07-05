648e8ed0429bc9303ae198f6d758eca98dfed0d9
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-07-05 12:37:50 +0300

    Implement Gzip64Decode

--- utils/compression.go
+++ utils/compression.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/base64"
+	"io"
 )
 
 func Gzip64Encode(data []byte) (string, error) {
@@ -16,4 +17,24 @@ func Gzip64Encode(data []byte) (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(compressedBuffer.Bytes()), nil
+}
+
+func Gzip64Decode(data string) ([]byte, error) {
+	decodedBytes, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return nil, err
+	}
+	gzipReader, err := gzip.NewReader(bytes.NewReader(decodedBytes))
+	if err != nil {
+		return nil, err
+	}
+	decompressedBytes, err := io.ReadAll(gzipReader)
+	if err != nil {
+		return nil, err
+	}
+	err = gzipReader.Close()
+	if err != nil {
+		return nil, err
+	}
+	return decompressedBytes, nil
 }
