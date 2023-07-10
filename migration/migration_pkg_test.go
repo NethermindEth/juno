cb228915a94eaa34da85ca7d4c09d1c3ad9b3314
Josh Klopfenstein <git@joshklop.com>
2023-07-09 21:24:33 -0700

    Only use "migration" terminology, not "revision"
    for consistency

--- migration/migration_pkg_test.go
+++ migration/migration_pkg_test.go
@@ -19,21 +19,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRevision0000(t *testing.T) {
+func TestMigration0000(t *testing.T) {
 	testDB := pebble.NewMemTest()
 	t.Cleanup(func() {
 		require.NoError(t, testDB.Close())
 	})
 
 	t.Run("empty DB", func(t *testing.T) {
-		require.NoError(t, testDB.View(revision0000))
+		require.NoError(t, testDB.View(migration0000))
 	})
 
 	t.Run("non-empty DB", func(t *testing.T) {
 		require.NoError(t, testDB.Update(func(txn db.Transaction) error {
 			return txn.Set([]byte("asd"), []byte("123"))
 		}))
-		require.EqualError(t, testDB.View(revision0000), "initial DB should be empty")
+		require.EqualError(t, testDB.View(migration0000), "initial DB should be empty")
 	})
 }
 
