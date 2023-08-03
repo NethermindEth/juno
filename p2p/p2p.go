95c73a42d9312b882b12a095b25fd7275111533a
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-08-03 14:32:55 +0300

    Add ability to subscribe/publish on topics

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -13,6 +13,7 @@ import (
 	"github.com/NethermindEth/juno/utils"
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/event"
 	"github.com/libp2p/go-libp2p/core/host"
@@ -32,10 +33,13 @@ type Service struct {
 	host      host.Host
 	bootPeers string
 	network   utils.Network
+	log       utils.SimpleLogger
 
-	dht *dht.IpfsDHT
+	dht    *dht.IpfsDHT
+	pubsub *pubsub.PubSub
+	topics map[string]*pubsub.Topic
 
-	log utils.SimpleLogger
+	runCtx context.Context
 }
 
 func New(
@@ -80,6 +84,7 @@ func New(
 		host:      p2phost,
 		network:   snNetwork,
 		dht:       p2pdht,
+		topics:    make(map[string]*pubsub.Topic),
 	}, nil
 }
 
@@ -157,7 +162,15 @@ func (s *Service) SubscribePeerConnectednessChanged(ctx context.Context) (<-chan
 }
 
 func (s *Service) Run(ctx context.Context) error {
-	err := s.dht.Bootstrap(ctx)
+	var err error
+
+	s.runCtx = ctx
+	s.pubsub, err = pubsub.NewGossipSub(s.runCtx, s.host)
+	if err != nil {
+		return err
+	}
+
+	err = s.dht.Bootstrap(s.runCtx)
 	if err != nil {
 		return err
 	}
@@ -170,7 +183,7 @@ func (s *Service) Run(ctx context.Context) error {
 		s.log.Infow("Listening on", "addr", addr)
 	}
 
-	<-ctx.Done()
+	<-s.runCtx.Done()
 	if err := s.dht.Close(); err != nil {
 		s.log.Warnw("Failed stopping DHT", "err", err.Error())
 	}
@@ -215,4 +228,60 @@ func (s *Service) NewStream(ctx context.Context, pids ...protocol.ID) (network.S
 			return nil, fmt.Errorf("no reachable peers supporting %s", protocol.ConvertToStrings(pids))
 		}
 	}
+}
+
+func (s *Service) joinTopic(topic string) (*pubsub.Topic, error) {
+	if existingTopic, found := s.topics[topic]; found {
+		return existingTopic, nil
+	}
+
+	newTopic, err := s.pubsub.Join(topic)
+	if err != nil {
+		return nil, err
+	}
+	s.topics[topic] = newTopic
+	return newTopic, nil
+}
+
+func (s *Service) SubscribeToTopic(topic string) (chan []byte, func(), error) {
+	t, joinErr := s.joinTopic(topic)
+	if joinErr != nil {
+		return nil, nil, joinErr
+	}
+
+	sub, subErr := t.Subscribe()
+	if subErr != nil {
+		return nil, nil, subErr
+	}
+
+	const bufferSize = 16
+	ch := make(chan []byte, bufferSize)
+	go func() {
+		for {
+			msg, err := sub.Next(s.runCtx)
+			if err != nil {
+				close(ch)
+				return
+			}
+			// only forward messages delivered by others
+			if msg.ReceivedFrom == s.host.ID() {
+				continue
+			}
+
+			select {
+			case ch <- msg.GetData():
+			case <-s.runCtx.Done():
+			}
+		}
+	}()
+	return ch, sub.Cancel, nil
+}
+
+func (s *Service) PublishOnTopic(topic string, data []byte) error {
+	t, joinErr := s.joinTopic(topic)
+	if joinErr != nil {
+		return joinErr
+	}
+
+	return t.Publish(s.runCtx, data)
 }
