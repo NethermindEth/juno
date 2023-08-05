332d2e30c6990c9763f6756c1a7e3b7807e1f85a
Atharva <adrooney322@gmail.com>
2023-08-05 14:46:16 +0100

    made history private

--- core/history.go
+++ core/history.go
@@ -11,27 +11,27 @@ import (
 
 var ErrCheckHeadState = errors.New("check head state")
 
-type History struct {
+type history struct {
 	txn db.Transaction
 }
 
-func NewHistory(txn db.Transaction) *History {
-	return &History{txn: txn}
+func NewHistory(txn db.Transaction) *history {
+	return &history{txn: txn}
 }
 
 func logDBKey(key []byte, height uint64) []byte {
 	return binary.BigEndian.AppendUint64(key, height)
 }
 
-func (h *History) logOldValue(key, value []byte, height uint64) error {
+func (h *history) logOldValue(key, value []byte, height uint64) error {
 	return h.txn.Set(logDBKey(key, height), value)
 }
 
-func (h *History) deleteLog(key []byte, height uint64) error {
+func (h *history) deleteLog(key []byte, height uint64) error {
 	return h.txn.Delete(logDBKey(key, height))
 }
 
-func (h *History) valueAt(key []byte, height uint64) ([]byte, error) {
+func (h *history) valueAt(key []byte, height uint64) ([]byte, error) {
 	it, err := h.txn.NewIterator()
 	if err != nil {
 		return nil, err
@@ -71,18 +71,18 @@ func storageLogKey(contractAddress, storageLocation *felt.Felt) []byte {
 }
 
 // LogContractStorage logs the old value of a storage location for the given contract which changed on height `height`
-func (h *History) LogContractStorage(contractAddress, storageLocation, oldValue *felt.Felt, height uint64) error {
+func (h *history) LogContractStorage(contractAddress, storageLocation, oldValue *felt.Felt, height uint64) error {
 	key := storageLogKey(contractAddress, storageLocation)
 	return h.logOldValue(key, oldValue.Marshal(), height)
 }
 
 // DeleteContractStorageLog deletes the log at the given height
-func (h *History) DeleteContractStorageLog(contractAddress, storageLocation *felt.Felt, height uint64) error {
+func (h *history) DeleteContractStorageLog(contractAddress, storageLocation *felt.Felt, height uint64) error {
 	return h.deleteLog(storageLogKey(contractAddress, storageLocation), height)
 }
 
 // ContractStorageAt returns the value of a storage location of the given contract at the height `height`
-func (h *History) ContractStorageAt(contractAddress, storageLocation *felt.Felt, height uint64) (*felt.Felt, error) {
+func (h *history) ContractStorageAt(contractAddress, storageLocation *felt.Felt, height uint64) (*felt.Felt, error) {
 	key := storageLogKey(contractAddress, storageLocation)
 	value, err := h.valueAt(key, height)
 	if err != nil {
@@ -96,15 +96,15 @@ func nonceLogKey(contractAddress *felt.Felt) []byte {
 	return db.ContractNonceHistory.Key(contractAddress.Marshal())
 }
 
-func (h *History) LogContractNonce(contractAddress, oldValue *felt.Felt, height uint64) error {
+func (h *history) LogContractNonce(contractAddress, oldValue *felt.Felt, height uint64) error {
 	return h.logOldValue(nonceLogKey(contractAddress), oldValue.Marshal(), height)
 }
 
-func (h *History) DeleteContractNonceLog(contractAddress *felt.Felt, height uint64) error {
+func (h *history) DeleteContractNonceLog(contractAddress *felt.Felt, height uint64) error {
 	return h.deleteLog(nonceLogKey(contractAddress), height)
 }
 
-func (h *History) ContractNonceAt(contractAddress *felt.Felt, height uint64) (*felt.Felt, error) {
+func (h *history) ContractNonceAt(contractAddress *felt.Felt, height uint64) (*felt.Felt, error) {
 	key := nonceLogKey(contractAddress)
 	value, err := h.valueAt(key, height)
 	if err != nil {
@@ -118,15 +118,15 @@ func classHashLogKey(contractAddress *felt.Felt) []byte {
 	return db.ContractClassHashHistory.Key(contractAddress.Marshal())
 }
 
-func (h *History) LogContractClassHash(contractAddress, oldValue *felt.Felt, height uint64) error {
+func (h *history) LogContractClassHash(contractAddress, oldValue *felt.Felt, height uint64) error {
 	return h.logOldValue(classHashLogKey(contractAddress), oldValue.Marshal(), height)
 }
 
-func (h *History) DeleteContractClassHashLog(contractAddress *felt.Felt, height uint64) error {
+func (h *history) DeleteContractClassHashLog(contractAddress *felt.Felt, height uint64) error {
 	return h.deleteLog(classHashLogKey(contractAddress), height)
 }
 
-func (h *History) ContractClassHashAt(contractAddress *felt.Felt, height uint64) (*felt.Felt, error) {
+func (h *history) ContractClassHashAt(contractAddress *felt.Felt, height uint64) (*felt.Felt, error) {
 	key := classHashLogKey(contractAddress)
 	value, err := h.valueAt(key, height)
 	if err != nil {
