c9e9b01b1a0e64f39264a502e8da5e32a192f6ef
Josh Klopfenstein <git@joshklop.com>
2023-09-26 11:18:09 -0500

    Set the default data directory correctly (#1249)
    We now use cwd/juno as the data directory.
    
    Previously, we set the default to the empty string "" and manually set
    the default directory in `node.New`. This meant the incorrect default
    was printed in two cases:
    
    1. `--help`
    2. When printing the config on boot.
    
    Finally, this centralizes all of our config management to the
    `cmd/juno` package; if `juno.NewCmd.Execute` returns without an error,
    the config is structurally valid.

--- node/node.go
+++ node/node.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/url"
 	"os"
-	"path/filepath"
 	"reflect"
 	"runtime"
 	"time"
@@ -85,13 +84,6 @@ type Node struct {
 // New sets the config and logger to the StarknetNode.
 // Any errors while parsing the config on creating logger will be returned.
 func New(cfg *Config, version string) (*Node, error) { //nolint:gocyclo,funlen
-	if cfg.DatabasePath == "" {
-		dirPrefix, err := utils.DefaultDataDir()
-		if err != nil {
-			return nil, err
-		}
-		cfg.DatabasePath = filepath.Join(dirPrefix, cfg.Network.String())
-	}
 	log, err := utils.NewZapLogger(cfg.LogLevel, cfg.Colour)
 	if err != nil {
 		return nil, err
--- node/node_test.go
+++ node/node_test.go
@@ -1,36 +1,13 @@
 package node_test
 
 import (
-	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/NethermindEth/juno/node"
 	"github.com/NethermindEth/juno/utils"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestDefaultDbPath(t *testing.T) {
-	defaultDataDir, err := utils.DefaultDataDir()
-	require.NoError(t, err)
-
-	networks := []utils.Network{utils.GOERLI, utils.MAINNET, utils.GOERLI2, utils.INTEGRATION}
-
-	for _, n := range networks {
-		t.Run(n.String(), func(t *testing.T) {
-			cfg := &node.Config{Network: n, DatabasePath: ""}
-			expectedCfg := node.Config{
-				Network:      n,
-				DatabasePath: filepath.Join(defaultDataDir, n.String()),
-			}
-			snNode, err := node.New(cfg, "1.2.3")
-			require.NoError(t, err)
-
-			assert.Equal(t, expectedCfg, snNode.Config())
-		})
-	}
-}
 
 // Create a new node with all services enabled.
 func TestNewNode(t *testing.T) {
