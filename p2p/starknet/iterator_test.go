d33f62d99a03977adb9f73c1f5c4005f6b0900ff
Kirill <paltsev.kir@gmail.com>
2023-10-09 14:09:44 +0400

    Fix gomock usage int iterator tests

--- p2p/starknet/iterator_test.go
+++ p2p/starknet/iterator_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/mocks"
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func TestIterator(t *testing.T) {
