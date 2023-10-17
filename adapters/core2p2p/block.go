616bfbe893d8036e566655175eebe7767bb4435f
Kirill <paltsev.kir@gmail.com>
2023-10-18 03:51:31 +0400

    Handle nil core structs in core2p2p functions

--- adapters/core2p2p/block.go
+++ adapters/core2p2p/block.go
@@ -7,6 +7,10 @@ import (
 )
 
 func AdaptBlockID(header *core.Header) *spec.BlockID {
+	if header == nil {
+		return nil
+	}
+
 	return &spec.BlockID{
 		Number: header.Number,
 		Header: AdaptHash(header.Hash),
@@ -14,6 +18,10 @@ func AdaptBlockID(header *core.Header) *spec.BlockID {
 }
 
 func AdaptEvent(e *core.Event) *spec.Event {
+	if e == nil {
+		return nil
+	}
+
 	return &spec.Event{
 		FromAddress: AdaptFelt(e.From),
 		Keys:        utils.Map(e.Keys, AdaptFelt),
