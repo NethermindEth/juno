b31285cb9d663da2e4489c33a6d93f875311d61d
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-10-24 12:55:13 +0300

    Add SyncTransaction

---
+++ db/sync_transaction.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"errors"
+	"sync"
+)
+
+type SyncTransaction struct {
+	lock sync.RWMutex
+	txn  Transaction
+}
+
+func NewSyncTransaction(txn Transaction) *SyncTransaction {
+	return &SyncTransaction{
+		txn: txn,
+	}
+}
+
+// Discard : see db.Transaction.Discard
+func (t *SyncTransaction) Discard() error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	return t.txn.Discard()
+}
+
+// Commit : see db.Transaction.Commit
+func (t *SyncTransaction) Commit() error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	return t.txn.Commit()
+}
+
+// Set : see db.Transaction.Set
+func (t *SyncTransaction) Set(key, val []byte) error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	return t.txn.Set(key, val)
+}
+
+// Delete : see db.Transaction.Delete
+func (t *SyncTransaction) Delete(key []byte) error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	return t.txn.Delete(key)
+}
+
+// Get : see db.Transaction.Get
+func (t *SyncTransaction) Get(key []byte, cb func([]byte) error) error {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	return t.txn.Get(key, cb)
+}
+
+// Impl : see db.Transaction.Impl
+func (t *SyncTransaction) Impl() any {
+	return t.txn
+}
+
+// NewIterator : see db.Transaction.NewIterator
+func (t *SyncTransaction) NewIterator() (Iterator, error) {
+	return nil, errors.New("sync transactions dont support iterators")
+}
