1440089e1f08e0de50b9b7655420c177f64a79ef
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-10-27 17:43:15 +0300

    Collect DB latency metrics

--- db/event_listener.go
+++ db/event_listener.go
@@ -1,15 +1,17 @@
 package db
 
+import "time"
+
 type EventListener interface {
-	OnIO(write bool)
+	OnIO(write bool, duration time.Duration)
 }
 
 type SelectiveListener struct {
-	OnIOCb func(write bool)
+	OnIOCb func(write bool, duration time.Duration)
 }
 
-func (l *SelectiveListener) OnIO(write bool) {
+func (l *SelectiveListener) OnIO(write bool, duration time.Duration) {
 	if l.OnIOCb != nil {
-		l.OnIOCb(write)
+		l.OnIOCb(write, duration)
 	}
 }
