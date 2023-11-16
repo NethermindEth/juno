84c08fbbed2c5e8d9f486415f6b3ccfb8d6ce720
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-11-16 10:48:28 +0300

    Fix duration of Transaction.Get metric including the user callback runtime

--- db/pebble/transaction.go
+++ db/pebble/transaction.go
@@ -94,7 +94,7 @@ func (t *Transaction) Get(key []byte, cb func([]byte) error) error {
 		return ErrDiscardedTransaction
 	}
 
-	defer func() { t.listener.OnIO(false, time.Since(start)) }()
+	defer t.listener.OnIO(false, time.Since(start))
 	if err != nil {
 		if errors.Is(err, pebble.ErrNotFound) {
 			return db.ErrKeyNotFound
