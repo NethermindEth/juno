59b5d032e4205847f36a1e38d0df6a4e82c1b01e
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-11-22 19:33:28 +0300

    Bump supported SN version

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -48,7 +48,7 @@ type Reader interface {
 
 var (
 	ErrParentDoesNotMatchHead = errors.New("block's parent hash does not match head block hash")
-	supportedStarknetVersion  = semver.MustParse("0.12.3")
+	supportedStarknetVersion  = semver.MustParse("0.13.0")
 )
 
 func checkBlockVersion(protocolVersion string) error {
