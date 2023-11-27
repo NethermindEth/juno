116aa8edfebc392fd0f79c4654b84d3ee9740809
Kirill <paltsev.kir@gmail.com>
2023-11-27 13:34:46 +0300

    Move adapters from vm package to core2sn

--- vm/class.go
+++ vm/class.go
@@ -16,22 +16,21 @@ import (
 	"fmt"
 	"unsafe"
 
+	"github.com/NethermindEth/juno/adapters/core2sn"
 	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
-	"github.com/NethermindEth/juno/starknet"
-	"github.com/NethermindEth/juno/utils"
 )
 
 func marshalCompiledClass(class core.Class) (json.RawMessage, error) {
 	switch c := class.(type) {
 	case *core.Cairo0Class:
-		compiledCairo0Class, err := makeDeprecatedVMClass(c)
+		compiledCairo0Class, err := core2sn.AdaptCairo0Class(c)
 		if err != nil {
 			return nil, err
 		}
 		return json.Marshal(compiledCairo0Class)
 	case *core.Cairo1Class:
-		compiledCairo1Class := makeCairo1CompiledClass(&c.Compiled)
+		compiledCairo1Class := core2sn.AdaptCompiledClass(&c.Compiled)
 		// we adapt the core type to the feeder type to avoid using JSON tags in core.Class.CompiledClass
 		return json.Marshal(compiledCairo1Class)
 	default:
@@ -45,132 +44,17 @@ func marshalDeclaredClass(class core.Class) (json.RawMessage, error) {
 
 	switch c := class.(type) {
 	case *core.Cairo0Class:
-		declaredClass, err = makeDeprecatedVMClass(c)
+		declaredClass, err = core2sn.AdaptCairo0Class(c)
 		if err != nil {
 			return nil, err
 		}
 	case *core.Cairo1Class:
-		declaredClass = makeSierraClass(c)
+		declaredClass = core2sn.AdaptSierraClass(c)
 	default:
 		return nil, fmt.Errorf("unsupported class type %T", c)
 	}
 
 	return json.Marshal(declaredClass)
-}
-
-func makeDeprecatedVMClass(class *core.Cairo0Class) (*starknet.Cairo0Definition, error) {
-	decompressedProgram, err := utils.Gzip64Decode(class.Program)
-	if err != nil {
-		return nil, err
-	}
-
-	constructors := make([]starknet.EntryPoint, 0, len(class.Constructors))
-	for _, entryPoint := range class.Constructors {
-		constructors = append(constructors, starknet.EntryPoint{
-			Selector: entryPoint.Selector,
-			Offset:   entryPoint.Offset,
-		})
-	}
-
-	external := make([]starknet.EntryPoint, 0, len(class.Externals))
-	for _, entryPoint := range class.Externals {
-		external = append(external, starknet.EntryPoint{
-			Selector: entryPoint.Selector,
-			Offset:   entryPoint.Offset,
-		})
-	}
-
-	handlers := make([]starknet.EntryPoint, 0, len(class.L1Handlers))
-	for _, entryPoint := range class.L1Handlers {
-		handlers = append(handlers, starknet.EntryPoint{
-			Selector: entryPoint.Selector,
-			Offset:   entryPoint.Offset,
-		})
-	}
-
-	return &starknet.Cairo0Definition{
-		Program: decompressedProgram,
-		Abi:     class.Abi,
-		EntryPoints: starknet.EntryPoints{
-			Constructor: constructors,
-			External:    external,
-			L1Handler:   handlers,
-		},
-	}, nil
-}
-
-func makeSierraClass(class *core.Cairo1Class) *starknet.SierraDefinition {
-	constructors := make([]starknet.SierraEntryPoint, 0, len(class.EntryPoints.Constructor))
-	for _, entryPoint := range class.EntryPoints.Constructor {
-		constructors = append(constructors, starknet.SierraEntryPoint{
-			Selector: entryPoint.Selector,
-			Index:    entryPoint.Index,
-		})
-	}
-
-	external := make([]starknet.SierraEntryPoint, 0, len(class.EntryPoints.External))
-	for _, entryPoint := range class.EntryPoints.External {
-		external = append(external, starknet.SierraEntryPoint{
-			Selector: entryPoint.Selector,
-			Index:    entryPoint.Index,
-		})
-	}
-
-	handlers := make([]starknet.SierraEntryPoint, 0, len(class.EntryPoints.L1Handler))
-	for _, entryPoint := range class.EntryPoints.L1Handler {
-		handlers = append(handlers, starknet.SierraEntryPoint{
-			Selector: entryPoint.Selector,
-			Index:    entryPoint.Index,
-		})
-	}
-
-	return &starknet.SierraDefinition{
-		Version: class.SemanticVersion,
-		Program: class.Program,
-		EntryPoints: starknet.SierraEntryPoints{
-			Constructor: constructors,
-			External:    external,
-			L1Handler:   handlers,
-		},
-	}
-}
-
-func makeCairo1CompiledClass(coreCompiledClass *core.CompiledClass) starknet.CompiledClass {
-	feederCompiledClass := new(starknet.CompiledClass)
-	feederCompiledClass.Bytecode = coreCompiledClass.Bytecode
-	feederCompiledClass.PythonicHints = coreCompiledClass.PythonicHints
-	feederCompiledClass.CompilerVersion = coreCompiledClass.CompilerVersion
-	feederCompiledClass.Hints = coreCompiledClass.Hints
-	feederCompiledClass.Prime = "0x" + coreCompiledClass.Prime.Text(16) //nolint:gomnd
-
-	feederCompiledClass.EntryPoints.External = make([]starknet.CompiledEntryPoint, len(coreCompiledClass.External))
-	for i, external := range coreCompiledClass.External {
-		feederCompiledClass.EntryPoints.External[i] = starknet.CompiledEntryPoint{
-			Selector: external.Selector,
-			Builtins: external.Builtins,
-			Offset:   external.Offset,
-		}
-	}
-
-	feederCompiledClass.EntryPoints.L1Handler = make([]starknet.CompiledEntryPoint, len(coreCompiledClass.L1Handler))
-	for i, external := range coreCompiledClass.L1Handler {
-		feederCompiledClass.EntryPoints.L1Handler[i] = starknet.CompiledEntryPoint{
-			Selector: external.Selector,
-			Builtins: external.Builtins,
-			Offset:   external.Offset,
-		}
-	}
-
-	feederCompiledClass.EntryPoints.Constructor = make([]starknet.CompiledEntryPoint, len(coreCompiledClass.Constructor))
-	for i, external := range coreCompiledClass.Constructor {
-		feederCompiledClass.EntryPoints.Constructor[i] = starknet.CompiledEntryPoint{
-			Selector: external.Selector,
-			Builtins: external.Builtins,
-			Offset:   external.Offset,
-		}
-	}
-
-	return *feederCompiledClass
 }
 
 func Cairo0ClassHash(class *core.Cairo0Class) (*felt.Felt, error) {
