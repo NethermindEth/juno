693ee467df8a5a76723733375f04d19eb4443152
Kirill <paltsev.kir@gmail.com>
2023-11-27 14:41:12 +0300

    Revert "Remove everything related to Cairo0 hash calculations"
    This reverts commit bdaa3a17d6eb7fefd80bfda288c245e58dd6a731.

--- core/class_test.go
+++ core/class_test.go
@@ -17,6 +17,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestClassV0Hash(t *testing.T) {
+	client, closeFn := feeder.NewTestClient(utils.GOERLI)
+	t.Cleanup(closeFn)
+
+	gw := adaptfeeder.New(client)
+	tests := []struct {
+		classHash string
+	}{
+		{
+			// https://alpha4.starknet.io/feeder_gateway/get_class_by_hash?classHash=0x010455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8
+			classHash: "0x010455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8",
+		},
+		{
+			// https://alpha4.starknet.io/feeder_gateway/get_class_by_hash?classHash=0x056b96c1d1bbfa01af44b465763d1b71150fa00c6c9d54c3947f57e979ff68c3
+			classHash: "0x056b96c1d1bbfa01af44b465763d1b71150fa00c6c9d54c3947f57e979ff68c3",
+		},
+		{
+			// https://alpha4.starknet.io/feeder_gateway/get_class_by_hash?classHash=0x0079e2d211e70594e687f9f788f71302e6eecb61d98efce48fbe8514948c8118
+			classHash: "0x0079e2d211e70594e687f9f788f71302e6eecb61d98efce48fbe8514948c8118",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("ClassHash", func(t *testing.T) {
+			hash := utils.HexToFelt(t, tt.classHash)
+			class, err := gw.Class(context.Background(), hash)
+			require.NoError(t, err)
+			got := class.Hash()
+			assert.Equal(t, hash, got)
+		})
+	}
+}
+
 func TestClassV1Hash(t *testing.T) {
 	client := feeder.NewTestClient(t, utils.Integration)
 	gw := adaptfeeder.New(client)
@@ -34,7 +67,7 @@ func TestClassV1Hash(t *testing.T) {
 			hash := utils.HexToFelt(t, tt.classHash)
 			class, err := gw.Class(context.Background(), hash)
 			require.NoError(t, err)
-			got := class.(*core.Cairo1Class).Hash()
+			got := class.Hash()
 			assert.Equal(t, hash, got)
 		})
 	}
@@ -54,6 +87,9 @@ func TestClassEncoding(t *testing.T) {
 				},
 				L1Handlers:   []core.EntryPoint{},
 				Constructors: []core.EntryPoint{},
+				BuiltinsHash: utils.HexToFelt(t, "0xDEADBEEF"),
+				ProgramHash:  utils.HexToFelt(t, "0xBEEFDEAD"),
+				BytecodeHash: utils.HexToFelt(t, "0xDEAD"),
 			},
 		},
 		{
