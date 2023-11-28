c71ba24f824c9861ddc2bb81b8ad455cf0ae4617
Kirill <paltsev.kir@gmail.com>
2023-11-28 14:06:23 +0300

    Revert "Revert "Implement casm class hash calculation (#1154)" (#1474)"
    This reverts commit 9448c9f56c33196c8aacc1d86c3cd23d3d7b424b.

--- adapters/core2p2p/class.go
+++ adapters/core2p2p/class.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 
 	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/p2p/starknet/spec"
-	"github.com/NethermindEth/juno/utils"
 )
 
-func AdaptClass(class core.Class) *spec.Class {
+func AdaptClass(class core.Class, compiledHash *felt.Felt) *spec.Class {
 	if class == nil {
 		return nil
 	}
@@ -16,47 +16,14 @@ func AdaptClass(class core.Class) *spec.Class {
 	switch v := class.(type) {
 	case *core.Cairo0Class:
 		return &spec.Class{
-			Class: &spec.Class_Cairo0{
-				Cairo0: &spec.Cairo0Class{
-					Abi:          v.Abi,
-					Externals:    utils.Map(v.Externals, adaptEntryPoint),
-					L1Handlers:   utils.Map(v.L1Handlers, adaptEntryPoint),
-					Constructors: utils.Map(v.Constructors, adaptEntryPoint),
-					Program:      []byte(v.Program),
-				},
-			},
+			CompiledHash: AdaptHash(compiledHash),
+			Definition:   []byte(v.Program),
 		}
 	case *core.Cairo1Class:
 		return &spec.Class{
-			Class: &spec.Class_Cairo1{
-				Cairo1: &spec.Cairo1Class{
-					Abi: []byte(v.Abi),
-					EntryPoints: &spec.Cairo1EntryPoints{
-						Externals:    utils.Map(v.EntryPoints.External, adaptSierra),
-						L1Handlers:   utils.Map(v.EntryPoints.L1Handler, adaptSierra),
-						Constructors: utils.Map(v.EntryPoints.Constructor, adaptSierra),
-					},
-					Program:              utils.Map(v.Program, AdaptFelt),
-					ContractClassVersion: v.SemanticVersion,
-					Compiled:             nil,
-				},
-			},
+			CompiledHash: AdaptHash(compiledHash),
 		}
 	default:
 		panic(fmt.Errorf("unsupported cairo class %T (version=%d)", v, class.Version()))
-	}
-}
-
-func adaptSierra(e core.SierraEntryPoint) *spec.SierraEntryPoint {
-	return &spec.SierraEntryPoint{
-		Index:    e.Index,
-		Selector: AdaptFelt(e.Selector),
-	}
-}
-
-func adaptEntryPoint(e core.EntryPoint) *spec.EntryPoint {
-	return &spec.EntryPoint{
-		Selector: AdaptFelt(e.Selector),
-		Offset:   AdaptFelt(e.Offset),
 	}
 }
