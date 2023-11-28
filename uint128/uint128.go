8cb721253cb277240631859f5c7a9c0bb070be87
Sarabraj Singh <singh.sarabraj@gmail.com>
2023-11-28 00:35:43 -0500

    refactored uint128 to more closely match the implementation over in *core.Felt

--- uint128/uint128.go
+++ uint128/uint128.go
@@ -5,89 +5,116 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"strings"
+	"sync"
 )
 
+// *Int[1] = high bits, more significant bits, upper 64 bits of a 128-bit value - ie [64:128]
+// *Int[0] = low bits, less significant bits, lower 64 bits of a 128-bit value - ie [0:64]
+// for example, if we have a 128-bit hex value 0x6e58133b38301a6cdfa34ca991c4ba39
+// *Int[0] (lower bits) should be 0xdfa34ca991c4ba39 or dfa34ca991c4ba39
+// *Int[i] (upper bits) should be 0x6e58133b38301a6c or 6e58133b38301a6c
+// an example of a call to this type can be : uint128.Int([]uint64{lo, high})
 type Int [2]uint64
 
-func SetUint64(lo uint64) *Int {
-	// i[1] = high bits
-	// i[0] = low bits
-	z := &Int{}
-	z.setUint128(0, lo)
-	return z
-}
-func SetUint128(hi, lo uint64) *Int {
-	z := &Int{}
-	z.setUint128(hi, lo)
-	return z
-}
+const (
+	Base16     = 16
+	ByteLength = 16
+	BitLength  = 128
+)
 
-func SetInt128(hex_str string) *Int {
-	z := &Int{}
-
-	z.parseHexString(hex_str)
-
-	return z
+var bigIntPool = sync.Pool{
+	New: func() interface{} {
+		return new(big.Int)
+	},
 }
 
-func (i *Int) setUint128(hi, lo uint64) error {
+func NewInt(x []uint64) (*Int, error) {
+	if len(x) > 2 {
+		return nil, fmt.Errorf("trying to add more than 128 bits to a 128-bit object")
+	}
+
+	res := bigIntPool.Get().(*big.Int)
+	defer bigIntPool.Put(res)
+
 	loBytes := make([]byte, 8)
 	hiBytes := make([]byte, 8)
 
-	binary.BigEndian.PutUint64(loBytes, lo)
-	binary.BigEndian.PutUint64(hiBytes, hi)
+	binary.BigEndian.PutUint64(loBytes, x[0])
+	binary.BigEndian.PutUint64(hiBytes, x[1])
 
-	err := i.setBytes(append(hiBytes, loBytes...))
-	if err != nil {
-		return fmt.Errorf("couldn't marshal %016x%016x into a uint128 type", lo, hi)
+	bytes := append(hiBytes, loBytes...)
+
+	res.SetBytes(bytes)
+	res.FillBytes(bytes[:])
+
+	i := &Int{}
+
+	return i.setBigInt(res), nil
+
+}
+
+func (i *Int) setBigInt(v *big.Int) *Int {
+	// we're expecting words to have a length of 2, which represents 2x uint64s in a slice
+	words := v.Bits()
+
+	for idx, word := range words {
+		i[idx] = uint64(word)
 	}
 
-	return nil
+	return i
+
+}
+
+func (i *Int) SetString(s string) (*Int, error) {
+	vv := bigIntPool.Get().(*big.Int)
+	defer bigIntPool.Put(vv)
+
+	if _, ok := vv.SetString(s, 0); !ok {
+		if _, ok := vv.SetString(s, Base16); !ok {
+			return i, fmt.Errorf("could not parse string=%s into big.Int", s)
+		}
+	}
+
+	bytes, err := parseHexString(s)
+
+	if err != nil || len(bytes) > ByteLength {
+		return nil, fmt.Errorf("can't fit string=%s into 128-bit uint; %s", s, err)
+	}
+
+	vv.FillBytes(bytes[:])
+
+	return i.setBigInt(vv), nil
+
 }
 
 func (i *Int) Bytes() []byte {
-	res := make([]byte, 16)
+	res := make([]byte, ByteLength)
 
-	binary.BigEndian.PutUint64(res[:8], i[1])
 	binary.BigEndian.PutUint64(res[8:], i[0])
+	binary.BigEndian.PutUint64(res[:8], i[1])
 
 	return res
 }
 
-func (i *Int) setBytes(buf []byte) error {
-	if len(buf) > 16 {
-		return fmt.Errorf("trying to set bytes larger than 16 bytes (128-bits) to a 128-bit container")
-	}
-
-	_ = buf[len(buf)-1] // bounds check hint to compiler; see golang.org/issue/14808
-
-	// upper 8 bytes / 64 bits
-	i[1] = binary.BigEndian.Uint64(buf[:8])
-	// lower 8 bytes / 64 bits
-	i[0] = binary.BigEndian.Uint64(buf[8:])
-
-	return nil
-}
-
 func (i *Int) UnmarshalJSON(data []byte) error {
 	var v string
+
 	if err := json.Unmarshal(data, &v); err != nil {
 		return err
 	}
 
-	if err := i.parseHexString(v); err != nil {
-		return err
-	}
+	_, err := i.SetString(v)
 
-	return nil
+	return err
 }
 
-func (i *Int) parseHexString(v string) error {
+func parseHexString(v string) ([]byte, error) {
 	if v == "0x0" {
-		i[1], i[0] = 0, 0
-		return nil
+		return make([]byte, ByteLength), nil
 	}
+
 	v = strings.TrimPrefix(v, "0x")
 
 	// might need a leading zero
@@ -97,23 +124,20 @@ func (i *Int) parseHexString(v string) error {
 
 	bytes, err := hex.DecodeString(v)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	padSize := 16 - len(bytes)
+	padSize := ByteLength - len(bytes)
 	if padSize > 0 {
 		padBytes := make([]byte, padSize)
 		bytes = append(padBytes, bytes...)
 	}
 
-	i[1] = binary.BigEndian.Uint64(bytes[:8])
-	i[0] = binary.BigEndian.Uint64(bytes[8:])
-
-	return nil
+	return bytes, nil
 }
 
 func (i Int) Equal(o *Int) bool {
-	return i[1] == o[1] && i[0] == o[0]
+	return i[0] == o[0] && i[1] == o[1]
 }
 
 func (i Int) String() string {
--- uint128/uint128_test.go
+++ uint128/uint128_test.go
@@ -8,108 +8,79 @@ import (
 func TestUint128Bytes(t *testing.T) {
 	tests := []struct {
 		description  string
-		input        *Int
+		loBits       uint64
+		hiBits       uint64
 		expected_arr []byte
 		expected_str string
+		wantedErr    bool
 	}{
 		{
 			description:  "128-bit #1",
-			input:        SetUint128(0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF),
-			expected_arr: []byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
-			expected_str: "ffffffffffffffffffffffffffffffff",
+			loBits:       0x0,
+			hiBits:       0x0,
+			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			expected_str: "00000000000000000000000000000000",
+			wantedErr:    false,
 		},
 		{
 			description:  "128-bit #2",
-			input:        SetUint128(0x123456789ABCDEF0, 0x0123456789ABCDEF),
-			expected_arr: []byte{18, 52, 86, 120, 154, 188, 222, 240, 1, 35, 69, 103, 137, 171, 205, 239},
-			expected_str: "123456789abcdef00123456789abcdef",
-		},
-		{
-			description:  "128-bit 3",
-			input:        SetUint128(0x1A4B7E9C2D3F5A6E, 0xF0D3B8A289C7E5B3),
-			expected_arr: []byte{26, 75, 126, 156, 45, 63, 90, 110, 240, 211, 184, 162, 137, 199, 229, 179},
-			expected_str: "1a4b7e9c2d3f5a6ef0d3b8a289c7e5b3",
-		},
-		{
-			description:  "128-bit 4",
-			input:        SetUint128(0x1A4B7E9C2D3F5A6E, 0x0),
-			expected_arr: []byte{26, 75, 126, 156, 45, 63, 90, 110, 0, 0, 0, 0, 0, 0, 0, 0},
-			expected_str: "1a4b7e9c2d3f5a6e0000000000000000",
-		},
-		{
-			description:  "128-bit 5",
-			input:        SetUint128(0x0, 0x0),
-			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			expected_str: "00000000000000000000000000000000",
-		},
-		{
-			description:  "128-bit 6",
-			input:        SetUint128(0x0, 0x1),
+			loBits:       0x1,
+			hiBits:       0x0,
 			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 			expected_str: "00000000000000000000000000000001",
+			wantedErr:    false,
 		},
 		{
-			description:  "128-bit 7",
-			input:        SetUint128(0x1, 0x0),
-			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-			expected_str: "00000000000000010000000000000000",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.description, func(t *testing.T) {
-			if test.expected_str != test.input.String() {
-				t.Errorf("Expected string=%s, got string=%s", test.expected_str, test.input.String())
-			}
-			result := test.input.Bytes()
-			if !byteSlicesEqual(result, test.expected_arr) {
-				t.Errorf("Expected arr=%x, got %x", test.expected_arr, result)
-			}
-		})
-	}
-}
-
-func TestUint64Bytes(t *testing.T) {
-	tests := []struct {
-		description  string
-		input        *Int
-		expected_arr []byte
-		expected_str string
-	}{
-		{
-			description:  "64-bit #1",
-			input:        SetUint64(0xFFFFFFFFFFFFFFFF),
-			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255},
-			expected_str: "0000000000000000ffffffffffffffff",
+			description:  "128-bit #3",
+			loBits:       0xFFFFFFFFFFFFFFFF,
+			hiBits:       0xFFFFFFFFFFFFFFFF,
+			expected_arr: []byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+			expected_str: "ffffffffffffffffffffffffffffffff",
+			wantedErr:    false,
 		},
 		{
-			description:  "64-bit #2",
-			input:        SetUint64(0x0123456789ABCDEF),
-			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 0, 1, 35, 69, 103, 137, 171, 205, 239},
-			expected_str: "00000000000000000123456789abcdef",
+			description:  "128-bit #4",
+			loBits:       0x0123456789ABCDEF,
+			hiBits:       0x123456789ABCDEF0,
+			expected_arr: []byte{18, 52, 86, 120, 154, 188, 222, 240, 1, 35, 69, 103, 137, 171, 205, 239},
+			expected_str: "123456789abcdef00123456789abcdef",
+			wantedErr:    false,
 		},
 		{
-			description:  "64-bit 3",
-			input:        SetUint64(0x0000000000000000),
-			expected_arr: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			expected_str: "00000000000000000000000000000000",
+			description:  "128-bit #5",
+			loBits:       0xF0D3B8A289C7E5B3,
+			hiBits:       0x1A4B7E9C2D3F5A6E,
+			expected_arr: []byte{26, 75, 126, 156, 45, 63, 90, 110, 240, 211, 184, 162, 137, 199, 229, 179},
+			expected_str: "1a4b7e9c2d3f5a6ef0d3b8a289c7e5b3",
+			wantedErr:    false,
 		},
 		{
-			description:  "64-bit 4",
-			input:        SetUint128(0x1A4B7E9C2D3F5A6E, 0x0),
+			description:  "128-bit #6",
+			loBits:       0x0,
+			hiBits:       0x1A4B7E9C2D3F5A6E,
 			expected_arr: []byte{26, 75, 126, 156, 45, 63, 90, 110, 0, 0, 0, 0, 0, 0, 0, 0},
 			expected_str: "1a4b7e9c2d3f5a6e0000000000000000",
+			wantedErr:    false,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			if test.expected_str != test.input.String() {
-				t.Errorf("Expected string=%s, got string=%s", test.expected_str, test.input.String())
+			arr := make([]uint64, 2)
+			arr[0] = test.loBits
+			arr[1] = test.hiBits
+			actual, err := NewInt(arr)
+			if err != nil {
+				if test.wantedErr {
+					return
+				}
+				t.Errorf("couldn't marshal []uint64={%v, %v} into *Int", test.loBits, test.hiBits)
 			}
-			result := test.input.Bytes()
-			if !byteSlicesEqual(result, test.expected_arr) {
-				t.Errorf("Expected arr=%x, got %x", test.expected_arr, result)
+			if test.expected_str != actual.String() {
+				t.Errorf("Expected string=%s, got string=%s", test.expected_str, actual.String())
+			}
+			if !byteSlicesEqual(actual.Bytes(), test.expected_arr) {
+				t.Errorf("Expected arr=%x, got %x", test.expected_arr, actual.Bytes())
 			}
 		})
 	}
@@ -125,43 +96,75 @@ func TestUnmarshalJsonToUint128(t *testing.T) {
 		{
 			description: "Valid JSON 1",
 			jsonInput:   `{"max_price_per_unit": "0x0"}`,
-			expected:    SetUint64(0),
-			wantedErr:   false,
+			expected: &Int{
+				0: 0x0,
+			},
+			wantedErr: false,
 		},
 		{
 			description: "Valid JSON 2",
 			jsonInput:   `{"max_price_per_unit": "0x1"}`,
-			expected:    SetUint64(1),
-			wantedErr:   false,
+			expected: &Int{
+				0: 0x1,
+			},
+			wantedErr: false,
 		},
 		{
 			description: "Valid JSON 3",
 			jsonInput:   `{"max_price_per_unit": "0x00000000000000010000000000000000"}`,
-			expected:    SetUint128(0x1, 0x0),
-			wantedErr:   false,
+			expected: &Int{
+				1: 0x1,
+			},
+			wantedErr: false,
 		},
 		{
 			description: "Valid JSON 4",
 			jsonInput:   `{"max_price_per_unit": "0x00000000000000010000000000000001"}`,
-			expected:    SetUint128(0x1, 0x1),
-			wantedErr:   false,
+			expected: &Int{
+				0: 0x1,
+				1: 0x1,
+			},
+			wantedErr: false,
 		},
 		{
 			description: "Valid JSON 5",
 			jsonInput:   `{"max_price_per_unit": "0x6e58133b38301a6cdfa34ca991c4ba39"}`,
-			expected:    SetUint128(0x6e58133b38301a6c, 0xdfa34ca991c4ba39),
-			wantedErr:   false,
+			expected: &Int{
+				0: 0xdfa34ca991c4ba39,
+				1: 0x6e58133b38301a6c,
+			},
+			wantedErr: false,
 		},
 		{
 			description: "Valid JSON 6",
 			jsonInput:   `{"max_price_per_unit": "0x5af3107a4000"}`,
-			expected:    SetUint64(0x5af3107a4000),
-			wantedErr:   false,
+			expected: &Int{
+				0: 0x5af3107a4000,
+			},
+			wantedErr: false,
 		},
 		{
 			description: "Invalid JSON 1",
 			jsonInput:   `{"max_price_per_unit": "foobar"}`,
-			expected:    SetUint128(0x6e58133b38301a6c, 0xdfa34ca991c4ba39),
+			expected:    &Int{},
+			wantedErr:   true,
+		},
+		{
+			description: "Invalid JSON 2",
+			jsonInput:   `{"max_price_per_unit": "q2i34jti0q2ngioawngioasnjgoanrjognwoignwejogniaewognkoaergnoarnggkangionw34gion3"}`,
+			expected:    &Int{},
+			wantedErr:   true,
+		},
+		{
+			description: "Invalid JSON 3",
+			jsonInput:   `{"max_price_per_unit": ""}`,
+			expected:    &Int{},
+			wantedErr:   true,
+		},
+		{
+			description: "Invalid JSON 4",
+			jsonInput:   `{"max_price_per_unit": "0xc4c53de93c6f98fa8a91d4f6683b06d2"}`,
+			expected:    &Int{},
 			wantedErr:   true,
 		},
 	}
@@ -174,11 +177,10 @@ func TestUnmarshalJsonToUint128(t *testing.T) {
 
 			err := json.Unmarshal([]byte(test.jsonInput), &data)
 			if (err != nil) != test.wantedErr {
-				t.Errorf("unable to unmarshal json; got %v, wantedErr %v", err, test.wantedErr)
-				return
-			}
-
-			if test.wantedErr {
+				if test.wantedErr {
+					return
+				}
+				t.Errorf("unable to unmarshal json; gotError %v, wantedErr %v", err, test.wantedErr)
 				return
 			}
 
@@ -193,22 +195,49 @@ func TestUint128FromHexString(t *testing.T) {
 	tests := []struct {
 		description string
 		textInput   string
-		expected64  *Int
-		expected128 *Int
+		expected    *Int
+		wantedErr   bool
 	}{
 		{
 			description: "String 1",
 			textInput:   "0x5af3107a4000",
-			expected64:  SetUint64(0x5af3107a4000),
-			expected128: SetUint128(0x0, 0x5af3107a4000),
+			expected: &Int{
+				0: 0x5af3107a4000,
+			},
+			wantedErr: false,
+		},
+		{
+			description: "String 2",
+			textInput:   "0x5af3107a40#$^#@($H#(HG(WG_00",
+			expected:    &Int{},
+			wantedErr:   true,
+		},
+		{
+			description: "String 3",
+			textInput:   "IAMNOTAHEXSTRING",
+			expected:    &Int{},
+			wantedErr:   true,
+		},
+		{
+			description: "String 4",
+			textInput:   "01234G6789ABCDEF",
+			expected:    &Int{},
+			wantedErr:   true,
 		},
 	}
 	{
 		for _, test := range tests {
 			t.Run(test.description, func(t *testing.T) {
-				result := SetInt128(test.textInput)
-				if !result.Equal(test.expected64) && !result.Equal(test.expected128) {
-					t.Errorf("got %v, expected64=%v, expected128=%v", result, test.expected64, test.expected128)
+				i := &Int{}
+				actual, err := i.SetString(test.textInput)
+				if err != nil {
+					if test.wantedErr {
+						return
+					}
+					t.Errorf("failed to set string %s on &Int{}", test.textInput)
+				}
+				if !actual.Equal(test.expected) {
+					t.Errorf("got %v, expected=%v", actual, test.expected)
 				}
 			})
 		}
