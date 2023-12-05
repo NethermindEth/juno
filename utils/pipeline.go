1ddefe31d2d6d025edc223ccdeb66a298af47451
Kirill <paltsev.kir@gmail.com>
2023-12-05 15:22:12 +0300

    Add PriorityQueue to pipelines

--- utils/pipeline.go
+++ utils/pipeline.go
@@ -1,5 +1,44 @@
 package utils
 
+func PriorityQueue[T any](highPriority, lowPriority <-chan T) <-chan T {
+	out := make(chan T)
+
+	go func() {
+		defer close(out)
+
+		for highPriority != nil || lowPriority != nil {
+			// first we always check highPriority channel for data
+			select {
+			case v, ok := <-highPriority:
+				if ok {
+					out <- v
+				} else {
+					highPriority = nil
+				}
+			default:
+			}
+
+			// order of cases in select stmt doesn't guarantee processing order
+			select {
+			case v, ok := <-highPriority:
+				if ok {
+					out <- v
+				} else {
+					highPriority = nil
+				}
+			case v, ok := <-lowPriority:
+				if ok {
+					out <- v
+				} else {
+					lowPriority = nil
+				}
+			}
+		}
+	}()
+
+	return out
+}
+
 func Pipeline[From any, To any](in <-chan From, f func(From) To) <-chan To {
 	out := make(chan To)
 
