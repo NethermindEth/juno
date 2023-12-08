bc4fd4d7325c00ba14f7f9f0f341699d960f06eb
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-12-08 14:46:35 +0300

    Add barebones builder with mempool validation facilities

---
+++ builder/builder_test.go
@@ -0,0 +1,63 @@
+package builder_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/NethermindEth/juno/blockchain"
+	"github.com/NethermindEth/juno/builder"
+	"github.com/NethermindEth/juno/clients/feeder"
+	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/db/pebble"
+	"github.com/NethermindEth/juno/mempool"
+	"github.com/NethermindEth/juno/mocks"
+	adaptfeeder "github.com/NethermindEth/juno/starknetdata/feeder"
+	"github.com/NethermindEth/juno/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestValidateAgainstPendingState(t *testing.T) {
+	testDB := pebble.NewMemTest(t)
+	mockCtrl := gomock.NewController(t)
+	mockVM := mocks.NewMockVM(mockCtrl)
+	bc := blockchain.New(testDB, utils.Integration, utils.NewNopZapLogger())
+	seqAddr := utils.HexToFelt(t, "0xDEADBEEF")
+	testBuilder := builder.New(seqAddr, bc, mockVM, utils.NewNopZapLogger())
+
+	client := feeder.NewTestClient(t, utils.Integration)
+	gw := adaptfeeder.New(client)
+
+	su, b, err := gw.StateUpdateWithBlock(context.Background(), 0)
+	require.NoError(t, err)
+
+	require.NoError(t, bc.StorePending(&blockchain.Pending{
+		Block:       b,
+		StateUpdate: su,
+	}))
+
+	userTxn := mempool.BroadcastedTransaction{
+		Transaction: &core.InvokeTransaction{
+			TransactionHash: utils.HexToFelt(t, "0x1337"),
+		},
+		DeclaredClass: &core.Cairo0Class{
+			Program: "best program",
+		},
+	}
+
+	mockVM.EXPECT().Execute([]core.Transaction{userTxn.Transaction},
+		[]core.Class{userTxn.DeclaredClass}, uint64(0), b.Timestamp, seqAddr,
+		gomock.Any(), utils.Integration, []*felt.Felt{}, false, false,
+		false, b.GasPrice, b.GasPriceSTRK, false).Return(nil, nil, nil)
+	assert.NoError(t, testBuilder.ValidateAgainstPendingState(&userTxn))
+
+	require.NoError(t, bc.Store(b, &core.BlockCommitments{}, su, nil))
+	mockVM.EXPECT().Execute([]core.Transaction{userTxn.Transaction},
+		[]core.Class{userTxn.DeclaredClass}, uint64(1), b.Timestamp+1, seqAddr,
+		gomock.Any(), utils.Integration, []*felt.Felt{}, false, false,
+		false, b.GasPrice, b.GasPriceSTRK, false).Return(nil, nil, errors.New("oops"))
+	assert.EqualError(t, testBuilder.ValidateAgainstPendingState(&userTxn), "oops")
+}
