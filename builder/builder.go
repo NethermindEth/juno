34bf9637e7e66bf82e9af2b05338d8115f76223c
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-12-08 14:46:35 +0300

    Add barebones builder with mempool validation facilities

---
+++ builder/builder.go
@@ -0,0 +1,68 @@
+package builder
+
+import (
+	"errors"
+
+	"github.com/NethermindEth/juno/blockchain"
+	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/db"
+	"github.com/NethermindEth/juno/mempool"
+	"github.com/NethermindEth/juno/utils"
+	"github.com/NethermindEth/juno/vm"
+)
+
+type Builder struct {
+	ownAddress felt.Felt
+
+	bc *blockchain.Blockchain
+	vm vm.VM
+
+	log utils.Logger
+}
+
+func New(ownAddr *felt.Felt, bc *blockchain.Blockchain, builderVM vm.VM, log utils.Logger) *Builder {
+	return &Builder{
+		ownAddress: *ownAddr,
+
+		bc: bc,
+		vm: builderVM,
+	}
+}
+
+// ValidateAgainstPendingState validates a user transaction against the pending state
+// only hard-failures result in an error, reverts are not reported back to caller
+func (b *Builder) ValidateAgainstPendingState(userTxn *mempool.BroadcastedTransaction) error {
+	declaredClasses := []core.Class{}
+	if userTxn.DeclaredClass != nil {
+		declaredClasses = []core.Class{userTxn.DeclaredClass}
+	}
+
+	nextHeight := uint64(0)
+	if height, err := b.bc.Height(); err == nil {
+		nextHeight = height + 1
+	} else if err != nil && !errors.Is(err, db.ErrKeyNotFound) {
+		return err
+	}
+
+	pendingBlock, err := b.bc.Pending()
+	if err != nil {
+		return err
+	}
+
+	state, stateCloser, err := b.bc.PendingState()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err = stateCloser(); err != nil {
+			b.log.Errorw("closing state in ValidateAgainstPendingState", "err", err)
+		}
+	}()
+
+	_, _, err = b.vm.Execute([]core.Transaction{userTxn.Transaction}, declaredClasses, nextHeight,
+		pendingBlock.Block.Timestamp, &b.ownAddress, state, b.bc.Network(), []*felt.Felt{},
+		false, false, false, pendingBlock.Block.GasPrice, pendingBlock.Block.GasPriceSTRK, false)
+	return err
+}
