7c341a679daa841c3c1213ae6ffb6dc2c02581d8
Josh Klopfenstein <git@joshklop.com>
2023-12-12 07:19:11 -0600

    Update clients/feeder/event_listener.go
    Co-authored-by: Ömer Faruk Irmak <omerfirmak@gmail.com>

--- clients/feeder/event_listener.go
+++ clients/feeder/event_listener.go
@@ -10,7 +10,7 @@ type SelectiveListener struct {
 	OnResponseCb func(urlPath string, status int, took time.Duration)
 }
 
-func (l SelectiveListener) OnResponse(urlPath string, status int, took time.Duration) {
+func (l *SelectiveListener) OnResponse(urlPath string, status int, took time.Duration) {
 	if l.OnResponseCb != nil {
 		l.OnResponseCb(urlPath, status, took)
 	}
