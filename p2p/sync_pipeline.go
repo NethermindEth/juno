33eec20f680fda29d62622eb2bcfb7d7445d7a82
IronGauntlets <aneequesafdar@gmail.com>
2023-12-15 21:52:33 +0000

    Add genBlockBodies()

--- p2p/sync_pipeline.go
+++ p2p/sync_pipeline.go
@@ -3,6 +3,8 @@ package p2p
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync"
 
 	"github.com/NethermindEth/juno/adapters/p2p2core"
 	"github.com/NethermindEth/juno/core"
@@ -10,7 +12,6 @@ import (
 	"github.com/NethermindEth/juno/p2p/starknet"
 	"github.com/NethermindEth/juno/p2p/starknet/spec"
 	"github.com/NethermindEth/juno/utils"
-	"github.com/davecgh/go-spew/spew"
 )
 
 func (s *syncService) startPipeline(ctx context.Context) {
@@ -30,14 +31,34 @@ func (s *syncService) startPipeline(ctx context.Context) {
 		s.log.Errorw("Failed to get current height", "err", err)
 	}
 
-	headersAndSigsCh, err := s.genBlockHeadersAndSigs(ctx, s.createIterator(BlockRange{nextHeight, bootNodeHeight}))
+	commonIt := s.createIterator(BlockRange{nextHeight, bootNodeHeight})
+	headersAndSigsCh, err := s.genBlockHeadersAndSigs(ctx, commonIt)
 	if err != nil {
 		s.log.Errorw("Failed to get block headers parts", "err", err)
 	}
 
-	for h := range utils.PipelineStage(ctx, headersAndSigsCh, adaptBlockHeadersAndSigs) {
-		spew.Dump(h)
-	}
+	blockBodiesCh, err := s.genBlockBodies(ctx, commonIt)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	//for h := range utils.PipelineStage(ctx, headersAndSigsCh, adaptBlockHeadersAndSigs) {
+	//	spew.Dump(h)
+	//}
+
+	go func() {
+		defer wg.Done()
+		for h := range headersAndSigsCh {
+			fmt.Println("Got Spec Block Header and Signatures:", "number:", h.header.Number)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for b := range blockBodiesCh {
+			fmt.Println("Got Spec Block Body parts:", "number:", b.id.Number)
+		}
+	}()
+	wg.Wait()
 }
 
 type blockHeaderAndSigs struct {
@@ -84,4 +105,45 @@ func adaptBlockHeadersAndSigs(headerAndSig blockHeaderAndSigs) core.Header {
 	header := p2p2core.AdaptBlockHeader(headerAndSig.header)
 	header.Signatures = utils.Map(headerAndSig.sig.GetSignatures(), p2p2core.AdaptSignature)
 	return header
+}
+
+func (s *syncService) genBlockBodies(ctx context.Context, it *spec.Iteration) (<-chan specBlockBody, error) {
+	blockIt, err := s.client.RequestBlockBodies(ctx, &spec.BlockBodiesRequest{Iteration: it})
+	if err != nil {
+		return nil, err
+	}
+
+	specBodiesCh := make(chan specBlockBody)
+	go func() {
+		defer close(specBodiesCh)
+		curBlockBody := new(specBlockBody)
+		// Assumes that all parts of the same block will arrive before the next block parts
+		// Todo: the above assumption may not be true. A peer may decide to send different parts of the block in different order
+		for res, valid := blockIt(); valid; res, valid = blockIt() {
+			switch res.BodyMessage.(type) {
+			case *spec.BlockBodiesResponse_Classes:
+				if curBlockBody.id == nil {
+					curBlockBody.id = res.GetId()
+				}
+				curBlockBody.classes = res.GetClasses()
+			case *spec.BlockBodiesResponse_Diff:
+				if curBlockBody.id == nil {
+					curBlockBody.id = res.GetId()
+				}
+				curBlockBody.stateDiff = res.GetDiff()
+			case *spec.BlockBodiesResponse_Proof:
+				if curBlockBody.id == nil {
+					curBlockBody.id = res.GetId()
+				}
+				curBlockBody.proof = res.GetProof()
+			case *spec.BlockBodiesResponse_Fin:
+				if curBlockBody.id != nil {
+					specBodiesCh <- *curBlockBody
+					curBlockBody = new(specBlockBody)
+				}
+			}
+		}
+	}()
+
+	return specBodiesCh, nil
 }
