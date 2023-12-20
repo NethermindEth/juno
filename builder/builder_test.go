7b4700bc705616d5d2023f11d2f2124211f1cba4
rianhughes <rian.hughes@physics.ox.ac.uk>
2023-12-20 14:13:16 +0200

    selector is correct

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -93,14 +93,14 @@ func TestGenesisStateDiff(t *testing.T) {
 	t.Run("valid non-empty genesis config", func(t *testing.T) {
 		accountClassHash, err := new(felt.Felt).SetString("0x04d07e40e93398ed3c76981e72dd1fd22557a78ce36c0515f679e27f0bb5bc5f")
 		require.NoError(t, err)
-		erc20ClassHash, err := new(felt.Felt).SetString("0x02a8846878b6ad1f54f6ba46f5f40e11cee755c677f130b2c4b60566c9003f1f")
-		require.NoError(t, err)
+		// erc20ClassHash, err := new(felt.Felt).SetString("0x02a8846878b6ad1f54f6ba46f5f40e11cee755c677f130b2c4b60566c9003f1f")
+		// require.NoError(t, err)
 
-		erc20Name := *new(felt.Felt).SetUint64(111)
-		erc20Symbol := *new(felt.Felt).SetUint64(222)
-		erc20Decimal := *new(felt.Felt).SetUint64(333)
-		erc20InitSup := *new(felt.Felt).SetUint64(444)
-		erc20Recipient := *new(felt.Felt).SetUint64(555)
+		// erc20Name := *new(felt.Felt).SetUint64(111)
+		// erc20Symbol := *new(felt.Felt).SetUint64(222)
+		// erc20Decimal := *new(felt.Felt).SetUint64(333)
+		// erc20InitSup := *new(felt.Felt).SetUint64(444)
+		// erc20Recipient := *new(felt.Felt).SetUint64(555)
 
 		genesisConfig := builder.GenesisConfig{
 			ChainID: network.ChainIDString(),
@@ -114,16 +114,16 @@ func TestGenesisStateDiff(t *testing.T) {
 					ClassHash:       *accountClassHash,
 					ConstructorArgs: []felt.Felt{*new(felt.Felt).SetUint64(222)},
 				},
-				"0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7": { // fee token address
-					ClassHash: *erc20ClassHash,
-					ConstructorArgs: []felt.Felt{
-						erc20Name,
-						erc20Symbol,
-						erc20Decimal,
-						erc20InitSup,
-						erc20Recipient,
-					},
-				},
+				// "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7": { // fee token address
+				// 	ClassHash: *erc20ClassHash,
+				// 	ConstructorArgs: []felt.Felt{
+				// 		erc20Name,
+				// 		erc20Symbol,
+				// 		erc20Decimal,
+				// 		erc20InitSup,
+				// 		erc20Recipient,
+				// 	},
+				// },
 			},
 			// FunctionCalls: []builder.FunctionCall{
 			// 	{
