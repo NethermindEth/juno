a2ec11766a47dcfa2d98429da5473acfd93dc938
rianhughes <rian.hughes@physics.ox.ac.uk>
2023-12-20 16:08:12 +0200

    wip - test fncalls

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -93,44 +93,31 @@ func TestGenesisStateDiff(t *testing.T) {
 	t.Run("valid non-empty genesis config", func(t *testing.T) {
 		accountClassHash, err := new(felt.Felt).SetString("0x04d07e40e93398ed3c76981e72dd1fd22557a78ce36c0515f679e27f0bb5bc5f")
 		require.NoError(t, err)
-		// erc20ClassHash, err := new(felt.Felt).SetString("0x02a8846878b6ad1f54f6ba46f5f40e11cee755c677f130b2c4b60566c9003f1f")
-		// require.NoError(t, err)
 
-		// erc20Name := *new(felt.Felt).SetUint64(111)
-		// erc20Symbol := *new(felt.Felt).SetUint64(222)
-		// erc20Decimal := *new(felt.Felt).SetUint64(333)
-		// erc20InitSup := *new(felt.Felt).SetUint64(444)
-		// erc20Recipient := *new(felt.Felt).SetUint64(555)
+		accountAddress, err := new(felt.Felt).SetString("0xdeadbeef")
+		require.NoError(t, err)
+
+		setPubKeySelector, err := new(felt.Felt).SetString("0x00bc0eb87884ab91e330445c3584a50d7ddf4b568f02fbeb456a6242cce3f5d9")
+		require.NoError(t, err)
 
 		genesisConfig := builder.GenesisConfig{
 			ChainID: network.ChainIDString(),
 			Classes: []string{
 				"./contracts/account.json",
-				"./contracts/erc20.json",
 			},
 			Contracts: map[string]builder.GenesisContractData{
-				"0xdeadbeef": { // account address // todo: needs to match what the constructor generates?
+				accountAddress.String(): {
 					ClassHash:       *accountClassHash,
 					ConstructorArgs: []felt.Felt{*new(felt.Felt).SetUint64(222)},
 				},
-				// "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7": { // fee token address
-				// 	ClassHash: *erc20ClassHash,
-				// 	ConstructorArgs: []felt.Felt{
-				// 		erc20Name,
-				// 		erc20Symbol,
-				// 		erc20Decimal,
-				// 		erc20InitSup,
-				// 		erc20Recipient,
-				// 	},
-				// },
 			},
-			// FunctionCalls: []builder.FunctionCall{
-			// 	{
-			// 		ContractAddress:    felt.Zero,
-			// 		EntryPointSelector: felt.Zero,
-			// 		Calldata:           []felt.Felt{},
-			// 	},
-			// },
+			FunctionCalls: []builder.FunctionCall{
+				{
+					ContractAddress:    *accountAddress,
+					EntryPointSelector: *setPubKeySelector,
+					Calldata:           []felt.Felt{*new(felt.Felt).SetUint64(123)},
+				},
+			},
 		}
 
 		resp, err := b.GenesisStateDiff(genesisConfig)
