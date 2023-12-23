3bf51ca6445bd31127acaab289db75c27df3ec87
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-12-23 13:17:59 +0300

    simplify

--- rpc/handlers.go
+++ rpc/handlers.go
@@ -1479,14 +1479,11 @@ func (h *Handler) simulateTransactions(id BlockID, transactions []BroadcastedTra
 	overallFees, traces, err := h.vm.Execute(txns, classes, blockNumber, header.Timestamp, sequencerAddress,
 		state, h.network, paidFeesOnL1, skipFeeCharge, skipValidate, errOnRevert, header.GasPrice, header.GasPriceSTRK, legacyTraceJSON)
 	if err != nil {
-		if isVMInternalErr(err) {
-			return nil, ErrInternal.CloneWithData(err.Error())
-		}
 		var txnExecutionError vm.TransactionExecutionError
 		if errors.As(err, &txnExecutionError) {
 			return nil, makeTransactionExecutionError(&txnExecutionError)
 		}
-		return nil, ErrUnexpectedError.CloneWithData(err.Error())
+		return nil, ErrInternal.CloneWithData(err.Error())
 	}
 
 	var result []SimulatedTransaction
@@ -1516,10 +1513,6 @@ func (h *Handler) simulateTransactions(id BlockID, transactions []BroadcastedTra
 	}
 
 	return result, nil
-}
-
-func isVMInternalErr(err error) bool {
-	return errors.Is(err, utils.ErrResourceBusy) || errors.Is(err, vm.ErrInternal)
 }
 
 func (h *Handler) TraceBlockTransactions(ctx context.Context, id BlockID) ([]TracedBlockTransaction, *jsonrpc.Error) {
@@ -1633,12 +1626,9 @@ func (h *Handler) traceBlockTransactions(ctx context.Context, block *core.Block,
 	_, traces, err := h.vm.Execute(block.Transactions, classes, blockNumber, block.Header.Timestamp,
 		sequencerAddress, state, h.network, paidFeesOnL1, false, false, false, block.Header.GasPrice, block.Header.GasPriceSTRK, legacyJSON)
 	if err != nil {
-		if isVMInternalErr(err) {
-			return nil, ErrInternal.CloneWithData(err.Error())
-		}
 		// Since we are tracing an existing block, we know that there should be no errors during execution. If we encounter any,
-		// report them as unexpected errors
-		return nil, ErrUnexpectedError.CloneWithData(err.Error())
+		// report them as internal errors
+		return nil, ErrInternal.CloneWithData(err.Error())
 	}
 
 	var result []TracedBlockTransaction
