d1f664cfea4e0a169bca12c58bcaf92d09da3229
Kanan Bayramov <kanan@nethermind.io>
2024-04-13 01:02:57 +0900

    new updates

--- p2p/sync.go
+++ p2p/sync.go
@@ -101,7 +101,7 @@ func (s *syncService) start(ctx context.Context) {
 		}
 
 		var nextHeight int
-		if curHeight, err := s.blockchain.Height(); err == nil { //nolint:govet
+		if curHeight, err := s.blockchain.Height(); err == nil {
 			nextHeight = int(curHeight) + 1
 		} else if !errors.Is(db.ErrKeyNotFound, err) {
 			s.log.Errorw("Failed to get current height", "err", err)
