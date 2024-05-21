8f9fee002d0a97275072e6a19654f85f90b09646
Osakpolor Obaseki <obasekiosa@gmail.com>
2024-05-21 17:05:10 +0100

    Add gossiper, proposer and decider interfaces

--- consensus/tendermint/message.go
+++ consensus/tendermint/message.go
@@ -2,3 +2,23 @@ package tendermint
 
 type Message struct {
 }
+
+type Gossiper interface {
+	SubmitMessageForBroadcast(msg interface{})   // adds a msg to be broadcast to the queue
+	SubmitMessage(msg interface{})               // takes a msg to be broadcast off the queue
+	ReceiveMessage() interface{}                 // takes a msg received off the queue
+	ReceiveMessageFromBroadcast(msg interface{}) // adds a msg received onto the queue
+	ClearAll()
+	ClearSubmit()
+	ClearReceive()
+}
+
+type Proposer interface {
+	Proposer() interface{}
+	IsProposer() uint8 // 000 no, 1 yes 2 unknown
+}
+
+type Decider interface {
+	SubmitDecision(decision interface{})
+	GetDecision(params map[string]interface{}) interface{}
+}
