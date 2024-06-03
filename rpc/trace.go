9c99d67be956cc826559f2daf700587265a81c34
Wojciech Zieba <wojciech.zieba@gmail.com>
2024-06-03 11:42:45 +0200

    Update trace fallback to v0.13.1

--- rpc/trace.go
+++ rpc/trace.go
@@ -15,7 +15,8 @@ import (
 	"github.com/NethermindEth/juno/vm"
 )
 
-var traceFallbackVersion = semver.MustParse("0.12.3")
+var traceFallbackVersion = semver.MustParse("0.13.1")
+var excludedVersion = "0.13.1.1"
 
 func adaptBlockTrace(block *BlockWithTxs, blockTrace *starknet.BlockTrace) ([]TracedBlockTransaction, error) {
 	if blockTrace == nil {
@@ -198,8 +199,8 @@ func (h *Handler) traceBlockTransactions(ctx context.Context, block *core.Block,
 	if !isPending {
 		if blockVer, err := core.ParseBlockVersion(block.ProtocolVersion); err != nil {
 			return nil, ErrUnexpectedError.CloneWithData(err.Error())
-		} else if blockVer.Compare(traceFallbackVersion) != 1 || h.forceFeederTracesForBlocks.Contains(block.Number) {
-			// version <= 0.12.3 or forcing fetch some blocks from feeder gateway
+		} else if (blockVer.Compare(traceFallbackVersion) != 1 && block.ProtocolVersion != excludedVersion) || h.forceFeederTracesForBlocks.Contains(block.Number) {
+			// version <= 0.13.1 and not 0.13.1.1 or forcing fetch some blocks from feeder gateway
 			return h.fetchTraces(ctx, block.Hash)
 		}
 
