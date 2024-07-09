a368e04d52fde99326d17252e97a9a7d4d7d8f95
Artem Streltsov <artem@streltsov.dev>
2024-07-10 00:07:03 +0500

    add nilerr linter

--- cmd/juno/juno.go
+++ cmd/juno/juno.go
@@ -248,7 +248,7 @@ func NewCmd(config *node.Config, run func(*cobra.Command, []string) error) *cobr
 		v.SetEnvPrefix("JUNO")
 		v.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
 		if err := v.BindPFlags(cmd.Flags()); err != nil {
-			return nil
+			return err
 		}
 
 		// TextUnmarshallerHookFunc allows us to unmarshal values that satisfy the
