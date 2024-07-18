e9f9a32e2443bc93ff1e153c1fd8edaea1d6078e
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-18 15:18:35 +0300

    fuzz test for map func

--- utils/slices_test.go
+++ utils/slices_test.go
@@ -1,23 +1,24 @@
-package utils
+package utils_test
 
 import (
 	"strconv"
 	"testing"
 
+	"github.com/NethermindEth/juno/utils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMap(t *testing.T) {
 	t.Run("nil slice", func(t *testing.T) {
 		var input []int
-		actual := Map(input, strconv.Itoa)
+		actual := utils.Map(input, strconv.Itoa)
 		assert.Nil(t, actual)
 	})
 	t.Run("slice with data", func(t *testing.T) {
 		input := []int{1, 2, 3, 4, 5, 6}
 		expected := []string{"1", "2", "3", "4", "5", "6"}
 
-		strings := Map(input, strconv.Itoa)
+		strings := utils.Map(input, strconv.Itoa)
 		assert.Equal(t, expected, strings)
 	})
 }
@@ -25,12 +26,12 @@ func TestMap(t *testing.T) {
 func TestFilter(t *testing.T) {
 	t.Run("nil slice", func(t *testing.T) {
 		var input []int
-		actual := Filter(input, func(int) bool { return false })
+		actual := utils.Filter(input, func(int) bool { return false })
 		assert.Nil(t, actual)
 	})
 	t.Run("filter some elements", func(t *testing.T) {
 		input := []int{1, 2, 3, 4, 5, 6}
-		actual := Filter(input, func(v int) bool { return v%2 == 0 })
+		actual := utils.Filter(input, func(v int) bool { return v%2 == 0 })
 		assert.Equal(t, []int{2, 4, 6}, actual)
 	})
 }
@@ -38,21 +39,21 @@ func TestFilter(t *testing.T) {
 func TestAll(t *testing.T) {
 	t.Run("nil slice", func(t *testing.T) {
 		var input []int
-		allValue := All(input, func(int) bool {
+		allValue := utils.All(input, func(int) bool {
 			return false
 		})
 		assert.True(t, allValue)
 	})
 	t.Run("no element matches the predicate", func(t *testing.T) {
 		input := []int{1, 2, 3, 4}
-		allEven := All(input, func(v int) bool {
+		allEven := utils.All(input, func(v int) bool {
 			return v%2 == 0
 		})
 		assert.False(t, allEven)
 	})
 	t.Run("all elements match the predicate", func(t *testing.T) {
 		input := []int{1, 3, 5, 7}
-		allOdd := All(input, func(v int) bool {
+		allOdd := utils.All(input, func(v int) bool {
 			return v%2 != 0
 		})
 		assert.True(t, allOdd)
