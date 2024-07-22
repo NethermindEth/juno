fa001558dded8a004852a7e5056f562cf7760bef
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-07-22 13:13:16 +0300

    FuzzPermutate added

--- core/crypto/poseidon_hash_pkg_test.go
+++ core/crypto/poseidon_hash_pkg_test.go
@@ -15,3 +15,15 @@ func TestPermutate(t *testing.T) {
 	assert.Equal(t, "1590252087433376791875644726012779423683501236913937337746052470473806035332", state[1].Text(10))
 	assert.Equal(t, "867921192302518434283879514999422690776342565400001269945778456016268852423", state[2].Text(10))
 }
+
+func FuzzPermutate(f *testing.F) {
+	f.Fuzz(func(t *testing.T, bytes1, bytes2, bytes3 []byte) {
+		state := []felt.Felt{
+			*new(felt.Felt).SetBytes(bytes1),
+			*new(felt.Felt).SetBytes(bytes2),
+			*new(felt.Felt).SetBytes(bytes3),
+		}
+
+		HadesPermutation(state)
+	})
+}
