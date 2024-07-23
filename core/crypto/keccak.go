f48be088146911fd52997933f19112db695d28d9
Kirill <paltsev.kir@gmail.com>
2024-07-23 21:24:16 +0300

    Remove returned error from crypto.StarknetKeccak function (#1963)

--- core/crypto/keccak.go
+++ core/crypto/keccak.go
@@ -1,6 +1,8 @@
 package crypto
 
 import (
+	"fmt"
+
 	"github.com/NethermindEth/juno/core/felt"
 	"golang.org/x/crypto/sha3"
 )
@@ -8,14 +10,16 @@ import (
 // StarknetKeccak implements [Starknet keccak]
 //
 // [Starknet keccak]: https://docs.starknet.io/architecture-and-concepts/cryptography/hash-functions/#starknet_keccak
-func StarknetKeccak(b []byte) (*felt.Felt, error) {
+func StarknetKeccak(b []byte) *felt.Felt {
 	h := sha3.NewLegacyKeccak256()
 	_, err := h.Write(b)
 	if err != nil {
-		return nil, err
+		// actual implementation (sha3.state{} type) doesn't return error in Write method
+		// we keep this panic as an assertion in case they will modify the implementation
+		panic(fmt.Errorf("failed to write to LegacyKeccak256 hash: %w", err))
 	}
 	d := h.Sum(nil)
 	// Remove the first 6 bits from the first byte
 	d[0] &= 3
-	return new(felt.Felt).SetBytes(d), nil
+	return new(felt.Felt).SetBytes(d)
 }
