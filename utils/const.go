9abf0495e9e6b535c4910a1fcb3c712023dfd5ae
weiihann <weihan774237@gmail.com>
2024-08-05 20:05:54 +0800

    Use constants for data sizes

--- utils/const.go
+++ utils/const.go
@@ -1,5 +1,8 @@
 package utils
 
 const (
+	KiloByte = 1024
 	Megabyte = 1024 * 1024
+	Gigabyte = 1024 * 1024 * 1024
+	Terabyte = 1024 * 1024 * 1024 * 1024
 )
--- utils/size.go
+++ utils/size.go
@@ -9,14 +9,14 @@ type DataSize float64
 //nolint:mnd
 func (d DataSize) String() string {
 	switch {
-	case d >= 1099511627776:
-		return fmt.Sprintf("%.2f TiB", d/1099511627776)
-	case d >= 1073741824:
-		return fmt.Sprintf("%.2f GiB", d/1073741824)
-	case d >= 1048576:
-		return fmt.Sprintf("%.2f MiB", d/1048576)
-	case d >= 1024:
-		return fmt.Sprintf("%.2f KiB", d/1024)
+	case d >= Terabyte:
+		return fmt.Sprintf("%.2f TiB", d/Terabyte)
+	case d >= Gigabyte:
+		return fmt.Sprintf("%.2f GiB", d/Gigabyte)
+	case d >= Megabyte:
+		return fmt.Sprintf("%.2f MiB", d/Megabyte)
+	case d >= KiloByte:
+		return fmt.Sprintf("%.2f KiB", d/KiloByte)
 	}
 	return fmt.Sprintf("%.2f B", d)
 }
