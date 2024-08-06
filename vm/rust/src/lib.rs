03c39d939a09918a1090d8a139f58f29ef5c10cf
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-06 16:53:46 +0300

    Refactor class_hash assignment in cairoVMCall function

--- vm/rust/src/lib.rs
+++ vm/rust/src/lib.rs
@@ -126,7 +126,7 @@ pub extern "C" fn cairoVMCall(
         calldata: Calldata(calldata_vec.into()),
         storage_address: contract_addr_felt.try_into().unwrap(),
         call_type: CallType::Call,
-        class_hash: class_hash,
+        class_hash,
         code_address: None,
         caller_address: ContractAddress::default(),
         initial_gas: get_versioned_constants(block_info.version).tx_initial_gas(),
