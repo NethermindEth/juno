6806ecdd7cec2d47e3baf08ca3bc97a6710220f4
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-06 17:12:09 +0300

    Refactor extern "C" functions in lib.rs

--- starknet/rust/src/lib.rs
+++ starknet/rust/src/lib.rs
@@ -1,27 +1,28 @@
-use std::ffi::{c_char, CStr, CString};
 use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
+use std::ffi::{c_char, CStr, CString};
 
 #[no_mangle]
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
 pub extern "C" fn compileSierraToCasm(sierra_json: *const c_char) -> *mut c_char {
     let sierra_json_str = unsafe { CStr::from_ptr(sierra_json) }.to_str().unwrap();
 
-    let sierra_class =
-        serde_json::from_str(sierra_json_str).map_err(|err| err.to_string());
+    let sierra_class = serde_json::from_str(sierra_json_str).map_err(|err| err.to_string());
     if let Err(e) = sierra_class {
-        return raw_cstr(e)
+        return raw_cstr(e);
     }
 
-    let casm_class = CasmContractClass::from_contract_class(sierra_class.unwrap(), true, usize::MAX)
-        .map_err(|err| err.to_string());
+    let casm_class =
+        CasmContractClass::from_contract_class(sierra_class.unwrap(), true, usize::MAX)
+            .map_err(|err| err.to_string());
     if let Err(e) = casm_class {
-        return raw_cstr(e)
+        return raw_cstr(e);
     }
 
     let casm_json = serde_json::to_string(&casm_class.unwrap());
     if let Err(e) = casm_json {
-        return raw_cstr(e.to_string())
+        return raw_cstr(e.to_string());
     }
-    return raw_cstr(casm_json.unwrap())
+    return raw_cstr(casm_json.unwrap());
 }
 
 fn raw_cstr(str: String) -> *mut c_char {
@@ -29,6 +30,7 @@ fn raw_cstr(str: String) -> *mut c_char {
 }
 
 #[no_mangle]
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
 pub extern "C" fn freeCstr(ptr: *mut c_char) {
     unsafe {
         if ptr.is_null() {
