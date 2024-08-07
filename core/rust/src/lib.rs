32fd743c774ec11a1bb2ce3dceecb57515f4873e
Daniil Ankushin <ankushin.daniil42@gmail.com>
2024-08-07 22:56:21 +0300

    Improve error handling in Rust (#2029)

--- core/rust/src/lib.rs
+++ core/rust/src/lib.rs
@@ -8,14 +8,18 @@ use std::{
 pub extern "C" fn Cairo0ClassHash(class_json_str: *const c_char, hash: *mut c_uchar) {
     let class_json = unsafe { CStr::from_ptr(class_json_str) }.to_str().unwrap();
     let class: Result<LegacyContractClass, serde_json::Error> = serde_json::from_str(class_json);
-    if class.is_err() {
-        return;
-    }
-    let class_hash = class.unwrap().class_hash();
-    if class_hash.is_err() {
-        return;
-    }
+
+    let class = match class {
+        Ok(c) => c,
+        Err(_) => return,
+    };
+
+    let class_hash = match class.class_hash() {
+        Ok(h) => h,
+        Err(_) => return,
+    };
+
     let hash_slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(hash, 32) };
-    let hash_bytes = class_hash.unwrap().to_bytes_be();
+    let hash_bytes = class_hash.to_bytes_be();
     hash_slice.copy_from_slice(hash_bytes.as_slice());
 }
