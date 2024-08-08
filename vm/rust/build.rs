d2dcf50a477fb0725bc1f99ba5b185f7721b38a9
Xander van der Goot <xandervandergoot@gmail.com>
2024-08-08 15:56:44 +0800

    chore: clean up build.rs

--- vm/rust/build.rs
+++ vm/rust/build.rs
@@ -7,26 +7,21 @@ use toml::{self, Table};
 fn main() {
     let cairo_native = "cairo-native";
 
-    // Can you have globs?
     println!("cargo::rerun-if-changed=./Cargo.toml");
     println!("cargo::rerun-if-changed=./Cargo.lock");
     let config = fs::read_to_string("./Cargo.toml").unwrap();
     let t: Table = toml::from_str(&config).unwrap();
-    let b = t
+    let cairo_native_toml_table = t
         .get("dependencies")
-        .expect("No dependencies found") // TODO(xrvdg) see TOML on how this section should be called
+        .expect("No dependencies found")
         .get(cairo_native)
-        .expect("dependency {cairo_native} not found"); // TODO(xrvdg) how to create a format message?
+        .expect("dependency cairo_native not found");
 
-    // Both of these are error that should not happen
-    // because if this doesn't exist it also doesn't exist in the lock file
+    // Dependency specified via path?
+    let maybe_path = cairo_native_toml_table
+        .get("path")
+        .and_then(|path| path.as_str());
 
-    // How do you chain the values? You can keep using and_then, but then you can't get give feedback over
-    // file not found
-
-    let maybe_path = b.get("path").and_then(|path| path.as_str());
-
-    // todo(xrvdg) Read TigerBeetle naming convention
     let lock_file = fs::read_to_string("./Cargo.lock").unwrap();
     let lock_table: Table = toml::from_str(&lock_file).unwrap();
     let package = lock_table.get("package").unwrap().as_array().unwrap();
@@ -37,17 +32,15 @@ fn main() {
         .as_table()
         .unwrap();
 
-    // checksum from if it comes from registry
+    // Dependency specified via registry?
     let maybe_checksum = cairo_native_lock_table
         .get("checksum")
         .and_then(|c| c.as_str());
 
-    // Should work for both unspecified revision and specified revision, at least for github links
-    // source is optional
-    // Todo(xrvdg) could probably use ? if I make it into a function
+    // Dependency specified via git?
     let maybe_rev = extract_rev_from_source(cairo_native_lock_table);
 
-    let rn: String = match (maybe_rev, maybe_path, maybe_checksum) {
+    let native_version: String = match (maybe_rev, maybe_path, maybe_checksum) {
         (Some(rev), _, _) => rev,
         (_, Some(path), _) => get_git_rev(path),
         (_, _, Some(checksum)) => checksum.to_string(),
@@ -55,23 +48,21 @@ fn main() {
     };
 
     // Set environment variable NATIVE_CACHE_VERSION
-    println!("cargo::rustc-env=NATIVE_VERSION={rn}");
+    println!("cargo::rustc-env=NATIVE_VERSION={native_version}");
 }
 
 fn get_git_rev(path: &str) -> String {
     let path = fs::canonicalize(path).unwrap();
-    let p = Command::new("git")
+    let out = Command::new("git")
         .args(["rev-parse", "HEAD"])
         .current_dir(path)
         .output();
-    // TODO(xrvdg) convert to expects
-    let out = String::from_utf8(p.unwrap().stdout);
-    out.unwrap()
+    let git_rev = String::from_utf8(out.unwrap().stdout);
+    git_rev.unwrap()
 }
 
+/// Extract the revision from git source
 fn extract_rev_from_source(lock_table: &toml::Table) -> Option<String> {
-    // We now swallow a parse error. This makes it a bit harder to debug if something goes wrong
-    // Can also add warning and use the panic that comes later
     let source = Url::parse(lock_table.get("source")?.as_str()?).ok()?;
     Some(String::from(
         source
