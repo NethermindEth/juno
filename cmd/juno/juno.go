e6c8cbeb025d6e4f4fb99a9db129c54e97230bac
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-20 11:55:53 +0300

    Rename downloadClasses to p2pDownloadClasses

--- cmd/juno/juno.go
+++ cmd/juno/juno.go
@@ -82,7 +82,7 @@ const (
 	callMaxStepsF           = "rpc-call-max-steps"
 	corsEnableF             = "rpc-cors-enable"
 	versionedConstantsFileF = "versioned-constants-file"
-	downloadClassesF        = "download-classes"
+	p2pDownloadClassesF     = "p2p-download-classes"
 
 	defaultConfig                   = ""
 	defaulHost                      = "localhost"
@@ -120,7 +120,7 @@ const (
 	defaultGwTimeout                = 5 * time.Second
 	defaultCorsEnable               = false
 	defaultVersionedConstantsFile   = ""
-	defaultDownloadClasses          = true
+	defaultP2pDownloadClasses       = true
 
 	configFlagUsage                       = "The YAML configuration file."
 	logLevelFlagUsage                     = "Options: trace, debug, info, warn, error."
@@ -153,26 +153,26 @@ const (
 		"These peers can be either Feeder or regular nodes."
 	p2pFeederNodeUsage = "EXPERIMENTAL: Run juno as a feeder node which will only sync from feeder gateway and gossip the new" +
 		" blocks to the network."
-	p2pPrivateKeyUsage   = "EXPERIMENTAL: Hexadecimal representation of a private key on the Ed25519 elliptic curve."
-	metricsUsage         = "Enables the Prometheus metrics endpoint on the default port."
-	metricsHostUsage     = "The interface on which the Prometheus endpoint will listen for requests."
-	metricsPortUsage     = "The port on which the Prometheus endpoint will listen for requests."
-	grpcUsage            = "Enable the HTTP gRPC server on the default port."
-	grpcHostUsage        = "The interface on which the gRPC server will listen for requests."
-	grpcPortUsage        = "The port on which the gRPC server will listen for requests."
-	maxVMsUsage          = "Maximum number for VM instances to be used for RPC calls concurrently"
-	maxVMQueueUsage      = "Maximum number for requests to queue after reaching max-vms before starting to reject incoming requests"
-	remoteDBUsage        = "gRPC URL of a remote Juno node"
-	rpcMaxBlockScanUsage = "Maximum number of blocks scanned in single starknet_getEvents call"
-	dbCacheSizeUsage     = "Determines the amount of memory (in megabytes) allocated for caching data in the database."
-	dbMaxHandlesUsage    = "A soft limit on the number of open files that can be used by the DB"
-	gwAPIKeyUsage        = "API key for gateway endpoints to avoid throttling" //nolint: gosec
-	gwTimeoutUsage       = "Timeout for requests made to the gateway"          //nolint: gosec
-	callMaxStepsUsage    = "Maximum number of steps to be executed in starknet_call requests. " +
+	p2pPrivateKeyUsage      = "EXPERIMENTAL: Hexadecimal representation of a private key on the Ed25519 elliptic curve."
+	p2pDownloadClassesUsage = "EXPERIMENTAL: Download classes from the gateway"
+	metricsUsage            = "Enables the Prometheus metrics endpoint on the default port."
+	metricsHostUsage        = "The interface on which the Prometheus endpoint will listen for requests."
+	metricsPortUsage        = "The port on which the Prometheus endpoint will listen for requests."
+	grpcUsage               = "Enable the HTTP gRPC server on the default port."
+	grpcHostUsage           = "The interface on which the gRPC server will listen for requests."
+	grpcPortUsage           = "The port on which the gRPC server will listen for requests."
+	maxVMsUsage             = "Maximum number for VM instances to be used for RPC calls concurrently"
+	maxVMQueueUsage         = "Maximum number for requests to queue after reaching max-vms before starting to reject incoming requests"
+	remoteDBUsage           = "gRPC URL of a remote Juno node"
+	rpcMaxBlockScanUsage    = "Maximum number of blocks scanned in single starknet_getEvents call"
+	dbCacheSizeUsage        = "Determines the amount of memory (in megabytes) allocated for caching data in the database."
+	dbMaxHandlesUsage       = "A soft limit on the number of open files that can be used by the DB"
+	gwAPIKeyUsage           = "API key for gateway endpoints to avoid throttling" //nolint: gosec
+	gwTimeoutUsage          = "Timeout for requests made to the gateway"          //nolint: gosec
+	callMaxStepsUsage       = "Maximum number of steps to be executed in starknet_call requests. " +
 		"The upper limit is 4 million steps, and any higher value will still be capped at 4 million."
 	corsEnableUsage             = "Enable CORS on RPC endpoints"
 	versionedConstantsFileUsage = "Use custom versioned constants from provided file"
-	downloadClassesUsage        = "Download classes from the gateway"
 )
 
 var Version string
@@ -358,7 +358,7 @@ func NewCmd(config *node.Config, run func(*cobra.Command, []string) error) *cobr
 	junoCmd.Flags().Bool(corsEnableF, defaultCorsEnable, corsEnableUsage)
 	junoCmd.Flags().String(versionedConstantsFileF, defaultVersionedConstantsFile, versionedConstantsFileUsage)
 	junoCmd.MarkFlagsMutuallyExclusive(p2pFeederNodeF, p2pPeersF)
-	junoCmd.Flags().Bool(downloadClassesF, defaultDownloadClasses, downloadClassesUsage)
+	junoCmd.Flags().Bool(p2pDownloadClassesF, defaultP2pDownloadClasses, p2pDownloadClassesUsage)
 
 	junoCmd.AddCommand(GenP2PKeyPair(), DBCmd(defaultDBPath))
 
