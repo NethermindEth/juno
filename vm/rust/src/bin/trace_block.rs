76ba31e379f1286d5b88ba56ded77c217ab2ba4c
Xander van der Goot <xandervandergoot@gmail.com>
2024-09-03 11:35:56 +0800

    concurrent tracing of blocks

--- vm/rust/src/bin/trace_block.rs
+++ vm/rust/src/bin/trace_block.rs
@@ -3,29 +3,28 @@ use std::time::{Duration, Instant};
 use clap::Parser;
 use starknet_core::types::BlockId;
 use starknet_providers::{jsonrpc::HttpTransport, JsonRpcClient, Provider, Url};
-use tokio;
+use tokio::{self, task::JoinSet};
 
 #[derive(Parser, Debug)]
 struct Args {
-    block: u64,
+    blocks: Vec<u64>,
     #[arg(short, long, default_value_t = str::to_string("http://localhost:6060/"))]
     url: String,
+    #[arg(default_value_t = 2)]
+    max_concurrent: usize,
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let args = Args::parse();
-
-    let client = JsonRpcClient::new(HttpTransport::new(Url::parse(&args.url).unwrap()));
-
-    let block_id = BlockId::Number(args.block);
-
+async fn trace_block(url: Url, block_id: BlockId) {
+    let client: JsonRpcClient<HttpTransport> = JsonRpcClient::new(HttpTransport::new(url));
     loop {
         let start = Instant::now();
         match client.trace_block_transactions(block_id).await {
             Ok(_a) => {
                 let elapsed = start.elapsed();
-                println!("finished in {} ", elapsed.as_secs());
+                println!(
+                    "Block {block_id:?} finished in {} seconds",
+                    elapsed.as_secs()
+                );
                 break;
             }
             Err(_) => {
@@ -35,4 +34,22 @@ async fn main() {
             }
         };
     }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let args = Args::parse();
+    let url = Url::parse(&args.url).unwrap();
+
+    let mut set = JoinSet::new();
+
+    for block in args.blocks {
+        let block_id = BlockId::Number(block);
+        while set.len() >= args.max_concurrent {
+            set.join_next().await;
+        }
+        set.spawn(trace_block(url.clone(), block_id));
+    }
+
+    while let Some(_output) = set.join_next().await {}
 }
