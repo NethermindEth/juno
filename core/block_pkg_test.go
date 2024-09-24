5d4db1d277dd90c3d02fc690a2d9c4ee0ad72b34
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-09-24 11:53:27 +0300

    Fix hashes overlap

--- core/block_pkg_test.go
+++ core/block_pkg_test.go
@@ -22,17 +22,17 @@ func TestTransactionCommitmentPoseidon(t *testing.T) {
 		// nil signature, empty signature and signature with some non-empty value
 		for i, sign := range []signature{nil, make(signature, 0), {new(felt.Felt).SetUint64(uint64(3))}} {
 			invoke := &InvokeTransaction{
-				TransactionHash:      new(felt.Felt).SetUint64(uint64(0 + i)),
+				TransactionHash:      new(felt.Felt).SetUint64(uint64(0 + i*3)),
 				TransactionSignature: sign,
 			}
 			deployAccount := &DeployAccountTransaction{
 				DeployTransaction: DeployTransaction{
-					TransactionHash: new(felt.Felt).SetUint64(uint64(1 + i)),
+					TransactionHash: new(felt.Felt).SetUint64(uint64(1 + i*3)),
 				},
 				TransactionSignature: sign,
 			}
 			declare := &DeclareTransaction{
-				TransactionHash:      new(felt.Felt).SetUint64(uint64(2 + i)),
+				TransactionHash:      new(felt.Felt).SetUint64(uint64(2 + i*3)),
 				TransactionSignature: sign,
 			}
 			txs = append(txs, invoke, deployAccount, declare)
@@ -40,7 +40,7 @@ func TestTransactionCommitmentPoseidon(t *testing.T) {
 
 		c, err := transactionCommitmentPoseidon(txs)
 		require.NoError(t, err)
-		expected := utils.HexToFelt(t, "0x4a802df8da3cfcaeb57991c076801a6a8a513345b8c213f649258949971f213")
+		expected := utils.HexToFelt(t, "0x602c33ad4fecd30bc8857e87554ae3a1b87dd090f93b4c5ffd5940e98cb712e")
 		assert.Equal(t, expected, c, "expected: %s, got: %s", expected, c)
 	})
 	t.Run("txs without signature", func(t *testing.T) {
