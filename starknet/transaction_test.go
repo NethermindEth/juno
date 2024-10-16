0dd096b8d71d2ea35ab61f3db9c9b188f1eb8f33
Kirill <paltsev.kir@gmail.com>
2024-10-16 15:30:12 +0400

    Update test case

--- starknet/transaction_test.go
+++ starknet/transaction_test.go
@@ -12,22 +12,33 @@ import (
 func TestUnmarshalExecutionStatus(t *testing.T) {
 	es := new(starknet.ExecutionStatus)
 
-	require.NoError(t, json.Unmarshal([]byte(`"SUCCEEDED"`), es))
-	assert.Equal(t, starknet.Succeeded, *es)
-
-	require.NoError(t, json.Unmarshal([]byte(`"REVERTED"`), es))
-	assert.Equal(t, starknet.Reverted, *es)
+	cases := map[string]starknet.ExecutionStatus{
+		"SUCCEEDED": starknet.Succeeded,
+		"REVERTED":  starknet.Reverted,
+	}
+	for str, expected := range cases {
+		quotedStr := `"` + str + `"`
+		require.NoError(t, json.Unmarshal([]byte(quotedStr), es))
+		assert.Equal(t, expected, *es)
+	}
 
 	require.ErrorContains(t, json.Unmarshal([]byte(`"ABC"`), es), "unknown ExecutionStatus")
 }
 
 func TestUnmarshalFinalityStatus(t *testing.T) {
 	fs := new(starknet.FinalityStatus)
-	require.NoError(t, json.Unmarshal([]byte(`"ACCEPTED_ON_L1"`), fs))
-	assert.Equal(t, starknet.AcceptedOnL1, *fs)
 
-	require.NoError(t, json.Unmarshal([]byte(`"ACCEPTED_ON_L2"`), fs))
-	assert.Equal(t, starknet.AcceptedOnL2, *fs)
+	cases := map[string]starknet.FinalityStatus{
+		"ACCEPTED_ON_L2": starknet.AcceptedOnL2,
+		"ACCEPTED_ON_L1": starknet.AcceptedOnL1,
+		"NOT_RECEIVED":   starknet.NotReceived,
+		"RECEIVED":       starknet.Received,
+	}
+	for str, expected := range cases {
+		quotedStr := `"` + str + `"`
+		require.NoError(t, json.Unmarshal([]byte(quotedStr), fs))
+		assert.Equal(t, expected, *fs)
+	}
 
 	require.ErrorContains(t, json.Unmarshal([]byte(`"ABC"`), fs), "unknown FinalityStatus")
 }
