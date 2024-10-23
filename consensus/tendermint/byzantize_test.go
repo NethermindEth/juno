e884d73b5dc814a29d77845a770f599981a07172
rian <rian.hughes@physics.ox.ac.uk>
2024-10-23 16:20:19 +0300

    Proposer proposes an invalid value

--- consensus/tendermint/byzantize_test.go
+++ consensus/tendermint/byzantize_test.go
@@ -71,4 +71,50 @@ func TestByzantine(t *testing.T) {
 		assert.Equal(t, uint(0), algo.state.height)
 		assert.Equal(t, uint(0), algo.state.round)
 	})
+
+	// Todo [rian]: I would have expected that a nil proposal is stored in the messages
+	t.Run("Proposer proposes an invalid value", func(t *testing.T) {
+		listeners, broadcasters := testListenersAndBroadcasters()
+		app, chain, vals := newApp(), newChain(), newVals()
+
+		vals.addValidator(*node2)
+		vals.addValidator(*node3)
+		vals.addValidator(*node4)
+		vals.addValidator(*myNode)
+
+		algo := New[value, felt.Felt, felt.Felt](*myNode, app, chain, vals, listeners, broadcasters, tm, tm, tm)
+
+		height, round, inValidValue := uint(0), uint(0), value(123)
+
+		firstProposal := Proposal[value, felt.Felt, felt.Felt]{
+			Height:     height,
+			Round:      round,
+			ValidRound: nil,
+			Value:      &inValidValue,
+			Sender:     *node2,
+		}
+		expectedPrevote := Prevote[felt.Felt, felt.Felt]{
+			Vote: Vote[felt.Felt, felt.Felt]{
+				Height: height,
+				Round:  round,
+				ID:     nil,
+				Sender: *myNode,
+			},
+		}
+		proposalListener := listeners.ProposalListener.(*senderAndReceiver[Proposal[value, felt.Felt, felt.Felt],
+			value, felt.Felt, felt.Felt])
+		proposalListener.send(firstProposal)
+
+		algo.Start()
+		time.Sleep(1 * time.Millisecond)
+		algo.Stop()
+
+		assert.Equal(t, 0, len(algo.messages.proposals[height][round][*node2]))
+		assert.Equal(t, 1, len(algo.messages.prevotes[height][round][*myNode]))
+		assert.Equal(t, expectedPrevote, algo.messages.prevotes[height][round][*myNode][0])
+
+		assert.Equal(t, prevote, algo.state.step)
+		assert.Equal(t, uint(0), algo.state.height)
+		assert.Equal(t, uint(0), algo.state.round)
+	})
 }
