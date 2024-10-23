ed7f2bcdb3d36875c76e73c62d18c5a5f408198c
rian <rian.hughes@physics.ox.ac.uk>
2024-10-23 16:55:33 +0300

    Multiple equivocation prevotes for same height and round but different values

--- consensus/tendermint/byzantize_test.go
+++ consensus/tendermint/byzantize_test.go
@@ -9,7 +9,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestByzantine(t *testing.T) {
+// Todo: remove these comments
+
+// Test cases covered:
+// 1. proposer casts two conflicting proposals.
+// 2. proposer proposes invalid value.
+
+// Test cases not covered:
+// 1. pre-votes for two different valid values
+// 2. pre-votes for an invalid value
+// ... many more ...
+
+func TestByzantineProposer(t *testing.T) {
 	myNode := new(felt.Felt).SetBytes([]byte("my node address"))
 	node2, node3, node4 := new(felt.Felt).SetUint64(2), new(felt.Felt).SetUint64(3), new(felt.Felt).SetUint64(4)
 	tm := func(r uint) time.Duration { return time.Second }
@@ -117,4 +128,91 @@ func TestByzantine(t *testing.T) {
 		assert.Equal(t, uint(0), algo.state.height)
 		assert.Equal(t, uint(0), algo.state.round)
 	})
+}
+
+func TestByzantinePrevoter(t *testing.T) {
+	myNode := new(felt.Felt).SetBytes([]byte("my node address"))
+	node2, node3, node4 := new(felt.Felt).SetUint64(2), new(felt.Felt).SetUint64(3), new(felt.Felt).SetUint64(4)
+	tm := func(r uint) time.Duration { return time.Second }
+
+	t.Run("Multiple equivocation prevotes for same height and round but different values", func(t *testing.T) {
+		listeners, broadcasters := testListenersAndBroadcasters()
+		app, chain, vals := newApp(), newChain(), newVals()
+
+		vals.addValidator(*node2)
+		vals.addValidator(*node3)
+		vals.addValidator(*node4)
+		vals.addValidator(*myNode)
+
+		algo := New[value, felt.Felt, felt.Felt](*myNode, app, chain, vals, listeners, broadcasters, tm, tm, tm)
+
+		height, round := uint(0), uint(0)
+		validValue1, validValue2, validValue3 := value(10), value(11), value(12)
+
+		proposal := Proposal[value, felt.Felt, felt.Felt]{
+			Height:     height,
+			Round:      round,
+			ValidRound: nil,
+			Value:      &validValue1,
+			Sender:     *node2,
+		}
+
+		validPrevote := Prevote[felt.Felt, felt.Felt]{
+			Vote: Vote[felt.Felt, felt.Felt]{
+				Height: height,
+				Round:  round,
+				ID:     utils.Ptr(validValue1.Hash()),
+				Sender: *node3,
+			},
+		}
+
+		equivocationPrevote1 := Prevote[felt.Felt, felt.Felt]{
+			Vote: Vote[felt.Felt, felt.Felt]{
+				Height: height,
+				Round:  round,
+				ID:     utils.Ptr(validValue2.Hash()),
+				Sender: *node3,
+			},
+		}
+		equivocationPrevote2 := Prevote[felt.Felt, felt.Felt]{
+			Vote: Vote[felt.Felt, felt.Felt]{
+				Height: height,
+				Round:  round,
+				ID:     utils.Ptr(validValue2.Hash()),
+				Sender: *node3,
+			},
+		}
+		equivocationPrevote3 := Prevote[felt.Felt, felt.Felt]{
+			Vote: Vote[felt.Felt, felt.Felt]{
+				Height: height,
+				Round:  round,
+				ID:     utils.Ptr(validValue3.Hash()),
+				Sender: *node3,
+			},
+		}
+
+		proposalListener := listeners.ProposalListener.(*senderAndReceiver[Proposal[value, felt.Felt, felt.Felt],
+			value, felt.Felt, felt.Felt])
+		proposalListener.send(proposal)
+
+		prevoteListener := listeners.PrevoteListener.(*senderAndReceiver[Prevote[felt.Felt, felt.Felt], value,
+			felt.Felt, felt.Felt])
+		prevoteListener.send(validPrevote)
+		prevoteListener.send(equivocationPrevote1)
+		prevoteListener.send(equivocationPrevote2)
+		prevoteListener.send(equivocationPrevote3)
+
+		algo.Start()
+		time.Sleep(1 * time.Millisecond)
+		algo.Stop()
+
+		assert.Equal(t, 1, len(algo.messages.proposals[height][round][*node2]))
+		assert.Equal(t, 1, len(algo.messages.prevotes[height][round][*myNode]))
+		assert.Equal(t, 4, len(algo.messages.prevotes[height][round][*node3]))
+
+		assert.Equal(t, prevote, algo.state.step)
+		assert.Equal(t, uint(0), algo.state.height)
+		assert.Equal(t, uint(0), algo.state.round)
+	})
+
 }
