7e785cc4715492b533d9a94e57749960db719086
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-12-10 11:45:50 +0700

    Shift variable out from loop

--- p2p/hashstorage/hash_storage.go
+++ p2p/hashstorage/hash_storage.go
@@ -15,8 +15,9 @@ var SepoliaBlockHashesMap = make(map[uint64]*felt.Felt, first0132SepoliaBlock)
 
 //nolint:gochecknoinits
 func init() {
+	var offset uint64
 	for i := uint64(0); i < first0132SepoliaBlock; i++ {
-		offset := i * 32
+		offset = i * 32
 		SepoliaBlockHashesMap[i] = new(felt.Felt).SetBytes(sepoliaBlockHashes[offset : offset+32])
 	}
 }
