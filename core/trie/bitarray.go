48248aef2e0cb1256f3c1744a052526f0df10728
weiihann <weihan774237@gmail.com>
2024-12-13 23:51:24 +0800

    looks gud

--- core/trie/bitarray.go
+++ core/trie/bitarray.go
@@ -12,49 +12,48 @@ const (
 
 var maxBitArray = [4]uint64{0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}
 
+// bitArray is a structure that represents a bit array with a max length of 255 bits.
+// The reason why 255 bits is the max length is because we only need up to 252 bits for the felt.
+// Though words can be used to represent 256 bits, we don't want to add an additional byte for the length.
+// It uses a little endian representation to do bitwise operations of the words efficiently.
+// Unlike normal bit arrays, it has a `len` field that represents the number of used bits.
+// For example, if len is 10, it means that the 2^9, 2^8, ..., 2^0 bits are used.
 type bitArray struct {
-	pos   uint8     // position of the current most significant bit (0-255)
+	len   uint8     // number of used bits
 	words [4]uint64 // little endian (i.e. words[0] is the least significant)
 }
 
+// Bytes returns the bytes representation of the bit array in big endian format.
 func (b *bitArray) Bytes() [32]byte {
 	var res [32]byte
 
 	switch {
-	case b.pos == 0:
+	case b.len == 0:
 		return res
-	case b.pos == 255:
-		binary.BigEndian.PutUint64(res[0:8], b.words[3])
-		binary.BigEndian.PutUint64(res[8:16], b.words[2])
-		binary.BigEndian.PutUint64(res[16:24], b.words[1])
-		binary.BigEndian.PutUint64(res[24:32], b.words[0])
-	case b.pos >= 192:
-		// For positions >= 192, we need to mask the most significant word (words[3])
-		// to zero out bits beyond the current position.
-		// Example: if pos = 201, then rem = 255 - 201 = 54
-		// mask = ^mask64 >> (54 - 1) = ^(1<<63) >> 53
-		// This creates a mask like: 0000000000000000000000000000000000000000000000000000001111111111
-		// When applied to words[3], it preserves only the 10 least significant bits
-		shift := 255 - b.pos
-		mask := ^mask64 >> (shift - 1)
+	case b.len >= 192:
+		// len is 0-based, so 255 (not 256) represents all bits used
+		// subtracting from 255 ensures correct mask when len=255
+		// For example, when len is 255, it means all bits from index 0
+		// to 254 are used (total of 255 bits).
+		// So when we create the mask, we shift 255 - 255 = 0 bits to the right.
+		// This creates a mask that covers all bits from index 0 to 254.
+		mask := ^mask64 >> (255 - b.len)
 		binary.BigEndian.PutUint64(res[0:8], b.words[3]&mask)
 		binary.BigEndian.PutUint64(res[8:16], b.words[2])
 		binary.BigEndian.PutUint64(res[16:24], b.words[1])
 		binary.BigEndian.PutUint64(res[24:32], b.words[0])
-	case b.pos >= 128:
-		shift := 191 - b.pos
-		mask := ^mask64 >> (shift - 1)
+	case b.len >= 128:
+		// Similar pattern for 191 boundary (3 words × 64 bits - 1)
+		mask := ^mask64 >> (191 - b.len)
 		binary.BigEndian.PutUint64(res[8:16], b.words[2]&mask)
 		binary.BigEndian.PutUint64(res[16:24], b.words[1])
 		binary.BigEndian.PutUint64(res[24:32], b.words[0])
-	case b.pos >= 64:
-		shift := 127 - b.pos
-		mask := ^mask64 >> (shift - 1)
+	case b.len >= 64:
+		mask := ^mask64 >> (127 - b.len)
 		binary.BigEndian.PutUint64(res[16:24], b.words[1]&mask)
 		binary.BigEndian.PutUint64(res[24:32], b.words[0])
 	default:
-		shift := 63 - b.pos
-		mask := ^mask64 >> (shift - 1)
+		mask := ^mask64 >> (63 - b.len)
 		binary.BigEndian.PutUint64(res[24:32], b.words[0]&mask)
 	}
 
@@ -67,17 +66,17 @@ func (b *bitArray) SetFelt(f *felt.Felt) *bitArray {
 	b.words[2] = binary.BigEndian.Uint64(res[8:16])
 	b.words[1] = binary.BigEndian.Uint64(res[16:24])
 	b.words[0] = binary.BigEndian.Uint64(res[24:32])
-	b.pos = felt.Bits - 1
+	b.len = felt.Bits - 1
 	return b
 }
 
 // Rsh shifts the bit array to the right by n bits.
 func (b *bitArray) Rsh(x *bitArray, n uint8) *bitArray {
-	if b.pos == 0 {
+	if b.len == 0 {
 		return b
 	}
 
-	if n >= b.pos {
+	if n >= b.len {
 		return b.clear()
 	}
 
@@ -88,13 +87,13 @@ func (b *bitArray) Rsh(x *bitArray, n uint8) *bitArray {
 		b.rsh192(x)
 		n -= 192
 		b.words[0] >>= n
-		b.pos -= n
+		b.len -= n
 	case n >= 128:
 		b.rsh128(x)
 		n -= 128
 		b.words[0] = (b.words[0] >> n) | (b.words[1] << (64 - n))
 		b.words[1] >>= n
-		b.pos -= n
+		b.len -= n
 	case n >= 64:
 		b.rsh64(x)
 		n -= 64
@@ -102,21 +101,21 @@ func (b *bitArray) Rsh(x *bitArray, n uint8) *bitArray {
 		b.words[1] = (b.words[1] >> n) | (b.words[2] << (64 - n))
 		b.words[2] = (b.words[2] >> n) | (b.words[3] << (64 - n))
 		b.words[3] >>= n
-		b.pos -= n
+		b.len -= n
 	default:
 		b.set(x)
 		b.words[3] = (b.words[3] >> n) | (b.words[2] << (64 - n))
 		b.words[2] = (b.words[2] >> n) | (b.words[1] << (64 - n))
 		b.words[1] = (b.words[1] >> n) | (b.words[0] << (64 - n))
 		b.words[0] >>= n
-		b.pos -= n
+		b.len -= n
 	}
 
 	return b
 }
 
 func (b *bitArray) set(x *bitArray) *bitArray {
-	b.pos = x.pos
+	b.len = x.len
 	b.words[0] = x.words[0]
 	b.words[1] = x.words[1]
 	b.words[2] = x.words[2]
@@ -137,7 +136,7 @@ func (b *bitArray) rsh192(x *bitArray) {
 }
 
 func (b *bitArray) clear() *bitArray {
-	b.pos = 0
+	b.len = 0
 	b.words[0], b.words[1], b.words[2], b.words[3] = 0, 0, 0, 0
 	return b
 }
