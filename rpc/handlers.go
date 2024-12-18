991c3a5be9f3ad4a1e686ec08a051d68ea2db567
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-12-18 11:00:26 +0700

    Update RPC v0.7  endpoint

--- rpc/handlers.go
+++ rpc/handlers.go
@@ -516,7 +516,7 @@ func (h *Handler) MethodsV0_7() ([]jsonrpc.Method, string) { //nolint: funlen
 		{
 			Name:    "starknet_estimateMessageFee",
 			Params:  []jsonrpc.Parameter{{Name: "message"}, {Name: "block_id"}},
-			Handler: h.EstimateMessageFee,
+			Handler: h.EstimateMessageFeeV0_7,
 		},
 		{
 			Name:    "starknet_traceTransaction",
