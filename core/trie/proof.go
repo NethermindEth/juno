e39ef5724f657c882f25905a232bdbfd73af3284
weiihann <weihan774237@gmail.com>
2024-12-30 18:46:36 +0800

    change IsBitSetFromMSB to IsBitSet

--- core/trie/proof.go
+++ core/trie/proof.go
@@ -161,7 +161,7 @@ func VerifyProof(root, keyFelt *felt.Felt, proof *ProofNodeSet, hash crypto.Hash
 			}
 			// Determine the next node to traverse based on the next bit position
 			expectedHash = node.LeftHash
-			if keyBits.IsBitSetFromMSB(curPos) {
+			if keyBits.IsBitSet(curPos) {
 				expectedHash = node.RightHash
 			}
 			curPos++
@@ -489,7 +489,7 @@ func handleBinaryNode(
 
 	// Calculate next position and determine to take left or right path
 	nextPos := curPos + 1
-	isRightPath := key.IsBitSetFromMSB(curPos)
+	isRightPath := key.IsBitSet(curPos)
 	nextHash := binary.LeftHash
 	if isRightPath {
 		nextHash = binary.RightHash
@@ -602,7 +602,7 @@ func hasRightElement(rootKey, key *BitArray, nodes *StorageNodeSet) bool {
 
 		// If we're taking a left path and there's a right sibling,
 		// then there are elements with larger values
-		isLeft := !key.IsBitSetFromMSB(cur.Len())
+		isLeft := !key.IsBitSet(cur.Len())
 		if isLeft && sn.node.RightHash != nil {
 			return true
 		}
