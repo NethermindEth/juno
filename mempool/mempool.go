f4fb79e82c9192a4b35a28659c6bb36e22af164d
rian <rian.hughes@physics.ox.ac.uk>
2025-01-06 17:48:48 +0200

    implement in-memory mempool

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
@@ -11,103 +12,221 @@ import (
 	"github.com/NethermindEth/juno/encoder"
 )
 
-type ValidatorFunc func(*BroadcastedTransaction) error
-
-type BroadcastedTransaction struct {
-	Transaction   core.Transaction
-	DeclaredClass core.Class
-}
-
 const (
 	poolLengthKey = "poolLength"
 	headKey       = "headKey"
 	tailKey       = "tailKey"
 )
 
+var ErrTxnPoolFull = errors.New("transaction pool is full")
+
 type storageElem struct {
 	Txn      BroadcastedTransaction
-	NextHash *felt.Felt
+	NextHash *felt.Felt   // persistent db
+	Next     *storageElem // in-memory
+}
+
+type BroadcastedTransaction struct {
+	Transaction   core.Transaction
+	DeclaredClass core.Class
+}
+
+type txnList struct {
+	head *storageElem
+	tail *storageElem
+	len  uint16
+	mu   sync.Mutex
 }
 
 // Pool stores the transactions in a linked list for its inherent FCFS behaviour
 type Pool struct {
-	db        db.DB
-	validator ValidatorFunc
-	txPushed  chan struct{}
+	db          db.DB
+	txPushed    chan struct{}
+	txnList     *txnList // in-memory
+	maxNumTxns  uint16
+	dbWriteChan chan *BroadcastedTransaction
+	wg          sync.WaitGroup
 }
 
-func New(poolDB db.DB) *Pool {
-	return &Pool{
-		db:        poolDB,
-		validator: func(_ *BroadcastedTransaction) error { return nil },
-		txPushed:  make(chan struct{}, 1),
+// New initializes the Pool and starts the database writer goroutine.
+// It is the responsibility of the user to call the cancel function if the context is cancelled
+func New(db db.DB, maxNumTxns uint16) (*Pool, func() error, error) {
+	pool := &Pool{
+		db:          db, // todo: txns should be deleted everytime a new block is stored (builder responsibility)
+		txPushed:    make(chan struct{}, 1),
+		txnList:     &txnList{},
+		maxNumTxns:  maxNumTxns,
+		dbWriteChan: make(chan *BroadcastedTransaction, maxNumTxns),
+	}
+
+	if err := pool.loadFromDB(); err != nil {
+		return nil, nil, fmt.Errorf("failed to load transactions from database into the in-memory transaction list: %v\n", err)
+	}
+
+	pool.wg.Add(1)
+	go pool.dbWriter()
+	closer := func() error {
+		close(pool.dbWriteChan)
+		pool.wg.Wait()
+		if err := pool.db.Close(); err != nil {
+			return fmt.Errorf("failed to close mempool database: %v", err)
+		}
+		return nil
+	}
+	return pool, closer, nil
+}
+
+func (p *Pool) dbWriter() {
+	defer p.wg.Done()
+	for {
+		select {
+		case txn, ok := <-p.dbWriteChan:
+			if !ok {
+				return
+			}
+			p.handleTransaction(txn)
+		}
 	}
 }
 
-// WithValidator adds a validation step to be triggered before adding a
-// BroadcastedTransaction to the pool
-func (p *Pool) WithValidator(validator ValidatorFunc) *Pool {
-	p.validator = validator
-	return p
+// loadFromDB restores the in-memory transaction pool from the database
+func (p *Pool) loadFromDB() error {
+	return p.db.View(func(txn db.Transaction) error {
+		len, err := p.LenDB()
+		if err != nil {
+			return err
+		}
+		if len >= p.maxNumTxns {
+			return ErrTxnPoolFull
+		}
+		headValue := new(felt.Felt)
+		err = p.headHash(txn, headValue)
+		if err != nil {
+			if errors.Is(err, db.ErrKeyNotFound) {
+				return nil
+			}
+			return err
+		}
+
+		currentHash := headValue
+		for currentHash != nil {
+			curElem, err := p.dbElem(txn, currentHash)
+			if err != nil {
+				return err
+			}
+
+			newNode := &storageElem{
+				Txn: curElem.Txn,
+			}
+
+			if curElem.NextHash != nil {
+				nxtElem, err := p.dbElem(txn, curElem.NextHash)
+				if err != nil {
+					return err
+				}
+				newNode.Next = &storageElem{
+					Txn: nxtElem.Txn,
+				}
+			}
+
+			p.txnList.mu.Lock()
+			if p.txnList.tail != nil {
+				p.txnList.tail.Next = newNode
+				p.txnList.tail = newNode
+			} else {
+				p.txnList.head = newNode
+				p.txnList.tail = newNode
+			}
+			p.txnList.len++
+			p.txnList.mu.Unlock()
+
+			currentHash = curElem.NextHash
+		}
+
+		return nil
+	})
 }
 
-// Push queues a transaction to the pool
-func (p *Pool) Push(userTxn *BroadcastedTransaction) error {
-	err := p.validator(userTxn)
-	if err != nil {
-		return err
-	}
-
-	err = p.rejectDuplicateTxn(userTxn)
-	if err != nil {
-		return err
-	}
-
-	if err := p.db.Update(func(txn db.Transaction) error {
-		tail := new(felt.Felt)
-		if err := p.tailHash(txn, tail); err != nil {
+func (p *Pool) handleTransaction(userTxn *BroadcastedTransaction) error {
+	return p.db.Update(func(dbTxn db.Transaction) error {
+		tailValue := new(felt.Felt)
+		if err := p.tailValue(dbTxn, tailValue); err != nil {
 			if !errors.Is(err, db.ErrKeyNotFound) {
 				return err
 			}
-			tail = nil
+			tailValue = nil
 		}
-		if err = p.putElem(txn, userTxn.Transaction.Hash(), &storageElem{
+
+		if err := p.putdbElem(dbTxn, userTxn.Transaction.Hash(), &storageElem{
 			Txn: *userTxn,
 		}); err != nil {
 			return err
 		}
 
-		if tail != nil {
-			var oldTail storageElem
-			oldTail, err = p.elem(txn, tail)
+		if tailValue != nil {
+			// Update old tail to point to the new item
+			var oldTailElem storageElem
+			oldTailElem, err := p.dbElem(dbTxn, tailValue)
 			if err != nil {
 				return err
 			}
-
-			// update old tail to point to the new item
-			oldTail.NextHash = userTxn.Transaction.Hash()
-			if err = p.putElem(txn, tail, &oldTail); err != nil {
+			oldTailElem.NextHash = userTxn.Transaction.Hash()
+			if err = p.putdbElem(dbTxn, tailValue, &oldTailElem); err != nil {
 				return err
 			}
 		} else {
-			// empty list, make new item both the head and the tail
-			if err = p.updateHead(txn, userTxn.Transaction.Hash()); err != nil {
+			// Empty list, make new item both the head and the tail
+			if err := p.updateHead(dbTxn, userTxn.Transaction.Hash()); err != nil {
 				return err
 			}
 		}
 
-		if err = p.updateTail(txn, userTxn.Transaction.Hash()); err != nil {
+		if err := p.updateTail(dbTxn, userTxn.Transaction.Hash()); err != nil {
 			return err
 		}
 
-		pLen, err := p.len(txn)
+		pLen, err := p.lenDB(dbTxn)
 		if err != nil {
 			return err
 		}
-		return p.updateLen(txn, pLen+1) // don't worry about overflows, highly unlikely
-	}); err != nil {
-		return err
+		return p.updateLen(dbTxn, uint16(pLen+1))
+	})
+}
+
+// Push queues a transaction to the pool and adds it to both the in-memory list and DB
+func (p *Pool) Push(userTxn *BroadcastedTransaction) error {
+	if p.txnList.len >= uint16(p.maxNumTxns) {
+		return ErrTxnPoolFull
 	}
+
+	// todo(rian this PR): validation
+
+	// todo: should db overloading block the in-memory mempool??
+	select {
+	case p.dbWriteChan <- userTxn:
+	default:
+		select {
+		case _, ok := <-p.dbWriteChan:
+			if !ok {
+				return errors.New("transaction pool database write channel is closed")
+			}
+			return ErrTxnPoolFull
+		default:
+			return ErrTxnPoolFull
+		}
+	}
+
+	p.txnList.mu.Lock()
+	newNode := &storageElem{Txn: *userTxn, Next: nil}
+	if p.txnList.tail != nil {
+		p.txnList.tail.Next = newNode
+		p.txnList.tail = newNode
+	} else {
+		p.txnList.head = newNode
+		p.txnList.tail = newNode
+	}
+	p.txnList.len++
+	p.txnList.mu.Unlock()
 
 	select {
 	case p.txPushed <- struct{}{}:
@@ -117,50 +236,23 @@ func (p *Pool) Push(userTxn *BroadcastedTransaction) error {
 	return nil
 }
 
-// Pop returns the transaction with the highest priority from the pool
+// Pop returns the transaction with the highest priority from the in-memory pool
 func (p *Pool) Pop() (BroadcastedTransaction, error) {
-	var nextTxn BroadcastedTransaction
-	return nextTxn, p.db.Update(func(txn db.Transaction) error {
-		headHash := new(felt.Felt)
-		err := p.headHash(txn, headHash)
-		if err != nil {
-			return err
-		}
+	p.txnList.mu.Lock()
+	defer p.txnList.mu.Unlock()
 
-		headElem, err := p.elem(txn, headHash)
-		if err != nil {
-			return err
-		}
+	if p.txnList.head == nil {
+		return BroadcastedTransaction{}, errors.New("transaction pool is empty")
+	}
 
-		if err = txn.Delete(headHash.Marshal()); err != nil {
-			return err
-		}
+	headNode := p.txnList.head
+	p.txnList.head = headNode.Next
+	if p.txnList.head == nil {
+		p.txnList.tail = nil
+	}
+	p.txnList.len--
 
-		if headElem.NextHash == nil {
-			// the list is empty now
-			if err = txn.Delete([]byte(headKey)); err != nil {
-				return err
-			}
-			if err = txn.Delete([]byte(tailKey)); err != nil {
-				return err
-			}
-		} else {
-			if err = p.updateHead(txn, headElem.NextHash); err != nil {
-				return err
-			}
-		}
-
-		pLen, err := p.len(txn)
-		if err != nil {
-			return err
-		}
-
-		if err = p.updateLen(txn, pLen-1); err != nil {
-			return err
-		}
-		nextTxn = headElem.Txn
-		return nil
-	})
+	return headNode.Txn, nil
 }
 
 // Remove removes a set of transactions from the pool
@@ -168,24 +260,25 @@ func (p *Pool) Remove(hash ...*felt.Felt) error {
 	return errors.New("not implemented")
 }
 
-// Len returns the number of transactions in the pool
-func (p *Pool) Len() (uint64, error) {
-	var l uint64
-	return l, p.db.View(func(txn db.Transaction) error {
-		var err error
-		l, err = p.len(txn)
-		return err
-	})
+// Len returns the number of transactions in the in-memory pool
+func (p *Pool) Len() uint16 {
+	return p.txnList.len
 }
 
-func (p *Pool) Wait() <-chan struct{} {
-	return p.txPushed
+// Len returns the number of transactions in the persistent pool
+func (p *Pool) LenDB() (uint16, error) {
+	txn, err := p.db.NewTransaction(false)
+	if err != nil {
+		return 0, err
+	}
+	defer txn.Discard()
+	return p.lenDB(txn)
 }
 
-func (p *Pool) len(txn db.Transaction) (uint64, error) {
-	var l uint64
+func (p *Pool) lenDB(txn db.Transaction) (uint16, error) {
+	var l uint16
 	err := txn.Get([]byte(poolLengthKey), func(b []byte) error {
-		l = binary.BigEndian.Uint64(b)
+		l = binary.BigEndian.Uint16(b)
 		return nil
 	})
 
@@ -195,8 +288,12 @@ func (p *Pool) len(txn db.Transaction) (uint64, error) {
 	return l, err
 }
 
-func (p *Pool) updateLen(txn db.Transaction, l uint64) error {
-	return txn.Set([]byte(poolLengthKey), binary.BigEndian.AppendUint64(nil, l))
+func (p *Pool) updateLen(txn db.Transaction, l uint16) error {
+	return txn.Set([]byte(poolLengthKey), binary.BigEndian.AppendUint16(nil, l))
+}
+
+func (p *Pool) Wait() <-chan struct{} {
+	return p.txPushed
 }
 
 func (p *Pool) headHash(txn db.Transaction, head *felt.Felt) error {
@@ -206,11 +303,24 @@ func (p *Pool) headHash(txn db.Transaction, head *felt.Felt) error {
 	})
 }
 
+func (p *Pool) HeadHash() (*felt.Felt, error) {
+	txn, err := p.db.NewTransaction(false)
+	if err != nil {
+		return nil, err
+	}
+	var head *felt.Felt
+	err = txn.Get([]byte(headKey), func(b []byte) error {
+		head = new(felt.Felt).SetBytes(b)
+		return nil
+	})
+	return head, err
+}
+
 func (p *Pool) updateHead(txn db.Transaction, head *felt.Felt) error {
 	return txn.Set([]byte(headKey), head.Marshal())
 }
 
-func (p *Pool) tailHash(txn db.Transaction, tail *felt.Felt) error {
+func (p *Pool) tailValue(txn db.Transaction, tail *felt.Felt) error {
 	return txn.Get([]byte(tailKey), func(b []byte) error {
 		tail.SetBytes(b)
 		return nil
@@ -221,7 +331,7 @@ func (p *Pool) updateTail(txn db.Transaction, tail *felt.Felt) error {
 	return txn.Set([]byte(tailKey), tail.Marshal())
 }
 
-func (p *Pool) elem(txn db.Transaction, itemKey *felt.Felt) (storageElem, error) {
+func (p *Pool) dbElem(txn db.Transaction, itemKey *felt.Felt) (storageElem, error) {
 	var item storageElem
 	err := txn.Get(itemKey.Marshal(), func(b []byte) error {
 		return encoder.Unmarshal(b, &item)
@@ -229,26 +339,10 @@ func (p *Pool) elem(txn db.Transaction, itemKey *felt.Felt) (storageElem, error)
 	return item, err
 }
 
-func (p *Pool) putElem(txn db.Transaction, itemKey *felt.Felt, item *storageElem) error {
+func (p *Pool) putdbElem(txn db.Transaction, itemKey *felt.Felt, item *storageElem) error {
 	itemBytes, err := encoder.Marshal(item)
 	if err != nil {
 		return err
 	}
 	return txn.Set(itemKey.Marshal(), itemBytes)
-}
-
-func (p *Pool) rejectDuplicateTxn(userTxn *BroadcastedTransaction) error {
-	txHash := userTxn.Transaction.Hash().Marshal()
-	err := p.db.View(func(txn db.Transaction) error {
-		return txn.Get(txHash, func(val []byte) error {
-			if val != nil {
-				return fmt.Errorf("transaction already exists in the mempool: %x", txHash)
-			}
-			return nil
-		})
-	})
-	if errors.Is(err, db.ErrKeyNotFound) {
-		return nil
-	}
-	return err
 }
--- mempool/mempool_test.go
+++ mempool/mempool_test.go
@@ -1,9 +1,9 @@
 package mempool_test
 
 import (
-	"errors"
-	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/NethermindEth/juno/blockchain"
 	"github.com/NethermindEth/juno/core"
@@ -15,19 +15,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func setupDatabase(dltExisting bool) (*db.DB, func(), error) {
+	dbPath := "testmempool"
+	if _, err := os.Stat(dbPath); err == nil {
+		if dltExisting {
+			if err := os.RemoveAll(dbPath); err != nil {
+				return nil, nil, err
+			}
+		}
+	} else if !os.IsNotExist(err) {
+		return nil, nil, err
+	}
+	db, err := pebble.New(dbPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	closer := func() {
+		// The db should be closed by the mempool closer function
+		os.RemoveAll(dbPath)
+	}
+	return &db, closer, nil
+}
+
 func TestMempool(t *testing.T) {
-	testDB := pebble.NewMemTest(t)
-	pool := mempool.New(testDB)
+	testDB, dbCloser, err := setupDatabase(true)
+	require.NoError(t, err)
+	defer dbCloser()
+	pool, closer, err := mempool.New(*testDB, 5)
+	defer closer()
+	require.NoError(t, err)
 	blockchain.RegisterCoreTypesToEncoder()
 
-	t.Run("empty pool", func(t *testing.T) {
-		l, err := pool.Len()
-		require.NoError(t, err)
-		assert.Equal(t, uint64(0), l)
+	l := pool.Len()
+	assert.Equal(t, uint16(0), l)
 
-		_, err = pool.Pop()
-		require.ErrorIs(t, err, db.ErrKeyNotFound)
-	})
+	_, err = pool.Pop()
+	require.Equal(t, err.Error(), "transaction pool is empty")
 
 	// push multiple to empty (1,2,3)
 	for i := uint64(1); i < 4; i++ {
@@ -37,21 +60,18 @@ func TestMempool(t *testing.T) {
 			},
 		}))
 
-		l, err := pool.Len()
-		require.NoError(t, err)
-		assert.Equal(t, i, l)
+		l := pool.Len()
+		assert.Equal(t, uint16(i), l)
 	}
 
 	// consume some (remove 1,2, keep 3)
 	for i := uint64(1); i < 3; i++ {
 		txn, err := pool.Pop()
-		fmt.Println("txn", txn.Transaction.Hash().String())
 		require.NoError(t, err)
 		assert.Equal(t, i, txn.Transaction.Hash().Uint64())
 
-		l, err := pool.Len()
-		require.NoError(t, err)
-		assert.Equal(t, 3-i, l)
+		l := pool.Len()
+		assert.Equal(t, uint16(3-i), l)
 	}
 
 	// push multiple to non empty (push 4,5. now have 3,4,5)
@@ -62,10 +82,16 @@ func TestMempool(t *testing.T) {
 			},
 		}))
 
-		l, err := pool.Len()
-		require.NoError(t, err)
-		assert.Equal(t, i-2, l)
+		l := pool.Len()
+		assert.Equal(t, uint16(i-2), l)
 	}
+
+	// push more than max
+	assert.ErrorIs(t, pool.Push(&mempool.BroadcastedTransaction{
+		Transaction: &core.InvokeTransaction{
+			TransactionHash: new(felt.Felt).SetUint64(123),
+		},
+	}), mempool.ErrTxnPoolFull)
 
 	// consume all (remove 3,4,5)
 	for i := uint64(3); i < 6; i++ {
@@ -73,33 +99,72 @@ func TestMempool(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, i, txn.Transaction.Hash().Uint64())
 	}
-
-	l, err := pool.Len()
-	require.NoError(t, err)
-	assert.Equal(t, uint64(0), l)
+	assert.Equal(t, uint16(0), l)
 
 	_, err = pool.Pop()
-	require.ErrorIs(t, err, db.ErrKeyNotFound)
+	require.Equal(t, err.Error(), "transaction pool is empty")
 
-	// reject duplicate txn
-	txn := mempool.BroadcastedTransaction{
-		Transaction: &core.InvokeTransaction{
-			TransactionHash: new(felt.Felt).SetUint64(2),
-		},
+}
+
+func TestRestoreMempool(t *testing.T) {
+	blockchain.RegisterCoreTypesToEncoder()
+
+	testDB, _, err := setupDatabase(true)
+	require.NoError(t, err)
+	pool, closer, err := mempool.New(*testDB, 1024)
+	require.NoError(t, err)
+
+	// Check both pools are empty
+	lenDB, err := pool.LenDB()
+	require.NoError(t, err)
+	assert.Equal(t, uint16(0), lenDB)
+	assert.Equal(t, uint16(0), pool.Len())
+
+	// push multiple transactions to empty mempool (1,2,3)
+	for i := uint64(1); i < 4; i++ {
+		assert.NoError(t, pool.Push(&mempool.BroadcastedTransaction{
+			Transaction: &core.InvokeTransaction{
+				TransactionHash: new(felt.Felt).SetUint64(i),
+			},
+		}))
+		assert.Equal(t, uint16(i), pool.Len())
 	}
-	require.NoError(t, pool.Push(&txn))
-	require.Error(t, pool.Push(&txn))
 
-	// validation error
-	pool = pool.WithValidator(func(bt *mempool.BroadcastedTransaction) error {
-		return errors.New("some error")
-	})
-	require.EqualError(t, pool.Push(&mempool.BroadcastedTransaction{}), "some error")
+	// check the db has stored the transactions
+	time.Sleep(100 * time.Millisecond)
+	lenDB, err = pool.LenDB()
+	require.NoError(t, err)
+	assert.Equal(t, uint16(3), lenDB)
+
+	// Close the mempool
+	require.NoError(t, closer())
+	testDB, dbCloser, err := setupDatabase(false)
+	require.NoError(t, err)
+	defer dbCloser()
+
+	poolRestored, closer2, err := mempool.New(*testDB, 1024)
+	require.NoError(t, err)
+	lenDB, err = poolRestored.LenDB()
+	require.NoError(t, err)
+	assert.Equal(t, uint16(3), lenDB)
+	assert.Equal(t, uint16(3), poolRestored.Len())
+
+	// Remove transactions
+	_, err = poolRestored.Pop()
+	require.NoError(t, err)
+	_, err = poolRestored.Pop()
+	require.NoError(t, err)
+	lenDB, err = poolRestored.LenDB()
+	assert.Equal(t, uint16(3), lenDB)
+	assert.Equal(t, uint16(1), poolRestored.Len())
+
+	closer2()
 }
 
 func TestWait(t *testing.T) {
 	testDB := pebble.NewMemTest(t)
-	pool := mempool.New(testDB)
+	pool, _, err := mempool.New(testDB, 1024)
+	require.NoError(t, err)
 	blockchain.RegisterCoreTypesToEncoder()
 
 	select {
