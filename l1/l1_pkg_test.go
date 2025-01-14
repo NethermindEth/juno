6ef9f3ee090e2a54444f507daa9f6ed2bf06510d
AnkushinDaniil <ankushin.daniil42@gmail.com>
2025-01-14 18:22:07 +0300

    Add l1 tests

--- l1/l1_pkg_test.go
+++ l1/l1_pkg_test.go
@@ -13,7 +13,9 @@ import (
 	"github.com/NethermindEth/juno/db/pebble"
 	"github.com/NethermindEth/juno/l1/contract"
 	"github.com/NethermindEth/juno/mocks"
+	"github.com/NethermindEth/juno/p2p"
 	"github.com/NethermindEth/juno/utils"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -338,7 +340,7 @@ func TestClient(t *testing.T) {
 			network := utils.Mainnet
 			chain := blockchain.New(pebble.NewMemTest(t), &network, nil)
 
-			client := NewClient(nil, chain, nopLog).WithResubscribeDelay(0).WithPollFinalisedInterval(time.Nanosecond)
+			client := NewClient(nil, chain, nopLog, nil).WithResubscribeDelay(0).WithPollFinalisedInterval(time.Nanosecond)
 
 			// We loop over each block and check that the state agrees with our expectations.
 			for _, block := range tt.blocks {
@@ -398,7 +400,7 @@ func TestUnreliableSubscription(t *testing.T) {
 	nopLog := utils.NewNopZapLogger()
 	network := utils.Mainnet
 	chain := blockchain.New(pebble.NewMemTest(t), &network, nil)
-	client := NewClient(nil, chain, nopLog).WithResubscribeDelay(0).WithPollFinalisedInterval(time.Nanosecond)
+	client := NewClient(nil, chain, nopLog, nil).WithResubscribeDelay(0).WithPollFinalisedInterval(time.Nanosecond)
 
 	err := errors.New("test err")
 	for _, block := range longSequenceOfBlocks {
@@ -464,5 +466,204 @@ func TestUnreliableSubscription(t *testing.T) {
 			}
 			assert.Equal(t, want, got)
 		}
+	}
+}
+
+func TestMakeSubscribtionsToBootnodes(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	nopLog := utils.NewNopZapLogger()
+	network := utils.Mainnet
+	address := common.HexToAddress("0x1234")
+	network.BootnodeRegistry = address
+	eventsChan := make(chan p2p.BootnodeRegistryEvent, 10)
+	chain := blockchain.New(pebble.NewMemTest(t), &network, nil)
+	client := NewClient(nil, chain, nopLog, eventsChan).WithResubscribeDelay(0).WithPollFinalisedInterval(time.Nanosecond)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	storedAddresses := []string{"0x1", "0x2", "0x3"}
+	addressesToAdd := []string{"0x4", "0x5", "0x6"}
+	addressToRemove := []string{"0x2", "0x5"}
+
+	subscriber := mocks.NewMockSubscriber(ctrl)
+	client.l1 = subscriber
+
+	subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(addressesToAdd[:2], nil).Times(1)
+	subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, sink chan<- *contract.BootnodeRegistryIPAdded) (*fakeSubscription, error) {
+			go func() {
+				for _, addr := range addressesToAdd {
+					sink <- &contract.BootnodeRegistryIPAdded{IpAddress: addr}
+				}
+			}()
+			return newFakeSubscription(), nil
+		}).
+		Times(1)
+
+	subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, sink chan<- *contract.BootnodeRegistryIPRemoved) (*fakeSubscription, error) {
+			go func() {
+				for _, addr := range addressToRemove {
+					sink <- &contract.BootnodeRegistryIPRemoved{IpAddress: addr}
+				}
+			}()
+			return newFakeSubscription(), nil
+		}).
+		Times(1)
+
+	require.NoError(t, client.makeSubscribtionsToBootnodes(ctx, 1))
+
+	expectedAddressesToAdd := make(map[string]struct{}, len(addressesToAdd)+len(storedAddresses))
+	for _, addr := range addressesToAdd {
+		expectedAddressesToAdd[addr] = struct{}{}
+	}
+	for _, addr := range storedAddresses {
+		expectedAddressesToAdd[addr] = struct{}{}
+	}
+	expectedAddressesToRemove := make(map[string]struct{}, len(addressToRemove))
+	for _, addr := range addressToRemove {
+		expectedAddressesToRemove[addr] = struct{}{}
+	}
+	select {
+	case event := <-eventsChan:
+		switch event.EventType {
+		case p2p.Add:
+			assert.Contains(t, expectedAddressesToAdd, event.Address)
+			delete(expectedAddressesToAdd, event.Address)
+		case p2p.Remove:
+			assert.Contains(t, expectedAddressesToRemove, event.Address)
+			delete(expectedAddressesToRemove, event.Address)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Expected IP address addition event")
+	}
+}
+
+func TestUnreliableSubscriptionToBootnodes(t *testing.T) {
+	t.Parallel()
+	address := common.HexToAddress("0x1234")
+	network := utils.Mainnet
+	network.BootnodeRegistry = address
+	chain := blockchain.New(pebble.NewMemTest(t), &network, nil)
+	nopLog := utils.NewNopZapLogger()
+	err := errors.New("test err")
+
+	testCases := []struct {
+		name        string
+		setupMock   func(subscriber *mocks.MockSubscriber)
+		timeOut     time.Duration
+		expectedErr error
+	}{
+		{
+			name: "GetIPAddresses error",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(nil, err).Times(1)
+			},
+			timeOut:     50 * time.Millisecond,
+			expectedErr: err,
+		},
+		{
+			name: "WatchIPAdded error",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(nil, nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).Return(nil, err).Times(1)
+				subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+			},
+			timeOut:     time.Millisecond,
+			expectedErr: context.DeadlineExceeded,
+		},
+		{
+			name: "WatchIPRemoved error",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(nil, nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+				subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).Return(nil, err).Times(1)
+				subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+			},
+			timeOut:     50 * time.Millisecond,
+			expectedErr: context.DeadlineExceeded,
+		},
+		{
+			name: "Addition subscription error",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(nil, nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).Return(newFakeSubscription(err), nil).Times(1)
+				subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+			},
+			timeOut:     50 * time.Millisecond,
+			expectedErr: context.DeadlineExceeded,
+		},
+		{
+			name: "Removal subscription error",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(nil, nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+				subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).Return(newFakeSubscription(err), nil).Times(1)
+				subscriber.EXPECT().WatchIPRemoved(gomock.Any(), gomock.Any()).Return(newFakeSubscription(), nil).Times(1)
+			},
+			timeOut:     50 * time.Millisecond,
+			expectedErr: context.DeadlineExceeded,
+		},
+		{
+			name: "Addition subscription expires",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).DoAndReturn(
+					func(_ context.Context, _ common.Address) ([]string, error) {
+						time.Sleep(100 * time.Millisecond)
+						return nil, nil
+					},
+				).Times(1)
+			},
+			expectedErr: context.DeadlineExceeded,
+			timeOut:     50 * time.Millisecond,
+		},
+		{
+			name: "Removal subscription expires",
+			setupMock: func(subscriber *mocks.MockSubscriber) {
+				subscriber.EXPECT().GetIPAddresses(gomock.Any(), address).Return(nil, nil).Times(1)
+				subscriber.EXPECT().WatchIPAdded(gomock.Any(), gomock.Any()).DoAndReturn(
+					func(_ context.Context, sink chan<- *contract.BootnodeRegistryIPAdded) (*fakeSubscription, error) {
+						time.Sleep(100 * time.Millisecond)
+						return newFakeSubscription(), nil
+					},
+				).Times(1)
+			},
+			timeOut:     50 * time.Millisecond,
+			expectedErr: context.DeadlineExceeded,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			eventsChan := make(chan p2p.BootnodeRegistryEvent, 10)
+			client := Client{
+				l1:               nil,
+				l2Chain:          chain,
+				log:              nopLog,
+				network:          chain.Network(),
+				resubscribeDelay: 0,
+				nonFinalisedLogs: make(map[uint64]*contract.StarknetLogStateUpdate, 0),
+				listener:         SelectiveListener{},
+				eventsToP2P:      eventsChan,
+			}
+
+			subscriber := mocks.NewMockSubscriber(ctrl)
+			client.l1 = subscriber
+			tc.setupMock(subscriber)
+
+			ctx, cancel := context.WithTimeout(context.Background(), tc.timeOut)
+			defer cancel()
+			err := client.makeSubscribtionsToBootnodes(ctx, 1)
+			require.ErrorIs(t, err, tc.expectedErr)
+		})
 	}
 }
