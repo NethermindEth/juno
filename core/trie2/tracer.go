4cbdaec6528c8e077010881ace9eab144762c0d8
weiihann <weihan774237@gmail.com>
2025-01-20 18:33:19 +0800

    at this point range proof tests pass 50%

--- core/trie2/tracer.go
+++ core/trie2/tracer.go
@@ -4,19 +4,19 @@ import (
 	"maps"
 )
 
-type tracer struct {
+type nodeTracer struct {
 	inserts map[Path]struct{}
 	deletes map[Path]struct{}
 }
 
-func newTracer() *tracer {
-	return &tracer{
+func newTracer() *nodeTracer {
+	return &nodeTracer{
 		inserts: make(map[Path]struct{}),
 		deletes: make(map[Path]struct{}),
 	}
 }
 
-func (t *tracer) onInsert(key *Path) {
+func (t *nodeTracer) onInsert(key *Path) {
 	k := *key
 	if _, present := t.deletes[k]; present {
 		return
@@ -24,7 +24,7 @@ func (t *tracer) onInsert(key *Path) {
 	t.inserts[k] = struct{}{}
 }
 
-func (t *tracer) onDelete(key *Path) {
+func (t *nodeTracer) onDelete(key *Path) {
 	k := *key
 	if _, present := t.inserts[k]; present {
 		return
@@ -32,19 +32,19 @@ func (t *tracer) onDelete(key *Path) {
 	t.deletes[k] = struct{}{}
 }
 
-func (t *tracer) reset() {
+func (t *nodeTracer) reset() {
 	t.inserts = make(map[Path]struct{})
 	t.deletes = make(map[Path]struct{})
 }
 
-func (t *tracer) copy() *tracer {
-	return &tracer{
+func (t *nodeTracer) copy() *nodeTracer {
+	return &nodeTracer{
 		inserts: maps.Clone(t.inserts),
 		deletes: maps.Clone(t.deletes),
 	}
 }
 
-func (t *tracer) deletedNodes() []Path {
+func (t *nodeTracer) deletedNodes() []Path {
 	keys := make([]Path, 0, len(t.deletes))
 	for k := range t.deletes {
 		keys = append(keys, k)
