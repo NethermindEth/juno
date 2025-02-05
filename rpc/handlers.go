c646d212fa6fb1741f20722a5276a3302ce02981
Ng Wei Han <47109095+weiihann@users.noreply.github.com>
2025-02-05 19:20:50 +0800

    Use `sync.Map` for subscriptions map (#2422)
    * Use sync.Map for subscriptions map
    
    * Add tests for Unsubscribe

--- rpc/handlers.go
+++ rpc/handlers.go
@@ -100,8 +100,7 @@ type Handler struct {
 	l1Heads    *feed.Feed[*core.L1Head]
 
 	idgen         func() uint64
-	mu            stdsync.Mutex // protects subscriptions.
-	subscriptions map[uint64]*subscription
+	subscriptions stdsync.Map // map[uint64]*subscription
 
 	blockTraceCache *lru.Cache[traceCacheKey, []TracedBlockTransaction]
 
@@ -136,12 +135,11 @@ func New(bcReader blockchain.Reader, syncReader sync.Reader, virtualMachine vm.V
 			}
 			return n
 		},
-		version:       version,
-		newHeads:      feed.New[*core.Header](),
-		reorgs:        feed.New[*sync.ReorgBlockRange](),
-		pendingTxs:    feed.New[[]core.Transaction](),
-		l1Heads:       feed.New[*core.L1Head](),
-		subscriptions: make(map[uint64]*subscription),
+		version:    version,
+		newHeads:   feed.New[*core.Header](),
+		reorgs:     feed.New[*sync.ReorgBlockRange](),
+		pendingTxs: feed.New[[]core.Transaction](),
+		l1Heads:    feed.New[*core.L1Head](),
 
 		blockTraceCache: lru.NewCache[traceCacheKey, []TracedBlockTransaction](traceCacheSize),
 		filterLimit:     math.MaxUint,
@@ -195,9 +193,11 @@ func (h *Handler) Run(ctx context.Context) error {
 	feed.Tee(l1HeadsSub, h.l1Heads)
 
 	<-ctx.Done()
-	for _, sub := range h.subscriptions {
+	h.subscriptions.Range(func(key, value any) bool {
+		sub := value.(*subscription)
 		sub.wg.Wait()
-	}
+		return true
+	})
 	return nil
 }
 
