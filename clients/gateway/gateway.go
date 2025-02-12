59de229ac14043a7d2dff7717f35be0c612a5e01
Hugo Demeyere <demeyere.hugo@gmail.com>
2025-02-12 16:31:10 +0100

    fix: Remove automatic indentation

--- clients/gateway/gateway.go
+++ clients/gateway/gateway.go
@@ -19,21 +19,21 @@ import (
 )
 
 var (
-	InvalidContractClass             ErrorCode = "StarknetErrorCode.INVALID_CONTRACT_CLASS"
-	UndeclaredClass                  ErrorCode = "StarknetErrorCode.UNDECLARED_CLASS"
-	ClassAlreadyDeclared             ErrorCode = "StarknetErrorCode.CLASS_ALREADY_DECLARED"
-	InsufficientMaxFee               ErrorCode = "StarknetErrorCode.INSUFFICIENT_MAX_FEE"
+	InvalidContractClass            ErrorCode = "StarknetErrorCode.INVALID_CONTRACT_CLASS"
+	UndeclaredClass                 ErrorCode = "StarknetErrorCode.UNDECLARED_CLASS"
+	ClassAlreadyDeclared            ErrorCode = "StarknetErrorCode.CLASS_ALREADY_DECLARED"
+	InsufficientMaxFee              ErrorCode = "StarknetErrorCode.INSUFFICIENT_MAX_FEE"
 	InsufficientResourcesForValidate ErrorCode = "StarknetErrorCode.INSUFFICIENT_RESOURCES_FOR_VALIDATE"
-	InsufficientAccountBalance       ErrorCode = "StarknetErrorCode.INSUFFICIENT_ACCOUNT_BALANCE"
-	ValidateFailure                  ErrorCode = "StarknetErrorCode.VALIDATE_FAILURE"
-	ContractBytecodeSizeTooLarge     ErrorCode = "StarknetErrorCode.CONTRACT_BYTECODE_SIZE_TOO_LARGE"
-	DuplicatedTransaction            ErrorCode = "StarknetErrorCode.DUPLICATED_TRANSACTION"
-	InvalidTransactionNonce          ErrorCode = "StarknetErrorCode.INVALID_TRANSACTION_NONCE"
-	CompilationFailed                ErrorCode = "StarknetErrorCode.COMPILATION_FAILED"
-	InvalidCompiledClassHash         ErrorCode = "StarknetErrorCode.INVALID_COMPILED_CLASS_HASH"
-	ContractClassObjectSizeTooLarge  ErrorCode = "StarknetErrorCode.CONTRACT_CLASS_OBJECT_SIZE_TOO_LARGE"
-	InvalidTransactionVersion        ErrorCode = "StarknetErrorCode.INVALID_TRANSACTION_VERSION"
-	InvalidContractClassVersion      ErrorCode = "StarknetErrorCode.INVALID_CONTRACT_CLASS_VERSION"
+	InsufficientAccountBalance      ErrorCode = "StarknetErrorCode.INSUFFICIENT_ACCOUNT_BALANCE"
+	ValidateFailure                 ErrorCode = "StarknetErrorCode.VALIDATE_FAILURE"
+	ContractBytecodeSizeTooLarge    ErrorCode = "StarknetErrorCode.CONTRACT_BYTECODE_SIZE_TOO_LARGE"
+	DuplicatedTransaction           ErrorCode = "StarknetErrorCode.DUPLICATED_TRANSACTION"
+	InvalidTransactionNonce         ErrorCode = "StarknetErrorCode.INVALID_TRANSACTION_NONCE"
+	CompilationFailed               ErrorCode = "StarknetErrorCode.COMPILATION_FAILED"
+	InvalidCompiledClassHash        ErrorCode = "StarknetErrorCode.INVALID_COMPILED_CLASS_HASH"
+	ContractClassObjectSizeTooLarge ErrorCode = "StarknetErrorCode.CONTRACT_CLASS_OBJECT_SIZE_TOO_LARGE"
+	InvalidTransactionVersion       ErrorCode = "StarknetErrorCode.INVALID_TRANSACTION_VERSION"
+	InvalidContractClassVersion     ErrorCode = "StarknetErrorCode.INVALID_CONTRACT_CLASS_VERSION"
 )
 
 type Client struct {
