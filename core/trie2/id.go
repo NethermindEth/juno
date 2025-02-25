26a135fc10da61ae6dafbbc05aec643bb953d622
weiihann <weihan774237@gmail.com>
2025-02-25 13:57:56 +0800

    feat(trie): use interface for trie identifiers

--- core/trie2/id.go
+++ core/trie2/id.go
@@ -5,6 +5,13 @@ import (
 	"github.com/NethermindEth/juno/db"
 )
 
+var (
+	_ TrieID = (*ClassTrieID)(nil)
+	_ TrieID = (*ContractTrieID)(nil)
+	_ TrieID = (*ContractStorageTrieID)(nil)
+	_ TrieID = (*EmptyTrieID)(nil)
+)
+
 type TrieType uint8
 
 const (
@@ -19,43 +26,43 @@ type ID struct {
 	Owner    felt.Felt // The contract address which the trie belongs to
 }
 
-// Returns the corresponding DB bucket for the trie
-func (id *ID) Bucket() db.Bucket {
-	switch id.TrieType {
-	case ClassTrie:
-		return db.ClassTrie
-	case ContractTrie:
-		if id.Owner == (felt.Felt{}) {
-			return db.ContractTrieContract
-		}
-		return db.ContractTrieStorage
-	case Empty:
-		return db.Bucket(0)
-	default:
-		panic("invalid trie type")
-	}
+// Represents a class trie
+type ClassTrieID struct{}
+
+func NewClassTrieID() *ClassTrieID          { return &ClassTrieID{} }
+func (ClassTrieID) Bucket() db.Bucket       { return db.ClassTrie }
+func (ClassTrieID) Owner() felt.Felt        { return felt.Zero }
+func (ClassTrieID) IsContractStorage() bool { return false }
+
+// Represents a contract trie
+type ContractTrieID struct{}
+
+func NewContractTrieID() *ContractTrieID          { return &ContractTrieID{} }
+func (id ContractTrieID) Bucket() db.Bucket       { return db.ContractTrieContract }
+func (id ContractTrieID) Owner() felt.Felt        { return felt.Zero }
+func (id ContractTrieID) IsContractStorage() bool { return false }
+
+// Represents a contract storage trie
+type ContractStorageTrieID struct {
+	owner felt.Felt
 }
 
-// Constructs an identifier for a class trie with the provided class trie root hash
-func ClassTrieID() *ID {
-	return &ID{
-		TrieType: ClassTrie,
-		Owner:    felt.Zero, // class trie does not have an owner
-	}
+func NewContractStorageTrieID(owner felt.Felt) *ContractStorageTrieID {
+	return &ContractStorageTrieID{owner: owner}
 }
 
-// Constructs an identifier for a contract trie or a contract's storage trie
-func ContractTrieID(owner felt.Felt) *ID {
-	return &ID{
-		TrieType: ContractTrie,
-		Owner:    owner,
-	}
+func NewContractStorageTrieIDFromFelt(owner felt.Felt) *ContractStorageTrieID {
+	return &ContractStorageTrieID{owner: owner}
 }
 
-// A general identifier, typically used for temporary trie
-func TrieID() *ID {
-	return &ID{
-		TrieType: Empty,
-		Owner:    felt.Zero,
-	}
-}
+func (id ContractStorageTrieID) Bucket() db.Bucket       { return db.ContractTrieStorage }
+func (id ContractStorageTrieID) Owner() felt.Felt        { return id.owner }
+func (id ContractStorageTrieID) IsContractStorage() bool { return true }
+
+// Represents an empty trie, only used for temporary purposes
+type EmptyTrieID struct{}
+
+func NewEmptyTrieID() *EmptyTrieID          { return &EmptyTrieID{} }
+func (EmptyTrieID) Bucket() db.Bucket       { return db.Bucket(0) }
+func (EmptyTrieID) Owner() felt.Felt        { return felt.Zero }
+func (EmptyTrieID) IsContractStorage() bool { return false }
