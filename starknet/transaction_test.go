4c5f492923f8396afef0d8fcfcfb04cfbc7d41b4
thiagodeev <thiagodeev@gmail.com>
2025-03-04 15:59:01 -0300

    Add tests

--- starknet/transaction_test.go
+++ starknet/transaction_test.go
@@ -44,4 +44,87 @@ func TestUnmarshalFinalityStatus(t *testing.T) {
 	require.ErrorContains(t, json.Unmarshal([]byte(`"ABC"`), fs), "unknown FinalityStatus")
 }
 
-// TODO: add more tests for other types (e.g. Resource, DataAvailabilityMode, etc.)
+func TestResourceMarshalText(t *testing.T) {
+	tests := []struct {
+		name     string
+		resource starknet.Resource
+		want     []byte
+		err      bool
+	}{
+		{
+			name:     "l1 gas",
+			resource: starknet.ResourceL1Gas,
+			want:     []byte("L1_GAS"),
+		},
+		{
+			name:     "l2 gas",
+			resource: starknet.ResourceL2Gas,
+			want:     []byte("L2_GAS"),
+		},
+		{
+			name:     "l1 data gas",
+			resource: starknet.ResourceL1DataGas,
+			want:     []byte("L1_DATA_GAS"),
+		},
+		{
+			name:     "error",
+			resource: starknet.Resource(0),
+			err:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.resource.MarshalText()
+			if tt.err {
+				require.Error(t, err)
+				require.Nil(t, got)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestResourceUnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name string
+		data []byte
+		want starknet.Resource
+		err  bool
+	}{
+		{
+			name: "l1 gas",
+			data: []byte("L1_GAS"),
+			want: starknet.ResourceL1Gas,
+		},
+		{
+			name: "l2 gas",
+			data: []byte("L2_GAS"),
+			want: starknet.ResourceL2Gas,
+		},
+		{
+			name: "l1 data gas",
+			data: []byte("L1_DATA_GAS"),
+			want: starknet.ResourceL1DataGas,
+		},
+		{
+			name: "error",
+			data: []byte("unknown"),
+			err:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got starknet.Resource
+			err := got.UnmarshalJSON(tt.data)
+			if tt.err {
+				require.Error(t, err)
+				require.Zero(t, got)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
