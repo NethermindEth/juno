998a15c98d5e7780d216b719b91ec58103678361
hudem1 <55464342+hudem1@users.noreply.github.com>
2025-03-05 21:59:08 +0100

    fix: Reverted INVOKE tx returns a non-nil ExecuteInvocation in trace (#2617)
    * fix: Reverted INVOKE tx returns a non-nil ExecuteInvocation in trace
    
    * fix: lint

--- rpc/v7/adapters.go
+++ rpc/v7/adapters.go
@@ -122,22 +122,23 @@ func AdaptFeederBlockTrace(block *BlockWithTxs, blockTrace *starknet.BlockTrace)
 			trace.ValidateInvocation = utils.HeapPtr(rpcv6.AdaptFeederFunctionInvocation(val))
 		}
 
+		var fnInvocation *rpcv6.FunctionInvocation
 		if fct := feederTrace.FunctionInvocation; fct != nil {
-			fnInvocation := utils.HeapPtr(rpcv6.AdaptFeederFunctionInvocation(fct))
+			fnInvocation = utils.HeapPtr(rpcv6.AdaptFeederFunctionInvocation(fct))
+		}
 
-			switch block.Transactions[index].Type {
-			case TxnDeploy, TxnDeployAccount:
-				trace.ConstructorInvocation = fnInvocation
-			case TxnInvoke:
-				trace.ExecuteInvocation = new(rpcv6.ExecuteInvocation)
-				if feederTrace.RevertError != "" {
-					trace.ExecuteInvocation.RevertReason = feederTrace.RevertError
-				} else {
-					trace.ExecuteInvocation.FunctionInvocation = fnInvocation
-				}
-			case TxnL1Handler:
-				trace.FunctionInvocation = fnInvocation
+		switch trace.Type {
+		case TxnDeploy, TxnDeployAccount:
+			trace.ConstructorInvocation = fnInvocation
+		case TxnInvoke:
+			trace.ExecuteInvocation = new(rpcv6.ExecuteInvocation)
+			if feederTrace.RevertError != "" {
+				trace.ExecuteInvocation.RevertReason = feederTrace.RevertError
+			} else {
+				trace.ExecuteInvocation.FunctionInvocation = fnInvocation
 			}
+		case TxnL1Handler:
+			trace.FunctionInvocation = fnInvocation
 		}
 
 		adaptedTraces[index] = TracedBlockTransaction{
