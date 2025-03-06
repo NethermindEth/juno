ba769eba6ed5c43c6f8ea4155f21700b18fd92f6
AnkushinDaniil <ankushin.daniil42@gmail.com>
2025-03-06 14:58:27 +0300

    Add balance

--- rpc/v8/estimate_fee_test.go
+++ rpc/v8/estimate_fee_test.go
@@ -3,12 +3,14 @@ package rpcv8_test
 import (
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"os"
 	"testing"
 
 	"github.com/NethermindEth/juno/adapters/sn2core"
 	"github.com/NethermindEth/juno/blockchain"
 	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/crypto"
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/db/pebble"
 	"github.com/NethermindEth/juno/jsonrpc"
@@ -241,7 +243,7 @@ func testStorage(t *testing.T, protocolVersion string) (*blockchain.Blockchain, 
 		},
 	}
 	genesisStateUpdate := &core.StateUpdate{
-		BlockHash: genesis.Header.Hash,
+		BlockHash: genesis.Hash,
 		NewRoot:   &felt.Zero,
 		OldRoot:   &felt.Zero,
 		StateDiff: &core.StateDiff{},
@@ -249,34 +251,53 @@ func testStorage(t *testing.T, protocolVersion string) (*blockchain.Blockchain, 
 
 	require.NoError(t, chain.Store(genesis, &core.BlockCommitments{}, genesisStateUpdate, nil))
 
-	accountAddr := new(felt.Felt).SetUint64(0xc01)
+	accountAddr := utils.HexToFelt(t, "0xc01")
 	_, _, accountClass := classFromFile(t, "../../cairo/account/target/dev/account_AccountUpgradeable.contract_class.json")
 	accountClassHash, err := accountClass.Hash()
 	require.NoError(t, err)
 
-	deployerAddr := new(felt.Felt).SetUint64(0xc02)
+	deployerAddr := utils.HexToFelt(t, "0xc02")
 	_, _, delployerClass := classFromFile(t, "../../cairo/universal_deployer/target/dev/universal_deployer_UniversalDeployer.contract_class.json")
 	delployerClassHash, err := delployerClass.Hash()
 	require.NoError(t, err)
 
+	ethFeeTokenAddr := utils.HexToFelt(t, "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7")
+	strkFeeTokenAddr := utils.HexToFelt(t, "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d")
+	_, _, erc20Class := classFromFile(t, "../../cairo/erc20/target/dev/erc20_ERC20Upgradeable.contract_class.json")
+	erc20ClassHash, err := erc20Class.Hash()
+	require.NoError(t, err)
+
+	accountBalanceKey := fromNameAndKey(t, "ERC20_balances", accountAddr)
+
 	stateUpdate := &core.StateUpdate{
 		OldRoot: &felt.Zero,
-		NewRoot: utils.HexToFelt(t, "0x1d653077282c72d3177d8339ceb8de61b9b4f5621317da6a2e6dc24ce32a6e3"),
+		NewRoot: utils.HexToFelt(t, "0x13bd4534776b6417fcee058e4236fcfa235f7d3a08d910b04899606de137661"),
 		StateDiff: &core.StateDiff{
 			DeployedContracts: map[felt.Felt]*felt.Felt{
-				*accountAddr:  accountClassHash,
-				*deployerAddr: delployerClassHash,
+				*accountAddr:      accountClassHash,
+				*deployerAddr:     delployerClassHash,
+				*ethFeeTokenAddr:  erc20ClassHash,
+				*strkFeeTokenAddr: erc20ClassHash,
+			},
+			StorageDiffs: map[felt.Felt]map[felt.Felt]*felt.Felt{
+				*ethFeeTokenAddr: {
+					*accountBalanceKey: utils.HexToFelt(t, "0x10000000000000000000000000000"),
+				},
+				*strkFeeTokenAddr: {
+					*accountBalanceKey: utils.HexToFelt(t, "0x10000000000000000000000000000"),
+				},
 			},
 		},
 	}
 	newClasses := map[felt.Felt]core.Class{
 		*accountClassHash:   accountClass,
 		*delployerClassHash: delployerClass,
+		*erc20ClassHash:     erc20Class,
 	}
 	block := &core.Block{
 		Header: &core.Header{
 			Hash:             utils.HexToFelt(t, "0xb01"),
-			ParentHash:       genesis.Header.Hash,
+			ParentHash:       genesis.Hash,
 			Number:           1,
 			GlobalStateRoot:  stateUpdate.NewRoot,
 			SequencerAddress: utils.HexToFelt(t, "0x1176a1bd84444c89232ec27754698e5d2e7e1a7f1539f12027f28b23ec9f3d8"),
@@ -310,4 +331,18 @@ func classFromFile(t *testing.T, path string) (*starknet.SierraDefinition, *star
 	require.NoError(t, err)
 
 	return snClass, compliedClass, class
+}
+
+func fromNameAndKey(t *testing.T, name string, key *felt.Felt) *felt.Felt {
+	t.Helper()
+	intermediate := new(felt.Felt).SetBytes(snUtils.Keccak256([]byte(name)))
+	byteArr := crypto.Pedersen(intermediate, key).Bytes()
+	value := new(big.Int).SetBytes(byteArr[:])
+
+	maxAddr, ok := new(big.Int).SetString("0x7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00", 0)
+	require.True(t, ok)
+
+	value = value.Mod(value, maxAddr)
+
+	return new(felt.Felt).SetBigInt(value)
 }
