548670f35f91aff589b54e560604f1cbbe925df0
IronGauntlets <aneequesafdar@gmail.com>
2025-03-14 21:57:06 +0400

    Refactor propose.go

--- consensus/tendermint/precommit.go
+++ consensus/tendermint/precommit.go
@@ -35,13 +35,6 @@ func (t *Tendermint[V, H, A]) handlePrecommit(p Precommit[H, A]) {
 
 		t.futureMessages.addPrecommit(p)
 
-		/*
-			Check upon condition line 55:
-
-				55: upon f + 1 {∗, h_p, round, ∗, ∗} with round > round_p do
-				56: 	StartRound(round)
-		*/
-
 		t.line55(p.Round)
 		return
 	}
@@ -50,25 +43,33 @@ func (t *Tendermint[V, H, A]) handlePrecommit(p Precommit[H, A]) {
 
 	proposalsForHR, _, precommitsForHR := t.messages.allMessages(p.Height, p.Round)
 
-	/*
-		Check the upon condition on line 49:
+	if t.line49WhenPrecommitIsReceived(p, proposalsForHR, precommitsForHR) {
+		return
+	}
 
-			49: upon {PROPOSAL, h_p, r, v, *} from proposer(h_p, r) AND 2f + 1 {PRECOMMIT, h_p, r, id(v)} while decision_p[h_p] = nil do
-			50: 	if valid(v) then
-			51: 		decisionp[hp] = v
-			52: 		h_p ← h_p + 1
-			53: 		reset lockedRound_p, lockedValue_p,	validRound_p and validValue_p to initial values and empty message log
-			54: 		StartRound(0)
+	t.line47(p, precommitsForHR)
+}
 
-		Fetching the relevant proposal implies the sender of the proposal was the proposer for that
-		height and round. Also, since only the proposals with valid value are added to the message set, the
-		validity of the proposal can be skipped.
+/*
+Check the upon condition on line 49:
 
-		There is no need to check decision_p[h_p] = nil since it is implied that decision are made
-		sequentially, i.e. x, x+1, x+2... .
+	49: upon {PROPOSAL, h_p, r, v, *} from proposer(h_p, r) AND 2f + 1 {PRECOMMIT, h_p, r, id(v)} while decision_p[h_p] = nil do
+	50: 	if valid(v) then
+	51: 		decisionp[hp] = v
+	52: 		h_p ← h_p + 1
+	53: 		reset lockedRound_p, lockedValue_p,	validRound_p and validValue_p to initial values and empty message log
+	54: 		StartRound(0)
 
-	*/
+Fetching the relevant proposal implies the sender of the proposal was the proposer for that
+height and round. Also, since only the proposals with valid value are added to the message set, the
+validity of the proposal can be skipped.
 
+There is no need to check decision_p[h_p] = nil since it is implied that decision are made
+sequentially, i.e. x, x+1, x+2... .
+*/
+func (t *Tendermint[V, H, A]) line49WhenPrecommitIsReceived(p Precommit[H, A], proposalsForHR map[A][]Proposal[V, H,
+	A], precommitsForHR map[A][]Precommit[H, A],
+) bool {
 	if p.ID != nil {
 		var (
 			proposal   *Proposal[V, H, A]
@@ -98,17 +99,19 @@ func (t *Tendermint[V, H, A]) handlePrecommit(p Precommit[H, A]) {
 			t.state.height++
 			t.startRound(0)
 
-			return
+			return true
 		}
 	}
+	return false
+}
 
-	/*
-		Check the upon condition on line 47:
+/*
+Check the upon condition on line 47:
 
-			47: upon 2f + 1 {PRECOMMIT, h_p, round_p, ∗} for the first time do
-			48: schedule OnTimeoutPrecommit(h_p , round_p) to be executed after timeoutPrecommit(round_p)
-	*/
-
+	47: upon 2f + 1 {PRECOMMIT, h_p, round_p, ∗} for the first time do
+	48: schedule OnTimeoutPrecommit(h_p , round_p) to be executed after timeoutPrecommit(round_p)
+*/
+func (t *Tendermint[V, H, A]) line47(p Precommit[H, A], precommitsForHR map[A][]Precommit[H, A]) {
 	vals := slices.Collect(maps.Keys(precommitsForHR))
 	if p.Round == t.state.round && !t.state.line47Executed &&
 		t.validatorSetVotingPower(vals) >= q(t.validators.TotalVotingPower(p.Height)) {
