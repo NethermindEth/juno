92d1677701162efcdc48dfba7de0b319bc4a9521
AnkushinDaniil <ankushin.daniil42@gmail.com>
2025-03-17 13:10:31 +0400

    test(rpc/v7): fix ENTRY_POINT_NOT_FOUND case

--- rpc/v7/trace_test.go
+++ rpc/v7/trace_test.go
@@ -1542,7 +1542,13 @@ func TestCall(t *testing.T) {
 		}
 		expectedErr := rpccore.ErrContractError
 		expectedErr.Data = rpcv7.ContractErrorData{
-			RevertError: json.RawMessage(`"` + rpccore.ErrEntrypointNotFound.Message + `"`),
+			RevertError: json.RawMessage(`"` +
+				fmt.Sprintf(
+					"Entry point EntryPointSelector(%s) not found in contract.",
+					selector.String(),
+				) +
+				`"`,
+			),
 		}
 
 		headsHeader := &core.Header{
