8c7931937eb07c73da670a8e16db45ea93d6eca8
Daniil Ankushin <ankushin.daniil42@gmail.com>
2025-03-18 12:57:27 +0400

    refactor: replace `interface{}` with `any` (#2649)
    refactor: replace interface{} with any

--- utils/nil_test.go
+++ utils/nil_test.go
@@ -8,13 +8,13 @@ import (
 
 func TestIsNil(t *testing.T) {
 	t.Run("both interface's type and value are nil", func(t *testing.T) {
-		var i interface{}
+		var i any
 		// assert.Nil() is not used to avoid assert package from using reflection
 		assert.True(t, IsNil(i))
 	})
 	t.Run("only interface's value is nil", func(t *testing.T) {
 		var arr []int
-		var i interface{} = arr
+		var i any = arr
 		// assert.Nil() is not used to avoid assert package from using reflection
 		assert.True(t, IsNil(i))
 	})
