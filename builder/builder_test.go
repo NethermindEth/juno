d9c2d0db120cf7e9b81ac4f9c9600c7641b96449
rian <rian.hughes@physics.ox.ac.uk>
2025-03-26 16:37:09 +0200

    very long timeout..

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -198,7 +198,7 @@ func TestPrefundedAccounts(t *testing.T) {
 	for _, txn := range expectedExnsInBlock {
 		rpcHandler.AddTransaction(t.Context(), txn)
 	}
-	ctx, cancel := context.WithTimeout(t.Context(), 10*blockTime)
+	ctx, cancel := context.WithTimeout(t.Context(), 100*blockTime)
 	defer cancel()
 
 	waitForTxns(ctx, t, blockTime, bc, 2)
