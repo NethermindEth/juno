e14b308c8004451c842d4f26263400807a979ba1
weiihann <weihan774237@gmail.com>
2025-04-28 13:33:08 +0800

    feat: add pebble/v2 package

---
+++ db/pebblev2/batch.go
@@ -0,0 +1,119 @@
+package pebblev2
+
+import (
+	"errors"
+	"time"
+
+	"github.com/NethermindEth/juno/db"
+	"github.com/NethermindEth/juno/db/dbutils"
+	pebble "github.com/cockroachdb/pebble/v2"
+)
+
+var _ db.Batch = (*batch)(nil)
+
+type batch struct {
+	batch    *pebble.Batch
+	db       *DB
+	size     int // size of the batch in bytes
+	listener db.EventListener
+}
+
+func NewBatch(dbBatch *pebble.Batch, db *DB, listener db.EventListener) *batch {
+	return &batch{
+		batch:    dbBatch,
+		db:       db,
+		listener: listener,
+	}
+}
+
+// Delete : see db.Transaction.Delete
+func (b *batch) Delete(key []byte) error {
+	start := time.Now()
+	defer func() { b.listener.OnIO(true, time.Since(start)) }()
+
+	if err := b.batch.Delete(key, pebble.Sync); err != nil {
+		return err
+	}
+	b.size += len(key)
+	return nil
+}
+
+func (b *batch) DeleteRange(start, end []byte) error {
+	return b.batch.DeleteRange(start, end, pebble.Sync)
+}
+
+//nolint:dupl
+func (b *batch) Get(key []byte, cb func(value []byte) error) error {
+	start := time.Now()
+	defer func() { b.listener.OnIO(false, time.Since(start)) }()
+
+	val, closer, err := b.batch.Get(key)
+	if err != nil {
+		if errors.Is(err, pebble.ErrNotFound) {
+			return db.ErrKeyNotFound
+		}
+		return err
+	}
+
+	if err := cb(val); err != nil {
+		return err
+	}
+
+	return closer.Close()
+}
+
+func (b *batch) Has(key []byte) (bool, error) {
+	_, closer, err := b.batch.Get(key)
+	if err != nil {
+		if errors.Is(err, pebble.ErrNotFound) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return true, closer.Close()
+}
+
+func (b *batch) NewIterator(lowerBound []byte, withUpperBound bool) (db.Iterator, error) {
+	var iter *pebble.Iterator
+	var err error
+
+	iterOpt := &pebble.IterOptions{LowerBound: lowerBound}
+	if withUpperBound {
+		iterOpt.UpperBound = dbutils.UpperBound(lowerBound)
+	}
+
+	iter, err = b.batch.NewIter(iterOpt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &iterator{iter: iter}, nil
+}
+
+func (b *batch) Put(key, value []byte) error {
+	if err := b.batch.Set(key, value, pebble.Sync); err != nil {
+		return err
+	}
+	b.size += len(key) + len(value)
+	return nil
+}
+
+func (b *batch) Size() int {
+	return b.size
+}
+
+func (b *batch) Write() error {
+	b.db.closeLock.RLock()
+	defer b.db.closeLock.RUnlock()
+
+	if b.db.closed {
+		return pebble.ErrClosed
+	}
+	return b.batch.Commit(pebble.Sync)
+}
+
+func (b *batch) Reset() {
+	b.batch.Reset()
+	b.size = 0
+}
---
+++ db/pebblev2/snapshot.go
@@ -0,0 +1,70 @@
+package pebblev2
+
+import (
+	"errors"
+	"time"
+
+	"github.com/NethermindEth/juno/db"
+	"github.com/NethermindEth/juno/db/dbutils"
+	pebble "github.com/cockroachdb/pebble/v2"
+)
+
+var _ db.Snapshot = (*snapshot)(nil)
+
+type snapshot struct {
+	snapshot *pebble.Snapshot
+	listener db.EventListener
+}
+
+func NewSnapshot(db *pebble.DB, listener db.EventListener) *snapshot {
+	return &snapshot{snapshot: db.NewSnapshot(), listener: listener}
+}
+
+func (s *snapshot) Has(key []byte) (bool, error) {
+	_, closer, err := s.snapshot.Get(key)
+	if err != nil {
+		return false, err
+	}
+
+	return true, closer.Close()
+}
+
+//nolint:dupl
+func (s *snapshot) Get(key []byte, cb func(value []byte) error) error {
+	start := time.Now()
+
+	defer func() {
+		s.listener.OnIO(false, time.Since(start))
+	}()
+	data, closer, err := s.snapshot.Get(key)
+	if err != nil {
+		if errors.Is(err, pebble.ErrNotFound) {
+			return db.ErrKeyNotFound
+		}
+		return err
+	}
+
+	if err := cb(data); err != nil {
+		return err
+	}
+
+	return closer.Close()
+}
+
+func (s *snapshot) NewIterator(prefix []byte, withUpperBound bool) (db.Iterator, error) {
+	iterOpt := &pebble.IterOptions{LowerBound: prefix}
+	if withUpperBound {
+		iterOpt.UpperBound = dbutils.UpperBound(prefix)
+	}
+
+	it, err := s.snapshot.NewIter(iterOpt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &iterator{iter: it}, nil
+}
+
+func (s *snapshot) Close() error {
+	return s.snapshot.Close()
+}
