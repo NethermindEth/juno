1493d2812a7648d81b140e18fe6eb586ffc0557a
infrmtcs <DuongDat.Informatics@rocketmail.com>
2025-05-13 13:10:54 +0700

    refactor: Move timeout rules back to its separate files

--- consensus/tendermint/tendermint.go
+++ consensus/tendermint/tendermint.go
@@ -233,24 +233,3 @@ func (t *stateMachine[V, H, A]) findProposal(r types.Round) *CachedProposal[V, H
 		ID:       utils.HeapPtr((*v.Value).Hash()),
 	}
 }
-
-func (t *stateMachine[V, H, A]) onTimeoutPropose(h types.Height, r types.Round) Action[V, H, A] {
-	if t.state.height == h && t.state.round == r && t.state.step == types.StepPropose {
-		return t.setStepAndSendPrevote(nil)
-	}
-	return nil
-}
-
-func (t *stateMachine[V, H, A]) onTimeoutPrevote(h types.Height, r types.Round) Action[V, H, A] {
-	if t.state.height == h && t.state.round == r && t.state.step == types.StepPrevote {
-		return t.setStepAndSendPrecommit(nil)
-	}
-	return nil
-}
-
-func (t *stateMachine[V, H, A]) onTimeoutPrecommit(h types.Height, r types.Round) Action[V, H, A] {
-	if t.state.height == h && t.state.round == r {
-		return t.startRound(r + 1)
-	}
-	return nil
-}
