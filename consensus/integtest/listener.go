948c253194d9b74750345566a311a435a441e1c4
Dat Duong <DuongDat.Informatics@rocketmail.com>
2025-05-15 17:55:31 +0700

    test: Integration tests for cluster of Tendermint drivers (#2790)
    * test: Integ tests for cluster of Tendermint drivers
    
    * refactor: Use value instead of reference for some types in consensus integ
    
    * refactor: Add some comments and rename to integtest
    
    * fix: Remove the future height and round restriction to avoid deadlock

---
+++ consensus/integtest/listener.go
@@ -0,0 +1,20 @@
+package integtest
+
+import (
+	"github.com/NethermindEth/juno/consensus/types"
+	"github.com/NethermindEth/juno/core/felt"
+)
+
+type listener[M types.Message[value, felt.Felt, felt.Felt]] struct {
+	ch chan M
+}
+
+func newListener[M types.Message[value, felt.Felt, felt.Felt]](buffer int) listener[M] {
+	return listener[M]{
+		ch: make(chan M, buffer),
+	}
+}
+
+func (l listener[M]) Listen() <-chan M {
+	return l.ch
+}
