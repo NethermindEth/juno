2e5fc63366b27d0aa65d189ce0d2f1747f35b58f
Turan Ege Caner <turan.caner@nethermind.io>
2025-05-20 16:34:58 +0300

    SubmittedTransactionCache: implementation and test

---
+++ rpc/rpccore/submitted_transaction_cache.go
@@ -0,0 +1,72 @@
+package rpccore
+
+import (
+	"sync"
+	"time"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/ethereum/go-ethereum/common/lru"
+)
+
+type SubmittedTransactionsCache struct {
+	cache    lru.BasicLRU[felt.Felt, time.Time]
+	entryTTL time.Duration
+	mu       sync.Mutex
+}
+
+func NewSubmittedTransactionsCache(capacity int, entryTTL time.Duration) *SubmittedTransactionsCache {
+	return &SubmittedTransactionsCache{
+		cache:    lru.NewBasicLRU[felt.Felt, time.Time](capacity),
+		entryTTL: entryTTL,
+	}
+}
+
+// Adds new entry to cache. Flushes all expired entries.
+func (c *SubmittedTransactionsCache) Add(txnHash felt.Felt) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.flush()
+	c.cache.Add(txnHash, time.Now())
+}
+
+// Returns true if the entry exists in the cache and has not exceeded its lifespan; otherwise, returns false.
+func (c *SubmittedTransactionsCache) Contains(txnHash felt.Felt) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	val, hit := c.cache.Get(txnHash)
+	if !hit {
+		return false
+	}
+
+	if time.Since(val) > c.entryTTL {
+		c.cache.Remove(txnHash)
+		return false
+	}
+
+	return true
+}
+
+// Removes all entries that have exceeded their lifespan.
+func (c *SubmittedTransactionsCache) Flush() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.flush()
+}
+
+// Removes all entries that have exceeded their lifespan.
+func (c *SubmittedTransactionsCache) flush() {
+	expiredKeys := make([]felt.Felt, 0)
+	for _, k := range c.cache.Keys() {
+		val, ok := c.cache.Peek(k)
+		if ok && time.Since(val) > c.entryTTL {
+			expiredKeys = append(expiredKeys, k)
+		}
+	}
+
+	for _, k := range expiredKeys {
+		c.cache.Remove(k)
+	}
+}
---
+++ rpc/rpccore/submitted_transaction_cache_test.go
@@ -0,0 +1,110 @@
+package rpccore_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/rpc/rpccore"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAdd(t *testing.T) {
+	capacity := 5
+	entryTTL := time.Second
+
+	t.Run("Insert to non-full cache", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.One
+		cache.Add(txnHash)
+		require.True(t, cache.Contains(txnHash))
+	})
+
+	t.Run("Inserting to full cache with expired txs", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		for i := range capacity {
+			txnHash := new(felt.Felt).SetUint64(uint64(i))
+			cache.Add(*txnHash)
+		}
+		/// Expire all txs in cache.
+		time.Sleep(entryTTL)
+
+		txnHash := new(felt.Felt).SetUint64(uint64(capacity))
+		cache.Add(*txnHash)
+		require.True(t, cache.Contains(*txnHash))
+
+		for i := range capacity {
+			require.False(t, cache.Contains(*new(felt.Felt).SetUint64(uint64(i))))
+		}
+	})
+
+	t.Run("Inserting to full cache with non-expired txs", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.Zero
+		cache.Add(txnHash)
+		require.True(t, cache.Contains(felt.Zero))
+		// Populates the cache to cap.
+		for i := 1; i < capacity; i++ {
+			txnHash := new(felt.Felt).SetUint64(uint64(i))
+			cache.Add(*txnHash)
+		}
+
+		txnHash = *new(felt.Felt).SetUint64(uint64(capacity))
+		// Expected to remove the least-recently-used entry.
+		cache.Add(txnHash)
+		require.True(t, cache.Contains(txnHash))
+		require.False(t, cache.Contains(felt.Zero))
+	})
+}
+
+func TestContains(t *testing.T) {
+	capacity := 5
+	entryTTL := time.Second
+
+	t.Run("Entry not found in cache", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.One
+		require.False(t, cache.Contains(txnHash))
+	})
+
+	t.Run("Valid entry found in cache", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.One
+		cache.Add(txnHash)
+		require.True(t, cache.Contains(txnHash))
+	})
+
+	t.Run("Entry found in cache but expired", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.One
+		cache.Add(txnHash)
+		/// Expire cache entry.
+		time.Sleep(entryTTL)
+		require.False(t, cache.Contains(txnHash))
+	})
+}
+
+func TestFlush(t *testing.T) {
+	capacity := 5
+	entryTTL := time.Second
+
+	t.Run("When there is no expired entry", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.One
+		cache.Add(txnHash)
+
+		cache.Flush()
+		require.True(t, cache.Contains(txnHash))
+	})
+
+	t.Run("When there are expired entries", func(t *testing.T) {
+		cache := rpccore.NewSubmittedTransactionsCache(capacity, entryTTL)
+		txnHash := felt.One
+		cache.Add(txnHash)
+
+		// Expire the entry
+		time.Sleep(entryTTL)
+		cache.Flush()
+		require.False(t, cache.Contains(txnHash))
+	})
+}
