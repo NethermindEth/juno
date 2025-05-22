46e705b49caa14fd865ec46204e118fae3daaf3f
Ege Caner <67071243+EgeCaner@users.noreply.github.com>
2025-05-22 13:07:36 +0300

    Cache for submitted txs (#2849)
    * SubmittedTransactionCache: implementation and test
    
    * Added SubmittedTransactionsCache parameters to Node.Config
    
    * Integrate cache into RPC
    
    * Added test cases for RECEIVED status
    
    * Fix linter warning
    
    * Update rpc/rpccore/submitted_transaction_cache_test.go
    
    Increase readability by storing txhashes and reusing them later rather than rescontructing hashes again
    
    Co-authored-by: Rian Hughes <rian.hughes@physics.ox.ac.uk>
    Signed-off-by: Ege Caner <67071243+EgeCaner@users.noreply.github.com>
    
    * Update rpc/rpccore/submitted_transaction_cache_test.go
    
    Increase readability by introducing `firstTxnHash` variable rather than using felt.Zero
    
    Co-authored-by: Rian Hughes <rian.hughes@physics.ox.ac.uk>
    Signed-off-by: Ege Caner <67071243+EgeCaner@users.noreply.github.com>
    
    * AddTransaction: return result as nil when err not nil
    
    * SubmittedTransactionsCache: Remove function added
    
    * rpcv6 and remove from cache on gw success response and adaptTransaction function introduced to reduce cyclomatic complexity
    
    * rpc v7, v8: remove from cache on gw success reponse
    
    * removed duplicated test on submitted txn cache test
    
    * extract txn cache tests in to its own test
    
    * txn cache: passing by reference rather than value
    
    * SubmittedTransactionsCache: Doc
    
    ---------
    
    Signed-off-by: Ege Caner <67071243+EgeCaner@users.noreply.github.com>
    Co-authored-by: Rian Hughes <rian.hughes@physics.ox.ac.uk>

--- rpc/v8/transaction.go
+++ rpc/v8/transaction.go
@@ -601,11 +601,25 @@ func (h *Handler) TransactionReceiptByHash(hash felt.Felt) (*TransactionReceipt,
 
 // AddTransaction relays a transaction to the gateway, or to the sequencer if enabled
 func (h *Handler) AddTransaction(ctx context.Context, tx BroadcastedTransaction) (*AddTxResponse, *jsonrpc.Error) { //nolint:gocritic
+	var (
+		res *AddTxResponse
+		err *jsonrpc.Error
+	)
 	if h.memPool != nil {
-		return h.addToMempool(&tx)
+		res, err = h.addToMempool(&tx)
 	} else {
-		return h.pushToFeederGateway(ctx, tx)
+		res, err = h.pushToFeederGateway(ctx, tx)
 	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if h.submittedTransactionsCache != nil {
+		h.submittedTransactionsCache.Add(tx.Hash)
+	}
+
+	return res, nil
 }
 
 func (h *Handler) addToMempool(tx *BroadcastedTransaction) (*AddTxResponse, *jsonrpc.Error) {
@@ -716,6 +730,17 @@ func (h *Handler) TransactionStatus(ctx context.Context, hash felt.Felt) (*Trans
 		txStatus, err := h.feederClient.Transaction(ctx, &hash)
 		if err != nil {
 			return nil, jsonrpc.Err(jsonrpc.InternalError, err.Error())
+		}
+
+		if h.submittedTransactionsCache != nil {
+			switch txStatus.FinalityStatus {
+			case starknet.NotReceived:
+				if h.submittedTransactionsCache.Contains(&hash) {
+					txStatus.FinalityStatus = starknet.Received
+				}
+			case starknet.AcceptedOnL2, starknet.AcceptedOnL1, starknet.Received:
+				h.submittedTransactionsCache.Remove(&hash)
+			}
 		}
 
 		status, err := adaptTransactionStatus(txStatus)
