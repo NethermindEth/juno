a3dbafc857b2b6f37ebdfb73ba55e58d15d4b53a
Dat Duong <DuongDat.Informatics@rocketmail.com>
2025-05-28 03:04:56 +0700

    feat: Add adapters for vote (#2841)

---
+++ consensus/p2p/vote/vote.go
@@ -0,0 +1,63 @@
+package vote
+
+import (
+	"errors"
+
+	"github.com/NethermindEth/juno/consensus/starknet"
+	"github.com/NethermindEth/juno/consensus/types"
+	"github.com/NethermindEth/juno/core/address"
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/core/hash"
+	"github.com/NethermindEth/juno/utils"
+	"github.com/starknet-io/starknet-p2pspecs/p2p/proto/common"
+	"github.com/starknet-io/starknet-p2pspecs/p2p/proto/consensus/consensus"
+)
+
+type VoteAdapter[H types.Hash, A types.Addr] interface {
+	ToVote(*consensus.Vote) (types.Vote[H, A], error)
+	FromVote(*types.Vote[H, A], consensus.Vote_VoteType) (consensus.Vote, error)
+}
+
+type starknetVoteAdapter struct{}
+
+var StarknetVoteAdapter VoteAdapter[hash.Hash, address.Address] = starknetVoteAdapter{}
+
+func (a starknetVoteAdapter) ToVote(vote *consensus.Vote) (starknet.Vote, error) {
+	var id *starknet.Hash
+	if proposalCommitment := vote.GetProposalCommitment().GetElements(); proposalCommitment != nil {
+		id = utils.HeapPtr(hash.Hash(felt.FromBytes(proposalCommitment)))
+	}
+
+	voter := vote.GetVoter().GetElements()
+	if voter == nil {
+		return starknet.Vote{}, errors.New("voter is nil")
+	}
+
+	return starknet.Vote{
+		MessageHeader: starknet.MessageHeader{
+			Height: types.Height(vote.GetBlockNumber()),
+			Round:  types.Round(vote.GetRound()),
+			Sender: address.Address(felt.FromBytes(voter)),
+		},
+		ID: id,
+	}, nil
+}
+
+func (a starknetVoteAdapter) FromVote(vote *starknet.Vote, voteType consensus.Vote_VoteType) (consensus.Vote, error) {
+	sender := vote.Sender.AsFelt().Bytes()
+
+	// This is optional since a vote can be NIL.
+	var id *common.Hash
+	if vote.ID != nil {
+		bytes := vote.ID.AsFelt().Bytes()
+		id = &common.Hash{Elements: bytes[:]}
+	}
+
+	return consensus.Vote{
+		VoteType:           voteType,
+		BlockNumber:        uint64(vote.MessageHeader.Height),
+		Round:              uint32(vote.MessageHeader.Round),
+		Voter:              &common.Address{Elements: sender[:]},
+		ProposalCommitment: id,
+	}, nil
+}
