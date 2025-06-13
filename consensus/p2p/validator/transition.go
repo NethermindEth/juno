a786259c1a8a9ae27637a67b3ff2fc8f348ed664
rian <rian.hughes@physics.ox.ac.uk>
2025-06-13 17:34:06 +0300

    tendermint: connect validators

--- consensus/p2p/validator/transition.go
+++ consensus/p2p/validator/transition.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 
+	"github.com/NethermindEth/juno/adapters/p2p2consensus"
 	"github.com/NethermindEth/juno/consensus/starknet"
 	"github.com/NethermindEth/juno/consensus/types"
+	val "github.com/NethermindEth/juno/consensus/validator"
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/utils"
 	"github.com/starknet-io/starknet-p2pspecs/p2p/proto/consensus/consensus"
@@ -49,14 +51,17 @@ type Transition interface {
 	) (*FinState, error)
 }
 
-type transition struct{}
+type transition[V types.Hashable[H], H types.Hash, A types.Addr] struct {
+	val.Validator[V, H, A]
+	network utils.Network // Todo
+}
 
-func NewTransition() Transition {
-	return &transition{}
+func NewTransition[V types.Hashable[H], H types.Hash, A types.Addr]() Transition {
+	return &transition[V, H, A]{}
 }
 
 // TODO: Implement this function properly
-func (t *transition) OnProposalInit(
+func (t *transition[V, H, A]) OnProposalInit(
 	ctx context.Context,
 	state *InitialState,
 	init *consensus.ProposalInit,
@@ -68,6 +73,13 @@ func (t *transition) OnProposalInit(
 
 	if init.Proposer == nil {
 		return nil, errNilProposer
+	}
+
+	// Todo: validate the inputs
+	adaptedProposalInit := p2p2consensus.AdaptProposalInit(init)
+	err := t.Validator.ProposalInit(&adaptedProposalInit)
+	if err != nil {
+		return nil, err
 	}
 	sender := starknet.Address(felt.FromBytes(init.Proposer.Elements))
 
@@ -82,7 +94,7 @@ func (t *transition) OnProposalInit(
 }
 
 // TODO: Implement this function properly
-func (t *transition) OnEmptyBlockCommitment(
+func (t *transition[V, H, A]) OnEmptyBlockCommitment(
 	ctx context.Context,
 	state *AwaitingBlockInfoOrCommitmentState,
 	commitment *consensus.ProposalCommitment,
@@ -95,11 +107,16 @@ func (t *transition) OnEmptyBlockCommitment(
 }
 
 // TODO: Implement this function properly
-func (t *transition) OnBlockInfo(
+func (t *transition[V, H, A]) OnBlockInfo(
 	ctx context.Context,
 	state *AwaitingBlockInfoOrCommitmentState,
 	blockInfo *consensus.BlockInfo,
 ) (*ReceivingTransactionsState, error) {
+
+	// Todo: validate the inputs
+	adaptedBlockInfo := p2p2consensus.AdaptBlockInfo(blockInfo)
+	t.Validator.BlockInfo(&adaptedBlockInfo)
+
 	return &ReceivingTransactionsState{
 		Header:     state.Header,
 		ValidRound: state.ValidRound,
@@ -108,11 +125,27 @@ func (t *transition) OnBlockInfo(
 }
 
 // TODO: Implement this function properly
-func (t *transition) OnTransactions(
+func (t *transition[V, H, A]) OnTransactions(
 	ctx context.Context,
 	state *ReceivingTransactionsState,
 	transactions []*consensus.ConsensusTransaction,
 ) (*ReceivingTransactionsState, error) {
+
+	// Todo: validate the inputs
+	txns := make([]types.Transaction, len(transactions))
+	for i := range transactions {
+		txn, class := p2p2consensus.AdaptTransaction(transactions[i], &t.network)
+		txns[i] = types.Transaction{
+			Transaction: txn,
+			Class:       class,
+		}
+	}
+
+	err := t.Validator.TransactionBatch(txns)
+	if err != nil {
+		return nil, err
+	}
+
 	return &ReceivingTransactionsState{
 		Header:     state.Header,
 		ValidRound: state.ValidRound,
@@ -121,11 +154,19 @@ func (t *transition) OnTransactions(
 }
 
 // TODO: Implement this function properly
-func (t *transition) OnProposalCommitment(
+func (t *transition[V, H, A]) OnProposalCommitment(
 	ctx context.Context,
 	state *ReceivingTransactionsState,
 	commitment *consensus.ProposalCommitment,
 ) (*AwaitingProposalFinState, error) {
+
+	// Todo: validate the inputs
+	adaptedCommitment := p2p2consensus.AdaptProposalCommitment(commitment)
+	err := t.Validator.ProposalCommitment(&adaptedCommitment)
+	if err != nil {
+		return nil, err
+	}
+
 	return &AwaitingProposalFinState{
 		Header:     state.Header,
 		ValidRound: state.ValidRound,
@@ -134,7 +175,7 @@ func (t *transition) OnProposalCommitment(
 }
 
 // TODO: Implement this function properly
-func (t *transition) OnProposalFin(
+func (t *transition[V, H, A]) OnProposalFin(
 	ctx context.Context,
 	state *AwaitingProposalFinState,
 	fin *consensus.ProposalFin,
@@ -142,6 +183,12 @@ func (t *transition) OnProposalFin(
 	// Check if expected and actual are both nil or both non-nil
 	if (state.Value == nil) != (fin.ProposalCommitment == nil) {
 		return nil, errHashMismatch
+	}
+	// Todo: validate the inputs
+	adaptedFin := p2p2consensus.AdaptProposalFin(fin)
+	err := t.Validator.ProposalFin(adaptedFin)
+	if err != nil {
+		return nil, err
 	}
 
 	// If both are non-nil, check if they are equal
