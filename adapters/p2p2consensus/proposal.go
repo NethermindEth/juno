9f83f961a8111c04f9f0f740ae2180e9243b3d41
Dat Duong <DuongDat.Informatics@rocketmail.com>
2025-06-24 06:35:38 +0700

    feat: Implement consensus2p2p adapters (#2911)

--- adapters/p2p2consensus/proposal.go
+++ adapters/p2p2consensus/proposal.go
@@ -6,31 +6,37 @@ import (
 	consensus "github.com/NethermindEth/juno/consensus/types"
 	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
-	"github.com/NethermindEth/juno/utils"
 	p2pconsensus "github.com/starknet-io/starknet-p2pspecs/p2p/proto/consensus/consensus"
 )
 
-func AdaptProposalInit(msg *p2pconsensus.ProposalInit) consensus.ProposalInit {
-	return consensus.ProposalInit{
-		BlockNum: msg.BlockNumber,
-		Proposer: *new(felt.Felt).SetBytes(msg.Proposer.Elements),
+func AdaptProposalInit(msg *p2pconsensus.ProposalInit) (consensus.ProposalInit, error) {
+	var validRound consensus.Round = -1
+	if msg.ValidRound != nil {
+		validRound = consensus.Round(*msg.ValidRound)
 	}
+
+	return consensus.ProposalInit{
+		BlockNum:   consensus.Height(msg.BlockNumber),
+		Round:      consensus.Round(msg.Round),
+		ValidRound: validRound,
+		Proposer:   *new(felt.Felt).SetBytes(msg.Proposer.Elements),
+	}, nil
 }
 
-func AdaptBlockInfo(msg *p2pconsensus.BlockInfo) consensus.BlockInfo {
+func AdaptBlockInfo(msg *p2pconsensus.BlockInfo) (consensus.BlockInfo, error) {
 	return consensus.BlockInfo{
 		BlockNumber:       msg.BlockNumber,
 		Builder:           *new(felt.Felt).SetBytes(msg.Builder.Elements),
 		Timestamp:         msg.Timestamp,
 		L2GasPriceFRI:     *p2p2core.AdaptUint128(msg.L2GasPriceFri),
-		L1GasPriceWEI:     *p2p2core.AdaptUint128(msg.L1DataGasPriceWei),
+		L1GasPriceWEI:     *p2p2core.AdaptUint128(msg.L1GasPriceWei),
 		L1DataGasPriceWEI: *p2p2core.AdaptUint128(msg.L1DataGasPriceWei),
 		EthToStrkRate:     *p2p2core.AdaptUint128(msg.EthToStrkRate),
 		L1DAMode:          core.L1DAMode(msg.L1DaMode),
-	}
+	}, nil
 }
 
-func AdaptProposalCommitment(msg *p2pconsensus.ProposalCommitment) consensus.ProposalCommitment {
+func AdaptProposalCommitment(msg *p2pconsensus.ProposalCommitment) (consensus.ProposalCommitment, error) {
 	return consensus.ProposalCommitment{
 		BlockNumber: msg.BlockNumber,
 		Builder:     *new(felt.Felt).SetBytes(msg.Builder.Elements),
@@ -53,22 +59,20 @@ func AdaptProposalCommitment(msg *p2pconsensus.ProposalCommitment) consensus.Pro
 		L2GasPriceFRI:         *p2p2core.AdaptUint128(msg.L2GasPriceFri),
 		L2GasUsed:             *p2p2core.AdaptUint128(msg.L2GasUsed),
 		L1DAMode:              core.L1DAMode(msg.L1DaMode),
-	}
+	}, nil
 }
 
-func AdaptProposalTransaction(msg *p2pconsensus.TransactionBatch, network *utils.Network) []consensus.Transaction {
+func AdaptProposalTransaction(msg *p2pconsensus.TransactionBatch) ([]consensus.Transaction, error) {
+	var err error
 	txns := make([]consensus.Transaction, len(msg.Transactions))
 	for i := range msg.Transactions {
-		txn, class := AdaptTransaction(msg.Transactions[i], network)
-		txns[i] = consensus.Transaction{
-			Transaction: txn,
-			Class:       class,
-			PaidFeeOnL1: nil, // Todo: this value is not passed in the spec.
+		if txns[i], err = AdaptTransaction(msg.Transactions[i]); err != nil {
+			return nil, err
 		}
 	}
-	return txns
+	return txns, nil
 }
 
-func AdaptProposalFin(msg *p2pconsensus.ProposalFin) consensus.ProposalFin {
-	return consensus.ProposalFin(*new(felt.Felt).SetBytes(msg.ProposalCommitment.Elements))
+func AdaptProposalFin(msg *p2pconsensus.ProposalFin) (consensus.ProposalFin, error) {
+	return consensus.ProposalFin(*new(felt.Felt).SetBytes(msg.ProposalCommitment.Elements)), nil
 }
