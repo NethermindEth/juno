afe994b1bbfb22462c0466e73410a0ea0f81a4c7
Turan Ege Caner <turan.caner@nethermind.io>
2025-06-25 10:43:30 +0300

    core pre_confirmed_block

---
+++ core/pre_confirmed.go
@@ -0,0 +1,182 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/core/trie"
+	"github.com/NethermindEth/juno/db"
+)
+
+var feltOne = new(felt.Felt).SetUint64(1)
+
+type PreConfirmed struct {
+	Block                 *Block
+	StateUpdate           *StateUpdate
+	TransactionStateDiffs []*StateDiff
+	CandidateTxHashes     map[felt.Felt]struct{}
+	NewClasses            map[felt.Felt]Class
+}
+
+func (p *PreConfirmed) HasCandidateTx(hash felt.Felt) bool {
+	if _, ok := p.CandidateTxHashes[hash]; ok {
+		return true
+	}
+	return false
+}
+
+type PreConfirmedState struct {
+	stateDiff  *StateDiff
+	newClasses map[felt.Felt]Class
+	head       StateReader
+}
+
+func NewPreConfirmedState(stateDiff *StateDiff, newClasses map[felt.Felt]Class, head StateReader) *PreConfirmedState {
+	return &PreConfirmedState{
+		stateDiff:  stateDiff,
+		newClasses: newClasses,
+		head:       head,
+	}
+}
+
+func (p *PreConfirmedState) ChainHeight() (uint64, error) {
+	return p.head.ChainHeight()
+}
+
+func (p *PreConfirmedState) StateDiff() *StateDiff {
+	return p.stateDiff
+}
+
+func (p *PreConfirmedState) ContractClassHash(addr *felt.Felt) (*felt.Felt, error) {
+	if classHash, ok := p.stateDiff.ReplacedClasses[*addr]; ok {
+		return classHash, nil
+	} else if classHash, ok = p.stateDiff.DeployedContracts[*addr]; ok {
+		return classHash, nil
+	}
+	return p.head.ContractClassHash(addr)
+}
+
+func (p *PreConfirmedState) ContractNonce(addr *felt.Felt) (*felt.Felt, error) {
+	if nonce, found := p.stateDiff.Nonces[*addr]; found {
+		return nonce, nil
+	} else if _, found = p.stateDiff.DeployedContracts[*addr]; found {
+		return &felt.Felt{}, nil
+	}
+	return p.head.ContractNonce(addr)
+}
+
+func (p *PreConfirmedState) ContractStorage(addr, key *felt.Felt) (*felt.Felt, error) {
+	if diffs, found := p.stateDiff.StorageDiffs[*addr]; found {
+		if value, found := diffs[*key]; found {
+			return value, nil
+		}
+	}
+	if _, found := p.stateDiff.DeployedContracts[*addr]; found {
+		return &felt.Felt{}, nil
+	}
+	return p.head.ContractStorage(addr, key)
+}
+
+// TODO(Ege): Might return custom error for execution requests for classes declared on preconfirmed block
+func (p *PreConfirmedState) Class(classHash *felt.Felt) (*DeclaredClass, error) {
+	if class, found := p.newClasses[*classHash]; found {
+		return &DeclaredClass{
+			At:    0,
+			Class: class,
+		}, nil
+	}
+
+	return p.head.Class(classHash)
+}
+
+func (p *PreConfirmedState) ClassTrie() (*trie.Trie, error) {
+	return nil, ErrHistoricalTrieNotSupported
+}
+
+func (p *PreConfirmedState) ContractTrie() (*trie.Trie, error) {
+	return nil, ErrHistoricalTrieNotSupported
+}
+
+func (p *PreConfirmedState) ContractStorageTrie(addr *felt.Felt) (*trie.Trie, error) {
+	return nil, ErrHistoricalTrieNotSupported
+}
+
+type PreConfirmedStateWriter struct {
+	*PreConfirmedState
+}
+
+func NewPreConfirmedStateWriter(stateDiff *StateDiff, newClasses map[felt.Felt]Class, head StateReader) PreConfirmedStateWriter {
+	return PreConfirmedStateWriter{
+		PreConfirmedState: &PreConfirmedState{
+			stateDiff:  stateDiff,
+			newClasses: newClasses,
+			head:       head,
+		},
+	}
+}
+
+func (p *PreConfirmedStateWriter) SetStorage(contractAddress, key, value *felt.Felt) error {
+	if _, found := p.stateDiff.StorageDiffs[*contractAddress]; !found {
+		p.stateDiff.StorageDiffs[*contractAddress] = make(map[felt.Felt]*felt.Felt)
+	}
+	p.stateDiff.StorageDiffs[*contractAddress][*key] = value.Clone()
+	return nil
+}
+
+func (p *PreConfirmedStateWriter) IncrementNonce(contractAddress *felt.Felt) error {
+	currentNonce, err := p.ContractNonce(contractAddress)
+	if err != nil {
+		return fmt.Errorf("get contract nonce: %v", err)
+	}
+	p.stateDiff.Nonces[*contractAddress] = currentNonce.Add(currentNonce, feltOne)
+	return nil
+}
+
+func (p *PreConfirmedStateWriter) SetClassHash(contractAddress, classHash *felt.Felt) error {
+	if _, err := p.head.ContractClassHash(contractAddress); err != nil {
+		if errors.Is(err, db.ErrKeyNotFound) {
+			p.stateDiff.DeployedContracts[*contractAddress] = classHash.Clone()
+			return nil
+		}
+		return fmt.Errorf("get latest class hash: %v", err)
+	}
+	p.stateDiff.ReplacedClasses[*contractAddress] = classHash.Clone()
+	return nil
+}
+
+// SetContractClass writes a new CairoV0 class to the PreConfirmedState
+// Assumption: SetCompiledClassHash should be called for CairoV1 contracts
+func (p *PreConfirmedStateWriter) SetContractClass(classHash *felt.Felt, class Class) error {
+	// Only declare the class if it has not already been declared, and return
+	// and unexepcted errors (ie any error that isn't db.ErrKeyNotFound)
+	_, err := p.Class(classHash)
+	if err == nil {
+		return errors.New("class already declared")
+	} else if !errors.Is(err, db.ErrKeyNotFound) {
+		return fmt.Errorf("get class: %v", err)
+	}
+
+	p.newClasses[*classHash] = class
+	if class.Version() == 0 {
+		p.stateDiff.DeclaredV0Classes = append(p.stateDiff.DeclaredV0Classes, classHash.Clone())
+	}
+	return nil
+}
+
+// SetCompiledClassHash writes CairoV1 classes to the PreConfirmed state
+// Assumption: SetContractClass was called for classHash and succeeded
+func (p *PreConfirmedStateWriter) SetCompiledClassHash(classHash, compiledClassHash *felt.Felt) error {
+	p.stateDiff.DeclaredV1Classes[*classHash] = compiledClassHash.Clone()
+	return nil
+}
+
+// StateDiffAndClasses returns the PreConfirmed state's internal data. The returned objects will continue to be
+// read and modified by the PreConfirmed state.
+func (p *PreConfirmedStateWriter) StateDiffAndClasses() (StateDiff, map[felt.Felt]Class) {
+	return *p.stateDiff, p.newClasses
+}
+
+func (p *PreConfirmedStateWriter) SetStateDiff(stateDiff *StateDiff) {
+	p.stateDiff = stateDiff
+}
