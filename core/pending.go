72380f3f17fe9732fdbd4f10b68d64ba4b5deca6
Turan Ege Caner <turan.caner@nethermind.io>
2025-07-02 17:04:40 +0300

    core PreConfirmed type and abstraction over pending data

--- core/pending.go
+++ core/pending.go
@@ -4,6 +4,61 @@ import (
 	"github.com/NethermindEth/juno/core/felt"
 )
 
+type PendingDataVariant uint8
+
+const (
+	PendingBlockVariant PendingDataVariant = iota + 1
+	PreConfirmedBlockVariant
+)
+
+type PendingDataInterface interface {
+	GetBlock() *Block
+	GetHeader() *Header
+	GetTransactions() []Transaction
+	GetStateUpdate() *StateUpdate
+	GetNewClasses() map[felt.Felt]Class
+	GetCandidateTransaction() []Transaction
+	Variant() PendingDataVariant
+}
+
+type PendingData struct {
+	data PendingDataInterface
+}
+
+func NewPendingData(data PendingDataInterface) PendingData {
+	return PendingData{
+		data: data,
+	}
+}
+
+func (p *PendingData) GetBlock() *Block {
+	return p.data.GetBlock()
+}
+
+func (p *PendingData) GetHeader() *Header {
+	return p.data.GetHeader()
+}
+
+func (p *PendingData) GetTransactions() []Transaction {
+	return p.data.GetTransactions()
+}
+
+func (p *PendingData) GetStateUpdate() *StateUpdate {
+	return p.data.GetStateUpdate()
+}
+
+func (p *PendingData) GetNewClasses() map[felt.Felt]Class {
+	return p.data.GetNewClasses()
+}
+
+func (p *PendingData) GetCandidateTransaction() []Transaction {
+	return p.data.GetCandidateTransaction()
+}
+
+func (p *PendingData) Variant() PendingDataVariant {
+	return p.data.Variant()
+}
+
 type PreConfirmed struct {
 	Block       *Block
 	StateUpdate *StateUpdate
@@ -21,4 +76,36 @@ func NewPreConfirmed(block *Block, stateUpdate *StateUpdate, transactionStateDif
 		TransactionStateDiffs: transactionStateDiffs,
 		CandidateTxs:          candidateTxs,
 	}
+}
+
+func (p *PreConfirmed) AsPendingData() PendingData {
+	return PendingData{data: p}
+}
+
+func (p *PreConfirmed) GetBlock() *Block {
+	return p.Block
+}
+
+func (p *PreConfirmed) GetHeader() *Header {
+	return p.Block.Header
+}
+
+func (p *PreConfirmed) GetTransactions() []Transaction {
+	return p.Block.Transactions
+}
+
+func (p *PreConfirmed) GetStateUpdate() *StateUpdate {
+	return p.StateUpdate
+}
+
+func (p *PreConfirmed) GetNewClasses() map[felt.Felt]Class {
+	return p.NewClasses
+}
+
+func (p *PreConfirmed) GetCandidateTransaction() []Transaction {
+	return p.CandidateTxs
+}
+
+func (p *PreConfirmed) Variant() PendingDataVariant {
+	return PreConfirmedBlockVariant
 }
