249bcb3ea41796e1cd274a2c48e923d26370768d
Turan Ege Caner <turan.caner@nethermind.io>
2025-07-02 17:04:05 +0300

    starknet PreconfirmedBlock type

---
+++ starknet/pre_confirmed_block.go
@@ -0,0 +1,33 @@
+package starknet
+
+import "github.com/NethermindEth/juno/core/felt"
+
+// Block object returned by the feeder in JSON format for "get_preconfirmed_block" endpoint
+type PreConfirmedBlock struct {
+	Status           string                `json:"status"`
+	Transactions     []*Transaction        `json:"transactions"`
+	Timestamp        uint64                `json:"timestamp"`
+	Version          string                `json:"starknet_version"`
+	Receipts         []*TransactionReceipt `json:"transaction_receipts"`
+	SequencerAddress *felt.Felt            `json:"sequencer_address"`
+	L1GasPrice       *GasPrice             `json:"l1_gas_price"`
+	L2GasPrice       *GasPrice             `json:"l2_gas_price"`
+	L1DAMode         L1DAMode              `json:"l1_da_mode"`
+	L1DataGasPrice   *GasPrice             `json:"l1_data_gas_price"`
+
+	TransactionStateDiffs []*StateDiff `json:"transaction_state_diffs"`
+}
+
+func (b *PreConfirmedBlock) L1GasPriceETH() *felt.Felt {
+	if b.L1GasPrice != nil {
+		return b.L1GasPrice.PriceInWei
+	}
+	return nil
+}
+
+func (b *PreConfirmedBlock) L1GasPriceSTRK() *felt.Felt {
+	if b.L1GasPrice != nil {
+		return b.L1GasPrice.PriceInFri
+	}
+	return nil
+}
