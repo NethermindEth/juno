5f5b1807ef521bac46de957ca7beb7ae6c36fa5b
MaksymMalicki <maksym.malicki.2115@gmail.com>
2025-07-08 21:33:08 +0200

    fix dbcmd revert

--- cmd/juno/dbcmd.go
+++ cmd/juno/dbcmd.go
@@ -283,7 +283,14 @@ func openDB(path string) (db.KeyValueStore, error) {
 		return nil, errors.New("database path does not exist")
 	}
 
-	database, err := pebble.New(path)
+	// Use the same defaults as the normal startup
+	const (
+		defaultCacheSizeMb = 1024
+		defaultMaxHandles  = 1024
+		defaultColour      = true
+	)
+
+	database, err := pebble.NewWithOptions(path, defaultCacheSizeMb, defaultMaxHandles, defaultColour)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open db: %w", err)
 	}
