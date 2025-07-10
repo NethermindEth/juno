de5a86a0c3338f208b7da4ece4a3aaea2e8f570e
Turan Ege Caner <turan.caner@nethermind.io>
2025-07-10 17:21:25 +0300

    increase submitted txn cache size

--- cmd/juno/juno.go
+++ cmd/juno/juno.go
@@ -139,7 +139,7 @@ const (
 	defaultSeqGenesisFile                     = ""
 	defaultSeqDisableFees                     = false
 	defaultHTTPUpdatePort                     = 0
-	defaultSubmittedTransactionsCacheSize     = 1024
+	defaultSubmittedTransactionsCacheSize     = 10_000
 	defaultSubmittedTransactionsCacheEntryTTL = 5 * time.Minute
 
 	configFlagUsage                       = "The YAML configuration file."
