da575895772c0ac3a72003265c9b008b12d9c000
thiagodeev <thiagodeev@gmail.com>
2025-07-10 21:53:47 -0300

    fix: update nonce API documentation link in rpcv9

--- rpc/v9/nonce.go
+++ rpc/v9/nonce.go
@@ -13,7 +13,7 @@ import (
 // Nonce returns the nonce associated with the given address in the given block number
 //
 // It follows the specification defined here:
-// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L633
+// https://github.com/starkware-libs/starknet-specs/blob/25533f8b7999219120a4a654709d47dc9376d640/api/starknet_api_openrpc.json#L851
 func (h *Handler) Nonce(id *BlockID, address felt.Felt) (*felt.Felt, *jsonrpc.Error) {
 	stateReader, stateCloser, rpcErr := h.stateByBlockID(id)
 	if rpcErr != nil {
