f8c0d6e360e3ff89fb1a391be1d7e4a48c12bc0b
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2025-07-29 11:33:21 +0200

    wait for synced status when running tests in CI

--- .github/workflow-scripts/verify_deployment.sh
+++ .github/workflow-scripts/verify_deployment.sh
@@ -9,11 +9,14 @@ SLEEP_DURATION=10
 
 for i in $(seq 1 $MAX_ATTEMPTS); do
   DEPLOYED_VERSION=$(curl -s -X POST "$URL" -H "Content-Type: application/json" -d '{"method": "juno_version", "jsonrpc": "2.0", "id": 0}' | jq -r '.result')
+  IS_SYNCING=$(curl -s -X POST "$URL" -H "Content-Type: application/json" -d '{"method": "starknet_syncing", "jsonrpc": "2.0", "id": 0}' | jq -r '.result')
+  echo "Attempt $i: Syncing status $IS_SYNCING"
   echo "Attempt $i: Deployed version $DEPLOYED_VERSION"
-  if [[ "$DEPLOYED_VERSION" == "$EXPECTED_VERSION" ]]; then
-    echo "Deployment version matches the image tag."
+  if [[ "$DEPLOYED_VERSION" == "$EXPECTED_VERSION" && "$IS_SYNCING" == "false" ]]; then
+    echo "Deployment version matches the image tag and Node is synced"
     exit 0
   fi
+
   sleep $SLEEP_DURATION
 done
 
