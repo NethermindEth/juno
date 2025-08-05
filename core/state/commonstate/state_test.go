07f59b8b5e8ec83260ad7e85af54ea74addc5dff
MaksymMalicki <maksym.malicki.2115@gmail.com>
2025-08-05 14:44:59 +0200

    increase codecov

---
+++ core/state/commonstate/state_test.go
@@ -0,0 +1,60 @@
+package commonstate_test
+
+import (
+	"testing"
+
+	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/core/state"
+	"github.com/NethermindEth/juno/core/state/commonstate"
+	"github.com/NethermindEth/juno/core/trie2/triedb"
+	"github.com/NethermindEth/juno/db/memory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCoreStateAdapter(t *testing.T) {
+	testDB := memory.New()
+	txn := testDB.NewIndexedBatch()
+
+	state := core.NewState(txn)
+	coreStateAdapter := commonstate.NewCoreStateAdapter(state)
+	assert.NotNil(t, coreStateAdapter)
+}
+
+func TestStateAdapter(t *testing.T) {
+	memDB := memory.New()
+	db, err := triedb.New(memDB, nil)
+	if err != nil {
+		panic(err)
+	}
+	stateDB := state.NewStateDB(memDB, db)
+	state, err := state.New(&felt.Zero, stateDB)
+	require.NoError(t, err)
+
+	stateAdapter := commonstate.NewStateAdapter(state)
+	assert.NotNil(t, stateAdapter)
+}
+
+func TestCoreStateReaderAdapter(t *testing.T) {
+	testDB := memory.New()
+	txn := testDB.NewIndexedBatch()
+
+	state := core.NewState(txn)
+	coreStateReaderAdapter := commonstate.NewCoreStateReaderAdapter(state)
+	assert.NotNil(t, coreStateReaderAdapter)
+}
+
+func TestStateReaderAdapter(t *testing.T) {
+	memDB := memory.New()
+	db, err := triedb.New(memDB, nil)
+	if err != nil {
+		panic(err)
+	}
+	stateDB := state.NewStateDB(memDB, db)
+	state, err := state.New(&felt.Zero, stateDB)
+	require.NoError(t, err)
+
+	stateReaderAdapter := commonstate.NewStateReaderAdapter(state)
+	assert.NotNil(t, stateReaderAdapter)
+}
---
+++ core/state/commontrie/trie_test.go
@@ -0,0 +1,78 @@
+package commontrie
+
+import (
+	"testing"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/core/trie"
+	"github.com/NethermindEth/juno/core/trie2"
+	"github.com/NethermindEth/juno/db"
+	"github.com/NethermindEth/juno/db/memory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTrieAdapter(t *testing.T) {
+	memDB := memory.New()
+	txn := memDB.NewIndexedBatch()
+	storage := trie.NewStorage(txn, db.ContractStorage.Key([]byte{0}))
+	trie, err := trie.NewTriePedersen(storage, 0)
+	require.NoError(t, err)
+	adapter := NewTrieAdapter(trie)
+
+	t.Run("Update", func(t *testing.T) {
+		err := adapter.Update(&felt.Zero, &felt.Zero)
+		require.NoError(t, err)
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		err := adapter.Update(&felt.Zero, &felt.Zero)
+		require.NoError(t, err)
+
+		gotValue, err := adapter.Get(&felt.Zero)
+		require.NoError(t, err)
+		assert.Equal(t, felt.Zero, gotValue)
+	})
+
+	t.Run("Hash", func(t *testing.T) {
+		hash, err := adapter.Hash()
+		require.NoError(t, err)
+		assert.Equal(t, felt.Zero, hash)
+	})
+
+	t.Run("HashFn", func(t *testing.T) {
+		hashFn := adapter.HashFn()
+		assert.NotNil(t, hashFn)
+	})
+}
+
+func TestTrie2Adapter(t *testing.T) {
+	trie, err := trie2.NewEmptyPedersen()
+	require.NoError(t, err)
+	adapter := NewTrie2Adapter(trie)
+
+	t.Run("Update", func(t *testing.T) {
+		err := adapter.Update(&felt.Zero, &felt.Zero)
+		require.NoError(t, err)
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		err := adapter.Update(&felt.Zero, &felt.Zero)
+		require.NoError(t, err)
+
+		gotValue, err := adapter.Get(&felt.Zero)
+		require.NoError(t, err)
+		assert.Equal(t, felt.Zero, gotValue)
+	})
+
+	t.Run("Hash", func(t *testing.T) {
+		hash, err := adapter.Hash()
+		require.NoError(t, err)
+		assert.Equal(t, felt.Zero, hash)
+	})
+
+	t.Run("HashFn", func(t *testing.T) {
+		hashFn := adapter.HashFn()
+		assert.NotNil(t, hashFn)
+	})
+}
