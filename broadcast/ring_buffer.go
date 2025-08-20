924479f4c2b2088ac13dc88bccf162e9574c4da6
Turan Ege Caner <turan.caner@nethermind.io>
2025-08-20 15:58:48 +0300

    utilize math/bits.Len64 for nextPow2 calculation

--- broadcast/ring_buffer.go
+++ broadcast/ring_buffer.go
@@ -3,6 +3,7 @@ package broadcast
 import (
 	"errors"
 	"fmt"
+	"math/bits"
 	"sync"
 )
 
@@ -142,13 +143,5 @@ func nextPowerOfTwo(x uint64) uint64 {
 	if x == 0 {
 		return 1
 	}
-	x--
-	x |= x >> 1
-	x |= x >> 2
-	x |= x >> 4
-	x |= x >> 8
-	x |= x >> 16
-	x |= x >> 32
-	x++
-	return x
+	return 1 << uint(bits.Len64(x-1))
 }
