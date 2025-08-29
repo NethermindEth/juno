9cbe7d159c800cd58bb04a8102ca8c90537f320b
Turan Ege Caner <turan.caner@nethermind.io>
2025-08-29 18:39:25 +0300

    Seperate PreLatestInterface and Union Interface of PendingData | PreLatestData

--- sync/pending.go
+++ sync/pending.go
@@ -62,14 +62,17 @@ func (p *Pending) GetPreLatest() *Pending {
 	return nil
 }
 
-type PendingDataWPreLatest interface {
-	core.PendingData
+type PreLatestData interface {
 	GetPreLatest() *Pending
 }
 
+type PendingDataWithPreLatest interface {
+	core.PendingData
+	PreLatestData
+}
 type PreConfirmedWithPreLatest struct {
-	PreConfirmed *core.PreConfirmed
-	PreLatest    *Pending
+	*core.PreConfirmed
+	PreLatest *Pending
 }
 
 func NewPreConfirmedWithPreLatest(
@@ -84,38 +87,6 @@ func NewPreConfirmedWithPreLatest(
 		PreConfirmed: &preConfirmed,
 		PreLatest:    preLatest,
 	}
-}
-
-func (p *PreConfirmedWithPreLatest) GetBlock() *core.Block {
-	return p.PreConfirmed.GetBlock()
-}
-
-func (p *PreConfirmedWithPreLatest) GetHeader() *core.Header {
-	return p.PreConfirmed.GetHeader()
-}
-
-func (p *PreConfirmedWithPreLatest) GetTransactions() []core.Transaction {
-	return p.PreConfirmed.GetTransactions()
-}
-
-func (p *PreConfirmedWithPreLatest) GetStateUpdate() *core.StateUpdate {
-	return p.PreConfirmed.GetStateUpdate()
-}
-
-func (p *PreConfirmedWithPreLatest) GetNewClasses() map[felt.Felt]core.Class {
-	return p.PreConfirmed.GetNewClasses()
-}
-
-func (p *PreConfirmedWithPreLatest) GetCandidateTransaction() []core.Transaction {
-	return p.PreConfirmed.GetCandidateTransaction()
-}
-
-func (p *PreConfirmedWithPreLatest) GetTransactionStateDiffs() []*core.StateDiff {
-	return p.PreConfirmed.GetTransactionStateDiffs()
-}
-
-func (p *PreConfirmedWithPreLatest) Variant() core.PendingDataVariant {
-	return core.PreConfirmedBlockVariant
 }
 
 func (p *PreConfirmedWithPreLatest) GetPreLatest() *Pending {
