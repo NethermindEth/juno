0e43476f17ed144e9622558500dc7a3111b814df
infrmtcs <DuongDat.Informatics@rocketmail.com>
2025-09-10 17:27:04 +0700

    feat: Rewrite WAL and avoid missing entries

--- consensus/types/wal/wal.go
+++ consensus/types/wal/wal.go
@@ -8,26 +8,32 @@ type Entry[V types.Hashable[H], H types.Hash, A types.Addr] interface {
 	GetHeight() types.Height
 }
 
-type WALProposal[V types.Hashable[H], H types.Hash, A types.Addr] types.Proposal[V, H, A]
+type Start types.Height
 
-func (p *WALProposal[V, H, A]) GetHeight() types.Height {
+func (h *Start) GetHeight() types.Height {
+	return types.Height(*h)
+}
+
+type Proposal[V types.Hashable[H], H types.Hash, A types.Addr] types.Proposal[V, H, A]
+
+func (p *Proposal[V, H, A]) GetHeight() types.Height {
 	return p.Height
 }
 
-type WALPrevote[H types.Hash, A types.Addr] types.Prevote[H, A]
+type Prevote[H types.Hash, A types.Addr] types.Prevote[H, A]
 
-func (p *WALPrevote[H, A]) GetHeight() types.Height {
+func (p *Prevote[H, A]) GetHeight() types.Height {
 	return p.Height
 }
 
-type WALPrecommit[H types.Hash, A types.Addr] types.Precommit[H, A]
+type Precommit[H types.Hash, A types.Addr] types.Precommit[H, A]
 
-func (p *WALPrecommit[H, A]) GetHeight() types.Height {
+func (p *Precommit[H, A]) GetHeight() types.Height {
 	return p.Height
 }
 
-type WALTimeout types.Timeout
+type Timeout types.Timeout
 
-func (p *WALTimeout) GetHeight() types.Height {
+func (p *Timeout) GetHeight() types.Height {
 	return p.Height
 }
