8fdb59a887c9b98d7fb2f044b965ec4cc55ca8e7
Turan Ege Caner <turan.caner@nethermind.io>
2025-09-19 16:46:20 +0300

    fix comment

--- starknet/fee_token_addresses.go
+++ starknet/fee_token_addresses.go
@@ -2,7 +2,9 @@ package starknet
 
 import "github.com/NethermindEth/juno/core/felt"
 
-// ContractAddresses represents the partial response from the "get_contract_addresses" endpoint
+// FeeTokenAddresses holds the L2 token contract addresses for STRK and ETH as returned by the
+// feeder gateway's "get_contract_addresses" endpoint. These addresses are used to
+// identify the respective fee tokens on the network.
 type FeeTokenAddresses struct {
 	StrkL2TokenAddress *felt.Felt `json:"strk_l2_token_address"`
 	EthL2TokenAddress  *felt.Felt `json:"eth_l2_token_address"`
