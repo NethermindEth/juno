c591c3aa7ac321945129b923203cb666ef2f531a
Turan Ege Caner <turan.caner@nethermind.io>
2025-09-19 16:41:15 +0300

    remove unusued type declaration

--- core/contract_addresses.go
+++
@@ -1,9 +0,0 @@
-package core
-
-import "github.com/NethermindEth/juno/core/felt"
-
-type ContractAddresses struct {
-	CoreContractAddress *felt.Felt
-	StrkL2TokenAddress  *felt.Felt
-	EthL2TokenAddress   *felt.Felt
-}
