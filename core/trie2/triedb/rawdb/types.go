566d8a7d1d7f8f4bca01e37f366ff2300aa0a9a3
MaksymMalicki <maksym.malicki.2115@gmail.com>
2025-10-18 16:14:21 +0200

    lint

--- core/trie2/triedb/rawdb/types.go
+++ core/trie2/triedb/rawdb/types.go
@@ -11,3 +11,5 @@ type (
 	contractNodesMap        = map[trieutils.Path]trienode.TrieNode
 	contractStorageNodesMap = map[felt.Felt]map[trieutils.Path]trienode.TrieNode
 )
+
+const ownerSize = felt.Bytes
