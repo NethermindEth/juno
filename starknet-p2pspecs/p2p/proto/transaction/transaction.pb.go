2744c912b1fdd9d275f2dfeba06442af4fd12877
Niloy Datta <niloy.datta@nethermind.io>
2025-10-21 17:30:35 +0600

    Reverted back the changes of starknet-p2pspects/p2p/proto/transaction/transaction.pb.go file

--- starknet-p2pspecs/p2p/proto/transaction/transaction.pb.go
+++ starknet-p2pspecs/p2p/proto/transaction/transaction.pb.go
@@ -7,14 +7,13 @@
 package transaction
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	class "github.com/starknet-io/starknet-p2pspecs/p2p/proto/class"
 	common "github.com/starknet-io/starknet-p2pspecs/p2p/proto/common"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
