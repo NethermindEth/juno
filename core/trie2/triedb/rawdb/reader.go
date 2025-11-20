373032dec9843b46c8bde1e7148f16862a194d62
MaksymMalicki <maksym.malicki.2115@gmail.com>
2025-11-20 15:49:09 +0100

    Commits from maksym/rawdb (squashed)

---
+++ core/trie2/triedb/rawdb/reader.go
@@ -0,0 +1,27 @@
+package rawdb
+
+import (
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/core/trie2/triedb/database"
+	"github.com/NethermindEth/juno/core/trie2/trieutils"
+)
+
+var _ database.NodeReader = (*reader)(nil)
+
+type reader struct {
+	id trieutils.TrieID
+	d  *Database
+}
+
+func (r *reader) Node(
+	owner *felt.Felt,
+	path *trieutils.Path,
+	hash *felt.Felt,
+	isLeaf bool,
+) ([]byte, error) {
+	return r.d.readNode(r.id, owner, path, isLeaf)
+}
+
+func (d *Database) NodeReader(id trieutils.TrieID) (database.NodeReader, error) {
+	return &reader{d: d, id: id}, nil
+}
