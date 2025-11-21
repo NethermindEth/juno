15808abcae14fbb769b0558db1baf78f8ec8ba90
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2025-11-21 17:18:39 +0100

    update rpcv8 as well

--- rpc/v8/storage.go
+++ rpc/v8/storage.go
@@ -77,7 +77,7 @@ func (h *Handler) StorageProof(
 	// We don't use the head query directly to avoid race condition where there is a new incoming block.
 	// Currently it's still working because we don't have revert yet.
 	// We should figure out a way to merge the two transactions.
-	head, err := h.bcReader.BlockByNumber(chainHeight)
+	head, err := h.bcReader.BlockHeaderByNumber(chainHeight)
 	if err != nil {
 		return nil, rpccore.ErrInternal.CloneWithData(err)
 	}
