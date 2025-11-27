e432967a982befd58060438f2c172fe4631f4db0
Dat Duong <DuongDat.Informatics@rocketmail.com>
2025-11-27 23:04:35 +0700

    feat: Typed key serializers (#3299)

---
+++ db/typed/key/key.go
@@ -0,0 +1,26 @@
+package key
+
+import "github.com/NethermindEth/juno/core/felt"
+
+type Serializer[K any] interface {
+	~struct{}
+	Marshal(K) []byte
+}
+
+var Empty = emptySerializer{}
+
+var Bytes = BytesSerializer{}
+
+var Uint64 = uint64Serializer{}
+
+func Marshal[K marshaler[K]]() marshalSerializer[K] {
+	return marshalSerializer[K]{}
+}
+
+var (
+	Felt            = Marshal[*felt.Felt]()
+	Address         = Marshal[*felt.Address]()
+	ClassHash       = Marshal[*felt.ClassHash]()
+	SierraClassHash = Marshal[*felt.SierraClassHash]()
+	TransactionHash = Marshal[*felt.TransactionHash]()
+)
