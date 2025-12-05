80ac930c4aad830b391a1f958c0eb3a882fe584c
MaksymMalicki <maksym.malicki.2115@gmail.com>
2025-12-05 14:14:31 +0100

    squash
    cleanups
    
    Integrate common interfaces into rawdb
    
    fixes after rebase
    
    minor linter changes
    
    clean-up blockchain.go, fix linter
    
    minor cleanups
    
    clean cache
    
    fix unit tests
    
    fixes after rebase

--- core/trie2/triedb/rawdb/database.go
+++ core/trie2/triedb/rawdb/database.go
@@ -8,12 +8,15 @@ import (
 	"github.com/NethermindEth/juno/core/trie2/trienode"
 	"github.com/NethermindEth/juno/core/trie2/trieutils"
 	"github.com/NethermindEth/juno/db"
+	"github.com/NethermindEth/juno/utils"
 	"github.com/NethermindEth/juno/utils/log"
 )
 
 var _ database.TrieDB = (*Database)(nil)
 
-type Config struct{}
+type Config struct {
+	CleanCacheSize uint64 // Maximum size (in bytes) for caching clean nodes
+}
 
 type Database struct {
 	disk db.KeyValueStore
@@ -22,7 +25,13 @@ type Database struct {
 	logger log.StructuredLogger
 }
 
-func New(disk db.KeyValueStore) *Database {
+func New(disk db.KeyValueStore, config *Config) *Database {
+	if config == nil {
+		config = &Config{
+			CleanCacheSize: 16 * utils.Megabyte,
+		}
+	}
+	cleanCache := newCleanCache(config.CleanCacheSize)
 	return &Database{
 		disk:   disk,
 		logger: log.NewNopZapLogger(),
@@ -80,14 +89,14 @@ func (d *Database) Update(
 	}
 
 	for path, n := range classNodes {
-		err := d.updateNode(batch, db.ClassTrie, &felt.Address{}, &path, n)
+		err := d.updateNode(batch, db.ClassTrie, &felt.Address{}, &path, n, true)
 		if err != nil {
 			return err
 		}
 	}
 
 	for path, n := range contractNodes {
-		err := d.updateNode(batch, db.ContractTrieContract, &felt.Address{}, &path, n)
+		err := d.updateNode(batch, db.ContractTrieContract, &felt.Address{}, &path, n, false)
 		if err != nil {
 			return err
 		}
@@ -95,7 +104,7 @@ func (d *Database) Update(
 
 	for owner, nodes := range contractStorageNodes {
 		for path, n := range nodes {
-			err := d.updateNode(batch, db.ContractTrieStorage, &owner, &path, n)
+			err := d.updateNode(batch, db.ContractTrieStorage, &owner, &path, n, false)
 			if err != nil {
 				return err
 			}
@@ -111,11 +120,17 @@ func (d *Database) updateNode(
 	owner *felt.Address,
 	path *trieutils.Path,
 	n trienode.TrieNode,
+	isClass bool,
 ) error {
 	if _, deleted := n.(*trienode.DeletedNode); deleted {
-		return trieutils.DeleteNodeByPath(batch, bucket, owner, path, n.IsLeaf())
+		err := trieutils.DeleteNodeByPath(batch, bucket, owner, path, n.IsLeaf())
+		if err != nil {
+			return err
+		}
+		d.cleanCache.deleteNode(owner, path, isClass)
+		return nil
 	}
-	return trieutils.WriteNodeByPath(
+	err := trieutils.WriteNodeByPath(
 		batch,
 		bucket,
 		owner,
@@ -123,6 +138,11 @@ func (d *Database) updateNode(
 		n.IsLeaf(),
 		n.Blob(),
 	)
+	if err != nil {
+		return err
+	}
+	d.cleanCache.putNode(owner, path, isClass, n.Blob())
+	return nil
 }
 
 // Commit is a no-op for the raw scheme; This method was added
