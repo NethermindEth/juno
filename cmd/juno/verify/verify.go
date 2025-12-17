f3e65cc3fb19c3ca5833acbc193358ff8f24720d
MaksymMalicki <maksym.malicki.2115@gmail.com>
2025-12-18 00:05:53 +0100

    add trie verifier

--- cmd/juno/verify/verify.go
+++ cmd/juno/verify/verify.go
@@ -1,6 +1,13 @@
 package verify
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/NethermindEth/juno/db"
+	"github.com/NethermindEth/juno/db/pebblev2"
+	"github.com/NethermindEth/juno/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -36,7 +43,13 @@ func verifyAll(cmd *cobra.Command, args []string) error {
 	}
 	defer database.Close()
 
-	trieVerifier := NewTrieVerifier(database)
+	logLevel := utils.NewLogLevel(utils.INFO)
+	logger, err := utils.NewZapLogger(logLevel, true)
+	if err != nil {
+		return fmt.Errorf("failed to create logger: %w", err)
+	}
+
+	trieVerifier := NewTrieVerifier(database, logger)
 
 	runner := &VerifyRunner{
 		Verifiers: []Verifier{
@@ -88,7 +101,27 @@ func runTrieVerify(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	verifier := NewTrieVerifier(database)
+	logLevel := utils.NewLogLevel(utils.INFO)
+	logger, err := utils.NewZapLogger(logLevel, true)
+	if err != nil {
+		return fmt.Errorf("failed to create logger: %w", err)
+	}
+
+	verifier := NewTrieVerifier(database, logger)
 	ctx := cmd.Context()
 	return verifier.Run(ctx, cfg)
+}
+
+func openDB(path string) (db.KeyValueStore, error) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return nil, errors.New("database path does not exist")
+	}
+
+	database, err := pebblev2.New(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open db: %w", err)
+	}
+
+	return database, nil
 }
