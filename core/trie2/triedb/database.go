b092553eacef38066802920c7b582b4d5a933cc1
MaksymMalicki <maksym.malicki.2115@gmail.com>
2026-02-25 22:04:09 +0100

    changes to the atomicity

--- core/trie2/triedb/database.go
+++ core/trie2/triedb/database.go
@@ -13,7 +13,6 @@ import (
 type Config struct {
 	PathConfig *pathdb.Config
 	HashConfig *hashdb.Config
-	RawConfig  *rawdb.Config
 }
 
 func New(disk db.KeyValueStore, config *Config) (database.TrieDB, error) {
