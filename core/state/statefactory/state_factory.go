1498f76fd8759246ed1931e058ffd37a3a231fd9
MaksymMalicki <maksym.malicki.2115@gmail.com>
2026-02-26 11:24:27 +0100

    linter

--- core/state/statefactory/state_factory.go
+++ core/state/statefactory/state_factory.go
@@ -30,6 +30,7 @@ func NewStateFactory(
 	}, nil
 }
 
+//nolint:staticcheck // SA1019: IndexedBatch required for deprecated state
 func (sf *StateFactory) NewState(
 	// todo: this should be *felt.StateRootHash
 	stateRoot *felt.Felt,
@@ -48,6 +49,7 @@ func (sf *StateFactory) NewState(
 	return state, nil
 }
 
+//nolint:staticcheck // SA1019: IndexedBatch required for deprecated state
 func (sf *StateFactory) NewStateReader(
 	stateRoot *felt.Felt,
 	txn db.IndexedBatch,
