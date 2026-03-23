fec50aec858e28a260873222b7d11893930fd9ff
MaksymMalicki <maksym.malicki.2115@gmail.com>
2026-03-23 18:38:12 +0100

    chore: cleanups, remove dead code

--- rpc/v6/pending_wrapper_test.go
+++ rpc/v6/pending_wrapper_test.go
@@ -69,6 +69,8 @@ func TestPendingDataWrapper_PendingData(t *testing.T) {
 
 		pending, err := handler.PendingData()
 		require.NoError(t, err)
+		_, isPreConfirmed := pending.(*core.PreConfirmed)
+		require.True(t, isPreConfirmed)
 		require.Equal(t, &expectedPending, pending)
 	})
 }
