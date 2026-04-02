9cda234a4ce444ec9183c963586a23b9deeb3569
MaksymMalicki <maksym.malicki.2115@gmail.com>
2026-04-02 18:40:17 +0200

    test: mock syncer should return

--- rpc/v10/events_test.go
+++ rpc/v10/events_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/NethermindEth/juno/blockchain"
 	"github.com/NethermindEth/juno/clients/feeder"
 	"github.com/NethermindEth/juno/core"
+	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/db/memory"
