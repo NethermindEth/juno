17e4fa138be2ce8ac4b88ebd79e7906d9ef17627
thiagodeev <thiagodeev@gmail.com>
2026-06-16 08:40:33 -0300

    test(rpc): add unit tests for EstimateMessageFee

--- rpc/v10/estimate_fee_test.go
+++ rpc/v10/estimate_fee_test.go
@@ -2,11 +2,13 @@ package rpcv10_test
 
 import (
 	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/NethermindEth/juno/blockchain/networks"
 	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/juno/db"
 	"github.com/NethermindEth/juno/jsonrpc"
 	"github.com/NethermindEth/juno/mocks"
 	"github.com/NethermindEth/juno/rpc/rpccore"
@@ -152,5 +154,66 @@ func TestEstimateFee(t *testing.T) {
 			Data:    "resource_bounds is required for this transaction type",
 		}
 		require.Equal(t, expectedErr, err)
+	})
+}
+
+func TestEstimateMessageFee(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	n := &networks.Mainnet
+	mockReader := mocks.NewMockReader(mockCtrl)
+	mockReader.EXPECT().Network().Return(n).AnyTimes()
+	mockVM := mocks.NewMockVM(mockCtrl)
+	logger := log.NewNopZapLogger()
+	handler := rpcv10.New(mockReader, nil, mockVM, logger)
+
+	blockID := rpcv10.BlockIDLatest()
+	to := felt.FromUint64[felt.Felt](0xABCD)
+	msg := &rpcv10.MsgFromL1{
+		To:       to,
+		Selector: felt.FromUint64[felt.Felt](0x1),
+		Payload:  []felt.Felt{felt.FromUint64[felt.Felt](0xCAFE)},
+	}
+
+	t.Run("contract not found returns ErrContractNotFound", func(t *testing.T) {
+		mockState := mocks.NewMockStateReader(mockCtrl)
+		mockReader.EXPECT().HeadState().Return(mockState, nopCloser, nil)
+		mockState.EXPECT().ContractClassHash(&msg.To).Return(felt.Felt{}, errors.New("missing"))
+
+		_, _, err := handler.EstimateMessageFee(t.Context(), msg, &blockID)
+		require.Equal(t, rpccore.ErrContractNotFound, err)
+	})
+
+	t.Run("block not found returns ErrBlockNotFound", func(t *testing.T) {
+		mockReader.EXPECT().StateAtBlockNumber(uint64(123)).Return(nil, nil, db.ErrKeyNotFound)
+		blockID := rpcv10.BlockIDFromNumber(123)
+		_, _, err := handler.EstimateMessageFee(t.Context(), msg, &blockID)
+		require.Equal(t, rpccore.ErrBlockNotFound, err)
+	})
+
+	t.Run("happy path returns FeeEstimate", func(t *testing.T) {
+		mockState := mocks.NewMockStateReader(mockCtrl)
+		mockReader.EXPECT().HeadState().Return(mockState, nopCloser, nil).Times(2)
+		mockReader.EXPECT().HeadsHeader().Return(&core.Header{}, nil)
+		mockState.EXPECT().ContractClassHash(&msg.To).Return(felt.Felt{}, nil)
+
+		mockVM.EXPECT().EstimateFee(
+			gomock.Any(),
+			nil,
+			gomock.Any(),
+			mockState,
+			gomock.Any(),
+		).Return(vm.ExecutionResults{
+			OverallFees:      []*felt.Felt{felt.NewFromUint64[felt.Felt](42)},
+			DataAvailability: []core.DataAvailability{{}},
+			GasConsumed:      []core.GasConsumed{{L1Gas: 1, L2Gas: 2, L1DataGas: 3}},
+			Traces:           []vm.TransactionTrace{{Type: vm.TxnL1Handler}},
+			NumSteps:         10,
+		}, nil)
+
+		feeEstimate, _, err := handler.EstimateMessageFee(t.Context(), msg, &blockID)
+		require.Nil(t, err)
+		assert.NotEmpty(t, feeEstimate)
 	})
 }
