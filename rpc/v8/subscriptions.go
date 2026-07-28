2b0d0fe923cba5845736828e718ed477791d08af
Dat Duong <DuongDat.Informatics@rocketmail.com>
2026-07-28 18:31:24 +0000

    feat(rpc): gate WSS requests and bound active subscriptions

--- rpc/v8/subscriptions.go
+++ rpc/v8/subscriptions.go
@@ -118,6 +118,9 @@ func (h *Handler) subscribe(
 	wsConn jsonrpc.Conn,
 	subscriber subscriber,
 ) (SubscriptionID, *jsonrpc.Error) {
+	if h.subscriptionLimiter != nil && !h.subscriptionLimiter.TryAcquire(1) {
+		return "", rpccore.ErrTooManySubscriptions
+	}
 	id := h.idgen()
 	//nolint:gosec // G118: cancel called in unsubscribe()
 	subscriptionCtx, subscriptionCtxCancel := context.WithCancel(wsConn.Context())
@@ -132,6 +135,9 @@ func (h *Handler) subscribe(
 	l1HeadSub, l1HeadRecv := getSubscription(subscriber.onL1Head, h.l1Heads)
 
 	sub.wg.Go(func() {
+		if h.subscriptionLimiter != nil {
+			defer h.subscriptionLimiter.Release(1)
+		}
 		defer func() {
 			h.unsubscribe(sub, id)
 			unsubscribeFeedSubscription(reorgSub)
