b22654d3157ed4f647dcbcf43d113e1698844d4c
thiagodeev <thiagodeev@gmail.com>
2026-07-28 16:59:27 -0300

    fix(rpc): update error handling in ClassAt method to compare error codes for class hash not found

--- rpc/v8/class.go
+++ rpc/v8/class.go
@@ -163,7 +163,7 @@ func (h *Handler) ClassAt(id BlockID, address felt.Felt) (*Class, *jsonrpc.Error
 	if err != nil {
 		// getClassAt only returns CONTRACT_NOT_FOUND / BLOCK_NOT_FOUND per spec;
 		// a class-hash miss here means the contract is not properly deployed.
-		if err == rpccore.ErrClassHashNotFound {
+		if err.Code == rpccore.ErrClassHashNotFound.Code {
 			return nil, rpccore.ErrContractNotFound
 		}
 		return nil, err
