b440e4c1140aa0d5fb1249ff0bc059d214654bad
Rafael Granza <rafaelgranzademello@gmail.com>
2026-08-05 15:04:28 -0300

    ci: test the packages behind the jsonv2 build tag

--- core/felt/slice_jsonv2.go
+++ core/felt/slice_jsonv2.go
@@ -10,23 +10,25 @@ import (
 	"fmt"
 )
 
-// MarshalJSONTo serialises the slice as a JSON array of 0x-hex strings.
 func (s Slice[F]) MarshalJSONTo(enc *jsontext.Encoder) error {
 	if s == nil {
 		return enc.WriteToken(jsontext.Null)
 	}
 
-	// account for quotes and commas per felt + '[' and ']' at the ends.
-	maxSize := len(s)*(MaxFeltAsHexSize+len(`"",`)) + len("[]")
+	// account for quotes and commas per felt + '[' and ']' at the ends - the last comma.
+	maxSize := len(s)*(MaxFeltAsHexSize+len(`"",`)) + len("[]") - len(",")
 	data := make([]byte, 1, maxSize)
 
+	var err error
 	data[0] = '['
 	for index := range s {
 		if index > 0 {
 			data = append(data, ',')
 		}
 		data = append(data, '"')
-		data, _ = Felt(s[index]).AppendText(data)
+		if data, err = Felt(s[index]).AppendText(data); err != nil {
+			return err
+		}
 		data = append(data, '"')
 	}
 	data = append(data, ']')
