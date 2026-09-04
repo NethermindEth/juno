f45464fbfd8d174ea637a6a7c37fc046770c2a07
Nazarii Denha <dengaaa2002@gmail.com>
2026-09-04 11:49:12 +0200

    move phases into internal package

--- migration/state/newstate/internal/headstate/migrator.go
+++ migration/state/newstate/internal/headstate/migrator.go
@@ -13,7 +13,7 @@ import (
 	"github.com/NethermindEth/juno/migration"
 	"github.com/NethermindEth/juno/migration/pipeline"
 	"github.com/NethermindEth/juno/migration/semaphore"
-	"github.com/NethermindEth/juno/migration/state/common"
+	"github.com/NethermindEth/juno/migration/state/newstate/internal/common"
 	"github.com/NethermindEth/juno/utils/log"
 )
 
