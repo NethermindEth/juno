abcb9ccce23d3223b718d9e998b697925aaa8bdf
zexoverz <faisalfirdani01@gmail.com>
2026-09-15 14:14:09 +0700

    test(genesis): cover the contract address of a genesis deploy account

--- genesis/genesis_test.go
+++ genesis/genesis_test.go
@@ -4,13 +4,17 @@ import (
 	"testing"
 
 	"github.com/NethermindEth/juno/blockchain/networks"
+	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
 	statetestutils "github.com/NethermindEth/juno/core/state/testutils"
 	"github.com/NethermindEth/juno/genesis"
+	"github.com/NethermindEth/juno/mocks"
+	rpc "github.com/NethermindEth/juno/rpc/v8"
 	"github.com/NethermindEth/juno/starknet/compiler"
 	"github.com/NethermindEth/juno/utils/log"
 	"github.com/NethermindEth/juno/vm"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func TestGenesisStateDiff(t *testing.T) {
@@ -82,5 +86,58 @@ func TestGenesisStateDiff(t *testing.T) {
 		}
 		require.Equal(t, len(genesisConfig.BootstrapAccounts), numFundedAccounts)
 		require.True(t, v3InvokeTxnTriggered)
+	})
+
+	t.Run("deploy account txn gets its contract address", func(t *testing.T) {
+		classHash := felt.NewUnsafeFromString[felt.Felt](
+			"0x402d6191ebe3ea289789edd160f3afa6600a389f1aad0ab7709b830653c6f08",
+		)
+		salt := felt.NewUnsafeFromString[felt.Felt]("0x123")
+		calldata := felt.Slice[felt.Felt]{*felt.NewFromUint64[felt.Felt](7)}
+		genesisConfig := genesis.GenesisConfig{
+			Txns: []rpc.Transaction{{
+				Hash:                felt.NewFromUint64[felt.Felt](1),
+				Type:                rpc.TxnDeployAccount,
+				Version:             felt.NewFromUint64[felt.Felt](1),
+				Nonce:               &felt.Zero,
+				MaxFee:              &felt.Zero,
+				ClassHash:           classHash,
+				ContractAddressSalt: salt,
+				ConstructorCallData: &calldata,
+				Signature:           &felt.Slice[felt.Felt]{},
+			}},
+		}
+		want := core.ContractAddress(&felt.Zero, classHash, salt, calldata)
+
+		mockVM := mocks.NewMockVM(gomock.NewController(t))
+		mockVM.EXPECT().BuildBlock(
+			gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(),
+		).DoAndReturn(func(
+			txns []core.Transaction,
+			_ []core.ClassDefinition,
+			_ []*felt.Felt,
+			_ *vm.BlockInfo,
+			_ core.StateReader,
+			_ vm.BuildBlockOptions,
+		) (vm.ExecutionResults, error) {
+			require.Len(t, txns, 1)
+			deployAccount, ok := txns[0].(*core.DeployAccountTransaction)
+			require.True(t, ok)
+			require.NotNil(t, deployAccount.ContractAddress)
+			require.Equal(t, want, *deployAccount.ContractAddress)
+			return vm.ExecutionResults{Traces: make([]vm.TransactionTrace, 1)}, nil
+		})
+
+		_, _, err := genesis.GenesisStateDiff(
+			t.Context(),
+			&genesisConfig,
+			mockVM,
+			network,
+			vm.DefaultMaxSteps,
+			vm.DefaultMaxGas,
+			statetestutils.UseNewState(),
+			nil,
+		)
+		require.NoError(t, err)
 	})
 }
